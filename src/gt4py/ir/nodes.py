2892999605eff523e08895dd9a5b64a740a5788a
Johann Dahm <johannd@vulcan.com>
2020-06-15 12:24:48 -0700

    Add parallel_interval to gt_ir.ApplyBlock

--- src/gt4py/ir/nodes.py
+++ src/gt4py/ir/nodes.py
@@ -147,6 +147,7 @@ Implementation IR
                 | FieldAccessor(symbol: str, intent: AccessIntent, extent: Extent)
 
     ApplyBlock(interval: AxisInterval,
+               [parallel_interval: List[AxisInterval],]
                local_symbols: Dict[str, VarDecl],
                body: BlockStmt)
 
@@ -739,6 +740,7 @@ class FieldAccessor(Accessor):
 @attribclass
 class ApplyBlock(Node):
     interval = attribute(of=AxisInterval)
+    parallel_interval = attribute(of=ListOf[AxisInterval], optional=True)
     local_symbols = attribute(of=DictOf[str, VarDecl])
     body = attribute(of=BlockStmt)
 
