6cf78431958895db80aa0db38fc76696e2701558
Eddie Davis <eddied@vulcan.com>
2020-07-30 20:29:19 +0000

    Fix CLI tests for Dawn backends

--- src/gt4py/backend/dawn_backends.py
+++ src/gt4py/backend/dawn_backends.py
@@ -691,6 +691,7 @@ class DawnGTX86Backend(BaseDawnBackend):
     name = "dawn:gtx86"
     options = _DAWN_BACKEND_OPTIONS
     storage_info = gt_backend.GTX86Backend.storage_info
+    languages = gt_backend.GTX86Backend.languages
 
     @classmethod
     def generate_extension(cls, stencil_id, definition_ir, options, **kwargs):
@@ -709,6 +710,7 @@ class DawnGTMCBackend(BaseDawnBackend):
     name = "dawn:gtmc"
     options = _DAWN_BACKEND_OPTIONS
     storage_info = gt_backend.GTMCBackend.storage_info
+    languages = gt_backend.GTMCBackend.languages
 
     @classmethod
     def generate_extension(cls, stencil_id, definition_ir, options, **kwargs):
@@ -728,6 +730,7 @@ class DawnGTCUDABackend(BaseDawnBackend):
     name = "dawn:gtcuda"
     options = _DAWN_BACKEND_OPTIONS
     storage_info = gt_backend.GTCUDABackend.storage_info
+    languages = gt_backend.GTCUDABackend.languages
 
     @classmethod
     def generate_extension(cls, stencil_id, definition_ir, options, **kwargs):
@@ -746,6 +749,7 @@ class DawnNaiveBackend(BaseDawnBackend):
     name = "dawn:naive"
     options = _DAWN_BACKEND_OPTIONS
     storage_info = gt_backend.GTX86Backend.storage_info
+    languages = gt_backend.GTX86Backend.languages
 
     @classmethod
     def generate_extension(cls, stencil_id, definition_ir, options, **kwargs):
@@ -764,6 +768,7 @@ class DawnOptBackend(BaseDawnBackend):
     name = "dawn:cxxopt"
     options = _DAWN_BACKEND_OPTIONS
     storage_info = gt_backend.GTX86Backend.storage_info
+    languages = gt_backend.GTX86Backend.languages
 
     @classmethod
     def generate_extension(cls, stencil_id, definition_ir, options, **kwargs):
@@ -783,6 +788,7 @@ class DawnCUDABackend(BaseDawnBackend):
     name = "dawn:cuda"
     options = _DAWN_BACKEND_OPTIONS
     storage_info = gt_backend.GTCUDABackend.storage_info
+    languages = gt_backend.GTCUDABackend.languages
 
     @classmethod
     def generate_extension(cls, stencil_id, definition_ir, options, **kwargs):
