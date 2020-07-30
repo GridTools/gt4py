de2911208722e74602d5eb23dbfe22dcb5360c2f
Eddie Davis <eddied@vulcan.com>
2020-07-30 09:27:12 -0700

    Merge with 'cuda_build_fix'

--- src/gt4py/backend/pyext_builder.py
+++ src/gt4py/backend/pyext_builder.py
@@ -115,14 +115,14 @@ def get_gt_pyext_build_opts(
             extra_link_args=extra_link_args,
         )
 
-    if uses_openmp:
-        cpp_flags = gt_config.build_settings["openmp_cppflags"]
-        if cpp_flags:
-            build_opts["extra_compile_args"].extend(cpp_flags)
+        if uses_openmp:
+            cpp_flags = gt_config.build_settings["openmp_cppflags"]
+            if cpp_flags:
+                build_opts["extra_compile_args"].extend(cpp_flags)
 
-        ld_flags = gt_config.build_settings["openmp_ldflags"]
-        if ld_flags:
-            build_opts["extra_link_args"].extend(ld_flags)
+            ld_flags = gt_config.build_settings["openmp_ldflags"]
+            if ld_flags:
+                build_opts["extra_link_args"].extend(ld_flags)
 
     return build_opts
 
