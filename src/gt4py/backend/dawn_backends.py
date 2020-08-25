8aa5d03b2fb8942e6a97b687c786eb0987e89664
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2020-08-25 10:50:05 -0700

    Rename 'VerticalRegionStmt' => 'VerticalRegionDeclStmt' (#154)

--- src/gt4py/backend/dawn_backends.py
+++ src/gt4py/backend/dawn_backends.py
@@ -250,7 +250,7 @@ class SIRConverter(gt_ir.IRNodeVisitor):
 
     def visit_ComputationBlock(
         self, node: gt_ir.ComputationBlock, **kwargs: Any
-    ) -> SIR.VerticalRegionStmt:
+    ) -> SIR.VerticalRegionDeclStmt:
         interval = self.visit(node.interval)
 
         body_ast = sir_utils.make_ast(self.visit(node.body, make_block=False))
