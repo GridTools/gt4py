9621c80220205369dacedbe8681ad616df1a7849
Linus Groner <linus.groner@cscs.ch>
2020-09-16 13:00:04 +0200

    started perftest utilities

--- src/gt4py/testing/utils.py
+++ src/gt4py/testing/utils.py
@@ -40,7 +40,7 @@ def standardize_dtype_dict(dtypes):
     """Standardizes the dtype dict as it can be specified for the stencil test suites.
     In the input dictionary, a selection of possible dtypes or just a single dtype can be specified for a set of fields
     or a single field. This function makes sure that all keys are tuples (by wrapping single field names and single
-    dtypes as 1-tuples) """
+    dtypes as 1-tuples)"""
     assert isinstance(dtypes, collections.abc.Mapping)
     assert all(
         (isinstance(k, str) or gt_utils.is_iterable_of(k, str)) for k in dtypes.keys()
@@ -68,3 +68,74 @@ def standardize_dtype_dict(dtypes):
     if not len(keys) == len(set(keys)):
         raise ValueError("Any field can be in only one group.")
     return result
+
+
+import gt4py
+from gt4py.backend.dace.base_backend import DaceOptimizer
+
+
+class ApplyOTFOptimizer(DaceOptimizer):
+    def transform_optimize(self, sdfg):
+        from gt4py.backend.dace.sdfg.transforms import OnTheFlyMapFusion
+
+        sdfg.apply_transformations_repeated(OnTheFlyMapFusion, validate=False)
+        return sdfg
+
+
+def build_dace_adhoc(
+    definition,
+    domain,
+    halo,
+    specialize_strides,
+    dtype,
+    passes,
+    alignment,
+    layout,
+    loop_order,
+    device,
+    **params,
+) -> gt4py.stencil_object.StencilObject:
+    backend_name = f"dace_adhoc_{device}_{dtype}_{loop_order}_{alignment}_"
+    backend_name += "_".join(str(int(h)) for h in halo) + "_"
+    backend_name += "_".join(str(int(d)) for d in domain) + "_"
+    backend_name += "_".join(str(int(s)) for s in specialize_strides) + "_"
+    backend_name += "_".join(type(p).__name__ for p in passes) + "_"
+    backend_name += "_".join(f"{k}_{v}" for k, v in params)
+
+    from gt4py.backend.dace.cpu_backend import CPUDaceBackend
+    from gt4py.backend.dace.gpu_backend import GPUDaceBackend
+    from gt4py.backend.dace.base_backend import CudaDaceOptimizer, DaceOptimizer
+    from gt4py.backend.concepts import register as register_backend
+
+    base_backend = CPUDaceBackend if device == "cpu" else GPUDaceBackend
+    base_optimizer = DaceOptimizer if device == "cpu" else CudaDaceOptimizer
+
+    class CompositeOptimizer(base_optimizer):
+        def __init__(self, passes):
+            self._passes = passes
+
+        def transform_library(self, sdfg):
+            for xform in self._passes:
+                sdfg = xform.transform_library(sdfg)
+            return sdfg
+
+        def transform_optimize(self, sdfg):
+            for xform in self._passes:
+                sdfg = xform.transform_optimize(sdfg)
+            return sdfg
+
+    @register_backend
+    class AdHocBackend(base_backend):
+        name = backend_name
+        storage_info = {
+            "alignment": alignment,  # will not affect temporaries currently
+            "device": device,  # change me
+            "layout_map": lambda m: layout,
+            "is_compatible_layout": lambda m: True,
+            "is_compatible_type": lambda m: True,
+        }
+        DEFAULT_OPTIMIZER = CompositeOptimizer(passes)
+
+    return gt4py.gtscript.stencil(
+        definition=definition, backend=backend_name, dtypes={"dtype": dtype}
+    )
