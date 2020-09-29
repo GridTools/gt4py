6efa4f2d388674cecab8f89a70d1a54e0278613c
Rico Häuselmann <ricoh@cscs.ch>
2020-09-29 14:17:59 +0200

    add ir_maker AugAssign test

---
+++ tests/test_unittest/test_ir_maker.py
@@ -0,0 +1,14 @@
+import ast
+
+from gt4py.frontend.gtscript_frontend import IRMaker
+from gt4py.ir.nodes import BinaryOperator, BinOpExpr
+
+
+def test_AugAssign():
+    ir_maker = IRMaker(None, None, None, domain=None, extra_temp_decls=None)
+    aug_assign = ast.parse("a += 1").body[0]
+
+    _, result = ir_maker.visit_AugAssign(aug_assign)
+
+    assert isinstance(result.value, BinOpExpr)
+    assert isinstance(result.value.op, BinaryOperator.ADD)
