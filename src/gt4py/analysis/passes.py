123d6a47506341c0e7a84f1fbd2b2b82902ae159
Johann Dahm <johannd@vulcan.com>
2020-10-09 08:02:47 -0700

    Apply suggestions from @egparades
    Co-authored-by: Enrique G. Paredes <18477+egparedes@users.noreply.github.com>

--- src/gt4py/analysis/passes.py
+++ src/gt4py/analysis/passes.py
@@ -35,7 +35,6 @@ from typing import (
 )
 
 from gt4py import definitions as gt_definitions
-from gt4py import gtscript
 from gt4py import ir as gt_ir
 from gt4py.analysis import (
     DomainBlockInfo,
@@ -913,8 +912,9 @@ class DataTypePass(TransformPass):
         def visit_NativeFuncCall(self, node: gt_ir.NativeFuncCall, **kwargs):
             self.generic_visit(node.args, **kwargs)
 
-            dtypes_list = [arg.data_type for arg in node.args]
-            dtypes_set = set(dtypes_list) - {gt_ir.DataType.DEFAULT}
+            dtypes_set = set(
+                arg.data_type for arg in node.args if arg.data_type != gt_ir.DataType.DEFAULT
+            )
 
             if len(dtypes_set) == 0:
                 data_type = gt_ir.DataType.DEFAULT
