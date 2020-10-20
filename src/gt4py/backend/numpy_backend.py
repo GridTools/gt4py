a56eb6ef1a6acd0aceb77dbc77588e0a58c17530
tobias <twicki12@gmail.com>
2020-10-20 20:51:59 +0200

    add explanation the else logic

--- src/gt4py/backend/numpy_backend.py
+++ src/gt4py/backend/numpy_backend.py
@@ -270,6 +270,10 @@ class NumPySourceGenerator(PythonSourceGenerator):
                 if target in self.block_info.symbols
                 else stmt.target.data_type
             )
+            # Check if this temporary variable / field already contains written information.
+            # If it does, it needs to be the else expression of the where, otherwise we set the else to nan.
+            # This ensures that we only write defined values.
+            # This check is not relevant for fields as they enter defined
             is_possible_else = not isinstance(stmt.target, gt_ir.VarRef) or (
                 stmt.target.name in self.var_refs_defined
             )
