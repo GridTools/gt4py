9ea0d792b8b29198a70a121485c44328f41d0b4f
Hannes Vogt <hannes@havogt.de>
2020-11-17 10:30:48 +0100

    GTC: Generic nodes testing (#254)

--- src/gt4py/gtc/common.py
+++ src/gt4py/gtc/common.py
@@ -168,6 +168,13 @@ def compute_kind(values: List[Expr]) -> ExprKind:
         return ExprKind.SCALAR
 
 
+class Literal(Expr):
+    # TODO when coming from python AST we know more than just the string representation, I suppose
+    value: Str
+    dtype: DataType
+    kind = ExprKind.SCALAR
+
+
 StmtT = TypeVar("StmtT")
 ExprT = TypeVar("ExprT")
 TargetT = TypeVar("TargetT")
@@ -181,13 +188,6 @@ class IfStmt(GenericNode, Generic[StmtT, ExprT]):
     @validator("cond")
     def condition_is_boolean(cls, cond):
         return verify_condition_is_boolean(cls, cond)
-
-
-class Literal(Expr):
-    # TODO when coming from python AST we know more than just the string representation, I suppose
-    value: Str
-    dtype: DataType
-    kind = ExprKind.SCALAR
 
 
 class AssignStmt(GenericNode, Generic[TargetT, ExprT]):
@@ -230,6 +230,7 @@ class BinaryOp(GenericNode, Expr, Generic[ExprT]):
 
 
 class TernaryOp(GenericNode, Expr, Generic[ExprT]):
+    # TODO parametrize cond expr separately?
     cond: ExprT
     true_expr: ExprT
     false_expr: ExprT
--- tests/test_unittest/test_gtc/test_common.py
+++ tests/test_unittest/test_gtc/test_common.py
@@ -6,10 +6,12 @@ from gt4py.gtc.common import (
     IfStmt,
     Literal,
     Expr,
+    Stmt,
     DataType,
     ExprKind,
     BinaryOp,
     TernaryOp,
+    AssignStmt,
     ArithmeticOperator,
     LogicalOperator,
     ComparisonOperator,
@@ -130,3 +132,63 @@ def test_dtype_propagation(node, expected):
 def test_invalid_nodes(invalid_node, expected_regex):
     with pytest.raises(ValidationError, match=expected_regex):
         invalid_node()
+
+
+# For pydantic, nodes are the same (convertible to each other) if all fields are same.
+# For checking, we need to make the Expr categories clearly different.
+# This behavior will most likely change in Eve in the future
+class ExprA(Expr):
+    dtype: DataType = DataType.FLOAT32
+    kind: ExprKind = ExprKind.FIELD
+    clearly_expr_a = ""
+
+
+class ExprB(Expr):
+    dtype: DataType = DataType.FLOAT32
+    kind: ExprKind = ExprKind.FIELD
+    clearly_expr_b = ""
+
+
+class StmtA(Stmt):
+    clearly_stmt_a = ""
+
+
+class StmtB(Stmt):
+    clearly_stmt_b = ""
+
+
+def test_AssignSmt_category():
+    Testee = AssignStmt[ExprA, ExprA]
+
+    Testee(left=ExprA(), right=ExprA())
+    with pytest.raises(ValidationError):
+        Testee(left=ExprB(), right=ExprA())
+        Testee(left=ExprA(), right=ExprB())
+
+
+def test_IfStmt_category():
+    Testee = IfStmt[StmtA, ExprA]
+
+    Testee(cond=ExprA(dtype=DataType.BOOL), true_branch=[StmtA()], false_branch=[StmtA()])
+    with pytest.raises(ValidationError):
+        Testee(cond=ExprA(dtype=DataType.BOOL), true_branch=[StmtB()], false_branch=[StmtA()])
+        Testee(cond=ExprA(dtype=DataType.BOOL), true_branch=[StmtA()], false_branch=[StmtB()])
+        Testee(cond=ExprB(dtype=DataType.BOOL), true_branch=[StmtA()], false_branch=[StmtA()])
+
+
+def test_BinaryOp_category():
+    Testee = BinaryOp[ExprA]
+
+    Testee(op=A_ARITHMETIC_OPERATOR, left=ExprA(), right=ExprA())
+    with pytest.raises(ValidationError):
+        Testee(op=A_ARITHMETIC_OPERATOR, left=ExprB(), right=ExprA())
+        Testee(op=A_ARITHMETIC_OPERATOR, left=ExprA(), right=ExprB())
+
+
+def test_TernaryOp_category():
+    Testee = TernaryOp[ExprA]
+
+    Testee(cond=ExprA(dtype=DataType.BOOL), true_expr=ExprA(), false_expr=ExprA())
+    with pytest.raises(ValidationError):
+        Testee(cond=ExprB(dtype=DataType.BOOL), true_expr=ExprB(), false_expr=ExprA())
+        Testee(cond=ExprA(dtype=DataType.BOOL), true_expr=ExprA(), false_expr=ExprB())
