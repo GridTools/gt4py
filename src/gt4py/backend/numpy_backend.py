64cc714d82543bbf4e2477d61ad890f3a879715d
Johann Dahm <johannd@vulcan.com>
2020-11-25 11:29:14 -0800

    Undo change in numpy backend

--- src/gt4py/backend/numpy_backend.py
+++ src/gt4py/backend/numpy_backend.py
@@ -121,14 +121,7 @@ class NumPySourceGenerator(PythonSourceGenerator):
         source_lines = []
 
         # Computations body is split in different vertical regions
-        assert (
-            sorted(
-                regions,
-                reverse=iteration_order == gt_ir.IterationOrder.BACKWARD,
-                key=lambda tup: tup[0],
-            )
-            == regions
-        )
+        assert sorted(regions, reverse=iteration_order == gt_ir.IterationOrder.BACKWARD) == regions
 
         for bounds, body in regions:
             region_lines = self._make_regional_computation(iteration_order, bounds, body)
