57bbf0e7834f8d93b6203d737e65e656efe75bc3
Johann Dahm <johannd@vulcan.com>
2020-12-04 08:50:43 -0800

    Add docstrings to ASTPass and ASTTransformPass

--- src/gt4py/utils/meta.py
+++ src/gt4py/utils/meta.py
@@ -200,6 +200,8 @@ def get_qualified_name_from_node(name_or_attribute, *, as_list=False):
 
 
 class ASTPass:
+    """Clone of the ast.NodeVisitor that supports forwarding **kwargs."""
+
     def __call__(self, func_or_source_or_ast):
         ast_root = get_ast(func_or_source_or_ast)
         return self.visit(ast_root)
@@ -222,6 +224,8 @@ class ASTPass:
 
 
 class ASTTransformPass(ASTPass):
+    """Clone of the ast.NodeTransformer that supports forwarding **kwargs."""
+
     def generic_visit(self, node, **kwargs):
         for field, old_value in ast.iter_fields(node):
             if isinstance(old_value, list):
