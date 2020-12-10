6c4e47931859511b3cc3770b0d1c250fbc6c186e
Hannes Vogt <vogt@hey.com>
2020-12-10 15:47:31 +0100

    comment a check

--- src/gt4py/gtc/passes/gtir_dtype_resolver.py
+++ src/gt4py/gtc/passes/gtir_dtype_resolver.py
@@ -42,12 +42,13 @@ class _GTIRResolveAuto(NodeTranslator):
         result = self.generic_visit(node, symtable=symtable)
         result = self._GTIRUpdateAutoDecl().visit(result, new_symbols=symtable)
 
-        assert all(
-            result.iter_tree()
-            .if_hasattr("dtype")
-            .getattr("dtype")
-            .map(lambda x: x not in [None, DataType.AUTO, DataType.INVALID, DataType.DEFAULT])
-        )
+        # TODO enable after FieldsMetaData is updated
+        # assert all(
+        #     result.iter_tree()
+        #     .if_hasattr("dtype")
+        #     .getattr("dtype")
+        #     .map(lambda x: x not in [None, DataType.AUTO, DataType.INVALID, DataType.DEFAULT])
+        # )
         return result
 
 
