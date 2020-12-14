9131ed5161ea7ef5bef69b15a7aa9f21e7e9f0e1
Hannes Vogt <vogt@hey.com>
2020-12-14 09:34:02 +0100

    nicer test pattern

--- tests/test_unittest/test_gtc/test_gtir_dtype_resolver.py
+++ tests/test_unittest/test_gtc/test_gtir_dtype_resolver.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 from gt4py.gtc import common
 from gt4py.gtc.common import DataType
@@ -27,85 +27,103 @@ def get_nodes_with_name(stencil: Stencil, name: str):
     return stencil.iter_tree().if_hasattr("name").filter(lambda node: node.name == name).to_list()
 
 
-@pytest.mark.parametrize(
-    "stencil,expected_dtypes",
-    [
-        (
-            # propagate dtype to FieldAccess
-            StencilBuilder()
-            .add_param(FieldDecl(name="field", dtype=A_ARITHMETIC_TYPE))
-            .add_par_assign_stmt(
+def propagate_dtype_to_FieldAccess():
+    return (
+        StencilBuilder()
+        .add_param(FieldDecl(name="field", dtype=A_ARITHMETIC_TYPE))
+        .add_par_assign_stmt(
+            ParAssignStmt(
+                left=FieldAccessBuilder("field").dtype(None).build(),
+                right=FieldAccessBuilder("field").dtype(None).build(),
+            )
+        )
+        .build(),
+        {"field": A_ARITHMETIC_TYPE},
+    )
+
+
+def resolve_AUTO_from_literal_to_temporary():
+    return (
+        StencilBuilder()
+        .add_vertical_loop(
+            VerticalLoopBuilder()
+            .add_temporary("tmp", DataType.AUTO)
+            .add_stmt(
                 ParAssignStmt(
-                    left=FieldAccessBuilder("field").dtype(None).build(),
+                    left=FieldAccessBuilder("tmp").dtype(None).build(),
+                    right=Literal(value="0", dtype=A_ARITHMETIC_TYPE),
+                )
+            )
+            .build()
+        )
+        .build(),
+        {"tmp": A_ARITHMETIC_TYPE},
+    )
+
+
+def resolve_AUTO_from_FieldDecl_to_FieldAccess_to_temporary():
+    return (
+        StencilBuilder()
+        .add_param(FieldDecl(name="field", dtype=A_ARITHMETIC_TYPE))
+        .add_vertical_loop(
+            VerticalLoopBuilder()
+            .add_temporary("tmp", DataType.AUTO)
+            .add_stmt(
+                ParAssignStmt(
+                    left=FieldAccessBuilder("tmp").dtype(None).build(),
                     right=FieldAccessBuilder("field").dtype(None).build(),
                 )
             )
-            .build(),
-            {"field": A_ARITHMETIC_TYPE},
-        ),
-        (
-            # resolve AUTO dtype for temporary: Literal -> temporary
-            StencilBuilder()
-            .add_vertical_loop(
-                VerticalLoopBuilder()
-                .add_temporary("tmp", DataType.AUTO)
-                .add_stmt(
-                    ParAssignStmt(
-                        left=FieldAccessBuilder("tmp").dtype(None).build(),
-                        right=Literal(value="0", dtype=A_ARITHMETIC_TYPE),
-                    )
+            .build()
+        )
+        .build(),
+        {"field": A_ARITHMETIC_TYPE, "tmp": A_ARITHMETIC_TYPE},
+    )
+
+
+def resolve_AUTO_from_FieldDecl_to_FieldAccess_to_temporary_to_FieldAccess_to_temporary():
+    return (
+        StencilBuilder()
+        .add_param(FieldDecl(name="field", dtype=A_ARITHMETIC_TYPE))
+        .add_vertical_loop(
+            VerticalLoopBuilder()
+            .add_temporary("tmp1", DataType.AUTO)
+            .add_temporary("tmp2", DataType.AUTO)
+            .add_stmt(
+                ParAssignStmt(
+                    left=FieldAccessBuilder("tmp1").dtype(None).build(),
+                    right=FieldAccessBuilder("field").dtype(None).build(),
                 )
-                .build()
             )
-            .build(),
-            {"tmp": A_ARITHMETIC_TYPE},
-        ),
-        (
-            # resolve AUTO dtype: FieldDecl -> FieldAccess -> temporary
-            StencilBuilder()
-            .add_param(FieldDecl(name="field", dtype=A_ARITHMETIC_TYPE))
-            .add_vertical_loop(
-                VerticalLoopBuilder()
-                .add_temporary("tmp", DataType.AUTO)
-                .add_stmt(
-                    ParAssignStmt(
-                        left=FieldAccessBuilder("tmp").dtype(None).build(),
-                        right=FieldAccessBuilder("field").dtype(None).build(),
-                    )
-                )
-                .build()
+            .add_stmt(
+                ParAssignStmt(
+                    left=FieldAccessBuilder("tmp2").dtype(None).build(),
+                    right=FieldAccessBuilder("tmp1").dtype(None).build(),
+                ),
             )
-            .build(),
-            {"field": A_ARITHMETIC_TYPE, "tmp": A_ARITHMETIC_TYPE},
-        ),
-        (
-            # resolve AUTO dtype: FieldDecl -> FieldAccess -> temporary -> FieldAccess -> temporary
-            StencilBuilder()
-            .add_param(FieldDecl(name="field", dtype=A_ARITHMETIC_TYPE))
-            .add_vertical_loop(
-                VerticalLoopBuilder()
-                .add_temporary("tmp1", DataType.AUTO)
-                .add_temporary("tmp2", DataType.AUTO)
-                .add_stmt(
-                    ParAssignStmt(
-                        left=FieldAccessBuilder("tmp1").dtype(None).build(),
-                        right=FieldAccessBuilder("field").dtype(None).build(),
-                    )
-                )
-                .add_stmt(
-                    ParAssignStmt(
-                        left=FieldAccessBuilder("tmp2").dtype(None).build(),
-                        right=FieldAccessBuilder("tmp1").dtype(None).build(),
-                    ),
-                )
-                .build()
-            )
-            .build(),
-            {"field": A_ARITHMETIC_TYPE, "tmp1": A_ARITHMETIC_TYPE, "tmp2": A_ARITHMETIC_TYPE},
-        ),
-    ],
+            .build()
+        )
+        .build(),
+        {"field": A_ARITHMETIC_TYPE, "tmp1": A_ARITHMETIC_TYPE, "tmp2": A_ARITHMETIC_TYPE},
+    )
+
+
+@pytest.fixture(
+    params=[
+        propagate_dtype_to_FieldAccess,
+        resolve_AUTO_from_literal_to_temporary,
+        resolve_AUTO_from_FieldDecl_to_FieldAccess_to_temporary,
+        resolve_AUTO_from_FieldDecl_to_FieldAccess_to_temporary_to_FieldAccess_to_temporary,
+    ]
 )
-def test_resolved_dtypes(stencil: Stencil, expected_dtypes: Dict[str, common.DataType]):
+def resolve_dtype_stencil_and_expected_dtypes(request):
+    return request.param()
+
+
+def test_resolved_dtypes(
+    resolve_dtype_stencil_and_expected_dtypes: Tuple[Stencil, Dict[str, common.DataType]]
+):
+    stencil, expected_dtypes = resolve_dtype_stencil_and_expected_dtypes
     # ensure consistency (input is not already fully resolved)
     for name, dtype in expected_dtypes.items():
         nodes = get_nodes_with_name(stencil, name)
