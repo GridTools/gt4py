5443186ebc41505793bdac91eb61d4b1753d5001
Rico Häuselmann <ricoh@cscs.ch>
2020-12-14 17:20:54 +0100

    gtc-integration/oir-to-npir: more node conversions

--- src/gt4py/gtc/python/oir_to_npir.py
+++ src/gt4py/gtc/python/oir_to_npir.py
@@ -1,46 +1,95 @@
 from typing import Dict, List, Optional
 
+from dataclasses import dataclass, field
 from eve.visitors import NodeTranslator
 
-from .. import oir, common
+from .. import common, oir
 from . import npir
 
 
 class OirToNpir(NodeTranslator):
     """Lower from optimizable IR (OIR) to numpy IR (NPIR)."""
 
-    def visit_Stencil(self, node: oir.Stencil) -> npir.Computation:
-        domain_padding = {"lower": [0, 0, 0], "upper": [0, 0, 0]}
-        vertical_passes = [
-            self.visit(vloop, domain_padding=domain_padding) for vloop in node.vertical_loops
-        ]
+    @dataclass
+    class Context:
+        """Context for a HorizontalExecution."""
+
+        domain_padding: Dict[str, List] = field(
+            default_factory=lambda: {"lower": [0, 0, 0], "upper": [0, 0, 0]}
+        )
+        parallel_k: Optional[bool] = None
+
+        def set_parallel_k(self, value: bool) -> "OirToNpir.Context":
+            return self.__class__(parallel_k=value, domain_padding=self.domain_padding)
+
+        def add_offset(self, axis: int, value: int) -> "OirToNpir.Context":
+            if value > 0:
+                current_value = self.domain_padding["upper"][axis]
+                self.domain_padding["upper"][axis] = max(current_value, value)
+            elif value < 0:
+                current_value = self.domain_padding["lower"][axis]
+                self.domain_padding["lower"][axis] = max(current_value, abs(value))
+            return self
+
+        def add_offsets(self, i: int, j: int, k: int) -> "OirToNpir.Context":
+            for axis_index, value in enumerate([i, j, k]):
+                self.add_offset(axis_index, value)
+            return self
+
+    def visit_Stencil(
+        self, node: oir.Stencil, *, ctx: Optional[Context] = None
+    ) -> npir.Computation:
+        ctx = ctx or self.Context()
+        vertical_passes = [self.visit(vloop, ctx=ctx) for vloop in node.vertical_loops]
 
         return npir.Computation(
             field_params=[decl.name for decl in node.params if isinstance(decl, oir.FieldDecl)],
             params=[decl.name for decl in node.params],
             vertical_passes=vertical_passes,
             domain_padding=npir.DomainPadding(
-                lower=domain_padding["lower"],
-                upper=domain_padding["upper"],
+                lower=ctx.domain_padding["lower"],
+                upper=ctx.domain_padding["upper"],
             ),
         )
 
     def visit_VerticalLoop(
-        self, node: oir.VerticalLoop, *, domain_padding: Optional[Dict[str, List]] = None
+        self, node: oir.VerticalLoop, *, ctx: Optional[Context] = None
     ) -> npir.VerticalPass:
+        ctx = ctx or self.Context()
+        parallel_k = True if node.loop_order == common.LoopOrder.PARALLEL else False
+        v_assigns = [
+            self.visit(h_exec, ctx=ctx.set_parallel_k(parallel_k))
+            for h_exec in node.horizontal_executions
+        ]
+        body = [i for j in v_assigns for i in j]
         return npir.VerticalPass(
-            body=self.visit(node.horizontal_executions, domain_padding=domain_padding),
+            body=body,
             lower=self.visit(node.interval.start),
             upper=self.visit(node.interval.end),
             direction=self.visit(node.loop_order),
         )
 
     def visit_AxisBound(self, node: oir.AxisBound, **kwargs) -> common.AxisBound:
-        return common.AxisBound(
-            level=node.level,
-            offset=node.offset
+        return common.AxisBound(level=node.level, offset=node.offset)
+
+    def visit_HorizontalExecution(
+        self, node: oir.HorizontalExecution, *, ctx: Optional[Context] = None
+    ) -> List[npir.VectorAssign]:
+        return self.visit(node.body, ctx=ctx)
+
+    def visit_AssignStmt(
+        self, node: oir.AssignStmt, *, ctx: Optional[Context] = None
+    ) -> npir.VectorAssign:
+        return npir.VectorAssign(
+            left=self.visit(node.left, ctx=ctx),
+            right=self.visit(node.right, ctx=ctx),
         )
 
-    def visit_HorizontalExecution(self, node: oir.HorizontalExecution) -> List[npir.VectorAssign]:
-        pass  # TODO
-
+    def visit_FieldAccess(self, node: oir.FieldAccess, *, ctx: Context) -> npir.FieldSlice:
+        ctx.add_offsets(node.offset.i, node.offset.j, node.offset.k)
+        return npir.FieldSlice(
+            name=str(node.name),
+            i_offset=npir.AxisOffset.i(node.offset.i),
+            j_offset=npir.AxisOffset.j(node.offset.j),
+            k_offset=npir.AxisOffset.k(node.offset.k, parallel=ctx.parallel_k),
+        )
--- tests/test_unittest/test_gtc/test_oir_to_npir.py
+++ tests/test_unittest/test_gtc/test_oir_to_npir.py
@@ -55,3 +55,65 @@ def test_vertical_loop_to_vertical_pass():
     vertical_pass = OirToNpir().visit(vertical_loop)
 
     assert vertical_pass.body == []
+
+
+def test_horizontal_execution_to_vector_assigns():
+    horizontal_execution = EMPTY_HORIZONTAL_EXECUTION
+    vector_assigns = OirToNpir().visit(horizontal_execution)
+    assert vector_assigns == []
+
+
+def test_assign_stmt_to_vector_assign():
+    assign_stmt = oir.AssignStmt(
+        left=oir.FieldAccess(
+            name="a", offset=common.CartesianOffset.zero(), dtype=common.DataType.FLOAT64
+        ),
+        right=oir.FieldAccess(
+            name="b", offset=common.CartesianOffset(i=-1, j=22, k=0), dtype=common.DataType.FLOAT64
+        ),
+    )
+
+    ctx = OirToNpir.Context().set_parallel_k(value=True)
+    v_assign = OirToNpir().visit(assign_stmt, ctx=ctx)
+    assert isinstance(v_assign, npir.VectorAssign)
+    assert v_assign.left.k_offset.parallel is True
+    assert v_assign.right.k_offset.parallel is True
+
+
+def test_field_access_to_field_slice():
+    field_access = oir.FieldAccess(
+        name="a",
+        offset=common.CartesianOffset(i=-1, j=2, k=0),
+        dtype=common.DataType.FLOAT64,
+    )
+
+    ctx = OirToNpir.Context()
+    # parallel k case
+    parallel_ctx = ctx.set_parallel_k(value=True)
+    parallel_field_slice = OirToNpir().visit(field_access, ctx=parallel_ctx)
+    assert parallel_field_slice.k_offset.parallel is True
+    assert parallel_field_slice.i_offset.offset.value == -1
+    assert parallel_ctx.domain_padding["lower"][0] == 1
+    assert parallel_ctx.domain_padding["upper"][1] == 2
+
+    # sequential k case
+    sequential_ctx = ctx.set_parallel_k(value=False)
+    sequential_field_slice = OirToNpir().visit(field_access, ctx=sequential_ctx)
+    assert sequential_field_slice.k_offset.parallel is False
+    assert sequential_field_slice.i_offset.offset.value == -1
+    assert parallel_ctx.domain_padding["lower"][0] == 1
+    assert parallel_ctx.domain_padding["upper"][1] == 2
+
+    # ctx.domain_padding should be correctly extended when visiting another field access
+    OirToNpir().visit(
+        oir.FieldAccess(
+            name="b",
+            offset=common.CartesianOffset(i=-2, j=-1, k=4),
+            dtype=common.DataType.FLOAT64,
+        ),
+        ctx=parallel_ctx,
+    )
+    assert ctx.domain_padding["lower"][0] == 2
+    assert ctx.domain_padding["lower"][1] == 1
+    assert ctx.domain_padding["upper"][1] == 2
+    assert ctx.domain_padding["upper"][2] == 4
