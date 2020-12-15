7e445648bb2fbb66ef0a7763b671f63b5671fd86
Hannes Vogt <vogt@hey.com>
2020-12-15 11:53:08 +0100

    simplify upcaster

--- src/gt4py/gtc/passes/gtir_upcaster.py
+++ src/gt4py/gtc/passes/gtir_upcaster.py
@@ -1,8 +1,13 @@
-from eve import NodeTranslator
+from typing import Dict, Iterable, List, Union
+from eve import NodeTranslator, Node
 
 from gt4py.gtc import gtir
 
+import itertools
+
 from devtools import debug
+from gt4py.gtc.common import DataType
+from gt4py.gtc.gtir import Expr
 
 
 def _upcast_nodes(*exprs):
@@ -10,6 +15,14 @@ def _upcast_nodes(*exprs):
     return map(
         lambda e: e if e.dtype == target_dtype else gtir.Cast(dtype=target_dtype, expr=e), exprs
     )
+
+
+def _update_node(node: Node, updated_children: Dict[str, Node]):
+    old_children = node.dict(include={*updated_children.keys()})
+    if any([old_children[k] != updated_children[k] for k in updated_children.keys()]):
+        return node.copy(update=updated_children)
+    else:
+        return node
 
 
 class _GTIRUpcasting(NodeTranslator):
@@ -20,34 +33,21 @@ class _GTIRUpcasting(NodeTranslator):
     """
 
     def visit_BinaryOp(self, node: gtir.BinaryOp, **kwargs):
-        left = self.visit(node.left)
-        right = self.visit(node.right)
-        left, right = _upcast_nodes(left, right)
-
-        if left != node.left or right != node.right:
-            return gtir.BinaryOp(left=left, right=right, op=node.op)
-        else:
-            return node
+        left, right = _upcast_nodes(self.visit(node.left), self.visit(node.right))
+        return _update_node(node, {"left": left, "right": right})
 
     def visit_TernaryOp(self, node: gtir.TernaryOp, **kwargs):
-        cond = self.visit(node.cond)
-        true_expr = self.visit(node.true_expr)
-        false_expr = self.visit(node.false_expr)
-        true_expr, false_expr = _upcast_nodes(true_expr, false_expr)
-
-        if true_expr != node.true_expr or false_expr != node.false_expr or cond != node.cond:
-            return gtir.TernaryOp(true_expr=true_expr, false_expr=false_expr, cond=cond)
-        else:
-            return node
+        true_expr, false_expr = _upcast_nodes(
+            self.visit(node.true_expr), self.visit(node.false_expr)
+        )
+        return _update_node(
+            node, {"true_expr": true_expr, "false_expr": false_expr, "cond": self.visit(node.cond)}
+        )
 
     def visit_NativeFuncCall(self, node: gtir.NativeFuncCall, **kwargs):
-        args = self.visit(node.args)
-        args = _upcast_nodes(args)
+        args = _upcast_nodes(self.visit(node.args))
 
-        if args != node.args:
-            return gtir.NativeFuncCall(args=args)
-        else:
-            return node
+        return _update_node(node, {"args": args})
 
 
 def upcast(node: gtir.Stencil):
