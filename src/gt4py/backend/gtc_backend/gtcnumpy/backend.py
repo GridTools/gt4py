fea7ba0c17c6648714838d4e18eff374c148ceea
Rico Häuselmann <ricoh@cscs.ch>
2020-12-19 11:46:30 +0100

    [wip] module generator

--- src/gt4py/backend/gtc_backend/gtcnumpy/backend.py
+++ src/gt4py/backend/gtc_backend/gtcnumpy/backend.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass, asdict
+from typing import List, Optional, cast
+
 from gt4py.backend.base import BaseBackend, BaseModuleGenerator, CLIBackendMixin, register
 from gt4py.gtc import gtir
 from gt4py.gtc.gtir_to_oir import GTIRToOIR
@@ -9,6 +12,94 @@ from gt4py.gtc.python.oir_to_npir import OirToNpir
 
 
 class GTCModuleGenerator(BaseModuleGenerator):
+
+    def __call__(self, builder: Optional["StencilBuilder"] = None, **kwargs: Any) -> str:
+        return gt_utils.text.format_source(
+            self.template.render(
+                imports=self.generate_imports(),
+                module_members=self.generate_module_members(),
+                class_name=self.generate_class_name(),
+                docstring=self.generate_docstring(),
+                gt_backend=self.generate_gt_backend(),
+                gt_source=self.generate_gt_source(),
+                gt_domain_info=self.generate_gt_domain_info(),
+                gt_parameter_info=self.generate_gt_parameter_info(),
+                gt_constants=self.generate_gt_constants(),
+                gt_options=self.generate_gt_options(),
+                stencil_signature=self.generate_signature(),
+                field_names=self.generate_field_names(),
+                param_names=self.generate_param_names(),
+                pre_run=self.generate_pre_run(),
+                post_run=self.generate_post_run(),
+                impelentation=self.generate_implementation(),
+            ),
+            line_length=self.SOURCE_LINE_LENGTH,
+        )
+
+    def generate_imports(self) -> str:
+        return "\n".join(super().generate_imports().splitlines().extend(
+            ["import numpy", "import computation"]
+        ))
+
+    def generate_module_members(self) -> str:
+        return self.generate_module_members()
+
+    def generate_class_name(self) -> str:
+        return self.builder.class_name
+
+    def generate_docstring(self) -> str:
+        return self.builder.definition_ir.docstring
+
+    def generate_gt_backend(self) -> str:
+        return self.backend_name
+
+    def generate_gt_source(self) -> Dict[str, str]:
+        if self.builder.definition_ir.sources is None:
+            return {}
+        return {
+            key: gt_utils.text.format_source(value, line_length=self.SOURCE_LINE_LENGTH)
+            for key, value in self.builder.definition_ir.sources
+        }
+
+    def generate_gt_domain_info(self) -> str:
+        return DomainInfoGenerator(self.backend.gtir)
+
+    def generate_gt_field_info(self) -> str:
+        return FieldInfoGenerator(self.backend.gtir)
+
+    def generate_gt_parameter_info(self) -> str:
+        return ParameterInfoGenerator(self.backend.gtir)
+
+    def generate_gt_constants(self) -> Dict[str, str]:
+        if not definition_ir.externals:
+            return {}
+        return {
+            name: repr(value)
+            for name, value in self.builder.definition_ir.externals.items()
+            if isinstance(value, numbers.Number)
+        }
+
+    def generate_gt_options(self) -> Dict[str, Any]:
+        return {
+            key: value
+            for key, value in self.builder.options.as_dict().items()
+            if key not in ["build_info"]
+        }
+
+    def generate_field_names(self) -> List[str]:
+        return self.backend.npir.field_params
+
+    def generate_param_names(self) -> List[str]:
+        return self.backend.gtir.params
+
+    def generate_implementation(self) -> str:
+        params = ["{name}={name}" for name in self.backend.gtir.params]
+        params.extend(["_domain_=_domain_", "_origin_=_origin_])
+        return f"computation.run({', '.join(params)})"
+
+    @property
+    def backend(self) -> GTCNumpyBackend:
+            return cast(GTCNumpyBackend, self.builder.backend)
 
 
 
@@ -27,15 +118,18 @@ class GTCNumpyBackend(BaseBackend, CLIBackendMixin):
     }
     languages = {"computation": "python", "bindings": ["python"]}
     MODULE_GENERATOR_CLASS = GTCModuleGenerator
+    GTIR_KEY = "gtc:gtir"
+
+    def generate_computation(self) -> Dict[str, Union[str, Dict]]:
+        computation_name = f"computation.py"
+        return {computation_name: NpirGen.apply(self.npir)}
 
     def generate_bindings(self, language_name: str) -> Dict[str, Union[str, Dict]]:
         super().generate_bindings(language_name)
-        return {"computation.py": NpirGen.apply(self.npir)}
+        return {self.builder.module_path.name: self.make_module_source()}
 
     def make_module_source(self) -> str:
-        field_info = FieldInfoGenerator.apply(self.gtir)
-        args_data = ArgsData()
-        args_data.field_info = field_info
+        return self.MODULE_GENERATOR_CLASS(self.builder)()
 
     def _make_gtir(self) -> gtir.Stencil
         gtir = FieldsMetadataPass().visit(DefIRToGTIR.apply(self.builder.definition_ir))
@@ -49,7 +143,7 @@ class GTCNumpyBackend(BaseBackend, CLIBackendMixin):
 
     @property
     def gtir(self) -> gtir.Stencil:
-        key = "gtc:gtir"
+        key = GTIR_KEY
         if not self.builder.backend_data[key]:
             self.builder.with_backend_data({key: self._make_gtir()})
         return self.builder.backend_data[key]
