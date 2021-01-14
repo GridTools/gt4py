8419f8cc2d6144d34ebf1d0a2017125884cab8cd
Enrique G. Paredes <18477+egparedes@users.noreply.github.com>
2021-01-14 14:09:07 +0100

    Add format_source to BuildOptions shash

--- src/gt4py/definitions.py
+++ src/gt4py/definitions.py
@@ -642,10 +642,10 @@ class BuildOptions(AttributeClassLike):
 
     name = attribute(of=str)
     module = attribute(of=str)
+    format_source = attribute(of=bool, default=True)
     backend_opts = attribute(of=DictOf[str, Any], factory=dict)
     build_info = attribute(of=dict, optional=True)
     rebuild = attribute(of=bool, default=False)
-    format_source = attribute(of=bool, default=True)
     _impl_opts = attribute(of=DictOf[str, Any], factory=dict)
 
     @property
@@ -656,7 +656,7 @@ class BuildOptions(AttributeClassLike):
     @property
     def shashed_id(self):
         result = gt_utils.shashed_id(
-            self.name, self.module, *tuple(sorted(self.backend_opts.items()))
+            self.name, self.module, self.format_source, *tuple(sorted(self.backend_opts.items()))
         )
 
         return result
