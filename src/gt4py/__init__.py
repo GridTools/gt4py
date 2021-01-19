cb0e04fafde2435bad72973bae666ee86b91e34a
Hannes Vogt <hannes@havogt.de>
2021-01-19 15:12:02 +0100

    New formatting and updated copyright year (#309)
    Co-authored-by: Enrique G. Paredes <enriqueg@cscs.ch>

--- src/gt4py/__init__.py
+++ src/gt4py/__init__.py
@@ -2,7 +2,7 @@
 #
 # GT4Py - GridTools4Py - GridTools for Python
 #
-# Copyright (c) 2014-2020, ETH Zurich
+# Copyright (c) 2014-2021, ETH Zurich
 # All rights reserved.
 #
 # This file is part the GT4Py project and the GridTools framework.
--- tests/test_unittest/test_cli.py
+++ tests/test_unittest/test_cli.py
@@ -1,4 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# GT4Py - GridTools4Py - GridTools for Python
+#
+# Copyright (c) 2014-2021, ETH Zurich
+# All rights reserved.
+#
+# This file is part the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 """Unit tests for the command line interface (CLI)."""
+
 import re
 import sys
 
@@ -98,9 +115,7 @@ def test_list_backends(clirunner, list_backends_line_pattern):
     result = clirunner.invoke(cli.gtpyc, ["list-backends"], catch_exceptions=False)
 
     assert result.exit_code == 0
-    assert re.findall(list_backends_line_pattern, result.output, re.MULTILINE), print(
-        result.output
-    )
+    assert re.findall(list_backends_line_pattern, result.output, re.MULTILINE), print(result.output)
 
 
 def test_gen_silent(clirunner, simple_stencil, tmp_path):
