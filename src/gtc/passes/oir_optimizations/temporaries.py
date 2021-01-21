1e933f38b4d25f45f06de5986e68fffc03ba6f36
Felix Thaler <thaler@cscs.ch>
2021-01-21 11:03:59 +0100

    Basic tmp -> scalar replacement

--- src/gtc/passes/oir_optimizations/temporaries.py
+++ src/gtc/passes/oir_optimizations/temporaries.py
@@ -15,118 +15,81 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import collections
-from typing import Any, Dict, List, Mapping, Set, Tuple
+from typing import Any, Dict, List, Set, Tuple, Union
 
 from eve import NodeTranslator, NodeVisitor
 from gtc import oir
 
 
 class TemporaryDisposal(NodeTranslator):
-    """
-    Removes unneeded temporary fields and accesses.
+    """Replaces temporary fields by scalars.
 
-    Replaces temporaries with a single write and single read access within a single
-    horizontal execution (and zero offsets) by direct assignment.
-
-    E.g. replaces the following sequence of statements within a horizontal execution
-        tmp[0, 0, 0] = expr()
-        (other statements...)
-        out[0, 0, 0] = tmp[0, 0, 0]
-    by
-        out[0, 0, 0] = expr()
-        (other statements...)
-
-    If there are any additional accesses to the output field between the two
-    temporary accesses or if there are multiple accesses to the temporary anywhere
-    inside the vertical loop, no optimization is performed.
-
-    For example, this is kept as is:
-        tmp[0, 0, 0] = expr()
-        foo[0, 0, 0] = inout[0, 0, 0]
-        inout[0, 0, 0] = tmp[0, 0, 0]
+    All temporary fields that are local to a horizontal execution and accesed
+    only without offsets are replaced by scalars.
     """
 
-    class WritesAndReads(NodeVisitor):
+    class LocalTemporaryFinder(NodeVisitor):
         def visit_FieldAccess(
-            self, node: oir.FieldAccess, *, other_accesses: Dict[str, int], **kwargs: Any
-        ) -> None:
-            other_accesses[node.name] += 1
-
-        def visit_AssignStmt(
             self,
-            node: oir.AssignStmt,
+            node: oir.FieldAccess,
             *,
-            basic_reads: Mapping[str, List[oir.AssignStmt]],
-            basic_writes: Mapping[str, List[oir.AssignStmt]],
-            other_accesses: Set[str],
+            access_map: Dict[str, List[Tuple[oir.VerticalLoop, oir.HorizontalExecution, bool]]],
+            vertical_loop: oir.VerticalLoop,
+            horizontal_execution: oir.HorizontalExecution,
             **kwargs: Any,
         ) -> None:
-            assert isinstance(node.left, oir.FieldAccess)
-            basic_writes[node.left.name].append(node)
-
-            if (
-                isinstance(node.right, oir.FieldAccess)
-                and node.right.offset.i == node.right.offset.j == node.right.offset.k == 0
-            ):
-                basic_reads[node.right.name].append(node)
-            else:
-                self.visit(node.right, other_accesses=other_accesses)
-
-        @classmethod
-        def apply(cls, node: oir.Stmt) -> Dict[str, Tuple[oir.AssignStmt, oir.AssignStmt]]:
-            basic_reads: Dict[str, List[oir.AssignStmt]] = collections.defaultdict(list)
-            basic_writes: Dict[str, List[oir.AssignStmt]] = collections.defaultdict(list)
-            other_accesses: Dict[str, int] = collections.Counter()
-            cls().visit(
-                node,
-                basic_reads=basic_reads,
-                basic_writes=basic_writes,
-                other_accesses=other_accesses,
+            data = (
+                vertical_loop,
+                horizontal_execution,
+                node.offset.i == node.offset.j == node.offset.k,
             )
+            if data not in access_map[node.name]:
+                access_map[node.name].append(data)
 
-            result = dict()
-            for field, bwrites in basic_writes.items():
-                breads = basic_reads[field]
-                if len(bwrites) == len(breads) == 1 and not other_accesses[field]:
-                    result[field] = breads[0], bwrites[0]
+        def visit_HorizontalExecution(
+            self,
+            node: oir.HorizontalExecution,
+            **kwargs: Any,
+        ) -> None:
+            self.generic_visit(node, horizontal_execution=node, **kwargs)
 
-            return result
+        def visit_VerticalLoop(self, node: oir.VerticalLoop, **kwargs: Any) -> None:
+            self.generic_visit(node, vertical_loop=node, **kwargs)
 
-    def visit_VerticalLoop(
-        self, node: oir.VerticalLoop, symtable: Mapping[str, Any], **kwargs: Any
-    ) -> oir.VerticalLoop:
-        result = self.generic_visit(node, **kwargs)
-        global_candidates = self.WritesAndReads.apply(result)
+        def visit_Stencil(
+            self, node: oir.Stencil, **kwargs: Any
+        ) -> Dict[str, Tuple[oir.VerticalLoop, oir.HorizontalExecution]]:
+            access_map: Dict[
+                str, List[Tuple[oir.VerticalLoop, oir.HorizontalExecution, bool]]
+            ] = collections.defaultdict(list)
+            self.generic_visit(node, access_map=access_map, **kwargs)
+            return {
+                field: accesses[0][:2]
+                for field, accesses in access_map.items()
+                if field in node.symtable_
+                and isinstance(node.symtable_[field], oir.Temporary)
+                and len(accesses) == 1
+                and accesses[0][2]
+            }
 
-        for horizontal_execution in result.horizontal_executions:
-            local_candidates = self.WritesAndReads.apply(horizontal_execution)
-            for field, (read, write) in local_candidates.items():
-                if not isinstance(symtable[field], oir.Temporary):
-                    # can not discard non-temporary fields
-                    continue
-
-                if field not in global_candidates:
-                    # temporary might be used in multiple horizontal executions
-                    continue
-
-                write_index = horizontal_execution.body.index(write)
-                read_index = horizontal_execution.body.index(read)
-                assert write_index < read_index, "unexpected write after read?"
-                for stmt in horizontal_execution.body[write_index + 1 : read_index]:
-                    # if there are any accesses to the destination field between the two to-be optimized accesses, we skip the optimization
-                    if any(
-                        name == read.left.name
-                        for name in stmt.iter_tree().if_isinstance(oir.FieldAccess).getattr("name")
-                    ):
-                        break
-                else:
-                    write.left = read.left
-                    horizontal_execution.body.remove(read)
-                    result.declarations.remove(symtable[field])
-
-        return result
+    class AccessReplacer(NodeTranslator):
+        def visit_FieldAccess(
+            self, node: oir.FieldAccess, *, local_tmps: Set[str], **kwargs: Any
+        ) -> Union[oir.FieldAccess, oir.ScalarAccess]:
+            if node.name in local_tmps:
+                return oir.ScalarAccess(name=node.name, dtype=node.dtype)
+            return self.generic_visit(node, **kwargs)
 
     def visit_Stencil(self, node: oir.Stencil, **kwargs: Any) -> oir.Stencil:
-        result = self.generic_visit(node, symtable=node.symtable_, **kwargs)
+        result = self.generic_visit(node, **kwargs)
+        local_tmps = self.LocalTemporaryFinder().visit(result)
+        for local_tmp, (vertical_loop, horizontal_execution) in local_tmps.items():
+            decl = result.symtable_[local_tmp]
+            vertical_loop.declarations.remove(decl)
+            horizontal_execution.declarations.append(
+                oir.LocalScalar(name=decl.name, dtype=decl.dtype, loc=decl.loc)
+            )
+
+        result = self.AccessReplacer().visit(result, local_tmps=local_tmps)
         result.collect_symbols()
         return result
