GridTools/gt4py

commit e0d60c4d85c1276e69e7ee9920cff2ff71121c23

Updated PruneKCacheFills and added comments

src/gtc/passes/oir_optimizations/caches.py
@@ -91,6 +91,14 @@ class KCacheDetection(NodeTranslator):
 
 
 class PruneKCacheFills(NodeTranslator):
+    """Prunes unneeded k-cache fills.
+
+    A fill is classified as required if at least one of the two following conditions holds in any of the loop sections:
+    * There is a read with offset in the direction of looping.
+    * The first centered access is a read access.
+    If none of the conditions holds for any loop section, the fill is considered as unneeded.
+    """
+
     def visit_KCache(self, node: oir.KCache, *, pruneable: Set[str], **kwargs: Any) -> oir.KCache:
         if node.name in pruneable:
             return oir.KCache(name=node.name, fill=False, flush=node.flush)
@@ -102,20 +110,22 @@ class PruneKCacheFills(NodeTranslator):
             return self.generic_visit(node, **kwargs)
         assert node.loop_order != common.LoopOrder.PARALLEL
 
-        accesses = AccessCollector.apply(node)
-        offsets = accesses.offsets()
-        center_accesses = [a for a in accesses.ordered_accesses() if a.offset == (0, 0, 0)]
+        def pruneable_fields(section: oir.VerticalLoopSection) -> Set[str]:
+            accesses = AccessCollector.apply(section)
+            offsets = accesses.offsets()
+            center_accesses = [a for a in accesses.ordered_accesses() if a.offset == (0, 0, 0)]
 
-        def requires_fill(field: str) -> bool:
-            k_offsets = (o[2] for o in offsets[field])
-            if node.loop_order == common.LoopOrder.FORWARD and max(k_offsets) > 0:
-                return True
-            if node.loop_order == common.LoopOrder.BACKWARD and min(k_offsets) < 0:
-                return True
-            return next(a.is_read for a in center_accesses if a.field == field)
+            def requires_fill(field: str) -> bool:
+                k_offsets = (o[2] for o in offsets[field])
+                if node.loop_order == common.LoopOrder.FORWARD and max(k_offsets) > 0:
+                    return True
+                if node.loop_order == common.LoopOrder.BACKWARD and min(k_offsets) < 0:
+                    return True
+                return next(a.is_read for a in center_accesses if a.field == field)
 
-        pruneable = {field for field in filling_fields if not requires_fill(field)}
+            return {field for field in filling_fields if not requires_fill(field)}
 
+        pruneable = set.intersection(*(pruneable_fields(section) for section in node.sections))
         return self.generic_visit(node, pruneable=pruneable, **kwargs)
 
 

commit 08a799293a526bef343caff7a5dc41df5c4ef2d6

Fix bug introduced with e0d60c4d85c1276e69e7ee9920cff2ff71121c23

src/gtc/passes/oir_optimizations/caches.py
@@ -121,7 +121,10 @@ class PruneKCacheFills(NodeTranslator):
                     return True
                 if node.loop_order == common.LoopOrder.BACKWARD and min(k_offsets) < 0:
                     return True
-                return next(a.is_read for a in center_accesses if a.field == field)
+                try:
+                    return next(a.is_read for a in center_accesses if a.field == field)
+                except StopIteration:
+                    return False
 
             return {field for field in filling_fields if not requires_fill(field)}
 