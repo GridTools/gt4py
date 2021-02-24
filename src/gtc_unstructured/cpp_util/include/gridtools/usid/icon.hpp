812d5ea7995b0e5bcbb23f98287161b23be4e021
Hannes Vogt <vogt@hey.com>
2021-02-24 14:34:16 +0100

    fix icon (fortran_array_view) connectivity_producer

--- src/gtc_unstructured/cpp_util/include/gridtools/usid/icon.hpp
+++ src/gtc_unstructured/cpp_util/include/gridtools/usid/icon.hpp
@@ -6,10 +6,10 @@
 namespace gridtools {
     template <class Connectivity, class MaxNeighbors>
     auto make_connectivity_producer(MaxNeighbors max_neighbors, Connectivity src) {
-        return [src](auto traits) {
+        return [max_neighbors, src](auto traits) {
             return gridtools::storage::builder<decltype(traits)>
                                 .template type<int>()
-                                .dimensions(at_key<integral_constant<int,0>>(sid::get_upper_bounds(src)), max_neighbors>{})
+                                .dimensions(at_key<integral_constant<int,0>>(sid::get_upper_bounds(src)), max_neighbors)
                                 .initializer([&src](std::size_t p, std::size_t n){
                                     auto ptr = sid::get_origin(src)();
                                     auto strides = sid::get_strides(src);
