0076e7eeb0ad937df39600dd63aa1e3dd70cd580
Linus Groner <linus.groner@cscs.ch>
2021-03-10 11:45:07 +0100

    fix bad merge conflict resolution

--- tests/test_integration/test_code_generation.py
+++ tests/test_integration/test_code_generation.py
@@ -213,7 +213,7 @@ def test_lower_dimensional_inputs(backend):
     assert field_1d.shape == (full_shape[-1],)
 
     stencil(field_3d, field_2d, field_1d, origin=(1, 1, 0), domain=(4, 3, 6))
-    np.testing.assert_allclose(field_3d.view(np.ndarray)[1:-1, 1:-2, :1], 3)
-    np.testing.assert_allclose(field_3d.view(np.ndarray)[1:-1, 1:-2, 1:], 2)
+    np.testing.assert_allclose(field_3d.to_numpy()[1:-1, 1:-2, :1], 3)
+    np.testing.assert_allclose(field_3d.to_numpy()[1:-1, 1:-2, 1:], 2)
 
     stencil(field_3d, field_2d, field_1d)
