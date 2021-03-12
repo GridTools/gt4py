eeefe71f1c3ef647e40ec918b24a2d96a2c58db8
Johann Dahm <johannd@vulcan.com>
2021-03-11 17:12:23 -0800

    Workaround GT issue in test_lower_dimensional_inputs

--- tests/test_integration/test_code_generation.py
+++ tests/test_integration/test_code_generation.py
@@ -192,7 +192,7 @@ def test_lower_dimensional_inputs(backend):
     ):
         with computation(FORWARD):
             with interval(0, 1):
-                field_2d = field_1d[1] + field_3d[0, 1, 0]
+                field_2d = field_1d[1]
 
         with computation(PARALLEL):
             with interval(0, -1):
@@ -204,7 +204,7 @@ def test_lower_dimensional_inputs(backend):
             with interval(0, 1):
                 field_3d = tmp[1, 0, 0] + field_1d[1]
             with interval(1, None):
-                field_3d = tmp[-1, 0, -1]
+                field_3d = tmp[-1, 0, 0]
 
     full_shape = (6, 6, 6)
     default_origin = (1, 1, 0)
