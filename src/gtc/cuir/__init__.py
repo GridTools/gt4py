abb27f0db163be5ef2dc7c2b78cbdeb4af37a8a4
Felix Thaler <thaler@cscs.ch>
2021-04-07 13:47:33 +0000

    GTC CUDA Backend (#347)
    Introduces a GTC-based CUDA-backend that is not using GridTools C++ stencil composition, but instead directly generating CUDA code (still based on GridTools infrastracture).

--- src/gtc/cuir/__init__.py
+++ src/gtc/cuir/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# GT4Py - GridTools4Py - GridTools for Python
+# GTC Toolchain - GT4Py Project - GridTools Framework
 #
 # Copyright (c) 2014-2021, ETH Zurich
 # All rights reserved.
 #
-# This file is part the GT4Py project and the GridTools framework.
+# This file is part of the GT4Py project and the GridTools framework.
 # GT4Py is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or any later
@@ -13,9 +13,3 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-from .cuda.backend import GTCCudaBackend
-from .gtcpp.backend import GTCGTCpuIfirstBackend, GTCGTCpuKfirstBackend, GTCGTGpuBackend
-
-
-__all__ = ["GTCCudaBackend", "GTCGTCpuIfirstBackend", "GTCGTCpuKfirstBackend", "GTCGTGpuBackend"]
