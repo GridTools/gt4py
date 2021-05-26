40bfac4bab10b5f618c614f78c87060955b74a9e
Eddie Davis <eddied@vulcan.com>
2021-05-26 17:06:59 +0000

    Restore GreedyMerging pass in GtCpp

--- src/gt4py/backend/gtc_backend/gtcpp/backend.py
+++ src/gt4py/backend/gtc_backend/gtcpp/backend.py
@@ -38,14 +38,13 @@ from gtc import gtir_to_oir
 from gtc.common import DataType
 from gtc.gtcpp import gtcpp, gtcpp_codegen, oir_to_gtcpp
 from gtc.passes.gtir_pipeline import GtirPipeline
-from gtc.passes.oir_dace_optimizations import GraphMerging, optimize_horizontal_executions
 from gtc.passes.oir_optimizations.caches import (
     IJCacheDetection,
     KCacheDetection,
     PruneKCacheFills,
     PruneKCacheFlushes,
 )
-from gtc.passes.oir_optimizations.horizontal_execution_merging import OnTheFlyMerging
+from gtc.passes.oir_optimizations.horizontal_execution_merging import GreedyMerging, OnTheFlyMerging
 from gtc.passes.oir_optimizations.pruning import NoFieldAccessPruning
 from gtc.passes.oir_optimizations.temporaries import (
     LocalTemporariesToScalars,
@@ -81,7 +80,7 @@ class GTCGTExtGenerator:
         }
 
     def _optimize_oir(self, oir):
-        oir = optimize_horizontal_executions(oir, GraphMerging)
+        oir = GreedyMerging().visit(oir)
         oir = AdjacentLoopMerging().visit(oir)
         oir = LocalTemporariesToScalars().visit(oir)
         oir = WriteBeforeReadTemporariesToScalars().visit(oir)
