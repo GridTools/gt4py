cd88e348121fd32fdb6125518732ae72acf6916a
Rico Häuselmann <ricoh@cscs.ch>
2021-06-29 16:56:38 +0200

    try to fix failing gtc:dace and gtc:cuda tests

--- tests/test_unittest/test_backend_api.py
+++ tests/test_unittest/test_backend_api.py
@@ -43,7 +43,7 @@ def test_generate_computation(backend, tmp_path):
     builder = StencilBuilder(init_1, backend=backend).with_caching(
         "nocaching", output_path=tmp_path / __name__ / "generate_computation"
     )
-    if backend.name.startswith("gtc:gt:"):
+    if backend.name.startswith("gtc:") and (backend.name != "gtc:numpy"):
         result = builder.backend.generate_computation(ir=builder.definition_ir)
     else:
         result = builder.backend.generate_computation()
