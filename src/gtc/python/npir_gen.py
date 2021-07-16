fix by means of broadcast_to

src/gtc/python/npir_gen.py
@@ -134,15 +134,22 @@ class NpirGen(TemplatedGenerator):
         "{name} = np.reshape({name}, ({shape}))\n_origin_['{name}'] = [{origin}]"
     )
 
-    def visit_FieldSlice(self, node: npir.FieldSlice, **kwargs: Any) -> Union[str, Collection[str]]:
-        kwargs.setdefault("mask_acc", "")
-        offsets = ", ".join(
-            self.visit(offset, **kwargs) if offset else ":"
-            for offset in [node.i_offset, node.j_offset, node.k_offset]
-        )
-        return self.generic_visit(node, offsets=offsets, **kwargs)
+    def visit_FieldSlice(
+        self, node: npir.FieldSlice, mask_acc="", mask_shape=None, **kwargs: Any
+    ) -> Union[str, Collection[str]]:
 
-    FieldSlice = FormatTemplate("{name}_[{offsets}]{mask_acc}")
+        offset = [node.i_offset, node.j_offset, node.k_offset]
+
+        offset_str = ", ".join(self.visit(off, **kwargs) if off else ":" for off in offset)
+
+        if mask_acc and any(off is None for off in offset):
+            arr_expr = f"np.broadcast_to({node.name}_[{offset_str}], (I-i, J-j, K-k))"
+        else:
+            arr_expr = f"{node.name}_[{offset_str}]"
+
+        return self.generic_visit(node, arr_expr=arr_expr, mask_acc=mask_acc, **kwargs)
+
+    FieldSlice = FormatTemplate("{arr_expr}{mask_acc}")
 
     def visit_EmptyTemp(
         self, node: npir.EmptyTemp, *, temp_name: str, **kwargs