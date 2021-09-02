9aecabdc896dd07d2b9fe0ecc5e7a52066c9f670
Johann Dahm <johann.dahm@gmail.com>
2021-09-01 21:29:41 -0700

    Remove unused import

--- src/gtc/passes/gtir_legacy_extents.py
+++ src/gtc/passes/gtir_legacy_extents.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Union
 
 from eve import NodeVisitor
 from eve.utils import XIterator
