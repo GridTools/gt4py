c26662c82a4ea2c0858160b8f10e0c2ad3f589ec
Hannes Vogt <vogt@hey.com>
2021-10-15 14:39:34 +0200

    execute shift once complete

--- src/iterator/embedded.py
+++ src/iterator/embedded.py
@@ -268,11 +268,11 @@ def execute_shift(pos, tag, index, *, offset_provider):
 # shift(tag)(inp) -> incomplete shift
 # shift(index)(shift(tag)(inp)) -> full shift
 # Therefore the following transformation holds
-# shift(e2c,0)(shift(v2c,2)(cell_field))
-# = shift(0)(shift(e2c)(shift(2)(shift(v2c)(cell_field))))
-# = shift(v2c, 2, e2c, 0)(cell_field)
-# = shift(v2c,e2c,2,0)(cell_field) <-- v2c,e2c twice incomplete shift
-# = shift(2,0)(shift(v2c,e2c)(cell_field))
+# shift(e2v,0)(shift(c2e,2)(cell_field))
+# = shift(0)(shift(e2v)(shift(2)(shift(c2e)(cell_field))))
+# = shift(c2e, 2, e2v, 0)(cell_field)
+# = shift(c2e,e2v,2,0)(cell_field) <-- v2c,e2c twice incomplete shift
+# = shift(2,0)(shift(c2e,e2v)(cell_field))
 # for implementations it means everytime we have an index, we can "execute" a concrete shift
 def group_offsets(*offsets):
     tag_stack = []
@@ -286,15 +286,16 @@ def group_offsets(*offsets):
             else:
                 tag_stack.append(offset)
         else:
-            assert not tag_stack
-            index_stack.append(offset)
+            if tag_stack:
+                tag = tag_stack.pop(0)
+                complete_offsets.append((tag, offset))
+            # assert not tag_stack
+            else:
+                index_stack.append(offset)
     return complete_offsets, tag_stack
 
 
-def shift_position(pos, *offsets, offset_provider):
-    complete_offsets, open_offsets = group_offsets(*offsets)
-    # assert not open_offsets # TODO enable this, check failing test and make everything saver
-
+def shift_position(pos, *complete_offsets, offset_provider):
     new_pos = pos.copy()
     for tag, index in complete_offsets:
         new_pos = execute_shift(new_pos, tag, index, offset_provider=offset_provider)
@@ -308,33 +309,37 @@ def get_open_offsets(*offsets):
 
 
 class MDIterator:
-    def __init__(self, field, pos, *, offsets=[], offset_provider, column_axis=None) -> None:
+    def __init__(
+        self, field, pos, *, incomplete_offsets=[], offset_provider, column_axis=None
+    ) -> None:
         self.field = field
         self.pos = pos
-        self.offsets = offsets
+        self.incomplete_offsets = incomplete_offsets
         self.offset_provider = offset_provider
         self.column_axis = column_axis
 
     def shift(self, *offsets):
+        complete_offsets, open_offsets = group_offsets(*self.incomplete_offsets, *offsets)
         return MDIterator(
             self.field,
-            self.pos,
-            offsets=[*offsets, *self.offsets],
+            shift_position(self.pos, *complete_offsets, offset_provider=self.offset_provider),
+            incomplete_offsets=open_offsets,
             offset_provider=self.offset_provider,
             column_axis=self.column_axis,
         )
 
     def max_neighbors(self):
-        open_offsets = get_open_offsets(*self.offsets)
-        assert open_offsets
-        assert isinstance(self.offset_provider[open_offsets[0].value], NeighborTableOffsetProvider)
-        return self.offset_provider[open_offsets[0].value].max_neighbors
+        assert self.incomplete_offsets
+        assert isinstance(
+            self.offset_provider[self.incomplete_offsets[0].value], NeighborTableOffsetProvider
+        )
+        return self.offset_provider[self.incomplete_offsets[0].value].max_neighbors
 
     def is_none(self):
-        return shift_position(self.pos, *self.offsets, offset_provider=self.offset_provider) is None
+        return self.pos is None
 
     def deref(self):
-        shifted_pos = shift_position(self.pos, *self.offsets, offset_provider=self.offset_provider)
+        shifted_pos = self.pos.copy()
 
         if not all(axis in shifted_pos.keys() for axis in self.field.axises):
             raise IndexError("Iterator position doesn't point to valid location for its field.")
@@ -362,7 +367,7 @@ def make_in_iterator(inp, pos, offset_provider, *, column_axis):
     return MDIterator(
         inp,
         new_pos,
-        offsets=[*sparse_dimensions],
+        incomplete_offsets=[*sparse_dimensions],
         offset_provider=offset_provider,
         column_axis=column_axis,
     )
