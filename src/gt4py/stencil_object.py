48658097bb9b921da6c153a743facb3142ee5297
Johann Dahm <johann.dahm@gmail.com>
2021-11-01 11:58:31 -0700

    Implement feedback

--- src/gt4py/stencil_object.py
+++ src/gt4py/stencil_object.py
@@ -19,7 +19,8 @@ import collections.abc
 import sys
 import time
 import warnings
-from typing import Any, Callable, Dict, Optional, Tuple, Union
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, Optional, Tuple, Union, cast
 
 import numpy as np
 
@@ -33,17 +34,13 @@ FieldType = Union[gt_storage.storage.Storage, np.ndarray]
 OriginType = Union[Tuple[int, int, int], Dict[str, Tuple[int, ...]]]
 
 
+@dataclass(frozen=True)
 class FrozenStencil:
-    def __init__(
-        self,
-        stencil_object: "StencilObject",
-        origin: Dict[str, Tuple[int, ...]],
-        domain: Tuple[int, ...],
-    ):
-        self.stencil_object = stencil_object
-        self.origin = origin
-        self.domain = domain
+    stencil_object: "StencilObject"
+    origin: Dict[str, Tuple[int, ...]]
+    domain: Tuple[int, ...]
 
+    def __post_init__(self):
         for name, field_info in self.stencil_object.field_info.items():
             if name not in self.origin or len(self.origin[name]) != field_info.ndim:
                 raise ValueError(
@@ -173,8 +170,7 @@ class StencilObject(abc.ABC):
         *,
         squeeze: bool = True,
     ) -> Shape:
-        """
-        Return the maximum domain size possible.
+        """Return the maximum domain size possible.
 
         Parameters
         ----------
@@ -212,7 +208,7 @@ class StencilObject(abc.ABC):
         else:
             return max_domain
 
-    def _validate_args(  # noqa: C901
+    def _validate_args(  # noqa: C901  # Function is too complex
         self,
         field_args: Dict[str, FieldType],
         param_args: Dict[str, Any],
@@ -316,7 +312,7 @@ class StencilObject(abc.ABC):
                         f"Origin for field {name} too small. Must be at least {min_origin}, is {field_domain_origin}"
                     )
 
-                spatial_domain = Shape(domain).filter_mask(field_domain_mask)
+                spatial_domain = cast(Shape, domain).filter_mask(field_domain_mask)
                 upper_indices = field_info.boundary.upper_indices.filter_mask(field_domain_mask)
                 min_shape = tuple(
                     o + d + h for o, d, h in zip(field_domain_origin, spatial_domain, upper_indices)
@@ -380,8 +376,7 @@ class StencilObject(abc.ABC):
         validate_args: bool = True,
         exec_info: Optional[Dict[str, Any]] = None,
     ) -> None:
-        """
-        Check and preprocess the provided arguments (called by :class:`StencilObject` subclasses).
+        """Check and preprocess the provided arguments (called by :class:`StencilObject` subclasses).
 
         Note that this function will always try to expand simple parameter values to complete
         data structures by repeating the same value as many times as needed.
