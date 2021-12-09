fe449fb0003f2cb2f9d900a361c838934bfb2093
Felix Thaler <thaler@cscs.ch>
2021-12-09 13:18:08 +0000

    More Math Builtins: sinh, cosh, tanh, asinh, acosh, atanh, gamma and cbrt  (#569)
    Added some missing math builtins. Most relevant change: this introduces scipy as a dependency, as numpy does not provide an implementation of the gamma function. Closes #525.

--- src/gtc/common.py
+++ src/gtc/common.py
@@ -160,11 +160,19 @@ class NativeFunction(StrEnum):
     ARCSIN = "arcsin"
     ARCCOS = "arccos"
     ARCTAN = "arctan"
+    SINH = "sinh"
+    COSH = "cosh"
+    TANH = "tanh"
+    ARCSINH = "arcsinh"
+    ARCCOSH = "arccosh"
+    ARCTANH = "arctanh"
 
     SQRT = "sqrt"
     POW = "pow"
     EXP = "exp"
     LOG = "log"
+    GAMMA = "gamma"
+    CBRT = "cbrt"
 
     ISFINITE = "isfinite"
     ISINF = "isinf"
@@ -193,10 +201,18 @@ NativeFunction.IR_OP_TO_NUM_ARGS = {
         NativeFunction.ARCSIN: 1,
         NativeFunction.ARCCOS: 1,
         NativeFunction.ARCTAN: 1,
+        NativeFunction.SINH: 1,
+        NativeFunction.COSH: 1,
+        NativeFunction.TANH: 1,
+        NativeFunction.ARCSINH: 1,
+        NativeFunction.ARCCOSH: 1,
+        NativeFunction.ARCTANH: 1,
         NativeFunction.SQRT: 1,
         NativeFunction.POW: 2,
         NativeFunction.EXP: 1,
         NativeFunction.LOG: 1,
+        NativeFunction.GAMMA: 1,
+        NativeFunction.CBRT: 1,
         NativeFunction.ISFINITE: 1,
         NativeFunction.ISINF: 1,
         NativeFunction.ISNAN: 1,
