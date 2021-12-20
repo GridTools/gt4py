638d856891709bff51528ec76f7e5ce335a9849e
Johann Dahm <johannd@vulcan.com>
2021-12-20 10:18:26 -0800

    Apply suggestions from code review
    Co-authored-by: Till Ehrengruber <t.ehrengruber@me.com>

--- src/gt4py/frontend/gtscript_frontend.py
+++ src/gt4py/frontend/gtscript_frontend.py
@@ -1101,18 +1101,18 @@ class IRMaker(ast.NodeVisitor):
                 if len(result.data_index) != len(self.fields[result.name].data_dims):
                     raise GTScriptSyntaxError(
                         f"Incorrect data index length {len(result.data_index)}. "
-                        f"Field {result.name} has {len(self.fields[result.name].data_dims)} data dimensions.",
+                        f"Invalid data dimension index. Field {result.name} has {len(self.fields[result.name].data_dims)} data dimensions.",
                         loc=gt_ir.Location.from_ast_node(node),
                     )
                 if any(
-                    isinstance(index, gt_ir.ScalarLiteral) and int(index.value) >= axis_length
+                    isinstance(index, gt_ir.ScalarLiteral) and not (0 <= int(index.value) < axis_length)
                     for index, axis_length in zip(
                         result.data_index, self.fields[result.name].data_dims
                     )
                 ):
                     raise GTScriptSyntaxError(
                         f"Data index out of bounds. "
-                        f"Found index {result.data_index}, but field {result.name} has dimensions {self.fields[result.name].data_dims}",
+                        f"Found index {result.data_index}, but field {result.name} has {self.fields[result.name].data_dims} data-dimensions",
                         loc=gt_ir.Location.from_ast_node(node),
                     )
             else:
