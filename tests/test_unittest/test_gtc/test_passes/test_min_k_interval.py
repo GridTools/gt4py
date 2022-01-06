Fix max_domain computation and min_k_interval tests

tests/test_unittest/test_gtc/test_passes/test_min_k_interval.py
@@ -155,7 +155,7 @@ def test_k_bounds_exec(definition, expected):
         field_a = gt4py.storage.zeros(
             backend=backend,
             default_origin=(0, 0, 0),
-            shape=(1, 1, required_field_size),
+            shape=(1, 1, expected_min_k_size),
             dtype=np.float64,
         )
         field_b = gt4py.storage.ones(