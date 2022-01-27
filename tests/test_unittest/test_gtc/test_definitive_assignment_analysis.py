9882829b73faa1e1499f12d481fce9abf1d378fa
Till Ehrengruber <till.ehrengruber@cscs.ch>
2022-01-27 09:59:02 +0100

    Added tests for nested if-elif-else statements, addressed reviewer comments

--- tests/test_unittest/test_gtc/test_definitive_assignment_analysis.py
+++ tests/test_unittest/test_gtc/test_definitive_assignment_analysis.py
@@ -8,19 +8,14 @@ from gt4py.gtscript import PARALLEL, Field, computation, interval, stencil
 from gt4py.stencil_builder import StencilBuilder
 from gtc.passes import gtir_definitive_assignment_analysis as daa
 
-
-class TestData(TypedDict):
-    valid: bool
-
-
 # A list of dictionaries containing a stencil definition and the expected test case outputs
-test_data: List[Tuple[Callable, TestData]] = []
+test_data: List[Tuple[Callable, bool]] = []
 
 
 def register_test_case(*, valid):
     def _wrapper(definition):
         global test_data
-        test_data.append((definition, {"valid": valid}))
+        test_data.append((definition, valid))
         return definition
 
     return _wrapper
@@ -29,6 +24,7 @@ def register_test_case(*, valid):
 # test cases
 @register_test_case(valid=False)
 def daa_0(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
+    """Invalid stencil with `tmp` undefined in one branch if a conditional on a boolean mask field"""
     with computation(PARALLEL):
         with interval(...):
             if mask:
@@ -38,6 +34,7 @@ def daa_0(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
 
 @register_test_case(valid=False)
 def daa_1(in_field: Field[float], mask: bool, out_field: Field[float]):
+    """Invalid stencil with `tmp` undefined in one branch if a conditional on a boolean mask parameter"""
     with computation(PARALLEL):
         with interval(...):
             if mask:
@@ -47,6 +44,7 @@ def daa_1(in_field: Field[float], mask: bool, out_field: Field[float]):
 
 @register_test_case(valid=True)
 def daa_2(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
+    """Valid stencil with `tmp` defined in one branch and only used inside of that branch."""
     with computation(PARALLEL):
         with interval(...):
             if mask:
@@ -56,6 +54,7 @@ def daa_2(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
 
 @register_test_case(valid=True)
 def daa_3(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
+    """Valid stencil with `tmp` defined in both branches of a conditional on a boolean field and used outside."""
     with computation(PARALLEL):
         with interval(...):
             if mask:
@@ -67,6 +66,7 @@ def daa_3(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
 
 @register_test_case(valid=False)
 def daa_4(in_field: Field[float], mask: bool, out_field: Field[float]):
+    """Valid stencil with `tmp` defined in both branches of a conditional on a boolean parameter and used outside."""
     with computation(PARALLEL):
         with interval(...):
             if mask:
@@ -78,6 +78,7 @@ def daa_4(in_field: Field[float], mask: bool, out_field: Field[float]):
 
 @register_test_case(valid=True)
 def daa_5(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
+    """Valid stencil with `tmp` defined in every branch of a nested if-statement and used outside."""
     with computation(PARALLEL):
         with interval(...):
             if mask:
@@ -92,6 +93,7 @@ def daa_5(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
 
 @register_test_case(valid=True)
 def daa_6(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
+    """Valid stencil with `tmp` defined in one branch only, but unconditionally overwritten outside before use."""
     with computation(PARALLEL):
         with interval(...):
             if mask:
@@ -101,7 +103,8 @@ def daa_6(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
 
 
 @register_test_case(valid=True)
-def daa_7(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
+def daa_7(in_field: Field[float], out_field: Field[float]):
+    """Valid stencil with `tmp` defined in both branches with a condition on a float field"""
     with computation(PARALLEL):
         with interval(...):
             if in_field > 0:
@@ -110,19 +113,61 @@ def daa_7(in_field: Field[float], mask: Field[bool], out_field: Field[float]):
                 tmp = in_field + 1
             out_field = tmp
 
+@register_test_case(valid=True)
+def daa_8(in_field: Field[float], out_field: Field[float]):
+    """Valid stencil with `tmp` defined in all three branches of if-elif-else statement"""
+    with computation(PARALLEL):
+        with interval(...):
+            if in_field > 0:
+                tmp = in_field
+            elif in_field == 0:
+                tmp = in_field + 1
+            else:
+                tmp = in_field + 2
+            out_field = tmp
 
-@pytest.mark.parametrize("definition,valid", [(s, d["valid"]) for s, d in test_data])
+@register_test_case(valid=True)
+def daa_8(in_field: Field[float], cond_field: Field[float], mask: bool, out_field: Field[float]):
+    """Valid stencil with `tmp` defined in all branches of a nested if-elif-else statement"""
+    with computation(PARALLEL):
+        with interval(...):
+            if in_field > 0:
+                tmp = in_field
+            elif in_field == 0:
+                if in_field+1 == 0:
+                    tmp = in_field + 1
+                else:
+                    tmp = in_field + 2
+            else:
+                tmp = in_field + 3
+            out_field = tmp
+
+@register_test_case(valid=False)
+def daa_8(in_field: Field[float], cond_field: Field[float], mask: bool, out_field: Field[float]):
+    """Invalid stencil with `tmp` defined in all, but one branches of a nested if-elif-else statement"""
+    with computation(PARALLEL):
+        with interval(...):
+            if in_field > 0:
+                tmp = in_field
+            elif in_field == 0:
+                if in_field+1 == 0:
+                    tmp = in_field + 1
+            else:
+                tmp = in_field + 3
+            out_field = tmp
+
+@pytest.mark.parametrize("definition,valid", [(stencil, valid) for stencil, valid in test_data])
 def test_daa(definition, valid):
     builder = StencilBuilder(definition, backend=from_name("debug"))
     gtir_stencil_expr = builder.gtir_pipeline.full()
-    invalid_accesses = daa.analyse(gtir_stencil_expr)
+    invalid_accesses = daa.analyze(gtir_stencil_expr)
     if valid:
         assert len(invalid_accesses) == 0
     else:
         assert len(invalid_accesses) == 1 and invalid_accesses[0].name == "tmp"
 
 
-@pytest.mark.parametrize("definition", [s for s, d in test_data if not d["valid"]])
+@pytest.mark.parametrize("definition", [stencil for stencil, valid in test_data if not valid])
 def test_daa_warn(definition):
     backend = "gtc:gt:cpu_ifirst"
     with pytest.warns(UserWarning, match="`tmp` may be uninitialized."):
