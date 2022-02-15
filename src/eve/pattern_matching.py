3c7d93a2dbb1ff7ae1152c41afe2b7d231c162f5
Till Ehrengruber <till.ehrengruber@cscs.ch>
2022-02-15 09:21:09 +0100

    Cleanup

--- src/eve/pattern_matching.py
+++ src/eve/pattern_matching.py
@@ -12,62 +12,10 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 import inspect
-from typing import Any
-from types import ModuleType
 from dataclasses import dataclass
+from types import ModuleType
+from typing import Any, Iterator
 
-def get_differences(a: Any, b: Any, path: str = ""):
-    """Compare two objects and return a list of differences.
-
-    If the arguments are lists or dictionaries comparison is recursively per item. Objects are compared
-    using equality operator or if the left-hand-side is an `ObjectPattern` its type and attributes
-    are compared to the right-hand-side object. Only the attributes of the `ObjectPattern` are used
-    for comparison, disregarding potential additional attributes of the right-hand-side.
-    """
-    if isinstance(a, ObjectPattern):
-        if not isinstance(b, a.cls):
-            yield [path, f"Expected an instance of class {a.cls.__name__}, but got {type(b).__name__}"]
-        else:
-            for k in a.attrs.keys():
-                if not hasattr(b, k):
-                    yield [path, f"Object has no attribute {k}."]
-                else:
-                    for diff in get_differences(a.attrs[k], getattr(b, k), path=f"{path}.{k}"):
-                        yield diff
-    elif isinstance(a, list):
-        if not isinstance(b, list):
-            yield [path, f"Expected list, but got {type(b).__name__}"]
-        elif len(a) != len(b):
-            yield [path, f"Expected list of length {len(a)}, but got length {len(b)}"]
-        else:
-            for i, (el_a, el_b) in enumerate(zip(a, b)):
-                for diff in get_differences(el_a, el_b, path=f"{path}[{i}]"):
-                    yield diff
-    elif isinstance(a, dict):
-        if not isinstance(b, dict):
-            yield path
-        elif set(a.keys()) != set(b.keys()):
-            a_min_b = set(a.keys()).difference(b.keys())
-            b_min_a = set(b.keys()).difference(a.keys())
-            if a_min_b:
-                missing_keys_str = "`" + '`, `'.join(map(str, a_min_b)) + "`"
-                yield [path,
-                       f"Expected dictionary with keys `{'`, `'.join(map(str, a.keys()))}`, but the following keys are missing: {missing_keys_str}"]
-            if b_min_a:
-                extra_keys_str = "`" + '`, `'.join(map(str, b_min_a)) + "`"
-                yield [path,
-                       f"Expected dictionary with keys `{'`, `'.join(map(str, a.keys()))}`, but the following keys are extra: {extra_keys_str}"]
-        elif not set(b.keys()).issubset(set(a.keys())):
-            missing_keys_str = "`"+'`, `'.join(map(str, set(a.keys()).difference(b.keys())))+"`"
-            yield [path,
-                   f"Expected dictionary with keys `{'`, `'.join(map(str, a.keys()))}`, but the following keys are missing: {missing_keys_str}"]
-        else:
-            for k, v_a, v_b in zip(a.keys(), a.values(), b.values()):
-                for diff in get_differences(v_a, v_b, path=f"{path}[\"{k}\"]"):
-                    yield diff
-    else:
-        if a != b:
-            yield [path, f"Objects are not equal. {a} != {b}"]
 
 @dataclass(frozen=True)
 class ObjectPattern:
@@ -81,20 +29,95 @@ class ObjectPattern:
         """
         diffs = list(get_differences(self, other))
         if raise_ and len(diffs) != 0:
-            diffs_str = '\n  '.join([f"  {self.cls.__name__}{path}: {msg}" for path, msg in diffs])
+            diffs_str = "\n  ".join([f"  {self.cls.__name__}{path}: {msg}" for path, msg in diffs])
             raise ValueError(f"Object and pattern don't match:\n  {diffs_str}")
-        return len(diffs)==0
+        return len(diffs) == 0
 
-    def __str__(self):
-        attrs_str = ', '.join([f"{str(k)}={str(v)}" for k, v in self.attrs.items()])
+    def __str__(self) -> str:
+        attrs_str = ", ".join([f"{str(k)}={str(v)}" for k, v in self.attrs.items()])
         return f"{self.cls.__name__}({attrs_str})"
+
 
 @dataclass(frozen=True)
 class ObjectPatternConstructor:
     cls: type
 
-    def __call__(self, **kwargs):
+    def __call__(self, **kwargs: Any) -> ObjectPattern:
         return ObjectPattern(self.cls, kwargs)
+
+
+def _get_differences_object_pattern(a: ObjectPattern, b: Any, path: str = "") -> Iterator[tuple[str, str]]:
+    if not isinstance(b, a.cls):
+        yield (
+            path,
+            f"Expected an instance of class {a.cls.__name__}, but got {type(b).__name__}",
+        )
+    else:
+        for k in a.attrs.keys():
+            if not hasattr(b, k):
+                yield (path, f"Value has no attribute {k}.")
+            else:
+                for diff in get_differences(a.attrs[k], getattr(b, k), path=f"{path}.{k}"):
+                    yield diff
+
+def _get_differences_list(a: list, b: Any, path: str = "") -> Iterator[tuple[str, str]]:
+    if not isinstance(b, list):
+        yield (path, f"Expected list, but got {type(b).__name__}")
+    elif len(a) != len(b):
+        yield (path, f"Expected list of length {len(a)}, but got length {len(b)}")
+    else:
+        for i, (el_a, el_b) in enumerate(zip(a, b)):
+            for diff in get_differences(el_a, el_b, path=f"{path}[{i}]"):
+                yield diff
+
+def _get_differences_dict(a: list, b: Any, path: str = "") -> Iterator[tuple[str, str]]:
+    if not isinstance(b, dict):
+        yield (path, f"Expected dict, but got {type(b).__name__}")
+    elif set(a.keys()) != set(b.keys()):
+        a_min_b = set(a.keys()).difference(b.keys())
+        b_min_a = set(b.keys()).difference(a.keys())
+        if a_min_b:
+            missing_keys_str = "`" + "`, `".join(map(str, a_min_b)) + "`"
+            yield (
+                path,
+                f"Expected dictionary with keys `{'`, `'.join(map(str, a.keys()))}`, but the following keys are missing: {missing_keys_str}",
+            )
+        if b_min_a:
+            extra_keys_str = "`" + "`, `".join(map(str, b_min_a)) + "`"
+            yield (
+                path,
+                f"Expected dictionary with keys `{'`, `'.join(map(str, a.keys()))}`, but the following keys are extra: {extra_keys_str}",
+            )
+    elif not set(b.keys()).issubset(set(a.keys())):
+        missing_keys_str = "`" + "`, `".join(map(str, set(a.keys()).difference(b.keys()))) + "`"
+        yield (
+            path,
+            f"Expected dictionary with keys `{'`, `'.join(map(str, a.keys()))}`, but the following keys are missing: {missing_keys_str}",
+        )
+    else:
+        for k, v_a, v_b in zip(a.keys(), a.values(), b.values()):
+            yield from get_differences(v_a, v_b, path=f'{path}["{k}"]')
+
+def get_differences(a: Any, b: Any, path: str = "") -> Iterator[tuple[str, str]]:
+    """Compare two objects and return a list of differences.
+
+    If the arguments are lists or dictionaries comparison is recursively per item. Objects are compared
+    using equality operator or if the left-hand-side is an `ObjectPattern` its type and attributes
+    are compared to the right-hand-side object. Only the attributes of the `ObjectPattern` are used
+    for comparison, disregarding potential additional attributes of the right-hand-side.
+    """
+    if isinstance(a, ObjectPattern):
+        yield from _get_differences_object_pattern(a, b, path)
+    elif isinstance(a, list):
+        yield from _get_differences_list(a, b, path)
+    elif isinstance(a, dict):
+        yield from _get_differences_dict(a, b, path)
+    else:
+        if type(a) != type(b):
+            yield (path, f"Expected a value of type {type(a).__name__}, but got {type(b).__name__}")
+        elif a != b:
+            yield (path, f"Values are not equal. `{a}` != `{b}`")
+
 
 @dataclass(frozen=True)
 class ModuleWrapper:
@@ -107,9 +130,10 @@ class ModuleWrapper:
     >>> assert foo_ir_.Foo(bar="baz").matches(foo_ir.Foo(bar="baz", foo="bar"))  # doctest: +SKIP
     >>> assert not foo_ir_.Foo(bar="bar").matches(foo_ir.Foo(bar="baz", foo="bar"))  # doctest: +SKIP
     """
+
     module: ModuleType
 
-    def __getattr__(self, item: str):
+    def __getattr__(self, item: str) -> ObjectPatternConstructor:
         val = getattr(self.module, item)
         if not inspect.isclass(val):
             raise ValueError("Only classes allowed.")
--- tests/eve_tests/unit_tests/test_pattern_matching.py
+++ tests/eve_tests/unit_tests/test_pattern_matching.py
@@ -2,8 +2,10 @@ import types
 from typing import Any, TypedDict
 
 import pytest
+
 from eve import Node
-from eve.pattern_matching import ObjectPattern, get_differences, ModuleWrapper
+from eve.pattern_matching import ModuleWrapper, ObjectPattern, get_differences
+
 
 class TestData(TypedDict):
     name: str
@@ -11,10 +13,13 @@ class TestData(TypedDict):
     b: Any
     expected_diff: list[tuple[str, str]]
 
+
 test_data: list[tuple[str, TestData]] = []
+
 
 def register_test_case(name, a, b, expected_diff):
     test_data.append((name, {"a": a, "b": b, "expected_diff": expected_diff}))
+
 
 class TestNode(Node):
     foo: str
@@ -22,29 +27,53 @@ class TestNode(Node):
 
 
 register_test_case("int_equal", 1, 1, [])
-register_test_case("int_unequal", 1, 2, [["a", "Objects are not equal. 1 != 2"]])
+register_test_case("int_unequal", 1, 2, [("a", "Values are not equal. `1` != `2`")])
 register_test_case("list_equal", [1], [1], [])
-register_test_case("list_unequal", [1], [1, 2], [["a", "Expected list of length 1, but got length 2"]])
+register_test_case(
+    "list_unequal", [1], [1, 2], [("a", "Expected list of length 1, but got length 2")]
+)
 register_test_case("dict_equal", {"foo": 1}, {"foo": 1}, [])
-register_test_case("dict_unequal_val", {"foo": 1}, {"foo": 2},
-                   [['a["foo"]', 'Objects are not equal. 1 != 2']])
-register_test_case("dict_unequal_key", {"foo": 1}, {"bar": 1},
-                   [['a', 'Expected dictionary with keys `foo`, but the following keys are missing: `foo`'],
-                    ['a', 'Expected dictionary with keys `foo`, but the following keys are extra: `bar`']])
-register_test_case("dict_unequal_empty", {"foo": 1}, {},
-                   [['a', 'Expected dictionary with keys `foo`, but the following keys are missing: `foo`']])
-register_test_case("node_pattern_match", ObjectPattern(TestNode, {"bar": "baz"}), TestNode(bar="baz", foo="bar"), [])
-register_test_case("node_pattern_match", ObjectPattern(TestNode, {"bar": "bar"}), TestNode(bar="baz", foo="bar"),
-                   [['a.bar', 'Objects are not equal. bar != baz']])
+register_test_case(
+    "dict_unequal_val", {"foo": 1}, {"foo": 2}, [('a["foo"]', "Values are not equal. `1` != `2`")]
+)
+register_test_case(
+    "dict_unequal_key",
+    {"foo": 1},
+    {"bar": 1},
+    [
+        ("a", "Expected dictionary with keys `foo`, but the following keys are missing: `foo`"),
+        ("a", "Expected dictionary with keys `foo`, but the following keys are extra: `bar`"),
+    ],
+)
+register_test_case(
+    "dict_unequal_empty",
+    {"foo": 1},
+    {},
+    [("a", "Expected dictionary with keys `foo`, but the following keys are missing: `foo`")],
+)
+register_test_case(
+    "node_pattern_match",
+    ObjectPattern(TestNode, {"bar": "baz"}),
+    TestNode(bar="baz", foo="bar"),
+    [],
+)
+register_test_case(
+    "node_pattern_match",
+    ObjectPattern(TestNode, {"bar": "bar"}),
+    TestNode(bar="baz", foo="bar"),
+    [("a.bar", "Values are not equal. `bar` != `baz`")],
+)
+
 
 @pytest.mark.parametrize("name,test_data", test_data)
 def test_all(name, test_data):
     a, b, expected_diff = test_data["a"], test_data["b"], test_data["expected_diff"]
     diff = list(get_differences(a, b, path="a"))
-    assert diff==expected_diff
+    assert diff == expected_diff
+
 
 def test_module_wrapper():
-    test_mod = types.ModuleType('test_mod')
+    test_mod = types.ModuleType("test_mod")
     test_mod.TestNode = TestNode
     test_mod_ = ModuleWrapper(test_mod)
 
