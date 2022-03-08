35e801dd6e77f6a1bab945be9d3f3c14ce4b3c3d
Johann Dahm <johann.dahm@gmail.com>
2022-03-08 09:27:47 -0800

    Fix to frontend check for regions race condition

--- src/gt4py/frontend/gtscript_frontend.py
+++ src/gt4py/frontend/gtscript_frontend.py
@@ -711,7 +711,7 @@ def _find_accesses_with_offsets(node: gt_ir.Node) -> Set[str]:
 
     class FindRefs(gt_ir.IRNodeVisitor):
         def visit_FieldRef(self, node: gt_ir.FieldAccessor) -> None:
-            if any(v != 0 for v in node.offset.values()):
+            if node.offset.get("I", 0) != 0 or node.offset.get("J", 0) != 0:
                 names.add(node.name)
 
     FindRefs().visit(node)
@@ -750,6 +750,7 @@ class IRMaker(ast.NodeVisitor):
         self.parsing_context = None
         self.iteration_order = None
         self.decls_stack = []
+        self.parsing_horizontal_region = False
         self.written_vars: Set[str] = set()
         gt_ir.NativeFunction.PYTHON_SYMBOL_TO_IR_OP = {
             "abs": gt_ir.NativeFunction.ABS,
@@ -1401,7 +1402,8 @@ class IRMaker(ast.NodeVisitor):
                     result.append(field_decl)
                 self.fields[field_decl.name] = field_decl
 
-            self.written_vars.add(name)
+            if not self.parsing_horizontal_region:
+                self.written_vars.add(name)
 
             axes = self.fields[name].axes
             par_axes_names = [axis.name for axis in gt_ir.Domain.LatLonGrid().parallel_axes]
@@ -1447,8 +1449,10 @@ class IRMaker(ast.NodeVisitor):
             if any(isinstance(child_node, ast.With) for child_node in node.body):
                 raise GTScriptSyntaxError("Cannot nest `with` node inside horizontal region")
 
+            self.parsing_horizontal_region = True
             intervals_dicts = self._visit_with_horizontal(node.items[0], loc)
             all_stmts = gt_utils.flatten([gt_utils.listify(self.visit(stmt)) for stmt in node.body])
+            self.parsing_horizontal_region = False
             stmts = list(filter(lambda stmt: isinstance(stmt, gt_ir.Decl), all_stmts))
             body_block = gt_ir.BlockStmt(
                 stmts=list(filter(lambda stmt: not isinstance(stmt, gt_ir.Decl), all_stmts)),
