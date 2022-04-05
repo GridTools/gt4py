4e321f947c08bbcb0496a8de4c641db96c61410c
Felix Thaler <thaler@cscs.ch>
2022-04-05 14:58:58 +0200

    Refactored GlobalTmps transformation, added tests

--- src/functional/iterator/transforms/global_tmps.py
+++ src/functional/iterator/transforms/global_tmps.py
@@ -1,23 +1,64 @@
-from typing import Dict, List
-
 from eve import NodeTranslator
-from functional.iterator import ir
+from functional.iterator import ir, type_inference
 from functional.iterator.runtime import CartesianAxis
 from functional.iterator.transforms.collect_shifts import CollectShifts
-from functional.iterator.transforms.common_type_deduction import CommonTypeDeduction
 from functional.iterator.transforms.eta_reduction import EtaReduction
 from functional.iterator.transforms.popup_tmps import PopupTmps
 from functional.iterator.transforms.prune_closure_inputs import PruneClosureInputs
 
 
-class CreateGlobalTmps(NodeTranslator):
-    @staticmethod
-    def _extend_domain(domain: ir.FunCall, offset_provider, shifts):
-        assert isinstance(domain.fun, ir.SymRef) and domain.fun.id == "domain"
-        assert all(isinstance(o, CartesianAxis) for o in offset_provider.values())
+AUTO_DOMAIN = ir.SymRef(id="_gtmp_auto_domain")
+
+
+def split_closures(node: ir.FencilDefinition):
+    tmps: list[ir.SymRef] = []
+
+    def handle_arg(arg):
+        if isinstance(arg, ir.SymRef):
+            return arg
+        if (
+            isinstance(arg, ir.FunCall)
+            and isinstance(arg.fun, ir.FunCall)
+            and isinstance(arg.fun.fun, ir.SymRef)
+            and arg.fun.fun.id == "lift"
+        ):
+            assert len(arg.fun.args) == 1
+            ref = ir.SymRef(id=f"_gtmp_{len(tmps)}")
+            tmps.append(ir.Sym(id=ref.id))
+            unlifted = ir.FunCall(fun=arg.fun.args[0], args=arg.args)
+            todos.append((ref, unlifted))
+            return ref
+        raise AssertionError()
+
+    closures = []
+    for closure in reversed(node.closures):
+        wrapped_stencil = ir.FunCall(fun=closure.stencil, args=closure.inputs)
+        popped_stencil = PopupTmps().visit(wrapped_stencil)
+
+        todos = [(closure.output, popped_stencil)]
+        domain = closure.domain
+        while todos:
+            output, call = todos.pop()
+            closure = ir.StencilClosure(
+                domain=domain,
+                stencil=call.fun,
+                output=output,
+                inputs=[handle_arg(arg) for arg in call.args],
+            )
+            closures.append(closure)
+            domain = AUTO_DOMAIN
+
+    return ir.FencilDefinition(
+        id=node.id, params=node.params + tmps, closures=list(reversed(closures))
+    ), [tmp.id for tmp in tmps]
+
+
+def update_cartesian_domains(node: ir.FencilDefinition, offset_provider):
+    def extend(domain, shifts):
+        assert isinstance(domain, ir.FunCall) and domain.fun == ir.SymRef(id="domain")
+        assert all(isinstance(axis, CartesianAxis) for axis in offset_provider.values())
 
         offset_limits = {k: (0, 0) for k in offset_provider.keys()}
-
         for shift in shifts:
             offsets = {k: 0 for k in offset_provider.keys()}
             for k, v in zip(shift[0::2], shift[1::2]):
@@ -62,97 +103,83 @@ class CreateGlobalTmps(NodeTranslator):
 
         return ir.FunCall(fun=domain.fun, args=named_ranges)
 
-    def visit_FencilDefinition(self, node: ir.FencilDefinition, *, offset_provider, register_tmp):
-        tmps: List[ir.Sym] = []
-
-        def handle_arg(arg):
-            if isinstance(arg, ir.SymRef):
-                return arg
-            if (
-                isinstance(arg, ir.FunCall)
-                and isinstance(arg.fun, ir.FunCall)
-                and arg.fun.fun.id == "lift"
-            ):
-                ref = ir.SymRef(id=f"tmp{len(tmps)}")
-                tmps.append(ir.Sym(id=ref.id))
-                assert len(arg.fun.args) == 1
-                unlifted = ir.FunCall(fun=arg.fun.args[0], args=arg.args)
-                todos.append((ref, unlifted))
-                return ref
-            raise AssertionError()
-
-        closures = []
-        tmp_domains = dict()
-        for closure in reversed(node.closures):
-            if not isinstance(closure.stencil, ir.Lambda):
-                if not isinstance(closure.stencil, ir.SymRef) or closure.stencil.id != "deref":
-                    raise NotImplementedError()
-                closure = ir.StencilClosure(
-                    domain=closure.domain,
-                    stencil=ir.Lambda(
-                        params=[ir.Sym(id="x")],
-                        expr=ir.FunCall(fun=closure.stencil, args=[ir.SymRef(id="x")]),
-                    ),
-                    output=closure.output,
-                    inputs=closure.inputs,
-                )
-            wrapped_stencil = ir.FunCall(fun=closure.stencil, args=closure.inputs)
-            popped_stencil = PopupTmps().visit(wrapped_stencil)
-            todos = [(closure.output, popped_stencil)]
-
-            shifts: Dict[str, List[tuple]] = dict()
+    closures = []
+    shifts: dict[str, list[tuple]] = dict()
+    domain = None
+    for closure in reversed(node.closures):
+        if closure.domain == AUTO_DOMAIN:
+            output_shifts = shifts.get(closure.output.id, [])
+            domain = extend(domain, output_shifts)
+            closure = ir.StencilClosure(
+                domain=domain, stencil=closure.stencil, output=closure.output, inputs=closure.inputs
+            )
+        else:
             domain = closure.domain
-            while todos:
-                output, call = todos.pop()
-                output_shifts: List[tuple] = shifts.get(output.id, [])
-                domain = self._extend_domain(domain, offset_provider, output_shifts)
-                if output.id in {tmp.id for tmp in tmps}:
-                    assert output.id not in tmp_domains
-                    tmp_domains[output.id] = domain
-                closure = EtaReduction().visit(
-                    PruneClosureInputs().visit(
-                        ir.StencilClosure(
-                            domain=domain,
-                            stencil=call.fun,
-                            output=output,
-                            inputs=[handle_arg(arg) for arg in call.args],
-                        )
-                    )
-                )
-                closures.append(closure)
-                local_shifts: Dict[str, List[tuple]] = dict()
-                lambda_fun = closure.stencil
-                if isinstance(lambda_fun, ir.SymRef):
-                    assert lambda_fun.id == "deref"
-                    continue
-                elif (
-                    isinstance(lambda_fun, ir.FunCall)
-                    and isinstance(lambda_fun.fun, ir.SymRef)
-                    and lambda_fun.fun.id == "scan"
-                ):
-                    lambda_fun = lambda_fun.args[0]
-                    params = lambda_fun.params[1:]
-                else:
-                    params = lambda_fun.params
-                CollectShifts().visit(lambda_fun, shifts=local_shifts)
-                input_map = {param.id: arg.id for param, arg in zip(params, closure.inputs)}
-                for k, v in local_shifts.items():
-                    shifts.setdefault(input_map[k], []).extend(v)
+            shifts = dict()
 
-        fencil = ir.FencilDefinition(
-            id=node.id, params=node.params + tmps, closures=list(reversed(closures))
-        )
+        closures.append(closure)
 
-        argtypes = CommonTypeDeduction.apply(fencil)
-        argnames = [p.id for p in fencil.params]
+        if closure.stencil == ir.SymRef(id="deref"):
+            continue
 
-        assert {tmp.id for tmp in tmps} == set(tmp_domains.keys())
+        local_shifts: dict[str, list[tuple]] = dict()
+        if isinstance(closure.stencil, ir.FunCall) and closure.stencil.fun == ir.SymRef(id="scan"):
+            # get params of scan function, but ignore accumulator
+            fun = closure.stencil.args[0]
+            params = fun.params[1:]
+        else:
+            assert isinstance(closure.stencil, ir.Lambda)
+            fun = closure.stencil
+            params = fun.params
+        CollectShifts().visit(fun, shifts=local_shifts)
+        input_map = {param.id: inp.id for param, inp in zip(params, closure.inputs)}
+        for param, shift in local_shifts.items():
+            shifts.setdefault(input_map[param], []).extend(shift)
+
+    return ir.FencilDefinition(id=node.id, params=node.params, closures=list(reversed(closures)))
+
+
+def collect_tmps_info(node: ir.FencilDefinition, tmps):
+    domains = {
+        closure.output.id: closure.domain for closure in node.closures if closure.output.id in tmps
+    }
+
+    def convert_type(dtype):
+        if isinstance(dtype, type_inference.Primitive):
+            return dtype.name
+        if isinstance(dtype, type_inference.Var):
+            return dtype.idx
+        if isinstance(dtype, type_inference.PartialTupleVar):
+            elems_dict = dict(dtype.elems)
+            assert len(elems_dict) == max(elems_dict) + 1
+            return tuple(convert_type(elems_dict[i]) for i in range(len(elems_dict)))
+        assert isinstance(dtype, type_inference.Tuple)
+        return tuple(convert_type(e) for e in dtype.elems)
+
+    fencil_type = type_inference.infer(node)
+    assert isinstance(fencil_type, type_inference.Fencil)
+    assert isinstance(fencil_type.params, type_inference.Tuple)
+    all_types = []
+    types = dict()
+    for param, dtype in zip(node.params, fencil_type.params.elems):
+        assert isinstance(dtype, type_inference.Val)
+        all_types.append(convert_type(dtype.dtype))
+        if param.id in tmps:
+            assert param.id not in types
+            t = all_types[-1]
+            types[param.id] = all_types.index(t) if isinstance(t, int) else t
+
+    return {tmp: (domains[tmp], types[tmp]) for tmp in tmps}
+
+
+class CreateGlobalTmps(NodeTranslator):
+    def visit_FencilDefinition(self, node: ir.FencilDefinition, *, offset_provider, register_tmp):
+        node, tmps = split_closures(node)
+        node = PruneClosureInputs().visit(node)
+        node = EtaReduction().visit(node)
+        node = update_cartesian_domains(node, offset_provider)
         if register_tmp is not None:
-            for tmp, domain in tmp_domains.items():
-                dtype = argtypes[argnames.index(tmp)]
-                if isinstance(dtype, int):
-                    # index of first argument with the same dtype
-                    dtype = argtypes.index(dtype)
-                register_tmp(fencil.id, tmp, domain, dtype)
-
-        return fencil
+            infos = collect_tmps_info(node, tmps)
+            for tmp, (domain, dtype) in infos.items():
+                register_tmp(node.id, tmp, domain, dtype)
+        return node
---
+++ tests/functional_tests/iterator_tests/test_global_tmps.py
@@ -0,0 +1,404 @@
+from functional.iterator import ir
+from functional.iterator.runtime import CartesianAxis
+from functional.iterator.transforms.global_tmps import (
+    collect_tmps_info,
+    split_closures,
+    update_cartesian_domains,
+)
+
+
+def test_split_closures():
+    testee = ir.FencilDefinition(
+        id="f",
+        params=[ir.Sym(id="d"), ir.Sym(id="inp"), ir.Sym(id="out")],
+        closures=[
+            ir.StencilClosure(
+                domain=ir.SymRef(id="d"),
+                stencil=ir.Lambda(
+                    params=[ir.SymRef(id="baz_inp")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[
+                            ir.FunCall(
+                                fun=ir.FunCall(
+                                    fun=ir.SymRef(id="lift"),
+                                    args=[
+                                        ir.Lambda(
+                                            params=[ir.Sym(id="bar_inp")],
+                                            expr=ir.FunCall(
+                                                fun=ir.SymRef(id="deref"),
+                                                args=[
+                                                    ir.FunCall(
+                                                        fun=ir.FunCall(
+                                                            fun=ir.SymRef(id="lift"),
+                                                            args=[
+                                                                ir.Lambda(
+                                                                    params=[ir.Sym(id="foo_inp")],
+                                                                    expr=ir.FunCall(
+                                                                        fun=ir.SymRef(id="deref"),
+                                                                        args=[
+                                                                            ir.SymRef(id="foo_inp")
+                                                                        ],
+                                                                    ),
+                                                                )
+                                                            ],
+                                                        ),
+                                                        args=[ir.SymRef(id="bar_inp")],
+                                                    )
+                                                ],
+                                            ),
+                                        )
+                                    ],
+                                ),
+                                args=[ir.SymRef(id="baz_inp")],
+                            )
+                        ],
+                    ),
+                ),
+                output=ir.Sym(id="out"),
+                inputs=[ir.Sym(id="inp")],
+            )
+        ],
+    )
+
+    expected = ir.FencilDefinition(
+        id="f",
+        params=[
+            ir.Sym(id="d"),
+            ir.Sym(id="inp"),
+            ir.Sym(id="out"),
+            ir.Sym(id="_gtmp_0"),
+            ir.Sym(id="_gtmp_1"),
+        ],
+        closures=[
+            ir.StencilClosure(
+                domain=ir.SymRef(id="_gtmp_auto_domain"),
+                stencil=ir.Lambda(
+                    params=[ir.Sym(id="foo_inp")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[ir.SymRef(id="foo_inp")],
+                    ),
+                ),
+                output=ir.SymRef(id="_gtmp_1"),
+                inputs=[ir.SymRef(id="inp")],
+            ),
+            ir.StencilClosure(
+                domain=ir.SymRef(id="_gtmp_auto_domain"),
+                stencil=ir.Lambda(
+                    params=[
+                        ir.Sym(id="bar_inp"),
+                        ir.Sym(id="t0"),
+                    ],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[
+                            ir.SymRef(id="t0"),
+                        ],
+                    ),
+                ),
+                output=ir.SymRef(id="_gtmp_0"),
+                inputs=[ir.SymRef(id="inp"), ir.SymRef(id="_gtmp_1")],
+            ),
+            ir.StencilClosure(
+                domain=ir.SymRef(id="d"),
+                stencil=ir.Lambda(
+                    params=[ir.SymRef(id="baz_inp"), ir.SymRef(id="t1")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[ir.SymRef(id="t1")],
+                    ),
+                ),
+                output=ir.SymRef(id="out"),
+                inputs=[ir.SymRef(id="inp"), ir.SymRef(id="_gtmp_0")],
+            ),
+        ],
+    )
+    actual, tmps = split_closures(testee)
+    assert tmps == ["_gtmp_0", "_gtmp_1"]
+    assert actual == expected
+
+
+def test_update_cartesian_domains():
+    testee = ir.FencilDefinition(
+        id="f",
+        params=[
+            ir.Sym(id="i"),
+            ir.Sym(id="j"),
+            ir.Sym(id="k"),
+            ir.Sym(id="inp"),
+            ir.Sym(id="out"),
+            ir.Sym(id="_gtmp_0"),
+            ir.Sym(id="_gtmp_1"),
+        ],
+        closures=[
+            ir.StencilClosure(
+                domain=ir.SymRef(id="_gtmp_auto_domain"),
+                stencil=ir.Lambda(
+                    params=[ir.Sym(id="foo_inp")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[ir.SymRef(id="foo_inp")],
+                    ),
+                ),
+                output=ir.SymRef(id="_gtmp_1"),
+                inputs=[ir.SymRef(id="inp")],
+            ),
+            ir.StencilClosure(
+                domain=ir.SymRef(id="_gtmp_auto_domain"),
+                stencil=ir.SymRef(id="deref"),
+                output=ir.SymRef(id="_gtmp_0"),
+                inputs=[ir.SymRef(id="_gtmp_1")],
+            ),
+            ir.StencilClosure(
+                domain=ir.FunCall(
+                    fun=ir.SymRef(id="domain"),
+                    args=[
+                        ir.FunCall(
+                            fun=ir.SymRef(id="named_range"),
+                            args=[
+                                ir.AxisLiteral(value=a),
+                                ir.IntLiteral(value=0),
+                                ir.SymRef(id=s),
+                            ],
+                        )
+                        for a, s in (("IDim", "i"), ("JDim", "j"), ("KDim", "k"))
+                    ],
+                ),
+                stencil=ir.Lambda(
+                    params=[ir.SymRef(id="baz_inp"), ir.SymRef(id="t1")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[
+                            ir.FunCall(
+                                fun=ir.FunCall(
+                                    fun=ir.SymRef(id="shift"),
+                                    args=[ir.OffsetLiteral(value="I"), ir.OffsetLiteral(value=1)],
+                                ),
+                                args=[ir.SymRef(id="t1")],
+                            )
+                        ],
+                    ),
+                ),
+                output=ir.SymRef(id="out"),
+                inputs=[ir.SymRef(id="inp"), ir.SymRef(id="_gtmp_0")],
+            ),
+        ],
+    )
+    expected = ir.FencilDefinition(
+        id="f",
+        params=[
+            ir.Sym(id="i"),
+            ir.Sym(id="j"),
+            ir.Sym(id="k"),
+            ir.Sym(id="inp"),
+            ir.Sym(id="out"),
+            ir.Sym(id="_gtmp_0"),
+            ir.Sym(id="_gtmp_1"),
+        ],
+        closures=[
+            ir.StencilClosure(
+                domain=ir.FunCall(
+                    fun=ir.SymRef(id="domain"),
+                    args=[
+                        ir.FunCall(
+                            fun=ir.SymRef(id="named_range"),
+                            args=[
+                                ir.AxisLiteral(value="IDim"),
+                                ir.IntLiteral(value=0),
+                                ir.FunCall(
+                                    fun=ir.SymRef(id="plus"),
+                                    args=[ir.SymRef(id="i"), ir.IntLiteral(value=1)],
+                                ),
+                            ],
+                        )
+                    ]
+                    + [
+                        ir.FunCall(
+                            fun=ir.SymRef(id="named_range"),
+                            args=[
+                                ir.AxisLiteral(value=a),
+                                ir.IntLiteral(value=0),
+                                ir.SymRef(id=s),
+                            ],
+                        )
+                        for a, s in (("JDim", "j"), ("KDim", "k"))
+                    ],
+                ),
+                stencil=ir.Lambda(
+                    params=[ir.Sym(id="foo_inp")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[ir.SymRef(id="foo_inp")],
+                    ),
+                ),
+                output=ir.SymRef(id="_gtmp_1"),
+                inputs=[ir.SymRef(id="inp")],
+            ),
+            ir.StencilClosure(
+                domain=ir.FunCall(
+                    fun=ir.SymRef(id="domain"),
+                    args=[
+                        ir.FunCall(
+                            fun=ir.SymRef(id="named_range"),
+                            args=[
+                                ir.AxisLiteral(value="IDim"),
+                                ir.IntLiteral(value=0),
+                                ir.FunCall(
+                                    fun=ir.SymRef(id="plus"),
+                                    args=[ir.SymRef(id="i"), ir.IntLiteral(value=1)],
+                                ),
+                            ],
+                        )
+                    ]
+                    + [
+                        ir.FunCall(
+                            fun=ir.SymRef(id="named_range"),
+                            args=[
+                                ir.AxisLiteral(value=a),
+                                ir.IntLiteral(value=0),
+                                ir.SymRef(id=s),
+                            ],
+                        )
+                        for a, s in (("JDim", "j"), ("KDim", "k"))
+                    ],
+                ),
+                stencil=ir.SymRef(id="deref"),
+                output=ir.SymRef(id="_gtmp_0"),
+                inputs=[ir.SymRef(id="_gtmp_1")],
+            ),
+            ir.StencilClosure(
+                domain=ir.FunCall(
+                    fun=ir.SymRef(id="domain"),
+                    args=[
+                        ir.FunCall(
+                            fun=ir.SymRef(id="named_range"),
+                            args=[
+                                ir.AxisLiteral(value=a),
+                                ir.IntLiteral(value=0),
+                                ir.SymRef(id=s),
+                            ],
+                        )
+                        for a, s in (("IDim", "i"), ("JDim", "j"), ("KDim", "k"))
+                    ],
+                ),
+                stencil=ir.Lambda(
+                    params=[ir.SymRef(id="baz_inp"), ir.SymRef(id="t1")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[
+                            ir.FunCall(
+                                fun=ir.FunCall(
+                                    fun=ir.SymRef(id="shift"),
+                                    args=[ir.OffsetLiteral(value="I"), ir.OffsetLiteral(value=1)],
+                                ),
+                                args=[ir.SymRef(id="t1")],
+                            )
+                        ],
+                    ),
+                ),
+                output=ir.SymRef(id="out"),
+                inputs=[ir.SymRef(id="inp"), ir.SymRef(id="_gtmp_0")],
+            ),
+        ],
+    )
+    actual = update_cartesian_domains(testee, {"I": CartesianAxis("IDim")})
+    assert actual == expected
+
+
+def test_collect_tmps_info():
+    tmp_domain = ir.FunCall(
+        fun=ir.SymRef(id="domain"),
+        args=[
+            ir.FunCall(
+                fun=ir.SymRef(id="named_range"),
+                args=[
+                    ir.AxisLiteral(value="IDim"),
+                    ir.IntLiteral(value=0),
+                    ir.FunCall(
+                        fun=ir.SymRef(id="plus"),
+                        args=[ir.SymRef(id="i"), ir.IntLiteral(value=1)],
+                    ),
+                ],
+            )
+        ]
+        + [
+            ir.FunCall(
+                fun=ir.SymRef(id="named_range"),
+                args=[
+                    ir.AxisLiteral(value=a),
+                    ir.IntLiteral(value=0),
+                    ir.SymRef(id=s),
+                ],
+            )
+            for a, s in (("JDim", "j"), ("KDim", "k"))
+        ],
+    )
+    testee = ir.FencilDefinition(
+        id="f",
+        params=[
+            ir.Sym(id="i"),
+            ir.Sym(id="j"),
+            ir.Sym(id="k"),
+            ir.Sym(id="inp"),
+            ir.Sym(id="out"),
+            ir.Sym(id="_gtmp_0"),
+            ir.Sym(id="_gtmp_1"),
+        ],
+        closures=[
+            ir.StencilClosure(
+                domain=tmp_domain,
+                stencil=ir.Lambda(
+                    params=[ir.Sym(id="foo_inp")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[ir.SymRef(id="foo_inp")],
+                    ),
+                ),
+                output=ir.SymRef(id="_gtmp_1"),
+                inputs=[ir.SymRef(id="inp")],
+            ),
+            ir.StencilClosure(
+                domain=tmp_domain,
+                stencil=ir.SymRef(id="deref"),
+                output=ir.SymRef(id="_gtmp_0"),
+                inputs=[ir.SymRef(id="_gtmp_1")],
+            ),
+            ir.StencilClosure(
+                domain=ir.FunCall(
+                    fun=ir.SymRef(id="domain"),
+                    args=[
+                        ir.FunCall(
+                            fun=ir.SymRef(id="named_range"),
+                            args=[
+                                ir.AxisLiteral(value=a),
+                                ir.IntLiteral(value=0),
+                                ir.SymRef(id=s),
+                            ],
+                        )
+                        for a, s in (("IDim", "i"), ("JDim", "j"), ("KDim", "k"))
+                    ],
+                ),
+                stencil=ir.Lambda(
+                    params=[ir.SymRef(id="baz_inp"), ir.SymRef(id="t1")],
+                    expr=ir.FunCall(
+                        fun=ir.SymRef(id="deref"),
+                        args=[
+                            ir.FunCall(
+                                fun=ir.FunCall(
+                                    fun=ir.SymRef(id="shift"),
+                                    args=[ir.OffsetLiteral(value="I"), ir.OffsetLiteral(value=1)],
+                                ),
+                                args=[ir.SymRef(id="t1")],
+                            )
+                        ],
+                    ),
+                ),
+                output=ir.SymRef(id="out"),
+                inputs=[ir.SymRef(id="inp"), ir.SymRef(id="_gtmp_0")],
+            ),
+        ],
+    )
+    expected = {"_gtmp_0": (tmp_domain, 3), "_gtmp_1": (tmp_domain, 3)}
+    actual = collect_tmps_info(testee, ["_gtmp_0", "_gtmp_1"])
+    assert actual == expected
