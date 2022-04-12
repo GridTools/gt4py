63619897187891a03b76b78791f0d80584208c6f
Felix Thaler <thaler@cscs.ch>
2022-04-12 16:23:29 +0200

    Support FencilWithTemporaries in type-checking backend

--- src/functional/iterator/backends/type_check.py
+++ src/functional/iterator/backends/type_check.py
@@ -1,6 +1,7 @@
 from functional.iterator import type_inference
 from functional.iterator.backends import backend
 from functional.iterator.transforms import apply_common_transforms
+from functional.iterator.transforms.global_tmps import FencilWithTemporaries
 
 
 def check(root, *args, **kwargs):
@@ -8,6 +9,8 @@ def check(root, *args, **kwargs):
     transformed = apply_common_transforms(
         root, use_tmps=kwargs.get("use_tmps", False), offset_provider=kwargs["offset_provider"]
     )
+    if isinstance(transformed, FencilWithTemporaries):
+        transformed = transformed.fencil
     print(type_inference.pretty_str(type_inference.infer(transformed)))
 
 
