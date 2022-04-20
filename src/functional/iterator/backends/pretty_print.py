05f00488c811e1ee05cd43f8c07e3f305248e689
Felix Thaler <thaler@cscs.ch>
2022-04-20 15:42:32 +0200

    More type annotations

--- src/functional/iterator/backends/pretty_print.py
+++ src/functional/iterator/backends/pretty_print.py
@@ -1,9 +1,12 @@
+from typing import Any
+
+from functional.iterator import ir
 from functional.iterator.backends import backend
 from functional.iterator.pretty_parser import pparse
 from functional.iterator.pretty_printer import pformat
 
 
-def pretty_print_and_check(root, *args, **kwargs):
+def pretty_print_and_check(root: ir.Node, *args: Any, **kwargs: Any) -> None:
     pretty = pformat(root)
     print(pretty)
     assert pparse(pretty) == root
