c0c4fdfc0bc4f1723e7df3cbced02db009bc6dd3
Felix Thaler <thaler@cscs.ch>
2022-04-20 15:25:23 +0200

    Introduce pretty_parser

--- src/functional/iterator/backends/pretty_print.py
+++ src/functional/iterator/backends/pretty_print.py
@@ -1,5 +1,12 @@
 from functional.iterator.backends import backend
-from functional.iterator.pretty_printer import pprint
+from functional.iterator.pretty_parser import pparse
+from functional.iterator.pretty_printer import pformat
 
 
-backend.register_backend("pretty_print", lambda prog, *args, **kwargs: pprint(prog))
+def pretty_print_and_check(root, *args, **kwargs):
+    pretty = pformat(root)
+    print(pretty)
+    assert pparse(pretty) == root
+
+
+backend.register_backend("pretty_print", pretty_print_and_check)
