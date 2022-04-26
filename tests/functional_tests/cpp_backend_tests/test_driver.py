73e549964aded5d4fb90fcd6b7a33642c2c3ab42
Hannes Vogt <vogt@hey.com>
2022-04-26 09:06:46 +0200

    popen -> run

--- tests/functional_tests/cpp_backend_tests/test_driver.py
+++ tests/functional_tests/cpp_backend_tests/test_driver.py
@@ -17,8 +17,9 @@ def _execute_cmake(backend_str: str):
     build_dir = _build_dir(backend_str)
     os.makedirs(build_dir, exist_ok=True)
     cmake = ["cmake", "-B", build_dir, f"-DBACKEND={backend_str}"]
-    cmake_proc = subprocess.Popen(cmake, cwd=_source_dir())
-    return cmake_proc.wait() == 0
+    cmake_proc = subprocess.run(cmake, cwd=_source_dir())
+    cmake_proc.check_returncode()
+    return True
 
 
 def _execute_build(backend_str: str):
@@ -29,14 +30,16 @@ def _execute_build(backend_str: str):
         "--parallel",
         str(len(os.sched_getaffinity(0))),
     ]
-    build_proc = subprocess.Popen(build)
-    return build_proc.wait() == 0
+    build_proc = subprocess.run(build)
+    build_proc.check_returncode()
+    return True
 
 
 def _execute_ctest(backend_str: str):
     ctest = "ctest"
-    ctest_proc = subprocess.Popen(ctest, cwd=_build_dir(backend_str))
-    return ctest_proc.wait() == 0
+    ctest_proc = subprocess.run(ctest, cwd=_build_dir(backend_str))
+    ctest_proc.check_returncode()
+    return True
 
 
 backends = ["naive"]
