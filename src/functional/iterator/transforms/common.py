f5f3cb00cb7abc0c1530ffaa308dc6d6ff6e4ca5
Hannes Vogt <vogt@hey.com>
2022-04-27 10:43:11 +0200

    this type annotation is almost correct, but it could also be type of dst, not sure how to do that

--- src/functional/iterator/transforms/common.py
+++ src/functional/iterator/transforms/common.py
@@ -11,7 +11,7 @@ def add_fundef(root: ir.FencilDefinition, fundef: ir.FunctionDefinition) -> ir.F
     )
 
 
-def replace_node(root: eve.Node, src: eve.Node, dst: eve.Node) -> eve.Node:
+def replace_node(root: eve.concepts.AnyNode, src: eve.Node, dst: eve.Node) -> eve.concepts.AnyNode:
     class ReplaceNode(eve.NodeTranslator):
         def visit_Node(self, node: eve.Node) -> eve.Node:
             if node is src:
