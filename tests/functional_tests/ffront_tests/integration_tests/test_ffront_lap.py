4974188aab6163bf803695db15d61fbb81051092
Hannes Vogt <vogt@hey.com>
2022-04-28 14:22:40 +0200

    formatting

--- tests/functional_tests/ffront_tests/integration_tests/test_ffront_lap.py
+++ tests/functional_tests/ffront_tests/integration_tests/test_ffront_lap.py
@@ -126,7 +126,7 @@ def lap_ref(inp):
 # TODO cleanup
 shape = (20, 20)
 as_ij = np_as_located_field(IDim, JDim)
-input = as_ij(np.fromfunction(lambda x, y: x ** 2 + y ** 2, shape))
+input = as_ij(np.fromfunction(lambda x, y: x**2 + y**2, shape))
 a4 = as_ij(np.ones(shape) * -4.0)  # TODO support scalar field
 
 
