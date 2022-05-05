6954a70760c67bb52bb42ffd97b76dcf0da6484b
Felix Thaler <thaler@cscs.ch>
2022-05-05 15:32:15 +0200

    Improved CSE pass

--- tests/functional_tests/iterator_tests/test_cse.py
+++ tests/functional_tests/iterator_tests/test_cse.py
@@ -49,3 +49,22 @@ def test_lambda_no_capture(fresh_uid_sequence):
     )
     actual = CSE().visit(testee)
     assert actual == expected
+
+
+def test_lambda_nested_capture(fresh_uid_sequence):
+    common = ir.FunCall(fun=ir.SymRef(id="plus"), args=[ir.SymRef(id="x"), ir.SymRef(id="y")])
+    testee = ir.FunCall(
+        fun=ir.Lambda(params=[ir.Sym(id="x"), ir.Sym(id="y")], expr=common), args=[common, common]
+    )
+    expected = ir.FunCall(
+        fun=ir.Lambda(
+            params=[ir.Sym(id="_cs_1")],
+            expr=ir.FunCall(
+                fun=ir.Lambda(params=[ir.Sym(id="x"), ir.Sym(id="y")], expr=common),
+                args=[ir.SymRef(id="_cs_1"), ir.SymRef(id="_cs_1")],
+            ),
+        ),
+        args=[common],
+    )
+    actual = CSE().visit(testee)
+    assert actual == expected
