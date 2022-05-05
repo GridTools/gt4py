7fef1244758d839c9a54724274704c81d8a96eeb
Felix Thaler <thaler@cscs.ch>
2022-05-05 15:32:15 +0200

    Improved CSE pass

--- src/functional/iterator/transforms/cse.py
+++ src/functional/iterator/transforms/cse.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import ChainMap
 
 from eve import NodeTranslator, NodeVisitor
 from eve.utils import UIDs
@@ -6,47 +6,65 @@ from functional.iterator import ir
 
 
 class CollectSubexpressions(NodeVisitor):
-    def visit_Lambda(self, node: ir.Lambda, *, subexprs: Counter):
-        self.generic_visit(node, subexprs=subexprs)
+    def visit_SymRef(
+        self, node: ir.SymRef, *, subexprs: dict[ir.Node, list[int]], refs: ChainMap[str, bool]
+    ) -> None:
+        if node.id in refs:
+            refs[node.id] = True
 
-        # filter out subexpressions that reference lambda parameters
-        params = {p.id for p in node.params}
-        for expr in list(subexprs.keys()):
-            refs = expr.iter_tree().if_isinstance(ir.SymRef).getattr("id").to_set()
-            if refs & params:
-                del subexprs[expr]
+    def visit_Lambda(
+        self, node: ir.Lambda, *, subexprs: dict[ir.Node, list[int]], refs: ChainMap[str, bool]
+    ) -> None:
+        r = refs.new_child({p.id: False for p in node.params})
+        self.generic_visit(node, subexprs=subexprs, refs=r)
 
-        subexprs[node] += 1
+        if not any(refs.maps[0].values()):
+            subexprs.setdefault(node, []).append(id(node))
 
-    def visit_FunCall(self, node: ir.Lambda, *, subexprs: Counter):
-        self.generic_visit(node, subexprs=subexprs)
+    def visit_FunCall(
+        self, node: ir.Lambda, *, subexprs: dict[ir.Node, list[int]], refs: ChainMap[str, bool]
+    ) -> None:
+        self.generic_visit(node, subexprs=subexprs, refs=refs)
         # do not collect (and thus deduplicate in CSE) shift(offsets…) calls
         if node.fun == ir.SymRef(id="shift"):
             return
-        subexprs[node] += 1
+        if not any(refs.maps[0].values()):
+            subexprs.setdefault(node, []).append(id(node))
 
 
 class CSE(NodeTranslator):
     def visit_FunCall(self, node):
         node = self.generic_visit(node)
 
-        subexprs = Counter()
-        CollectSubexpressions().visit(node, subexprs=subexprs)
-        expr_map = {
-            expr: UIDs.sequential_id(prefix="_cs") for expr, count in subexprs.items() if count > 1
-        }
+        # collect expressions
+        subexprs = dict[ir.Node, list[int]]()
+        refs = ChainMap()
+        CollectSubexpressions().visit(node, subexprs=subexprs, refs=refs)
+
+        # collect multiple occurrences and map them to fresh symbols
+        expr_map = dict[int, ir.SymRef]()
+        params = []
+        args = []
+        for expr, ids in subexprs.items():
+            if len(ids) > 1:
+                expr_id = UIDs.sequential_id(prefix="_cs")
+                params.append(ir.Sym(id=expr_id))
+                args.append(expr)
+                expr_ref = ir.SymRef(id=expr_id)
+                for i in ids:
+                    expr_map[i] = expr_ref
+
         if not expr_map:
             return node
 
+        # apply remapping
         class Replace(NodeTranslator):
             def visit_Expr(self, node):
-                if node in expr_map:
-                    return ir.SymRef(id=expr_map[node])
+                if id(node) in expr_map:
+                    return expr_map[id(node)]
                 return self.generic_visit(node)
 
         return ir.FunCall(
-            fun=ir.Lambda(
-                params=[ir.Sym(id=p) for p in expr_map.values()], expr=Replace().visit(node)
-            ),
-            args=list(expr_map.keys()),
+            fun=ir.Lambda(params=params, expr=Replace().visit(node)),
+            args=args,
         )
