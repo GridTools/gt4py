c1acc6e86c7c5350396c04f8829cac81c542b77a
Felix Thaler <thaler@cscs.ch>
2022-05-12 10:52:02 +0200

    Fix initializer literals in library

--- src/functional/iterator/library.py
+++ src/functional/iterator/library.py
@@ -3,10 +3,10 @@ from functional.iterator.builtins import reduce
 
 def sum_(fun=None):
     if fun is None:
-        return reduce(lambda a, b: a + b, 0)
+        return reduce(lambda a, b: a + b, 0.0)
     else:
-        return reduce(lambda first, a, b: first + fun(a, b), 0)  # TODO tracing for *args
+        return reduce(lambda first, a, b: first + fun(a, b), 0.0)  # TODO tracing for *args
 
 
 def dot(a, b):
-    return reduce(lambda acc, a_n, c_n: acc + a_n * c_n, 0)(a, b)
+    return reduce(lambda acc, a_n, c_n: acc + a_n * c_n, 0.0)(a, b)
