fc3a924cf2fb974bcce6fd89c5836fadc30cd688
Ben Weber <dev.ben.weber@gmail.com>
2022-05-30 13:45:58 +0200

    Initial iterator IR tests

--- src/functional/iterator/builtins.py
+++ src/functional/iterator/builtins.py
@@ -23,6 +23,33 @@ __all__ = [
     "scan",
     "domain",
     "named_range",
+    "abs",
+    "min",
+    "max",
+    "mod",
+    "sin",
+    "cos",
+    "tan",
+    "arcsin",
+    "arccos",
+    "arctan",
+    "sinh",
+    "cosh",
+    "tanh",
+    "arcsinh",
+    "arccosh",
+    "arctanh",
+    "sqrt",
+    "exp",
+    "log",
+    "gamma",
+    "cbrt",
+    "isfinite",
+    "isinf",
+    "isnan",
+    "floor",
+    "ceil",
+    "trunc",
 ]
 
 builtin_dispatch = Dispatcher()
@@ -135,4 +162,142 @@ def make_tuple(*args):
 
 @builtin_dispatch
 def tuple_get(*args):
+    raise BackendNotSelectedError()
+
+
+# FIXME(ben): various of the following built-ins will shadow python built-ins!
+# We should find a good way around this...
+
+@builtin_dispatch
+def abs(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def min(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def max(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def mod(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def sin (*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def cos(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def tan(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def arcsin(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def arccos(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def arctan(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def sinh(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def cosh(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def tanh(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def arcsinh(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def arccosh(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def arctanh(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def sqrt(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def exp(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def log(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def gamma(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def cbrt(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def isfinite(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def isinf(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def isnan(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def floor(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def ceil(*args):
+    raise BackendNotSelectedError()
+
+
+@builtin_dispatch
+def trunc(*args):
     raise BackendNotSelectedError()
