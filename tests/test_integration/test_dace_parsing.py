74dbf3a45db95b60d3855401fea23627746ea771
Linus Groner <linus.groner@cscs.ch>
2022-05-31 15:55:26 +0200

    reset to master

--- tests/test_integration/test_dace_parsing.py
+++ tests/test_integration/test_dace_parsing.py
@@ -1,3 +1,17 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2022, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import pathlib
 import re
 
@@ -25,6 +39,9 @@ def dace_env():
         / "dacecache"
     )
     with dace.config.temporary_config():
+        # Setting max_concurrent_streams to -1 configures dace to only use the default stream.
+        dace.config.Config.set("compiler", "cuda", "max_concurrent_streams", value=-1)
+        dace.config.Config.set("compiler", "cpu", "openmp_sections", value=False)
         dace.config.Config.set("compiler", "cpu", "args", value="")
         dace.config.Config.set("compiler", "allow_view_arguments", value=True)
         dace.config.Config.set("default_build_folder", value=str(gt_cache_path))
