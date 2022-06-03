51b8db5000f4095b2b75959e29f60d4e76d0bc1f
nfarabullini <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>
2022-06-03 16:12:38 +0200

    fixes

--- src/functional/ffront/foast_passes/unroll_power_op.py
+++ src/functional/ffront/foast_passes/unroll_power_op.py
@@ -15,20 +15,19 @@ class UnrollPowerOp(eve.NodeTranslator):
     def visit_BinOp(self, node: foast.BinOp) -> foast.BinOp:
         if node.op == foast.BinaryOperator.POW:
             if not type(node.right) is foast.Constant:
-                raise FieldOperatorPowerError.from_powOp_node(
+                raise FieldOperatorPowerError.from_foast_node(
                     node,
                     msg=f"'Exponent must be a constant value, but got '{type(node.right)}.",
                 )
-            if type_info.extract_dtype(cast(ct.ScalarType, node.right.type)).kind not in [
-                ct.ScalarKind.INT32,
-                ct.ScalarKind.INT64,
-            ]:
-                raise FieldOperatorPowerError.from_powOp_node(
-                    node,
-                    msg="Only integer values greater than zero allowed in the power operation.",
-                )
-            if int(node.right.value) == 0:
-                raise FieldOperatorPowerError.from_powOp_node(
+            if (
+                type_info.extract_dtype(cast(ct.ScalarType, node.right.type)).kind
+                not in [
+                    ct.ScalarKind.INT32,
+                    ct.ScalarKind.INT64,
+                ]
+                or int(node.right.value) == 0
+            ):
+                raise FieldOperatorPowerError.from_foast_node(
                     node,
                     msg="Only integer values greater than zero allowed in the power operation.",
                 )
@@ -51,6 +50,31 @@ class UnrollPowerOp(eve.NodeTranslator):
 class FieldOperatorPowerError(GTSyntaxError, SyntaxWarning):
     """Exceptions for unsupported power operators."""
 
+    def __init__(
+        self,
+        msg="",
+        *,
+        lineno=0,
+        offset=0,
+        filename=None,
+        end_lineno=None,
+        end_offset=None,
+        text=None,
+    ):
+        super().__init__(msg, (filename, lineno, offset, text, end_lineno, end_offset))
+
     @classmethod
-    def from_foast_node(cls, node: foast.LocatedNode, *, msg: str = ""):
-        return cls(msg)
+    def from_foast_node(
+        cls,
+        node: foast.LocatedNode,
+        *,
+        msg: str = "",
+    ):
+        return cls(
+            msg,
+            lineno=node.location.line,
+            offset=node.location.column,
+            filename=node.location.source,
+            end_lineno=node.location.end_line,
+            end_offset=node.location.end_column,
+        )
