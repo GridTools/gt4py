bd9015b6798f833753c48cbe3e5888f176aaa3c6
Hannes Vogt <vogt@hey.com>
2022-07-27 14:04:22 +0200

    cleanup

--- tests/functional_tests/ffront_tests/test_past_interface.py
+++ tests/functional_tests/ffront_tests/test_past_interface.py
@@ -1,30 +1,17 @@
-"""Test field view interface.
-
-Basic Interface Tests
-=====================
-
-    - declare a connectivity
-    - create and run a stencil
-        - field args declaration
-        - scalar args
-    - create and run a fencil
-        - pass fields
-        - pass connectivities (at run time, later at compile time too)
-        - out field
-        - think about ways to pass backend/connectivities etc
-            (in function signature / in functor config method / with block)
-    - built-in field operators
-        - arithmetics
-        - shift
-        - neighbor reductions
-        - math functions: abs(), max(), min, mod(), sin(), cos(), tan(), arcsin(), arccos(),
-            arctan(), sqrt(), exp(), log(), isfinite(), isinf(), isnan(), floor(), ceil(), trunc()
-    - evaluation test cases
-"""
-import re
+# GT4Py Project - GridTools Framework
+#
+# Copyright (c) 2014-2021, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 from typing import Tuple
-
-import pytest
 
 from functional.common import Field
 from functional.ffront.decorator import field_operator
