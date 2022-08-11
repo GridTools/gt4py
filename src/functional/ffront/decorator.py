a20068e904f27421c7e422fd4c764434a03618f5
Till Ehrengruber <till.ehrengruber@cscs.ch>
2022-08-11 13:36:37 +0200

    Address reviewer comments

--- src/functional/ffront/decorator.py
+++ src/functional/ffront/decorator.py
@@ -666,7 +666,9 @@ def scan_operator(
     init: Scalar = 0.0,
     externals=None,
     backend=None,
-) -> FieldOperator[foast.ScanOperator]:
+) -> FieldOperator[foast.ScanOperator] | Callable[
+    [types.FunctionType], FieldOperator[foast.ScanOperator]
+]:
     """
     Generate an implementation of the scan operator from a Python function object.
 
