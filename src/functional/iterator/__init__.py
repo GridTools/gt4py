f5ad6a085c094cf4c61ff5f991741ff47720e3ca
Enrique G. Paredes <enriqueg@cscs.ch>
2022-08-16 17:06:05 +0200

    Fix missing renamings

--- src/functional/iterator/__init__.py
+++ src/functional/iterator/__init__.py
@@ -1,18 +1,1 @@
-from typing import Optional, Union
-
-from packaging.version import LegacyVersion, Version, parse
-from pkg_resources import DistributionNotFound, get_distribution  # type: ignore
-
 from . import tracing  # noqa: F401 # ignore unused
-
-
-__all__: list = []
-
-try:
-    __version__: str = get_distribution("gt4py").version
-except DistributionNotFound:
-    __version__ = "X.X.X.unknown"
-
-__versioninfo__: Optional[Union[LegacyVersion, Version]] = parse(__version__)
-
-del DistributionNotFound, LegacyVersion, Version, get_distribution, parse
