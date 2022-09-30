6dd586203eeb799b0fbc8154c0f8308e8191ed66
Péter Kardos <kardospeter1994@hotmail.com>
2022-09-30 16:03:54 +0200

    revert

--- src/functional/ffront/decorator.py
+++ src/functional/ffront/decorator.py
@@ -93,11 +93,7 @@ def _get_closure_vars_recursively(closure_vars: dict[str, Any]) -> dict[str, Any
 
 
 def _filter_closure_vars_by_type(closure_vars: dict[str, Any], *types) -> dict[str, Any]:
-    return dict(
-        (name, value)
-        for name, value in closure_vars.items()
-        if any(isinstance(value, type_) for type_ in types)
-    )
+    return dict((name, value) for name, value in closure_vars.items() if isinstance(value, types))
 
 
 def _deduce_grid_type(
