73952a8cb8369e005bad63984aa2cc5888441eac
Johann Dahm <johann.dahm@gmail.com>
2022-10-19 16:10:41 -0700

    Assert interval endpoints are not None

--- src/gtc/dace/expansion/tasklet_codegen.py
+++ src/gtc/dace/expansion/tasklet_codegen.py
@@ -238,6 +238,8 @@ class TaskletCodegen(codegen.TemplatedGenerator):
         for axis, interval in zip(dcir.Axis.dims_horizontal(), node.intervals):
             it_sym, dom_sym = axis.iteration_symbol(), axis.domain_symbol()
 
+            assert interval.start is not None and interval.end is not None
+
             min_val = get_axis_bound_str(interval.start, dom_sym)
             max_val = get_axis_bound_str(interval.end, dom_sym)
 
