0751231c95c38d59cfaed1274405510b3be463e5
Till Ehrengruber <till.ehrengruber@cscs.ch>
2022-10-21 04:57:37 +0200

    Fix doctest

--- src/functional/ffront/type_info.py
+++ src/functional/ffront/type_info.py
@@ -85,7 +85,7 @@ def apply_to_primitive_constituents(
     >>> int_type = ct.ScalarType(kind=ct.ScalarKind.INT)
     >>> tuple_type = ct.TupleType(types=[int_type, int_type])
     >>> print(apply_to_primitive_constituents(tuple_type, lambda primitive_type: ct.FieldType(dims=[], dtype=primitive_type)))
-    tuple[Field[[], dtype=int64],Field[[], dtype=int64]]
+    tuple[Field[[], dtype=int64], Field[[], dtype=int64]]
     """
     if isinstance(symbol_type, ct.TupleType):
         return ct.TupleType(
