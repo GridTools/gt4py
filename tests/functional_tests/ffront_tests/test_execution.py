13d6f7744b0e6e4a0877c8fc48c88966d651a0c6
nfarabullini <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>
2022-10-21 11:13:17 +0200

    Edits to test

--- tests/functional_tests/ffront_tests/test_execution.py
+++ tests/functional_tests/ffront_tests/test_execution.py
@@ -1145,6 +1145,9 @@ def test_domain(fieldview_backend):
 
 
 def test_domain_input_bounds(fieldview_backend):
+    if fieldview_backend == gtfn_cpu.run_gtfn:
+        pytest.skip("FloorDiv and Power not fully supported in gtfn.")
+
     size = 10
     a = np_as_located_field(IDim, JDim)(np.ones((size, size)))
     lower_i = 1
@@ -1167,13 +1170,47 @@ def test_domain_input_bounds(fieldview_backend):
         fieldop_domain(
             a,
             out=a,
-            domain={IDim: (0 + 1 * lower_i - 0, upper_i // 1), JDim: (lower_j**1, upper_j)},
+            domain={IDim: (lower_i, upper_i // 1), JDim: (lower_j**1, upper_j)},
         )
 
     program_domain(a, lower_i, upper_i, lower_j, upper_j, offset_provider={})
 
     expected = np.asarray(a)
     expected[1:9, 4:6] = 1 + 1
+
+    assert np.allclose(expected, a)
+
+
+def test_domain_input_bounds_1(fieldview_backend):
+    size = 10
+    a = np_as_located_field(IDim, JDim)(np.ones((size, size)) * 2)
+    lower_i = 1
+    upper_i = 9
+    lower_j = 4
+    upper_j = 6
+
+    @field_operator(backend=fieldview_backend)
+    def fieldop_domain(a: Field[[IDim, JDim], float64]) -> Field[[IDim, JDim], float64]:
+        return a * a
+
+    @program
+    def program_domain(
+        a: Field[[IDim, JDim], float64],
+        lower_i: int64,
+        upper_i: int64,
+        lower_j: int64,
+        upper_j: int64,
+    ):
+        fieldop_domain(
+            a,
+            out=a,
+            domain={IDim: (1 * lower_i, upper_i + 0), JDim: (lower_j - 0, upper_j)},
+        )
+
+    program_domain(a, lower_i, upper_i, lower_j, upper_j, offset_provider={})
+
+    expected = np.asarray(a)
+    expected[1:9, 4:6] = 2 * 2
 
     assert np.allclose(expected, a)
 
