e0c8ce17cd64739cf0df7c332e237c9788222f6c
Till Ehrengruber <till.ehrengruber@cscs.ch>
2022-11-04 10:19:33 +0100

    Small fix

--- tests/functional_tests/ffront_tests/test_type_deduction.py
+++ tests/functional_tests/ffront_tests/test_type_deduction.py
@@ -379,7 +379,7 @@ def test_adding_bool():
 
     with pytest.raises(
         FieldOperatorTypeDeductionError,
-        match=(r"Type Field\[\.\.\., dtype=bool\] can not be used in operator `\+`!"),
+        match=(r"Type Field\[\.\.\., bool\] can not be used in operator `\+`!"),
     ):
         _ = FieldOperatorParser.apply_to_function(add_bools)
 
@@ -395,7 +395,7 @@ def test_binop_nonmatching_dims():
     with pytest.raises(
         FieldOperatorTypeDeductionError,
         match=(
-            r"Could not promote `Field\[\[X], dtype=float64\]` and `Field\[\[Y\], dtype=float64\]` to common type in call to +."
+            r"Could not promote `Field\[\[X], float64\]` and `Field\[\[Y\], float64\]` to common type in call to +."
         ),
     ):
         _ = FieldOperatorParser.apply_to_function(nonmatching)
@@ -407,7 +407,7 @@ def test_bitopping_float():
 
     with pytest.raises(
         FieldOperatorTypeDeductionError,
-        match=(r"Type Field\[\.\.\., dtype=float64\] can not be used in operator `\&`! "),
+        match=(r"Type Field\[\.\.\., float64\] can not be used in operator `\&`! "),
     ):
         _ = FieldOperatorParser.apply_to_function(float_bitop)
 
@@ -418,7 +418,7 @@ def test_signing_bool():
 
     with pytest.raises(
         FieldOperatorTypeDeductionError,
-        match=r"Incompatible type for unary operator `\-`: `Field\[\.\.\., dtype=bool\]`!",
+        match=r"Incompatible type for unary operator `\-`: `Field\[\.\.\., bool\]`!",
     ):
         _ = FieldOperatorParser.apply_to_function(sign_bool)
 
@@ -429,7 +429,7 @@ def test_notting_int():
 
     with pytest.raises(
         FieldOperatorTypeDeductionError,
-        match=r"Incompatible type for unary operator `not`: `Field\[\.\.\., dtype=int64\]`!",
+        match=r"Incompatible type for unary operator `not`: `Field\[\.\.\., int64\]`!",
     ):
         _ = FieldOperatorParser.apply_to_function(not_int)
 
