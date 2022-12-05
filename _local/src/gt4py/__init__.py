dbedbab845a7aafcb90e3857f882de752251a324
Enrique G. Paredes <enriqueg@cscs.ch>
2022-12-05 09:51:06 +0100

    Prepare repository and add initial version of migration script

---
+++ _local/src/gt4py/__init__.py
@@ -0,0 +1,17 @@
+# GT4Py - GridTools4Py - GridTools for Python
+#
+# Copyright (c) 2014-2022, ETH Zurich
+# All rights reserved.
+#
+# This file is part the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Python API to develop performance portable applications for weather and climate."""
+
+from .__about__ import __version__, __version_info__
