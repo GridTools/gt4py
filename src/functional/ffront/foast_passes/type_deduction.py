c2b650e10a4db0607967f4ac46c0dd0b2e68f733
Samuel <kellerhalssamuel@gmail.com>
2022-12-06 12:09:26 +0100

    Tag TODO

--- src/functional/ffront/foast_passes/type_deduction.py
+++ src/functional/ffront/foast_passes/type_deduction.py
@@ -278,7 +278,7 @@ class FieldOperatorTypeDeduction(traits.VisitorWithSymbolTableTrait, NodeTransla
                 if isinstance(index, tuple):
                     lower, upper = index
                     new_type = ct.TupleType(types=[t for t in values.type.types[lower:upper]])
-                    # todo: possibly change definition of Starred node to not need nested id attribute.
+                    # TODO(samkellerhals): possibly change definition of Starred node to not need nested id attribute.
                     new_target = foast.Starred(
                         id=foast.DataSymbol(
                             id=old_target.id.id, location=old_target.location, type=new_type  # type: ignore
