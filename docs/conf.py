6602cfab5064d8742359738aba4021a5768a5c0f
Hannes Vogt <hannes@havogt.de>
2022-12-21 15:07:12 +0100

    Documentation with logo (#1099)

--- docs/conf.py
+++ docs/conf.py
@@ -148,6 +148,7 @@ todo_include_todos = False
 # a list of builtin themes.
 # alabaster, classic, pyramid, sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
+html_logo = "_static/logo.svg"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -238,3 +239,7 @@ texinfo_documents = [
         "Miscellaneous",
     )
 ]
+
+
+def setup(sphinx):
+    sphinx.add_css_file("css/cscs.css")
