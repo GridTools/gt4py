a186aed2e17e905c6ce94873fc3bc24ca17acac4
mroethlin <Matthias.Roethlin@MeteoSwiss.ch>
2022-12-28 12:12:00 +0100

    compiling again from my user

--- src/functional/program_processors/codegens/gtfn/codegen.py
+++ src/functional/program_processors/codegens/gtfn/codegen.py
@@ -127,7 +127,7 @@ class GTFNCodegen(codegen.TemplatedGenerator):
         if isinstance(node.fun, gtfn_ir_common.SymRef) and node.fun.id in self._builtins_mapping:
             return self.generic_visit(node, fun_name=self._builtins_mapping[node.fun.id])
         if isinstance(node.fun, gtfn_ir_common.SymRef) and node.fun.id in gtfn_ir.GTFN_BUILTINS:
-            qualified_fun_name = f"{node.fun.id}"
+            qualified_fun_name = f"gtfn::{node.fun.id}"
             return self.generic_visit(node, fun_name=qualified_fun_name)
         return self.generic_visit(node, fun_name=self.visit(node.fun))
 
