221c07a66132e797952847e6037a9feebc96aaa4
Hannes Vogt <hannes@havogt.de>
2023-01-04 16:00:23 +0000

    fix typing

--- src/functional/iterator/transforms/unroll_reduce.py
+++ src/functional/iterator/transforms/unroll_reduce.py
@@ -1,5 +1,6 @@
 import dataclasses
 from collections.abc import Iterable, Iterator
+from typing import TypeGuard
 
 from eve import NodeTranslator
 from eve.utils import UIDGenerator
@@ -7,23 +8,58 @@ from functional import common
 from functional.iterator import ir
 
 
+def _is_shifted(arg: ir.Expr) -> TypeGuard[ir.FunCall]:
+    return (
+        isinstance(arg, ir.FunCall)
+        and isinstance(arg.fun, ir.FunCall)
+        and arg.fun.fun == ir.SymRef(id="shift")
+    )
+
+
+def _is_applied_lift(arg: ir.Expr) -> TypeGuard[ir.FunCall]:
+    return (
+        isinstance(arg, ir.FunCall)
+        and isinstance(arg.fun, ir.FunCall)
+        and arg.fun.fun == ir.SymRef(id="lift")
+    )
+
+
+def _is_shifted_or_lifted_and_shifted(arg: ir.Expr) -> TypeGuard[ir.FunCall]:
+    return _is_shifted(arg) or (
+        _is_applied_lift(arg)
+        and any(_is_shifted_or_lifted_and_shifted(nested_arg) for nested_arg in arg.args)
+    )
+
+
 def _get_shifted_args(reduce_args: Iterable[ir.Expr]) -> Iterator[ir.FunCall]:
     return filter(
-        lambda arg: isinstance(  # type: ignore [arg-type] # see https://github.com/python/mypy/issues/12682
-            arg, ir.FunCall
-        )
-        and isinstance(arg.fun, ir.FunCall)
-        and arg.fun.fun == ir.SymRef(id="shift"),
+        _is_shifted_or_lifted_and_shifted,
         reduce_args,
     )
+
+
+def _is_list_of_funcalls(lst: list) -> TypeGuard[list[ir.FunCall]]:
+    return all(isinstance(f, ir.FunCall) for f in lst)
+
+
+def _get_partial_offset(arg: ir.FunCall) -> ir.OffsetLiteral:
+    if _is_shifted(arg):
+        assert isinstance(arg.fun, ir.FunCall)
+        offset = arg.fun.args[-1]
+        assert isinstance(offset, ir.OffsetLiteral)
+        return offset
+    else:
+        assert _is_applied_lift(arg)
+        assert _is_list_of_funcalls(arg.args)
+        partial_offsets = [_get_partial_offset(arg) for arg in arg.args]
+        assert all(o == partial_offsets[0] for o in partial_offsets)
+        return partial_offsets[0]
 
 
 def _get_connectivity(reduce_args: Iterable[ir.Expr], offset_provider) -> common.Connectivity:
     connectivities = []
     for arg in _get_shifted_args(reduce_args):
-        assert isinstance(arg.fun, ir.FunCall)
-        assert isinstance(arg.fun.args[-1], ir.OffsetLiteral)
-        connectivities.append(offset_provider[arg.fun.args[-1].value])
+        connectivities.append(offset_provider[_get_partial_offset(arg).value])
 
     if not connectivities:
         raise RuntimeError("Couldn't detect partial shift in any arguments of reduce.")
