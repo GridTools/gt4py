e099bc7cf3ff008c60288fd6bd375a56622fe4bd
Hannes Vogt <hannes@havogt.de>
2023-01-25 14:13:07 +0000

    add testcase

--- tests/functional_tests/iterator_tests/test_scan.py
+++ tests/functional_tests/iterator_tests/test_scan.py
@@ -1,51 +1,56 @@
 import numpy as np
 
-from functional.common import DimensionKind
-from functional.ffront.decorator import field_operator, program, scan_operator
-from functional.ffront.fbuiltins import (
-    Dimension,
-    Field,
-    FieldOffset,
-    broadcast,
-    float64,
-    int32,
-    int64,
-    max_over,
-    min_over,
-    neighbor_sum,
-    where,
-)
+from functional.common import Dimension, DimensionKind
+from functional.iterator.builtins import cartesian_domain, deref, lift, named_range, scan, shift
 from functional.iterator.embedded import (
     NeighborTableOffsetProvider,
     index_field,
     np_as_located_field,
 )
+from functional.iterator.runtime import fundef, offset
 from functional.program_processors.codegens.gtfn import gtfn_backend
 from functional.program_processors.runners import gtfn_cpu, roundtrip
 
+from .conftest import run_processor
 
-def test_scan_in_field_op():
-    size = 10
+
+def test_scan_in_field_op(program_processor, lift_mode):
+    program_processor, validate = program_processor
+
+    isize = 1
+    ksize = 3
     IDim = Dimension("I", kind=DimensionKind.HORIZONTAL)
     KDim = Dimension("K", kind=DimensionKind.VERTICAL)
-    Koff = FieldOffset("Koff", source=KDim, target=(KDim,))
-    inp = np_as_located_field(IDim, KDim)(np.ones((size, size)))
-    out = np_as_located_field(IDim, KDim)(np.zeros((size, size)))
+    Koff = offset("Koff")
+    inp = np_as_located_field(IDim, KDim)(np.ones((isize, ksize)))
+    out = np_as_located_field(IDim, KDim)(np.zeros((isize, ksize)))
 
-    @scan_operator(axis=KDim, forward=True, init=0.0)
-    def sum(state: float, k: float, kp: float):
-        return state + k + kp
+    reference = np.zeros((isize, ksize - 1))
+    reference[:, 0] = inp[:, 0] + inp[:, 1]
+    for k in range(1, ksize - 1):
+        reference[:, k] = reference[:, k - 1] + inp[:, k] + inp[:, k + 1]
 
-    @field_operator
-    def shifted(inp: Field[[IDim, KDim], float]):
-        return inp(Koff[1])
+    @fundef
+    def sum(state, k, kp):
+        return state + deref(k) + deref(kp)
 
-    @field_operator
-    def wrapped(inp: Field[[IDim, KDim], float]):
-        return sum(inp, shifted(inp))
+    @fundef
+    def shifted(inp):
+        return deref(shift(Koff, 1)(inp))
 
-    @program
-    def prog(inp: Field[[IDim, KDim], float], out: Field[[IDim, KDim], float]):
-        wrapped(inp, out=out, domain={IDim: (0, 4), KDim: (0, 4)})
+    @fundef
+    def wrapped(inp):
+        return scan(sum, True, 0.0)(inp, lift(shifted)(inp))
 
-    # TODO test
+    run_processor(
+        wrapped[cartesian_domain(named_range(IDim, 0, isize), named_range(KDim, 0, ksize - 1))],
+        program_processor,
+        inp,
+        out=out,
+        lift_mode=lift_mode,
+        offset_provider={"I": IDim, "K": KDim, "Koff": KDim},
+        column_axis=KDim,
+    )
+
+    if validate:
+        assert np.allclose(out[:, :-1], reference)
