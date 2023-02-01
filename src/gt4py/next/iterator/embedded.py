6917dc30583b1deb970908fe73e4c29662837f74
nfarabullini <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>
2023-02-01 15:12:33 +0100

    Edit to embedded

--- src/gt4py/next/iterator/embedded.py
+++ src/gt4py/next/iterator/embedded.py
@@ -1055,7 +1055,7 @@ def np_as_located_field(
         def getter(indices):
             if len(indices) > 1 and not offsets:
                 for i, j in zip(a.shape, indices):
-                    if type(i) == type(j) and j >= i:
+                    if isinstance(i, int) and isinstance(j, (np.int64, np.int32)) and j >= i:
                         return np.nan
             return a[_shift_field_indices(indices, offsets) if offsets else indices]
 
