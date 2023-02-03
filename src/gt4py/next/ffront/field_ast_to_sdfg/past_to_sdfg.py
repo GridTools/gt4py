6f0100b1e9b44a6cc57d266255eb6f67cc47d50b
Péter Kardos <kardospeter1994@hotmail.com>
2023-02-03 14:23:32 +0100

    tmp

--- src/gt4py/next/ffront/field_ast_to_sdfg/past_to_sdfg.py
+++ src/gt4py/next/ffront/field_ast_to_sdfg/past_to_sdfg.py
@@ -1,25 +1,53 @@
+from typing import Any
+
 import dace
 
 import gt4py.eve as eve
 from gt4py.next.ffront import program_ast as past
 from gt4py.next.type_system import type_specifications as ts
 from .utility import type_spec_to_dtype
+from .foast_to_sdfg import FoastToSDFG
 
 
 class PastToSDFG(eve.NodeVisitor):
     sdfg: dace.SDFG
+    last_state: dace.SDFGState
+    closure_vars: dict[str, Any]
+
+    def visit_Symbol(self, node: past.Symbol):
+        ...
 
     def visit_Name(self, node: past.Name):
         ...
 
     def visit_Call(self, node: past.Call):
-        ...
+        callee = node.func.id
+        args = set(self.visit(arg) for arg in node.args)
+        assert not node.kwargs
+
+        foast_node = self.closure_vars[callee].foast_node
+        translator = FoastToSDFG()
+        call_result_name = translator.visit(foast_node)
+        callee_sdfg = translator.sdfg
+
+        self.last_state = self.sdfg.add_state_after(self.last_state)
+        nsdfg_node = self.last_state.add_nested_sdfg(callee_sdfg, parent=None, inputs=args, outputs={call_result_name})
+        input_accesses = {arg: self.last_state.add_access(arg) for arg in args}
+        output_access = self.last_state.add_access(call_result_name)
+
+
+
+
 
     def visit_Constant(self, node: past.Constant):
         ...
 
     def visit_Program(self, node: past.Program):
-        ...
+        self.sdfg = dace.SDFG(name=node.id)
+        self.last_state = self.sdfg.add_state("state", True)
+
+        for stmt in node.body:
+            self.visit(stmt)
 
     def visit_Program_that_adds_stuff(self, node: past.Program):
         self.sdfg = dace.SDFG(name=node.id)
