20c1d1500f57b23af94016946b753892191425c0
Péter Kardos <kardospeter1994@hotmail.com>
2023-02-08 19:53:28 +0100

    basic itir to sdfg

--- src/gt4py/next/program_processors/runners/dace_iterator/__init__.py
+++ src/gt4py/next/program_processors/runners/dace_iterator/__init__.py
@@ -1,8 +1,19 @@
+import numpy as np
+
 import gt4py.next.iterator.ir as itir
 from gt4py.next.program_processors.processor_interface import program_executor
 from gt4py.next.iterator.transforms import apply_common_transforms
 from .itir_to_sdfg import ItirToSDFG
-from gt4py.next.type_system import type_translation
+from gt4py.next.type_system import type_translation, type_specifications as ts
+from gt4py.next.iterator.embedded import LocatedField
+from typing import Any
+
+
+def convert_arg(arg: Any):
+    if isinstance(arg, LocatedField):
+        return np.asarray(arg)
+    return arg
+
 
 @program_executor
 def run_dace_iterator(program: itir.FencilDefinition, *args, **kwargs) -> None:
@@ -11,9 +22,21 @@ def run_dace_iterator(program: itir.FencilDefinition, *args, **kwargs) -> None:
 
     program = apply_common_transforms(program, offset_provider=offset_provider, force_inline_lift=True)
     sdfg_gen = ItirToSDFG(param_types=arg_types, offset_provider=offset_provider)
-    sdfg_gen.visit(program)
-
-    sdfg = sdfg_gen.sdfg
+    sdfg = sdfg_gen.visit(program)
+    
     sdfg.view()
 
-    raise ValueError("")
+    regular_args = {
+        name.id: convert_arg(arg)
+        for name, arg in zip(program.params, args)
+    }
+
+    array_params = [param for param, type_ in zip(program.params, arg_types) if isinstance(type_, ts.FieldType)]
+    array_args = [arg for arg in args if isinstance(arg, LocatedField)]
+    shape_args = {
+        str(sym): size
+        for param, arg in zip(array_params, array_args)
+        for sym, size in zip(sdfg.arrays[str(param.id)].shape, np.asarray(arg).shape)
+    }
+
+    sdfg(**regular_args, **shape_args)
--- src/gt4py/next/program_processors/runners/dace_iterator/itir_to_sdfg.py
+++ src/gt4py/next/program_processors/runners/dace_iterator/itir_to_sdfg.py
@@ -16,27 +16,100 @@ class ItirToSDFG(eve.NodeVisitor):
         self.param_types = param_types
         self.offset_providers = offset_provider
 
-    def visit_StencilClosure(self, node: itir.StencilClosure) -> dace.SDFG:
+    def visit_StencilClosure(self, node: itir.StencilClosure, array_table: dict[str, dace.data.Array]) -> dace.SDFG:
         sdfg = dace.SDFG(name="stencil_closure")
 
         assert isinstance(node.output, itir.SymRef)
 
-        for input in node.inputs:
-            shape = [rng[1] - rng[0] for _, rng in domain]
-            sdfg.add_array(node.output.id, shape=shape, dtype=dace.float64)
-
         domain = self._visit_named_range(node.domain)
-        shape = [rng[1] - rng[0] for _, rng in domain]
+        shape = array_table[node.output.id].shape
         sdfg.add_array(node.output.id, shape=shape, dtype=dace.float64)
 
+        for inp in node.inputs:
+            shape = array_table[node.output.id].shape  # Assume same as output
+            sdfg.add_array(inp.id, shape=shape, dtype=dace.float64)
 
+        input_memlets = {
+            f"{inp.id}_entire": dace.Memlet(
+                data=str(inp.id), subset=",".join(f"0:{size}" for size in sdfg.arrays[inp.id].shape)
+            )
+            for inp in node.inputs
+        }
+        output_memlets = {
+            f"{node.output.id}_element": dace.Memlet(
+                data=str(node.output.id),
+                subset=",".join(f"i_{dim}" for dim, _ in domain),
+            )
+        }
+
+        map_range = {
+            f"i_{dim}": f"{lb}:{ub}"
+            for dim, (lb, ub) in domain
+        }
+        last_state = sdfg.add_state()
+        last_state.add_mapped_tasklet(
+            name="addition",
+            map_ranges=map_range,
+            inputs=input_memlets,
+            code=f"{node.output.id}_element = [](){{ return 7; }}();",
+            language=dace.Language.CPP,
+            outputs=output_memlets,
+            external_edges=True,
+            schedule=dace.ScheduleType.Sequential,
+        )
+
+        return sdfg
 
     def visit_FencilDefinition(self, node: itir.FencilDefinition):
         sdfg = dace.SDFG(name=node.id)
-        last_state = sdfg.add_state("state", True)
+        last_state = sdfg.add_state()
+
+        for param, type_ in zip(node.params, self.param_types):
+            if isinstance(type_, ts.FieldType):
+                shape = (dace.symbol(sdfg.temp_data_name()) for _ in range(len(type_.dims)))
+                sdfg.add_array(str(param.id), shape=shape, dtype=dace.float64)
+            elif isinstance(type_, ts.ScalarType):
+                sdfg.add_symbol(param.id, type_spec_to_dtype(type_))
+            else:
+                raise NotImplementedError()
 
         for closure in node.closures:
-            self.visit(closure)
+            last_state = sdfg.add_state_after(last_state)
+
+            closure_sdfg = self.visit(closure, array_table=sdfg.arrays)
+            nsdfg_node = last_state.add_nested_sdfg(
+                closure_sdfg,
+                None,
+                inputs={str(inp.id) for inp in closure.inputs},
+                outputs={str(closure.output.id)}
+            )
+
+            input_accesses = [last_state.add_access(inp.id) for inp in closure.inputs]
+            output_access = last_state.add_access(closure.output.id)
+
+            for inner_name, access_node in zip(closure.inputs, input_accesses):
+                last_state.add_edge(
+                    access_node,
+                    None,
+                    nsdfg_node,
+                    inner_name.id,
+                    dace.Memlet(
+                        data=access_node.data, subset=", ".join(f"0:{sh}" for sh in sdfg.arrays[access_node.data].shape)
+                    ),
+                )
+
+            last_state.add_edge(
+                nsdfg_node,
+                closure.output.id,
+                output_access,
+                None,
+                dace.Memlet(
+                    data=output_access.data, subset=", ".join(f"0:{sh}" for sh in sdfg.arrays[output_access.data].shape)
+                ),
+            )
+
+        sdfg.validate()
+        return sdfg
 
     def _visit_named_range(self, node: itir.FunCall) -> tuple[sympy.Basic, ...]:
         # cartesian_domain(named_range(IDim, start, end))
