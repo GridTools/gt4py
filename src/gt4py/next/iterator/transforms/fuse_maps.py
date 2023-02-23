68a21758c0b62d3e5389e267a423048d399257a8
Hannes Vogt <hannes@havogt.de>
2023-02-23 14:47:37 +0000

    first map fuse

---
+++ src/gt4py/next/iterator/transforms/fuse_maps.py
@@ -0,0 +1,103 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2023, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import dataclasses
+from typing import TypeGuard
+
+from gt4py.eve import NodeTranslator, traits
+from gt4py.eve.utils import UIDGenerator
+from gt4py.next.iterator import ir
+from gt4py.next.iterator.transforms.inline_lambdas import inline_lambda
+
+
+def _is_map(node: ir.Node) -> TypeGuard[ir.FunCall]:
+    return (
+        isinstance(node, ir.FunCall)
+        and isinstance(node.fun, ir.FunCall)
+        and node.fun.fun == ir.SymRef(id="map_")
+    )
+
+
+@dataclasses.dataclass(frozen=True)
+class FuseMaps(traits.VisitorWithSymbolTableTrait, NodeTranslator):
+    """
+    Fuses nested `map_`s.
+
+    Preconditions:
+      - `FunctionDefinitions` are inlined
+
+    Example:
+        map(λ(x, y)->f(x, y))(a, map(λ(z, w)->g(z, w))(b, c))
+    to
+        TODOmap(λ(a, b, c) → f(a, g(b, c)))(a, b, c)
+
+
+    map(λ(x, b,c)->f(x, (λ(z, w)->g(z, w))(b,c)))(a, map(λ(z, w)->g(z, w))(b, c))
+
+    λ(x, b, c)->(λ(x, y)->f(x, y))(x, λ(z, w)->g(z, w))(b,c)
+
+
+    Algorithm:
+      - example: map(λ(x, y)->f(x, y))(a, map(λ(z, w)->g(z, w))(b, c))
+      - the arguments to the mapped operation are either `SymRef`s or calls to `neighbor` or calls to `deref`
+      - the new op is a lambda where the to-inline argument is replaced by the
+      - create a new op from
+    """
+
+    uids: UIDGenerator = dataclasses.field(init=False, repr=False, default_factory=UIDGenerator)
+
+    def _as_lambda(self, fun: ir.SymRef | ir.Lambda, param_count: int) -> ir.Lambda:
+        if isinstance(fun, ir.Lambda):
+            return fun
+        params = [ir.Sym(id=self.uids.sequential_id(prefix="sym")) for _ in range(param_count)]
+        return ir.Lambda(
+            params=params,
+            expr=ir.FunCall(fun=fun, args=[ir.SymRef(id=p.id) for p in params]),
+        )
+
+    # TODO think about clashes
+    def visit_FunCall(self, node: ir.FunCall, **kwargs):
+        if _is_map(node):
+            if _is_map(node.args[1]):  # TODO inline only the second arg
+                assert isinstance(node.fun, ir.FunCall)
+                outer_op = self._as_lambda(node.fun.args[0], len(node.args))
+                assert isinstance(node.args[1].fun, ir.FunCall)
+                inner_op = self._as_lambda(node.args[1].fun.args[0], len(node.args[1].args))
+                new_body = inline_lambda(
+                    ir.FunCall(
+                        fun=outer_op,
+                        args=[
+                            ir.SymRef(id=outer_op.params[0].id),
+                            inline_lambda(
+                                ir.FunCall(
+                                    fun=inner_op,
+                                    args=[*(ir.SymRef(id=param.id) for param in inner_op.params)],
+                                )
+                            ),
+                        ],
+                    )
+                )
+                new_op = ir.Lambda(
+                    params=[
+                        outer_op.params[0],
+                        *inner_op.params,
+                    ],  # TODO remaining params from outer_op
+                    expr=new_body,
+                )
+                result = ir.FunCall(
+                    fun=ir.FunCall(fun=ir.SymRef(id="map_"), args=[new_op]),
+                    args=[node.args[0], *node.args[1].args],
+                )
+                return result
+        return self.generic_visit(node)
---
+++ tests/next_tests/iterator_tests/test_fuse_maps.py
@@ -0,0 +1,126 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2023, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from gt4py.next.iterator import ir
+from gt4py.next.iterator.transforms.fuse_maps import FuseMaps
+
+
+def _map(op: ir.Expr, *args: ir.Expr) -> ir.FunCall:
+    return ir.FunCall(fun=ir.FunCall(fun=ir.SymRef(id="map_"), args=[op]), args=[*args])
+
+
+def _wrap_in_lambda(fun: ir.Expr, *params: str) -> ir.Lambda:
+    return ir.Lambda(
+        params=[ir.Sym(id=p) for p in params],
+        expr=ir.FunCall(fun=fun, args=[ir.SymRef(id=arg) for arg in params]),
+    )
+
+
+def test_simple():
+    testee = _map(
+        ir.SymRef(id="plus"),
+        ir.SymRef(id="a"),
+        _map(ir.SymRef(id="multiplies"), ir.SymRef(id="b"), ir.SymRef(id="c")),
+    )
+
+    expected = _map(
+        ir.Lambda(
+            params=[ir.Sym(id="a"), ir.Sym(id="b"), ir.Sym(id="c")],
+            expr=ir.FunCall(
+                fun=ir.SymRef(id="plus"),
+                args=[
+                    ir.SymRef(id="a"),
+                    ir.FunCall(
+                        fun=ir.SymRef(id="multiplies"), args=[ir.SymRef(id="b"), ir.SymRef(id="c")]
+                    ),
+                ],
+            ),
+        ),
+        ir.SymRef(id="a"),
+        ir.SymRef(id="b"),
+        ir.SymRef(id="c"),
+    )
+
+    actual = FuseMaps().visit(testee)
+    # assert expected == actual #TODO pattern match the result
+
+
+def test_simple_with_lambdas():
+    testee = _map(
+        _wrap_in_lambda(ir.SymRef(id="plus"), "x", "y"),
+        ir.SymRef(id="a"),
+        _map(
+            _wrap_in_lambda(ir.SymRef(id="multiplies"), "z", "w"),
+            ir.SymRef(id="b"),
+            ir.SymRef(id="c"),
+        ),
+    )
+
+    expected = _map(
+        ir.Lambda(
+            params=[ir.Sym(id="x"), ir.Sym(id="z"), ir.Sym(id="w")],
+            expr=ir.FunCall(
+                fun=ir.SymRef(id="plus"),
+                args=[
+                    ir.SymRef(id="x"),
+                    ir.FunCall(
+                        fun=ir.SymRef(id="multiplies"), args=[ir.SymRef(id="z"), ir.SymRef(id="w")]
+                    ),
+                ],
+            ),
+        ),
+        ir.SymRef(id="a"),
+        ir.SymRef(id="b"),
+        ir.SymRef(id="c"),
+    )
+
+    actual = FuseMaps().visit(testee)
+    assert expected == actual
+
+
+# def test_multiple_maps():
+#     testee = _map(
+#         _wrap_in_lambda(ir.SymRef(id="plus"), "x", "y"),
+#         _map(
+#             _wrap_in_lambda(ir.SymRef(id="multiplies"), "z", "w"),
+#             ir.SymRef(id="a"),
+#             ir.SymRef(id="b"),
+#         ),
+#         _map(
+#             _wrap_in_lambda(ir.SymRef(id="multiplies"), "ww", "www"),
+#             ir.SymRef(id="c"),
+#             ir.SymRef(id="d"),
+#         ),
+#     )
+
+#     expected = _map(
+#         ir.Lambda(
+#             params=[ir.Sym(id="x"), ir.Sym(id="z"), ir.Sym(id="w")],
+#             expr=ir.FunCall(
+#                 fun=ir.SymRef(id="plus"),
+#                 args=[
+#                     ir.SymRef(id="x"),
+#                     ir.FunCall(
+#                         fun=ir.SymRef(id="multiplies"), args=[ir.SymRef(id="z"), ir.SymRef(id="w")]
+#                     ),
+#                 ],
+#             ),
+#         ),
+#         ir.SymRef(id="a"),
+#         ir.SymRef(id="b"),
+#         ir.SymRef(id="c"),
+#     )
+
+#     actual = FuseMaps().visit(testee)
+#     assert expected == actual
