5167bdfbfec15c1e33d264debabd6cc478a6192e
Hannes Vogt <hannes@havogt.de>
2023-03-02 09:47:54 +0000

    add nested reduction test

--- tests/next_tests/ffront_tests/test_execution.py
+++ tests/next_tests/ffront_tests/test_execution.py
@@ -354,6 +354,28 @@ def test_nested_tuple_return():
     assert np.allclose(2 * a_I_float.array() + b_I_float.array(), out_I_float)
 
 
+def test_nested_reduction(reduction_setup, fieldview_backend):
+    rs = reduction_setup
+    Edge = rs.Edge
+    Vertex = rs.Vertex
+    V2EDim = rs.V2EDim
+    E2VDim = rs.E2VDim
+    V2E = rs.V2E
+    E2V = rs.E2V
+
+    out = np_as_located_field(Edge)(np.zeros([rs.num_edges], dtype=np.int64))
+
+    @field_operator(backend=fieldview_backend)
+    def testee(inp: Field[[Edge], int64]) -> Field[[Edge], int64]:
+        tmp = neighbor_sum(inp(V2E), axis=V2EDim)
+        return neighbor_sum(tmp(E2V), axis=E2VDim)
+
+    testee(rs.inp, out=out, offset_provider=rs.offset_provider)
+
+    expected = np.sum(np.sum(rs.inp[rs.v2e_table], axis=1)[rs.e2v_table], axis=1)
+    assert np.allclose(out, expected)
+
+
 def test_tuple_return_2(reduction_setup):
     rs = reduction_setup
     Edge = rs.Edge
