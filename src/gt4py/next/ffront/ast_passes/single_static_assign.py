80a05c82528169466a055c758c44e9e7ff964f4b
Till Ehrengruber <till.ehrengruber@cscs.ch>
2023-05-04 11:51:40 +0200

    Extend Single Static Assignment (SSA) pass to support if statements

--- src/gt4py/next/ffront/ast_passes/single_static_assign.py
+++ src/gt4py/next/ffront/ast_passes/single_static_assign.py
@@ -11,10 +11,103 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
 
 import ast
+import typing
 
 from gt4py.next.ffront.fbuiltins import TYPE_BUILTIN_NAMES
+
+
+def _make_assign(target: str, source: str, location_node: ast.AST):
+    result = ast.Assign(
+        targets=[ast.Name(ctx=ast.Store(), id=target)], value=ast.Name(ctx=ast.Load(), id=source)
+    )
+    for node in ast.walk(result):
+        ast.copy_location(node, location_node)
+    return result
+
+
+def is_guaranteed_to_return(node: ast.stmt | list[ast.stmt]) -> bool:
+    if isinstance(node, list):
+        return any(is_guaranteed_to_return(child) for child in node)
+    if isinstance(node, ast.Return):
+        return True
+    if isinstance(node, ast.If):
+        return is_guaranteed_to_return(node.body) and is_guaranteed_to_return(node.orelse)
+    return False
+
+
+class Versioning:
+    """Helper class to keep track of whether versioning (definedness)."""
+
+    # invariant: if a version is an `int`, it's not negative
+    _versions: dict[str, None | int]
+
+    def __init__(self):
+        self._versions = {}
+
+    def define(self, name: str) -> None:
+        if name not in self._versions:
+            self._versions[name] = None
+
+    def assign(self, name: str) -> None:
+        if self.is_versioned(name):
+            self._versions[name] = typing.cast(int, self._versions[name]) + 1
+        else:
+            self._versions[name] = 0
+
+    def is_defined(self, name: str) -> bool:
+        return name in self._versions
+
+    def is_versioned(self, name: str) -> bool:
+        return self.is_defined(name) and self._versions[name] is not None
+
+    def __getitem__(self, name: str) -> None | int:
+        return self._versions[name]
+
+    def __iter__(self) -> typing.Iterator[tuple[str, None | int]]:
+        return iter(self._versions.items())
+
+    def copy(self) -> Versioning:
+        copy = Versioning()
+        copy._versions = {**self._versions}
+        return copy
+
+    @staticmethod
+    def merge(a: Versioning, b: Versioning) -> Versioning:
+        versions_a, version_b = a._versions, b._versions
+        names = set(versions_a.keys()) & set(version_b.keys())
+
+        merged_versioning = Versioning()
+        merged_versions = merged_versioning._versions
+
+        for name in names:
+            merged_versions[name] = Versioning._merge_versions(versions_a[name], version_b[name])
+
+        return merged_versioning
+
+    @staticmethod
+    def _merge_versions(a: None | int, b: None | int) -> None | int:
+        if a is None:
+            return b
+        elif b is None:
+            return a
+        return max(a, b)
+
+
+class NameEncoder:
+    """Helper class to encode names of versioned variables."""
+
+    _separator: str
+
+    def __init__(self, separator: str):
+        self._separator = separator
+
+    def encode_name(self, name: str, versions: Versioning) -> str:
+        if versions.is_versioned(name):
+            return f"{name}{self._separator}{versions[name]}"
+        return name
 
 
 class SingleStaticAssignPass(ast.NodeTransformer):
@@ -48,11 +141,16 @@ class SingleStaticAssignPass(ast.NodeTransformer):
         a__2 = 3 + a__1
         return a__2
 
-    Note that each variable name is assigned only once and never updated / overwritten.
+    Note that each variable name is assigned only once (per branch) and never updated / overwritten.
 
     Note also that after parsing, running the pass and unparsing we get invalid but
     readable python code. This is ok because this pass is not intended for
     python-to-python translation.
+
+    WARNING: This pass is not intended as a general-purpose SSA transformation.
+    The pass does not support any general Python AST. Known limitations include:
+        * Nested functions aren't supported
+        * While loops aren't supported
     """
 
     class RhsRenamer(ast.NodeTransformer):
@@ -63,17 +161,16 @@ class SingleStaticAssignPass(ast.NodeTransformer):
         """
 
         @classmethod
-        def apply(cls, name_counter, separator, node):
-            return cls(name_counter, separator).visit(node)
+        def apply(cls, versioning: Versioning, name_encoder: NameEncoder, node: ast.AST):
+            return cls(versioning, name_encoder).visit(node)
 
-        def __init__(self, name_counter, separator):
+        def __init__(self, versioning: Versioning, name_encoder: NameEncoder):
             super().__init__()
-            self.name_counter: dict[str, int] = name_counter
-            self.separator: str = separator
+            self.versioning: Versioning = versioning
+            self.name_encoder: NameEncoder = name_encoder
 
         def visit_Name(self, node: ast.Name) -> ast.Name:
-            if node.id in self.name_counter:
-                node.id = f"{node.id}{self.separator}{self.name_counter[node.id]}"
+            node.id = self.name_encoder.encode_name(node.id, self.versioning)
             return node
 
     @classmethod
@@ -82,11 +179,78 @@ class SingleStaticAssignPass(ast.NodeTransformer):
 
     def __init__(self, separator="__"):
         super().__init__()
-        self.name_counter: dict[str, int] = {}
-        self.separator: str = separator
+        self.versioning: Versioning = Versioning()
+        self.name_encoder: NameEncoder = NameEncoder(separator)
 
-    def _rename(self, node):
-        return self.RhsRenamer.apply(self.name_counter, self.separator, node)
+    def _rename(self, node: ast.AST):
+        return self.RhsRenamer.apply(self.versioning, self.name_encoder, node)
+
+    def visit_FunctionDef(self, node: ast.FunctionDef):
+        # For practical purposes, this is sufficient, but really not general at all.
+        # However, the algorithm was never intended to be general.
+
+        old_versioning = self.versioning.copy()
+
+        for arg in node.args.args:
+            self.versioning.define(arg.arg)
+
+        node.body = [self.visit(stmt) for stmt in node.body]
+
+        self.versioning = old_versioning
+        return node
+
+    def visit_If(self, node: ast.If) -> ast.If:
+        old_versioning = self.versioning
+
+        node.test = self._rename(node.test)
+
+        self.versioning = old_versioning.copy()
+        node.body = [self.visit(el) for el in node.body]
+        body_versioning = self.versioning
+        body_returns = is_guaranteed_to_return(node.body)
+
+        self.versioning = old_versioning.copy()
+        node.orelse = [self.visit(el) for el in node.orelse]
+        orelse_versioning = self.versioning
+        orelse_returns = is_guaranteed_to_return(node.orelse)
+
+        if body_returns and not orelse_returns:
+            self.versioning = orelse_versioning
+            return node
+
+        if orelse_returns and not body_returns:
+            self.versioning = body_versioning
+            return node
+
+        if body_returns and orelse_returns:
+            self.versioning = Versioning()
+            return node
+
+        assert not body_returns and not orelse_returns
+
+        self.versioning = Versioning.merge(body_versioning, orelse_versioning)
+
+        # ensure both branches conclude with the same unique names
+        for name, merged_version in self.versioning:
+            body_version = body_versioning[name]
+            orelse_version = orelse_versioning[name]
+
+            if body_version != merged_version:
+                new_assign = _make_assign(
+                    self.name_encoder.encode_name(name, self.versioning),
+                    self.name_encoder.encode_name(name, body_versioning),
+                    node,
+                )
+                node.body.append(new_assign)
+            elif orelse_version != merged_version:
+                new_assign = _make_assign(
+                    self.name_encoder.encode_name(name, self.versioning),
+                    self.name_encoder.encode_name(name, orelse_versioning),
+                    node,
+                )
+                node.orelse.append(new_assign)
+
+        return node
 
     def visit_Assign(self, node: ast.Assign) -> ast.Assign:
         # first update rhs names to reference the latest version
@@ -104,18 +268,19 @@ class SingleStaticAssignPass(ast.NodeTransformer):
             node.value = self._rename(node.value)
             node.target = self.visit(node.target)
         elif isinstance(node.target, ast.Name):
-            target_id = node.target.id
+            # An empty annotation always applies to the next assignment.
+            # So we need to use the correct versioning, but also ensure
+            # we restore the old versioning afterwards, because no assignment
+            # actually happens.
+            old_versioning = self.versioning.copy()
             node.target = self.visit(node.target)
-            self.name_counter[target_id] -= 1
+            self.versioning = old_versioning
         return node
 
     def visit_Name(self, node: ast.Name) -> ast.Name:
         if node.id in TYPE_BUILTIN_NAMES:
             return node
-        elif node.id in self.name_counter:
-            self.name_counter[node.id] += 1
-            node.id = f"{node.id}{self.separator}{self.name_counter[node.id]}"
-        else:
-            self.name_counter[node.id] = 0
-            node.id = f"{node.id}{self.separator}0"
+
+        self.versioning.assign(node.id)
+        node.id = self.name_encoder.encode_name(node.id, self.versioning)
         return node
--- tests/next_tests/unit_tests/ffront_tests/ast_passes_tests/test_single_static_assign.py
+++ tests/next_tests/unit_tests/ffront_tests/ast_passes_tests/test_single_static_assign.py
@@ -133,3 +133,666 @@ def test_empty_annotated_assign():
     assert lines[0] == "a__0 = 0"
     assert lines[1] == "a__1: int"
     assert lines[2] == "b__0 = a__0"
+
+
+def test_if():
+    lines = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = 1
+            else:
+                a = 2
+            a = 3
+            """
+        )
+    ).splitlines()
+
+    assert lines[1] == "    a__0 = 1"
+    assert lines[3] == "    a__0 = 2"
+    assert lines[4] == "a__1 = 3"
+
+
+def test_if_variable_condition():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if b:
+                a = 1
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if b:
+            a__0 = 1
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_nested_if():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = 1
+            else:
+                a = 2
+                if True:
+                    a = 3
+                a = 4
+            a = 5
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            a__0 = 1
+            a__2 = a__0
+        else:
+            a__0 = 2
+            if True:
+                a__1 = 3
+            else:
+                a__1 = a__0
+            a__2 = 4
+        a__3 = 5
+    """
+    ).strip()
+
+    assert result == expected
+
+
+def test_nested_if_chain():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = 1
+            else:
+                a = 2
+                if True:
+                    a = a+1
+                a = a+1
+            a = a+1
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            a__0 = 1
+            a__2 = a__0
+        else:
+            a__0 = 2
+            if True:
+                a__1 = a__0 + 1
+            else:
+                a__1 = a__0
+            a__2 = a__1 + 1
+        a__3 = a__2 + 1
+    """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_branch_local():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = 0
+                a = a + 1
+            else:
+                b = 1
+                b = b + 1
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            a__0 = 0
+            a__1 = a__0 + 1
+        else:
+            b__0 = 1
+            b__1 = b__0 + 1
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_only_one_branch():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = 0
+            b = a
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            a__0 = 0
+        b__0 = a
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_only_one_branch_other():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = 0
+            else:
+                c = 1
+            b = c
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            a__0 = 0
+        else:
+            c__0 = 1
+        b__0 = c
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_nested_all_branches_defined():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                if True:
+                    a = 0
+                    a = a + 1
+                    a = a + 1
+                    a = a + 1
+                    a = a + 1
+                else:
+                    a = 0
+                    a = a + 1
+                    a = a + 1
+            else:
+                a = 0
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            if True:
+                a__0 = 0
+                a__1 = a__0 + 1
+                a__2 = a__1 + 1
+                a__3 = a__2 + 1
+                a__4 = a__3 + 1
+            else:
+                a__0 = 0
+                a__1 = a__0 + 1
+                a__2 = a__1 + 1
+                a__4 = a__2
+        else:
+            a__0 = 0
+            a__4 = a__0
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_elif_all_branches_defined():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = 0
+                a = a + 1
+                a = a + 1
+                a = a + 1
+                a = a + 1
+            elif True:
+                a = 0
+                a = a + 1
+                a = a + 1
+            else:
+                a = 0
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            a__0 = 0
+            a__1 = a__0 + 1
+            a__2 = a__1 + 1
+            a__3 = a__2 + 1
+            a__4 = a__3 + 1
+        else:
+            if True:
+                a__0 = 0
+                a__1 = a__0 + 1
+                a__2 = a__1 + 1
+            else:
+                a__0 = 0
+                a__2 = a__0
+            a__4 = a__2
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_nested_ifs_single_change():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            a = 0
+            if True:
+                b = 0
+            elif True:
+                if True:
+                    b = 1
+                else:
+                    if True:
+                        b = 2
+                        # because of this nested change, all branches need additions
+                        a = a + 1
+                    else:
+                        b = 3
+            else:
+                b = 4
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        a__0 = 0
+        if True:
+            b__0 = 0
+            a__1 = a__0
+        elif True:
+            if True:
+                b__0 = 1
+                a__1 = a__0
+            elif True:
+                b__0 = 2
+                a__1 = a__0 + 1
+            else:
+                b__0 = 3
+                a__1 = a__0
+        else:
+            b__0 = 4
+            a__1 = a__0
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_one_sided_inside_function():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f(a):
+                if True:
+                    a = a + 1
+                return a
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f(a):
+            if True:
+                a__0 = a + 1
+            else:
+                a__0 = a
+            return a__0
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_preservers_definite_assignment_analysis1():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f():
+                if True:
+                    a = 1
+                else:
+                    b = 0
+                return a, b
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f():
+            if True:
+                a__0 = 1
+            else:
+                b__0 = 0
+            return (a, b)
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_preservers_definite_assignment_analysis2():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f():
+                if True:
+                    a = 1
+                else:
+                    a = 0
+                return a, b
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f():
+            if True:
+                a__0 = 1
+            else:
+                a__0 = 0
+            return (a__0, b)
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_preservers_definite_assignment_analysis3():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f():
+                a = 0
+                if True:
+                    a = 1
+                return a, b
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f():
+            a__0 = 0
+            if True:
+                a__1 = 1
+            else:
+                a__1 = a__0
+            return (a__1, b)
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_broken_collisions():
+    # Known bug of the current SSA implementation
+
+    result = ast.unparse(
+        ssaify_string(
+            """
+            a = a__0 + 1
+            return a, a__0
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        a__0 = a__0 + 1
+        return (a__0, a__0)
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_collision_function_parameters():
+    # An earlier version couldn't handle this case correctly
+
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f(a, a__0):
+                return a__0
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f(a, a__0):
+            return a__0
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_broken_if():
+    # Known bug of the current SSA implementation
+
+    result = ast.unparse(
+        ssaify_string(
+            """
+            if True:
+                a = a + 1
+            return a
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        if True:
+            a__0 = a + 1
+        return a
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_annotated_assign():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            a: int # annotations always apply to the next assignment
+            a = a + 1
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        a__0: int
+        a__0 = a + 1
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_true_branch_returns():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f(a, b):
+                if True:
+                    a = a + 1
+                    d = 5
+                    return a
+                else:
+                    b = b + 1
+                    e = 6
+                return a, b, d, e
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f(a, b):
+            if True:
+                a__0 = a + 1
+                d__0 = 5
+                return a__0
+            else:
+                b__0 = b + 1
+                e__0 = 6
+            return (a, b__0, d, e__0)
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_false_branch_returns():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f(a, b):
+                b = a + 3
+                if True:
+                    b = b + 1
+                    e = 6
+                else:
+                    a = a + 1
+                    d = 5
+                    return a
+                return a, b, d, e
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f(a, b):
+            b__0 = a + 3
+            if True:
+                b__1 = b__0 + 1
+                e__0 = 6
+            else:
+                a__0 = a + 1
+                d__0 = 5
+                return a__0
+            return (a, b__1, d, e__0)
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_both_branches_return():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f(a, b):
+                if True:
+                    b = b + 1
+                    e = 6
+                    return e
+                else:
+                    a = a + 1
+                    d = 5
+                    return a
+                return a, b, d, e # this is dead-code
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+        def f(a, b):
+            if True:
+                b__0 = b + 1
+                e__0 = 6
+                return e__0
+            else:
+                a__0 = a + 1
+                d__0 = 5
+                return a__0
+            return (a, b, d, e)
+        """
+    ).strip()
+
+    assert result == expected
+
+
+def test_if_nested_returns():
+    result = ast.unparse(
+        ssaify_string(
+            """
+            def f(a, b):
+                if True:
+                    b = b + 1
+                    e = 6
+                    if e == b:
+                        return e
+                    else:
+                        return b
+                else:
+                    a = a + 1
+                    d = 5
+                return a, b, d, e
+            """
+        )
+    )
+
+    expected = textwrap.dedent(
+        """
+            def f(a, b):
+                if True:
+                    b__0 = b + 1
+                    e__0 = 6
+                    if e__0 == b__0:
+                        return e__0
+                    else:
+                        return b__0
+                else:
+                    a__0 = a + 1
+                    d__0 = 5
+                return (a__0, b, d__0, e)
+        """
+    ).strip()
+
+    assert result == expected
