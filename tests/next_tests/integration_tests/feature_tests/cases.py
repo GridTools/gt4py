9f03c439d766e99209ce069c5f79d054e4c9e10b
Till Ehrengruber <till.ehrengruber@cscs.ch>
2023-05-31 12:59:25 +0200

    Remove IJKIntField type alias

--- tests/next_tests/integration_tests/feature_tests/cases.py
+++ tests/next_tests/integration_tests/feature_tests/cases.py
@@ -49,7 +49,6 @@ from next_tests.integration_tests.feature_tests.ffront_tests.ffront_test_utils i
 # mypy does not accept [IDim, ...] as a type
 IField: TypeAlias = common.Field[[IDim], np.float64]  # type: ignore [valid-type]
 IJKField: TypeAlias = common.Field[[IDim, JDim, KDim], np.float64]  # type: ignore [valid-type]
-IJKIntField: TypeAlias = common.Field[[IDim, JDim, KDim], np.int64]  # type: ignore [valid-type]
 VField: TypeAlias = common.Field[[Vertex], np.float64]  # type: ignore [valid-type]
 EField: TypeAlias = common.Field[[Edge], np.float64]  # type: ignore [valid-type]
 
