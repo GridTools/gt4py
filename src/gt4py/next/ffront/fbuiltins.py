edceb8106c55d5aff880b40fb5e215eb8da9bf1e
Rico Häuselmann <ricoh@cscs.ch>
2023-06-06 16:11:31 +0200

    shorten fbuiltins.__all__ cleanup

--- src/gt4py/next/ffront/fbuiltins.py
+++ src/gt4py/next/ffront/fbuiltins.py
@@ -201,8 +201,7 @@ BUILTIN_NAMES = TYPE_BUILTIN_NAMES + FUN_BUILTIN_NAMES
 BUILTINS = {name: globals()[name] for name in BUILTIN_NAMES}
 
 __all__ = BUILTIN_NAMES
-__all__.remove("Field")
-__all__.remove("Dimension")
+__all__ = [*(set(BUILTIN_NAMES) - {"Dimension", "Field"})]
 
 
 # TODO(tehrengruber): FieldOffset and runtime.Offset are not an exact conceptual
