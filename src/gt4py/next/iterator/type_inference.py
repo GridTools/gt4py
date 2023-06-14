60fbaf2f0b78a74284bc69e3e98834b40afd2355
Till Ehrengruber <till.ehrengruber@cscs.ch>
2023-06-15 01:39:21 +0200

    Fix type inference

--- src/gt4py/next/iterator/type_inference.py
+++ src/gt4py/next/iterator/type_inference.py
@@ -442,9 +442,9 @@ BUILTIN_TYPES: dict[str, Type] = {
     ),
     "if_": FunctionType(
         args=Tuple.from_elems(
-            Val_BOOL_T1, Val(kind=T2, dtype=T0, size=T1), Val(kind=T2, dtype=T0, size=T1)
+            Val_BOOL_T1, T2, T2
         ),
-        ret=Val(kind=T2, dtype=T0, size=T1),
+        ret=T2,
     ),
     "lift": FunctionType(
         args=Tuple.from_elems(
