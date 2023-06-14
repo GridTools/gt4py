218b0be285f709bbced3da10d61f5bd98e3b91d7
Till Ehrengruber <till.ehrengruber@cscs.ch>
2023-06-15 01:42:00 +0200

    Fix format

--- src/gt4py/next/iterator/type_inference.py
+++ src/gt4py/next/iterator/type_inference.py
@@ -441,9 +441,7 @@ BUILTIN_TYPES: dict[str, Type] = {
         ret=Val_BOOL_T1,
     ),
     "if_": FunctionType(
-        args=Tuple.from_elems(
-            Val_BOOL_T1, T2, T2
-        ),
+        args=Tuple.from_elems(Val_BOOL_T1, T2, T2),
         ret=T2,
     ),
     "lift": FunctionType(
