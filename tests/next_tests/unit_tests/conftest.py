ced9e3a539b88e8e856ee99b69d8ff1e9e70da6c
Linus Groner <linus.groner@cscs.ch>
2023-06-22 12:55:12 +0200

    add dace to conftest runners

--- tests/next_tests/unit_tests/conftest.py
+++ tests/next_tests/unit_tests/conftest.py
@@ -22,7 +22,7 @@ from gt4py.next import common
 from gt4py.next.iterator import ir as itir, pretty_parser, pretty_printer, runtime, transforms
 from gt4py.next.program_processors import processor_interface as ppi
 from gt4py.next.program_processors.formatters import gtfn, lisp, type_check
-from gt4py.next.program_processors.runners import double_roundtrip, gtfn_cpu, roundtrip
+from gt4py.next.program_processors.runners import double_roundtrip, gtfn_cpu, roundtrip, dace_iterator
 
 
 @pytest.fixture(
@@ -64,6 +64,7 @@ def get_processor_id(processor):
         (double_roundtrip.executor, True),
         (gtfn_cpu.run_gtfn, True),
         (gtfn_cpu.run_gtfn_imperative, True),
+        (dace_iterator.run_dace_iterator, True),
         (gtfn.format_sourcecode, False),
     ],
     ids=lambda p: get_processor_id(p[0]),
