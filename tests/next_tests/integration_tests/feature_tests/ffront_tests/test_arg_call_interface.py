2ba5b45ddd50ea1a54c429e704de578f6544a758
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2023-07-12 10:10:16 +0200

    fix[next]: ensure scan operator `init`, `carry`, return type is consistent (#1286)
    ensure scan operator `init`, `carry`, return type is consistent

--- tests/next_tests/integration_tests/feature_tests/ffront_tests/test_arg_call_interface.py
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/test_arg_call_interface.py
@@ -20,7 +20,8 @@ import pytest
 
 from gt4py.next.common import Field
 from gt4py.next.ffront.decorator import field_operator, program, scan_operator
-from gt4py.next.ffront.fbuiltins import int64
+from gt4py.next.ffront.fbuiltins import int32, int64
+from gt4py.next.ffront.foast_passes.type_deduction import FieldOperatorTypeDeductionError
 from gt4py.next.program_processors.runners import dace_iterator, gtfn_cpu
 
 from next_tests.integration_tests import cases
@@ -235,3 +236,39 @@ def test_call_scan_operator_from_program(cartesian_case):
         ref=(ref, ref, ref, ref),
         comparison=lambda out, ref: all(map(np.allclose, zip(out, ref))),
     )
+
+
+def test_scan_wrong_return_type(cartesian_case):
+    with pytest.raises(
+        FieldOperatorTypeDeductionError,
+        match=(r"Argument `init` to scan operator `testee_scan` must have same type as its return"),
+    ):
+
+        @scan_operator(axis=KDim, forward=True, init=0)
+        def testee_scan(
+            state: int32,
+        ) -> float:
+            return 1.0
+
+        @program
+        def testee(qc: cases.IKFloatField, param_1: int32, param_2: float, scalar: float):
+            testee_scan(qc, param_1, param_2, scalar, out=(qc, param_1, param_2))
+
+
+def test_scan_wrong_state_type(cartesian_case):
+    with pytest.raises(
+        FieldOperatorTypeDeductionError,
+        match=(
+            r"Argument `init` to scan operator `testee_scan` must have same type as `state` argument"
+        ),
+    ):
+
+        @scan_operator(axis=KDim, forward=True, init=0)
+        def testee_scan(
+            state: float,
+        ) -> int32:
+            return 1
+
+        @program
+        def testee(qc: cases.IKFloatField, param_1: int32, param_2: float, scalar: float):
+            testee_scan(qc, param_1, param_2, scalar, out=(qc, param_1, param_2))
