054e4e6a3416e9b704da41574e7e0ea7913b8006
Enrique G. Paredes <18477+egparedes@users.noreply.github.com>
2023-08-09 08:52:51 +0200

    feat: low-level buffer allocation utilities for new storages (#1303)
    Add new buffer allocation utilities and concepts definitions.
    Main changes:
    - New definition of core concepts dealing with data: DType, Device, ScalarTypes
    - New BullerAllocator protocol to support different buffer allocation strategies depending on the device type.
    - Refactoring of existing cartesian storages allocation tools using the new buffer allocators
    
    Co-authored-by: Hannes Vogt <vogt@hey.com>
    Co-authored-by: Stefano Ubbiali <subbiali@phys.ethz.ch>

--- src/gt4py/_core/__init__.py
+++ src/gt4py/_core/__init__.py
@@ -11,25 +11,3 @@
 # distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-# flake8: noqa: F401
-
-"""GridTools storages utilities."""
-
-from . import cartesian
-from .cartesian import layout
-from .cartesian.interface import empty, from_array, full, ones, zeros  # noqa: F401
-from .cartesian.layout import from_name, register
-
-
-__all__ = [
-    "cartesian",
-    "layout",
-    "empty",
-    "from_array",
-    "full",
-    "ones",
-    "zeros",
-    "from_name",
-    "register",
-]
--- src/gt4py/storage/cartesian/interface.py
+++ src/gt4py/storage/cartesian/interface.py
@@ -12,10 +12,15 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import annotations
+
 import numbers
-from typing import Any, Optional, Sequence, Union
+from typing import Any, Optional, Protocol, Sequence, Tuple, Union
 
 import numpy as np
+
+from gt4py.storage import allocators
+from gt4py.storage.cartesian import layout, utils as storage_utils
 
 
 try:
@@ -34,14 +39,23 @@ else:
     ArrayLike = Any  # type: ignore[misc]  # assign multiple types in both branches
     DTypeLike = Any  # type: ignore[misc]  # assign multiple types in both branches
 
-from . import layout, utils as storage_utils
+
+# Protocols
+class GTDimsInterface(Protocol):
+    __gt_dims__: Tuple[str, ...]
 
 
-def _error_on_invalid_backend(backend):
+class GTOriginInterface(Protocol):
+    __gt_origin__: Tuple[int, ...]
+
+
+# Helper functions
+def _error_on_invalid_preset(backend):
     if backend not in layout.REGISTRY:
         raise RuntimeError(f"Storage preset '{backend}' is not registered.")
 
 
+# Public interface
 def empty(
     shape: Sequence[int],
     dtype: DTypeLike = np.float64,
@@ -83,7 +97,7 @@ def empty(
         ValueError
             If illegal or inconsistent arguments are specified.
     """
-    _error_on_invalid_backend(backend)
+    _error_on_invalid_preset(backend)
     storage_info = layout.from_name(backend)
     assert storage_info is not None
     if storage_info["device"] == "gpu":
@@ -95,13 +109,15 @@ def empty(
         aligned_index, shape, dtype, dimensions
     )
 
-    _error_on_invalid_backend(backend)
+    _error_on_invalid_preset(backend)
 
     alignment = storage_info["alignment"]
     layout_map = storage_info["layout_map"](dimensions)
+    assert allocators.is_valid_layout_map(layout_map)
 
     dtype = np.dtype(dtype)
     _, res = allocate_f(shape, layout_map, dtype, alignment * dtype.itemsize, aligned_index)
+
     return res
 
 
@@ -307,11 +323,9 @@ def from_array(
         ValueError
             If illegal or inconsistent arguments are specified.
     """
-    is_cupy_array = cp is not None and isinstance(data, cp.ndarray)
-    asarray = storage_utils.as_cupy if is_cupy_array else storage_utils.as_numpy
-    shape = asarray(data).shape
+    shape = storage_utils.asarray(data).shape
     if dtype is None:
-        dtype = asarray(data).dtype
+        dtype = storage_utils.asarray(data).dtype
     dtype = np.dtype(dtype)
     if dtype.shape:
         if dtype.shape and not shape[-dtype.ndim :] == dtype.shape:
@@ -325,87 +339,8 @@ def from_array(
         dimensions=dimensions,
     )
 
-    if cp is not None and isinstance(storage, cp.ndarray):
-        storage[...] = storage_utils.as_cupy(data)
-    else:
-        storage[...] = storage_utils.as_numpy(data)
+    layout_info = layout.from_name(backend)
+    assert layout_info is not None
+    storage[...] = storage_utils.asarray(data, device=layout_info["device"])
 
     return storage
-
-
-if dace is not None:
-
-    def dace_descriptor(
-        shape: Sequence[int],
-        dtype: DTypeLike = np.float64,
-        *,
-        backend: str,
-        aligned_index: Optional[Sequence[int]] = None,
-        dimensions: Optional[Sequence[str]] = None,
-    ) -> dace.data.Array:
-        """Return a DaCe data descriptor which describes performance-optimal strides and alignment.
-
-        Parameters
-        ----------
-            shape : `Sequence` of `int`
-                The shape of the resulting data descriptor.
-            dtype :  DTypeLike, optional
-                The dtype encoded in the resulting data descriptor.
-
-        Keyword Arguments
-        -----------------
-            backend : `str`
-                The target backend for which the described allocation is optimized.
-            aligned_index: `Sequence` of `int`, optional
-                Indicate the index of the resulting array that most commonly corresponds to the origin of the compute
-                domain. If not passed, it is aligned at the data origin.
-            dimensions: `Sequence` of `str`, optional
-                Indicate the semantic meaning of the dimensions in the data descriptor. Only used for determining
-                optimal strides, the information is not stored.
-
-        Returns
-        -------
-            DaCe data descriptor
-                With strides that encode padding and aligned to provide optimal performance for the given `backend` and
-                `aligned_index`
-
-        Raises
-        -------
-            TypeError
-                If arguments of an unexpected type are specified.
-            ValueError
-                If illegal or inconsistent arguments are specified.
-        """
-        aligned_index, shape, dtype, dimensions = storage_utils.normalize_storage_spec(
-            aligned_index, shape, dtype, dimensions
-        )
-        itemsize = dtype.itemsize
-        storage_info = layout.from_name(backend)
-        assert storage_info is not None
-        layout_map = storage_info["layout_map"](dimensions)
-
-        order_idx = storage_utils.idx_from_order([i for i in layout_map if i is not None])
-        padded_shape = storage_utils.compute_padded_shape(
-            shape, storage_info["alignment"], order_idx
-        )
-
-        strides = storage_utils.strides_from_padded_shape(padded_shape, order_idx, itemsize)
-
-        storage = (
-            dace.StorageType.GPU_Global
-            if storage_info["device"] == "gpu"
-            else dace.StorageType.CPU_Heap
-        )
-        start_offset = int(np.array([aligned_index]) @ np.array([strides]).T) // itemsize
-
-        total_size = int(int(np.array([shape]) @ np.array([strides]).T) // itemsize)
-
-        start_offset = start_offset % storage_info["alignment"]
-        return dace.data.Array(
-            shape=shape,
-            strides=[s // itemsize for s in strides],
-            dtype=dace.typeclass(str(dtype)),
-            storage=storage,
-            total_size=total_size,
-            start_offset=start_offset,
-        )
