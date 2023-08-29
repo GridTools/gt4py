26f683dfe0e1076e7b67150035ad5f0bdc3416de
Rico Häuselmann <ricoh@cscs.ch>
2023-08-29 10:56:08 +0200

    fix rename in gtfn_module tests

--- tests/next_tests/unit_tests/program_processor_tests/codegens_tests/gtfn_tests/test_gtfn_module.py
+++ tests/next_tests/unit_tests/program_processor_tests/codegens_tests/gtfn_tests/test_gtfn_module.py
@@ -65,7 +65,7 @@ def fencil_example():
 
 def test_codegen(fencil_example):
     fencil, parameters = fencil_example
-    module = gtfn_module.translate_program(
+    module = gtfn_module.translate_program_cpu(
         stages.ProgramCall(fencil, parameters, {"offset_provider": {}})
     )
     assert module.entry_point.name == fencil.id
