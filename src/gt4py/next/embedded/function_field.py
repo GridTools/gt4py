Fix broadcast

src/gt4py/next/embedded/function_field.py
@@ -72,15 +72,17 @@ class FunctionField(common.Field[common.DimsT, core_defs.ScalarT], common.FieldB
 
         if __debug__:
             try:
-                num_params = len(self.domain)
-                target_shape = tuple(1 for _ in range(num_params))
-                np.fromfunction(self.func, target_shape)
+                self._trigger_func()
             except Exception as e:
                 params = _get_params(self.func)
                 raise embedded_exceptions.FunctionFieldError(
                     self.__class__.__name__,
-                    f"Invariant violation: len(self.domain) ({num_params}) does not match the number of parameters of the provided function ({params})",
+                    f"Invariant violation: len(self.domain) ({len(self.domain)}) does not match the number of parameters of the provided function ({params})",
                 )
+
+    def _trigger_func(self):
+        target_shape = tuple(1 for _ in range(len(self.domain)))
+        return np.fromfunction(self.func, target_shape)
 
     @property
     def dtype(self) -> core_defs.DType[core_defs.ScalarT]:
@@ -94,6 +96,11 @@ class FunctionField(common.Field[common.DimsT, core_defs.ScalarT], common.FieldB
 
     @property
     def ndarray(self) -> core_defs.NDArrayObject | int | float:
+
+        # handle case where we have a constant FunctionField where field.ndarray is a scalar
+        if isinstance(self._trigger_func(), (int, float)) and not self.domain.is_finite():
+            return np.full(tuple(1 for _ in self.domain.shape), self.func())
+
         if not self.domain.is_finite():
             raise embedded_exceptions.InfiniteRangeNdarrayError(
                 self.__class__.__name__, self.domain

src/gt4py/next/embedded/nd_array_field.py
@@ -25,7 +25,7 @@ from numpy import typing as npt
 from gt4py._core import definitions as core_defs
 from gt4py.next import common
 from gt4py.next.embedded import common as embedded_common
-from gt4py.next.embedded.common import broadcast_domain, find_index_of_dim
+from gt4py.next.embedded.common import broadcast_domain, _compute_domain_slice
 from gt4py.next.ffront import fbuiltins
 
 
@@ -59,8 +59,8 @@ def _make_binary_array_field_intrinsic_func(builtin_name: str, array_builtin_nam
         if hasattr(b, "__gt_builtin_func__"):  # common.is_field(b):
             if not a.domain == b.domain:
                 domain_intersection = a.domain & b.domain
-                a_broadcasted = _broadcast(a, domain_intersection.dims)
-                b_broadcasted = _broadcast(b, domain_intersection.dims)
+                a_broadcasted = fbuiltins.broadcast(a, domain_intersection.dims)
+                b_broadcasted = fbuiltins.broadcast(b, domain_intersection.dims)
                 a_slices = _get_slices_from_domain_slice(a_broadcasted.domain, domain_intersection)
                 b_slices = _get_slices_from_domain_slice(b_broadcasted.domain, domain_intersection)
                 new_data = op(a_broadcasted.ndarray[a_slices], b_broadcasted.ndarray[b_slices])
@@ -336,14 +336,7 @@ if jnp:
 def _broadcast(field: common.Field, new_dimensions: tuple[common.Dimension, ...]) -> common.Field:
     domain_slice = _compute_domain_slice(field, new_dimensions)
     named_ranges = broadcast_domain(field, new_dimensions)
-    ndarray_ = field.ndarray
-
-    # handle case where we have a constant FunctionField where field.ndarray is a scalar
-    if isinstance(ndarray_, (int, float)):
-        shape = [len(rng) for rng in field.domain.ranges]
-        ndarray_ = np.full(shape, ndarray_)
-
-    return common.field(ndarray_[tuple(domain_slice)], domain=common.Domain(*named_ranges))
+    return common.field(field.ndarray[tuple(domain_slice)], domain=common.Domain(*named_ranges))
 
 
 def _builtins_broadcast(
@@ -414,15 +407,3 @@ def _compute_slice(
         return rng - domain.ranges[pos].start
     else:
         raise ValueError(f"Can only use integer or UnitRange ranges, provided type: {type(rng)}")
-
-
-def _compute_domain_slice(
-    field: common.Field, new_dimensions: tuple[common.Dimension, ...]
-) -> Sequence[slice | None]:
-    domain_slice: list[slice | None] = []
-    for dim in new_dimensions:
-        if find_index_of_dim(dim, field.domain) is not None:
-            domain_slice.append(slice(None))
-        else:
-            domain_slice.append(np.newaxis)
-    return domain_slice