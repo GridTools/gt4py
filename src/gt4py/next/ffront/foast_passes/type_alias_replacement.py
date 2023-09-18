1c196acb137164a004d8c20c0f2f24bf4b7d8e43
Nina Burgdorfer <Nina.Burgdorfer@meteoswiss.ch>
2023-09-18 16:48:07 +0200

    Address requested changes

--- src/gt4py/next/ffront/foast_passes/type_alias_replacement.py
+++ src/gt4py/next/ffront/foast_passes/type_alias_replacement.py
@@ -13,9 +13,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any
 
-from gt4py.eve.concepts import SourceLocation
+from gt4py.eve.concepts import SourceLocation, SymbolName, SymbolRef
 import gt4py.next.ffront.field_operator_ast as foast
 from gt4py.eve import NodeTranslator, traits
 from gt4py.next.ffront import dialect_ast_enums
@@ -26,7 +26,7 @@ from gt4py.next.type_system import type_specifications as ts
 @dataclass
 class TypeAliasReplacement(NodeTranslator, traits.VisitorWithSymbolTableTrait):
     """
-    Replace Type Aliases with their actual type
+    Replace Type Aliases with their actual type.
 
     After this pass, the type aliases used for explicit construction of literal
     values and for casting field values are replaced by their actual types.
@@ -37,51 +37,46 @@ class TypeAliasReplacement(NodeTranslator, traits.VisitorWithSymbolTableTrait):
     @classmethod
     def apply(cls, node: foast.FunctionDefinition, closure_vars: dict[str, Any]) -> tuple[
         foast.FunctionDefinition, dict[str, Any]]:
-        instance = cls(closure_vars=closure_vars)
-        foast_node = instance.visit(node)
-
-        for key, value in closure_vars.copy().items():
+        foast_node = cls(closure_vars=closure_vars).visit(node)
+        closures_copy = closure_vars.copy()
+        for key, value in closures_copy.items():
             if isinstance(value, type) and key not in TYPE_BUILTIN_NAMES:
                 closure_vars[value.__name__] = closure_vars.pop(key)
 
         return foast_node, closure_vars
 
-    def _get_actual_type_name(self, id: str) -> Optional[str]:
+    def _get_actual_type_name(self, id: SymbolName | SymbolRef) -> str | SymbolName | SymbolRef:
         if id in self.closure_vars and isinstance(self.closure_vars[id], type) and id not in TYPE_BUILTIN_NAMES:
             return self.closure_vars[id].__name__
-        return None
+        return id
 
     def visit_Name(self, node: foast.Name, **kwargs) -> foast.Name:
         actual_type_name = self._get_actual_type_name(node.id)
-        if actual_type_name:
-            node.id = actual_type_name
-        return foast.Name(id=node.id, location=node.location, type=node.type)
+        new_node_id = actual_type_name if not isinstance(actual_type_name, SymbolRef) else node.id
+        return foast.Name(id=new_node_id, location=node.location, type=node.type)
 
-    def _add_actual_type_to_closure_vars(self, var_id: str, location: SourceLocation) -> foast.Symbol:
-        actual_type_name = self._get_actual_type_name(var_id)
-        if actual_type_name:
+    def _add_actual_type_to_closure_vars(self, var: foast.Symbol, location: SourceLocation) -> foast.Symbol:
+        actual_type_name = self._get_actual_type_name(var.id)
+        if not isinstance(actual_type_name, SymbolName):
             return foast.Symbol(
                 id=actual_type_name,
                 type=ts.FunctionType(
                     pos_or_kw_args={},
                     kw_only_args={},
                     pos_only_args=[ts.DeferredType(constraint=ts.ScalarType)],
-                    returns=ts.ScalarType(kind=get_scalar_kind(self.closure_vars[var_id]))
+                    returns=ts.ScalarType(kind=get_scalar_kind(self.closure_vars[var.id]))
                 ),
                 namespace=dialect_ast_enums.Namespace.CLOSURE,
                 location=location
             )
-        return None
+        return var
 
     def visit_FunctionDefinition(self, node: foast.FunctionDefinition, **kwargs) -> foast.FunctionDefinition:
         new_closure_vars = []
 
         for var in node.closure_vars:
-            actual_type_symbol = self._add_actual_type_to_closure_vars(var.id, node.location)
-            if actual_type_symbol and actual_type_symbol not in new_closure_vars:
-                new_closure_vars.append(actual_type_symbol)
-            else:
-                new_closure_vars.append(var)  # Keep the variable as is
+            actual_type_symbol = self._add_actual_type_to_closure_vars(var, node.location)
+            new_closure_vars.append(actual_type_symbol)
 
         return foast.FunctionDefinition(
             id=node.id,
--- tests/next_tests/unit_tests/ffront_tests/foast_passes_tests/test_type_alias_replacement.py
+++ tests/next_tests/unit_tests/ffront_tests/foast_passes_tests/test_type_alias_replacement.py
@@ -12,6 +12,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import pytest
 from typing import TypeAlias
 import typing
 
@@ -19,7 +20,7 @@ import ast
 import gt4py.next as gtx
 from gt4py.next.ffront.fbuiltins import astype
 from gt4py.next.ffront.func_to_foast import FieldOperatorParser
-from gt4py.next.ffront.dialect_parser import parse_source_definition
+from gt4py.next.ffront.dialect_parser import parse_source_definition, DialectParser
 from gt4py.next import float32, float64
 from gt4py.next.ffront.foast_passes.type_alias_replacement import TypeAliasReplacement
 from gt4py.next.ffront.ast_passes.fix_missing_locations import FixMissingLocations
@@ -33,79 +34,19 @@ from gt4py.next.ffront.ast_passes import (
 )
 
 TDim = gtx.Dimension("TDim")  # Meaningless dimension, used for tests.
+vpfloat: TypeAlias = float32
+wpfloat: TypeAlias = float64
 
-def test_type_alias_replacement_vpfloat():
-    vpfloat: TypeAlias = float32
+@pytest.mark.parametrize("test_input,expected", [(vpfloat, "float32"), (wpfloat, "float64")])
+def test_type_alias_replacement(test_input, expected):
 
- #  Parse a function 
-    def fieldOp_with_TypeAlias(a: gtx.Field[[TDim], vpfloat]) -> gtx.Field[[TDim], vpfloat] :
-        return vpfloat("3.1418") + astype(a, vpfloat)
-    
-    # Steps of apply_to_function() from DialectParser
-    src = SourceDefinition.from_function(fieldOp_with_TypeAlias)
-    closure_vars = get_closure_vars_from_function(fieldOp_with_TypeAlias)
-    annotations = typing.get_type_hints(fieldOp_with_TypeAlias)
+    # Parse a function 
+    def fieldOp_with_TypeAlias(a: gtx.Field[[TDim], test_input], b: gtx.Field[[TDim], float32]) -> gtx.Field[[TDim], test_input] :
+        return test_input("3.1418") + astype(a, test_input)
 
-    # Steps of apply() from DialectParser
-    definition_ast: ast.AST
-    definition_ast = parse_source_definition(src)
-    definition_ast = RemoveDocstrings.apply(definition_ast)
-    definition_ast = FixMissingLocations.apply(definition_ast)
-
-    # Steps of _preprocess_definition_ast() from FieldOperatorParser
-    sta = StringifyAnnotationsPass.apply(definition_ast)
-    ssa = SingleStaticAssignPass.apply(sta)
-    sat = SingleAssignTargetPass.apply(ssa)
-    ucc = UnchainComparesPass.apply(sat)
-
-    # Instance creation
-    foast_node = FieldOperatorParser(source_definition=src,
-                                     closure_vars=closure_vars,
-                                     annotations=annotations)
-    
-    # Apply TypeAliasReplacement (func to test)
-    foast_node = TypeAliasReplacement.apply(foast_node.visit(ucc), closure_vars)[0]
-   # foast_node = FieldOperatorTypeDeduction.apply(foast_node)
+    foast_tree = FieldOperatorParser.apply_to_function(fieldOp_with_TypeAlias)
 
     assert (
-            foast_node.body.stmts[0].value.left.func.id == "float32" and
-            foast_node.body.stmts[0].value.right.args[1].id == "float32"
-    )
-
-def test_type_alias_replacement_wpfloat():
-    wpfloat: TypeAlias = float64
-
-    #  Parse a function
-    def fieldOp_with_TypeAlias(a: gtx.Field[[TDim], wpfloat]) -> gtx.Field[[TDim], wpfloat]:
-        return wpfloat("3.1418") + astype(a, wpfloat)
-
-    # Steps of apply_to_function() from DialectParser
-    src = SourceDefinition.from_function(fieldOp_with_TypeAlias)
-    closure_vars = get_closure_vars_from_function(fieldOp_with_TypeAlias)
-    annotations = typing.get_type_hints(fieldOp_with_TypeAlias)
-
-    # Steps of apply() from DialectParser
-    definition_ast: ast.AST
-    definition_ast = parse_source_definition(src)
-    definition_ast = RemoveDocstrings.apply(definition_ast)
-    definition_ast = FixMissingLocations.apply(definition_ast)
-
-    # Steps of _preprocess_definition_ast() from FieldOperatorParser
-    sta = StringifyAnnotationsPass.apply(definition_ast)
-    ssa = SingleStaticAssignPass.apply(sta)
-    sat = SingleAssignTargetPass.apply(ssa)
-    ucc = UnchainComparesPass.apply(sat)
-
-    # Instance creation
-    foast_node = FieldOperatorParser(source_definition=src,
-                                     closure_vars=closure_vars,
-                                     annotations=annotations)
-
-    # Apply TypeAliasReplacement (func to test)
-    foast_node = TypeAliasReplacement.apply(foast_node.visit(ucc), closure_vars)[0]
-    # foast_node = FieldOperatorTypeDeduction.apply(foast_node)
-
-    assert (
-            foast_node.body.stmts[0].value.left.func.id == "float64" and
-            foast_node.body.stmts[0].value.right.args[1].id == "float64"
+            foast_tree.body.stmts[0].value.left.func.id == expected and
+            foast_tree.body.stmts[0].value.right.args[1].id == expected
     )
