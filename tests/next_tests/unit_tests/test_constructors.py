bfbc65cd35d0dd6e41e018b94b5658f9dad3101b
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2023-11-14 17:28:14 +0100

    Address open reviewer's comments in test_constructors

--- tests/next_tests/unit_tests/test_constructors.py
+++ tests/next_tests/unit_tests/test_constructors.py
@@ -30,6 +30,88 @@ K = gtx.Dimension("K")
 sizes = {I: 10, J: 10, K: 10}
 
 
+# TODO: parametrize with gpu backend and compare with cupy array
+@pytest.mark.parametrize(
+    "allocator, device",
+    [
+        [next_allocators.StandardCPUFieldBufferAllocator(), None],
+        [None, core_defs.Device(core_defs.DeviceType.CPU, 0)],
+    ],
+)
+def test_empty(allocator, device):
+    a = np.empty([sizes[I], sizes[J]]).astype(gtx.float32)
+    ref = gtx.empty(
+        domain={I: range(sizes[I]), J: range(sizes[J])},
+        dtype=core_defs.dtype(np.float32),
+        allocator=allocator,
+        device=device,
+    )
+    assert ref.shape, a.shape
+
+
+# TODO: parametrize with gpu backend and compare with cupy array
+@pytest.mark.parametrize(
+    "allocator, device",
+    [
+        [next_allocators.StandardCPUFieldBufferAllocator(), None],
+        [None, core_defs.Device(core_defs.DeviceType.CPU, 0)],
+    ],
+)
+def test_zeros(allocator, device):
+    ref = gtx.zeros(
+        common.Domain(
+            dims=(I, J), ranges=(common.UnitRange(0, sizes[I]), common.UnitRange(0, sizes[J]))
+        ),
+        dtype=core_defs.dtype(np.float32),
+        allocator=allocator,
+        device=device,
+    )
+    a = np.zeros((sizes[I], sizes[J])).astype(gtx.float32)
+
+    assert np.array_equal(ref.ndarray, a)
+
+
+# TODO: parametrize with gpu backend and compare with cupy array
+@pytest.mark.parametrize(
+    "allocator, device",
+    [
+        [next_allocators.StandardCPUFieldBufferAllocator(), None],
+        [None, core_defs.Device(core_defs.DeviceType.CPU, 0)],
+    ],
+)
+def test_ones(allocator, device):
+    ref = gtx.ones(
+        common.Domain(dims=(I, J), ranges=(common.UnitRange(0, 10), common.UnitRange(0, 10))),
+        dtype=core_defs.dtype(np.float32),
+        allocator=allocator,
+        device=device,
+    )
+    a = np.ones((sizes[I], sizes[J])).astype(gtx.float32)
+
+    assert np.array_equal(ref.ndarray, a)
+
+
+# TODO: parametrize with gpu backend and compare with cupy array
+@pytest.mark.parametrize(
+    "allocator, device",
+    [
+        [next_allocators.StandardCPUFieldBufferAllocator(), None],
+        [None, core_defs.Device(core_defs.DeviceType.CPU, 0)],
+    ],
+)
+def test_full(allocator, device):
+    ref = gtx.full(
+        domain={I: range(sizes[I] - 2), J: (sizes[J] - 2)},
+        fill_value=42.0,
+        dtype=core_defs.dtype(np.float32),
+        allocator=allocator,
+        device=device,
+    )
+    a = np.full((sizes[I] - 2, sizes[J] - 2), 42.0).astype(gtx.float32)
+
+    assert np.array_equal(ref.ndarray, a)
+
+
 def test_as_field():
     a = np.random.rand(sizes[I]).astype(gtx.float32)
     ref = gtx.as_field([I], a)
@@ -53,9 +135,7 @@ def test_as_field_origin():
     assert np.allclose(domain_range, [(-1, 9), (-2, 8)])
 
 
-# for as_field, check that the domain is correct depending on data origin and domain itself
-
-
+# check that `as_field()` domain is correct depending on data origin and domain itself
 def test_field_wrong_dims():
     with pytest.raises(
         ValueError,
@@ -93,72 +173,3 @@ def test_field_wrong_origin():
 @pytest.mark.xfail(reason="aligned_index not supported yet")
 def test_aligned_index():
     gtx.as_field([I], np.random.rand(sizes[I]).astype(gtx.float32), aligned_index=[I, 0])
-
-
-@pytest.mark.parametrize(
-    "allocator, device",
-    [[roundtrip.backend, None], [None, core_defs.Device(core_defs.DeviceType.CPU, 0)]],
-)
-def test_empty(allocator, device):
-    a = np.empty([sizes[I], sizes[J]]).astype(gtx.float32)
-    ref = gtx.empty(
-        domain={I: range(sizes[I]), J: range(sizes[J])},
-        dtype=core_defs.dtype(np.float32),
-        allocator=allocator,
-        device=device,
-    )
-    assert ref.shape, a.shape
-
-
-@pytest.mark.parametrize(
-    "allocator, device",
-    [[roundtrip.backend, None], [None, core_defs.Device(core_defs.DeviceType.CPU, 0)]],
-)
-def test_zeros(allocator, device):
-    ref = gtx.zeros(
-        common.Domain(
-            dims=(I, J), ranges=(common.UnitRange(0, sizes[I]), common.UnitRange(0, sizes[J]))
-        ),
-        dtype=core_defs.dtype(np.float32),
-        allocator=allocator,
-        device=device,
-    )
-    a = np.zeros((sizes[I], sizes[J])).astype(gtx.float32)
-
-    assert np.array_equal(ref.ndarray, a)
-
-
-# parametrize with gpu backend and compare with cupy array
-
-
-@pytest.mark.parametrize(
-    "allocator, device",
-    [[roundtrip.backend, None], [None, core_defs.Device(core_defs.DeviceType.CPU, 0)]],
-)
-def test_ones(allocator, device):
-    ref = gtx.ones(
-        common.Domain(dims=(I, J), ranges=(common.UnitRange(0, 10), common.UnitRange(0, 10))),
-        dtype=core_defs.dtype(np.float32),
-        allocator=allocator,
-        device=device,
-    )
-    a = np.ones((sizes[I], sizes[J])).astype(gtx.float32)
-
-    assert np.array_equal(ref.ndarray, a)
-
-
-@pytest.mark.parametrize(
-    "allocator, device",
-    [[roundtrip.backend, None], [None, core_defs.Device(core_defs.DeviceType.CPU, 0)]],
-)
-def test_full(allocator, device):
-    ref = gtx.full(
-        domain={I: range(sizes[I] - 2), J: (sizes[J] - 2)},
-        fill_value=42.0,
-        dtype=core_defs.dtype(np.float32),
-        allocator=allocator,
-        device=device,
-    )
-    a = np.full((sizes[I] - 2, sizes[J] - 2), 42.0).astype(gtx.float32)
-
-    assert np.array_equal(ref.ndarray, a)
