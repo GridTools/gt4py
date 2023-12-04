a901f807ee6b61c48c17176d0bda8090646affe4
Edoardo Paone <edoardo.paone@cscs.ch>
2023-12-04 12:04:10 +0100

    [dace] Fix for deref builtin

--- src/gt4py/next/program_processors/runners/dace_iterator/itir_to_tasklet.py
+++ src/gt4py/next/program_processors/runners/dace_iterator/itir_to_tasklet.py
@@ -306,11 +306,8 @@ def builtin_can_deref(
         )
         return [ValueExpr(result_node, dace.dtypes.bool)]
 
-    # this iterator is accessing a neighbor table, so it should return an index
-    assert iterator.dtype in dace.dtypes.INTEGER_TYPES
-
     # create tasklet to check that field indices are non-negative (-1 is invalid)
-    args = [ValueExpr(access_node, iterator.dtype) for access_node in iterator.indices.values()]
+    args = [ValueExpr(access_node, _INDEX_DTYPE) for access_node in iterator.indices.values()]
     internals = [f"{arg.value.data}_v" for arg in args]
     expr_code = " and ".join([f"{v} >= 0" for v in internals])
 
