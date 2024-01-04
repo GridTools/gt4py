38c052c42a01c30c2e71ca17fabe01127a7af460
nfarabullini <nicoletta.farabullini@c2sm.ethz.ch>
2024-01-04 10:19:43 +0100

    ran pre-commit

--- src/gt4py/next/ffront/experimental.py
+++ src/gt4py/next/ffront/experimental.py
@@ -25,7 +25,7 @@ def as_offset(
     /,
 ) -> common.ConnectivityField:
     global_index_arr = np.arange(field.ndarray.shape[0]) + field.ndarray
-    return common.connectivity(global_index_arr, codomain=offset_, domain=field.domain)
+    return common.connectivity(global_index_arr, codomain=offset_.source, domain=field.domain)
 
 
 EXPERIMENTAL_FUN_BUILTIN_NAMES = ["as_offset"]
