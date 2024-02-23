57391e217392932154c093c462654befa20b87d8
Edoardo Paone <edoardo.paone@cscs.ch>
2024-02-24 00:38:42 +0100

    [dace] Remove empty states

--- src/gt4py/next/program_processors/runners/dace_iterator/itir_to_tasklet.py
+++ src/gt4py/next/program_processors/runners/dace_iterator/itir_to_tasklet.py
@@ -660,12 +660,24 @@ def builtin_if(
                     )
             result_values.append(ValueExpr(current_state.add_access(var), desc.dtype))
 
-    # if all branches are symbolic expressions, the true/false states can be removed
-    # as well as the conditional state transition
     if tbr_state.is_empty() and fbr_state.is_empty():
+        # if all branches are symbolic expressions, the true/false states can be removed
+        # as well as the conditional state transition
         sdfg.remove_nodes_from([tbr_state, fbr_state])
     else:
-        sdfg.remove_edge(sdfg.edges_between(stmt_state, join_state)[0])
+        if tbr_state.is_empty():
+            sdfg.edges_between(stmt_state, join_state)[0].condition = sdfg.edges_between(
+                stmt_state, tbr_state
+            )[0].condition
+            sdfg.remove_node(tbr_state)
+        elif fbr_state.is_empty():
+            sdfg.edges_between(stmt_state, join_state)[0].condition = sdfg.edges_between(
+                stmt_state, fbr_state
+            )[0].condition
+            sdfg.remove_node(fbr_state)
+        else:
+            sdfg.remove_edge(sdfg.edges_between(stmt_state, join_state)[0])
+        # the if-statement condition is not used in current state
         current_state.remove_node(ctx_stmt_node.value)
 
     return result_values
