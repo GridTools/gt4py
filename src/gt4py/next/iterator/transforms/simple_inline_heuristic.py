38c60aeff8d7d959ccda28b6ca05b5f819d3c5ba
Till Ehrengruber <till.ehrengruber@cscs.ch>
2024-03-05 23:53:03 +0100

    Fix format

--- src/gt4py/next/iterator/transforms/simple_inline_heuristic.py
+++ src/gt4py/next/iterator/transforms/simple_inline_heuristic.py
@@ -38,7 +38,7 @@ def is_eligible_for_inlining(arg: ir.FunCall, is_scan_pass_context: bool) -> boo
 
     assert isinstance(arg.fun, ir.FunCall)  # for mypy
     (stencil,) = arg.fun.args
-    # Don’t inline scans, i.e. exclude `↑(scan(...))(...)`
+    # Do not inline scans, i.e. exclude `↑(scan(...))(...)`
     if isinstance(stencil, ir.FunCall) and stencil.fun == ir.SymRef(id="scan"):
         return False
 
