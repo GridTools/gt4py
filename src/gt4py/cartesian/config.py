704df316fdf05e8a3896d4293223b0e490b87191
Florian Deconinck <deconinck.florian@gmail.com>
2024-03-20 12:34:16 -0400

    Update src/gt4py/cartesian/config.py
    Co-authored-by: Hannes Vogt <hannes@havogt.de>

--- src/gt4py/cartesian/config.py
+++ src/gt4py/cartesian/config.py
@@ -87,6 +87,6 @@ os.environ.setdefault("DACE_CONFIG", os.path.join(os.path.abspath("."), ".dace.c
 
 DACE_DEFAULT_BLOCK_SIZE: str = os.environ.get("DACE_DEFAULT_BLOCK_SIZE", "64,8,1")
 
-# Deerecate by default the gtc `cuda` backend. Use GT4PY_GTC_CUDA_USE=1 to turn it on
+# Deprecate by default the gtc `cuda` backend. Use GT4PY_GTC_CUDA_USE=1 to turn it on
 # at user own risks.
 GT4PY_GTC_CUDA_USE = bool(int(os.environ.get("GT4PY_GTC_CUDA_USE", "0")))
