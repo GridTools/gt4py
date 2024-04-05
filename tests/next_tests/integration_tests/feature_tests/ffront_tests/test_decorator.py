f4b1a4ea2fca2bbb37a723b39b2e6aa5d735d8c1
DropD <rico.haeuselmann@gmail.com>
2024-04-05 15:49:28 +0200

    add regression test for Program.itir

---
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/test_decorator.py
@@ -0,0 +1,35 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2023, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from gt4py import next as gtx
+from gt4py.next.iterator import ir as itir
+
+from next_tests.integration_tests import cases
+from next_tests.integration_tests.cases import cartesian_case
+from next_tests.integration_tests.feature_tests.ffront_tests.ffront_test_utils import (
+    exec_alloc_descriptor,
+)
+
+
+def test_program_itir_regression(cartesian_case):
+    @gtx.field_operator(backend=None)
+    def testee_op(a: cases.IField) -> cases.IField:
+        return a
+
+    @gtx.program(backend=None)
+    def testee(a: cases.IField, out: cases.IField):
+        testee_op(a, out=out)
+
+    assert isinstance(testee.itir, itir.FencilDefinition)
+    assert isinstance(testee.with_backend(cartesian_case.executor).itir, itir.FencilDefinition)
