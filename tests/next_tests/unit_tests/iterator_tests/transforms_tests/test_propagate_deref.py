2c06bc668aaacb21075fd39b179985d9e7172ae3
Till Ehrengruber <till.ehrengruber@cscs.ch>
2024-04-18 14:48:37 +0200

    refactor[next]: Use is_call_to instead of equality comparison with itir.Ref. (#1532)
    In the new ITIR type inference #1531 IR nodes store their type in the node itself. While we initially exclude the attribute from equality comparison we should nonetheless avoid comparison of nodes that only differ in type. This PR removes many of this occurrences.

--- tests/next_tests/unit_tests/iterator_tests/transforms_tests/test_propagate_deref.py
+++ tests/next_tests/unit_tests/iterator_tests/transforms_tests/test_propagate_deref.py
@@ -16,9 +16,17 @@ from gt4py.next.iterator.ir_utils import ir_makers as im
 from gt4py.next.iterator.transforms.propagate_deref import PropagateDeref
 
 
-def test_deref_propagation():
+def test_deref_let_propagation():
     testee = im.deref(im.call(im.lambda_("inner_it")(im.lift("stencil")("inner_it")))("outer_it"))
     expected = im.call(im.lambda_("inner_it")(im.deref(im.lift("stencil")("inner_it"))))("outer_it")
+
+    actual = PropagateDeref.apply(testee)
+    assert actual == expected
+
+
+def test_deref_if_propagation():
+    testee = im.deref(im.if_("cond", "true_branch", "false_branch"))
+    expected = im.if_("cond", im.deref("true_branch"), im.deref("false_branch"))
 
     actual = PropagateDeref.apply(testee)
     assert actual == expected
