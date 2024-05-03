ad54e4242e10ea8a8fbb582934bd335a4baa97bd
DropD <rico.haeuselmann@gmail.com>
2024-05-03 14:53:36 +0200

    remove cuda from tested backends altogether, try instafail

--- tests/cartesian_tests/unit_tests/backend_tests/test_backend.py
+++ tests/cartesian_tests/unit_tests/backend_tests/test_backend.py
@@ -176,7 +176,7 @@ def test_toolchain_profiling(backend_name: str, mode: int, rebuild: bool):
 @pytest.fixture
 def enable_cuda_not_set():
     value = os.environ.pop("GT4PY_GTC_ENABLE_CUDA", None)
-    yield
+    yield "GT4PY_GTC_ENABLE_CUDA" not in os.environ
     if value:
         os.environ["GT4PY_GTC_ENABLE_CUDA"] = value
 
