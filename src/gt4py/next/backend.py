6a3c93cdf421bcd8a75c68e4e793dff5cdb5a19a
DropD <rico.haeuselmann@gmail.com>
2024-06-13 10:24:29 +0200

    make past_to_itir cached in default transforms

--- src/gt4py/next/backend.py
+++ src/gt4py/next/backend.py
@@ -73,7 +73,11 @@ class FieldopTransformWorkflow(workflow.NamedStepSequenceWithArgs):
         dataclasses.field(default=past_process_args.past_process_args)
     )
     past_to_itir: workflow.Workflow[ffront_stages.PastClosure, stages.ProgramCall] = (
-        dataclasses.field(default_factory=past_to_itir.PastToItirFactory)
+        dataclasses.field(
+            default_factory=lambda: workflow.CachedStep(
+                past_to_itir.PastToItirFactory(), hash_function=ffront_stages.fingerprint_stage
+            )
+        )
     )
 
     foast_to_itir: workflow.Workflow[ffront_stages.FoastOperatorDefinition, itir.Expr] = (
@@ -129,7 +133,11 @@ class ProgramTransformWorkflow(workflow.NamedStepSequenceWithArgs):
         )
     )
     past_to_itir: workflow.Workflow[ffront_stages.PastClosure, stages.ProgramCall] = (
-        dataclasses.field(default_factory=past_to_itir.PastToItirFactory)
+        dataclasses.field(
+            default_factory=lambda: workflow.CachedStep(
+                past_to_itir.PastToItirFactory(), hash_function=ffront_stages.fingerprint_stage
+            )
+        )
     )
 
 
@@ -172,4 +180,5 @@ class Backend(Generic[core_defs.DeviceTypeT]):
     def __gt_allocator__(
         self,
     ) -> next_allocators.FieldBufferAllocatorProtocol[core_defs.DeviceTypeT]:
+        return self.allocator
         return self.allocator
