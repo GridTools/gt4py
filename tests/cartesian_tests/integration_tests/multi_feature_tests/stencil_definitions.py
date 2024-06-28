615ddcb551e265c28642ca92ff4e590e200befeb
Edoardo Paone <edoardo.paone@cscs.ch>
2024-06-28 15:51:54 +0200

    Disable cartesian data_type test for int8

--- tests/cartesian_tests/integration_tests/multi_feature_tests/stencil_definitions.py
+++ tests/cartesian_tests/integration_tests/multi_feature_tests/stencil_definitions.py
@@ -83,13 +83,12 @@ def arithmetic_ops(field_a: Field3D, field_b: Field3D):
     with computation(PARALLEL), interval(...):
         field_a = (((((field_b + 42.0) - 42.0) * +42.0) / -42.0) % 42.0) ** 2
 
-
 @register
 def data_types(
     bool_field: gtscript.Field[bool],
-    # npbool_field: gtscript.Field[np.bool_],
+    npbool_field: gtscript.Field[np.bool_],
     int_field: gtscript.Field[int],
-    int8_field: gtscript.Field[np.int8],
+#   int8_field: gtscript.Field[np.int8],
     int16_field: gtscript.Field[np.int16],
     int32_field: gtscript.Field[np.int32],
     int64_field: gtscript.Field[np.int64],
@@ -99,9 +98,9 @@ def data_types(
 ):
     with computation(PARALLEL), interval(...):
         bool_field = True
-        # npbool_field = False
+        npbool_field = False
         int_field = 2147483647
-        int8_field = 127
+#       int8_field = 127
         int16_field = 32767
         int32_field = 2147483647
         int64_field = 9223372036854775807
