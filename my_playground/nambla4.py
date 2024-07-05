74bd468199c07e4d201334bae8737f74fb1d73f1
Philip Mueller <philip.mueller@cscs.ch>
2024-07-05 09:58:52 +0200

    Updated it now seems to work.

--- my_playground/nambla4.py
+++ my_playground/nambla4.py
@@ -87,26 +87,26 @@ def make_syms(**kwargs: np.ndarray) -> dict[str, int]:
 
 def build_nambla4_gtir():
     edge_k_domain = im.call("unstructured_domain")(
-        im.call("named_range")(itir.AxisLiteral(value=Edge.value), 0, "num_edges"),
-        im.call("named_range")(itir.AxisLiteral(value=KDim.value), 0, "num_k_levels"),
-    )
-    edge_k_domain = im.call("cartesian_domain")(
-        im.call("named_range")(itir.AxisLiteral(value=Edge.value), 0, "num_edges"),
-        im.call("named_range")(itir.AxisLiteral(value=KDim.value), 0, "num_k_levels"),
+        im.call("named_range")(itir.AxisLiteral(value=Edge.value, kind=Edge.kind), 0, "num_edges"),
+        im.call("named_range")(
+            itir.AxisLiteral(value=KDim.value, kind=KDim.kind), 0, "num_k_levels"
+        ),
     )
 
     num_edges = 27
     num_k_levels = 10
 
+    EK_FTYPE = ts.FieldType(dims=[Edge, KDim], dtype=wpfloat)
+
     nabla4prog = itir.Program(
         id="nabla4_partial",
         function_definitions=[],
         params=[
-            itir.Sym(id="T"),
-            itir.Sym(id="N"),
-            itir.Sym(id="nab4"),
-            itir.Sym(id="num_edges"),
-            itir.Sym(id="num_k_levels"),
+            itir.Sym(id="T", type=EK_FTYPE),
+            itir.Sym(id="N", type=EK_FTYPE),
+            itir.Sym(id="nab4", type=EK_FTYPE),
+            itir.Sym(id="num_edges", type=SIZE_TYPE),
+            itir.Sym(id="num_k_levels", type=SIZE_TYPE),
         ],
         declarations=[],
         body=[
@@ -123,22 +123,13 @@ def build_nambla4_gtir():
         ],
     )
 
-    EK_FTYPE = ts.FieldType(dims=[Edge, KDim], dtype=wpfloat)
-
-    arg_types = [
-        EK_FTYPE,
-        EK_FTYPE,
-        EK_FTYPE,
-        SIZE_TYPE,
-        SIZE_TYPE,
-    ]
     offset_provider = {}
 
     N = np.random.rand(num_edges, num_k_levels)
-    T = np.random.rand(num_k_levels)
+    T = np.random.rand(num_edges, num_k_levels)
     nab4 = np.empty_like(N)
 
-    sdfg = dace_backend.build_sdfg_from_gtir(nabla4prog, arg_types, offset_provider)
+    sdfg = dace_backend.build_sdfg_from_gtir(nabla4prog, offset_provider)
 
     call_args = dict(
         T=T,
@@ -153,6 +144,7 @@ def build_nambla4_gtir():
     ref = nabla4_np(**call_args)
 
     assert np.allclose(ref, nab4)
+    print(f"Test succeeded")
 
 
 if "__main__" == __name__:
