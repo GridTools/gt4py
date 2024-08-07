d603812731affc3d8042a7057b2e3a102591649b
Edoardo Paone <edoardo.paone@cscs.ch>
2024-08-07 10:02:21 +0200

    edit

--- tests/next_tests/integration_tests/feature_tests/ffront_tests/ffront_test_utils.py
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/ffront_test_utils.py
@@ -93,7 +93,7 @@ def exec_alloc_descriptor(request) -> Generator[ppi.ProgramProcessor, None, None
     if xdist and request.node.get_closest_marker(pytest.mark.requires_gpu.name):
         import cupy
         num_gpu_devices = cupy.cuda.runtime.getDeviceCount()
-        if num_gpu_devices > 0 and xdist.is_xdist_worker(request):
+        if num_gpu_devices > 1 and xdist.is_xdist_worker(request):
             wid = xdist.get_xdist_worker_id(request)
             wid_stripped = re.search(r".*(\d+)", wid).group(1)
 
