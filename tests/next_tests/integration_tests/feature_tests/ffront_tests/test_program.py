f18e4d605974c63ed26de04d28c781a0a40a32bb
Hannes Vogt <hannes@havogt.de>
2024-08-13 14:39:02 +0200

    License Change to BSD-3-Clause (#1613)
    GT4Py is now licensed under the BSD license. SPDX-License-Identifier:
    BSD-3-Clause
    
    Please, refer to the LICENSE file in the root directory.
    
    Contributors to GT4Py are **not required** to sign the GridTools
    Contributor Assignment Agreement.

--- tests/next_tests/integration_tests/feature_tests/ffront_tests/test_program.py
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/test_program.py
@@ -1,16 +1,10 @@
 # GT4Py - GridTools Framework
 #
-# Copyright (c) 2014-2023, ETH Zurich
+# Copyright (c) 2014-2024, ETH Zurich
 # All rights reserved.
 #
-# This file is part of the GT4Py project and the GridTools framework.
-# GT4Py is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or any later
-# version. See the LICENSE.txt file at the top-level directory of this
-# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
 
 # TODO(tehrengruber): All field operators and programs should be executable
 #  as is at some point. Adopt tests to also run on the regular python objects.
