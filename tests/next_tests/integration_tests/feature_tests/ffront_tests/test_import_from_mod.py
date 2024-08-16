5a93b0fedb462d5f22fccc1fe8f87d0e418943e5
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2024-08-16 12:02:37 +0200

    edits to test to include all backends

--- tests/next_tests/integration_tests/feature_tests/ffront_tests/test_import_from_mod.py
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/test_import_from_mod.py
@@ -10,18 +10,27 @@ from tests.next_tests.integration_tests.feature_tests.ffront_tests import dimens
 from tests.next_tests.integration_tests.cases import IDim, KDim
 import gt4py.next as gtx
 import numpy as np
+from tests.next_tests.integration_tests import cases
+from tests.next_tests.integration_tests.cases import cartesian_case
+
+from tests.next_tests.integration_tests.feature_tests.ffront_tests.ffront_test_utils import (
+    exec_alloc_descriptor,
+    mesh_descriptor,
+)
 
 
-def test_import_dims_module():
+def test_import_dims_module(cartesian_case):
     @gtx.field_operator
-    def mod_op(f: gtx.Field[[IDim, KDim], float]) -> gtx.Field[[IDim, KDim], float]:
+    def mod_op(f: cases.IKField) -> cases.IKField:
         return f
 
     @gtx.program
-    def mod_prog(f: gtx.Field[[IDim, KDim], float], out: gtx.Field[[IDim, KDim], float]):
+    def mod_prog(f: cases.IKField, out: cases.IKField):
         mod_op(f, out=out, domain={dims.IDim: (0, 8), dims.KDim: (0, 3)})
 
-    f = gtx.as_field([IDim, KDim], np.ones([10, 10]))
-    out = gtx.as_field([IDim, KDim], np.zeros([10, 10]))
-    mod_prog(f, out, offset_provider={})
-    assert np.allclose(out.asnumpy()[0:8, 0:3], f.asnumpy()[0:8, 0:3])
+    f = cases.allocate(cartesian_case, mod_prog, "f")()
+    out = cases.allocate(cartesian_case, mod_prog, "out")()
+    expected = np.zeros_like(f.asnumpy())
+    expected[0:8, 0:3] = f.asnumpy()[0:8, 0:3]
+
+    cases.verify(cartesian_case, mod_prog, f, out=out, ref=expected)
