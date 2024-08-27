8bef26b681267568655d8808b10009e9ed756317
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2024-08-27 11:06:24 +0200

    edit to include dimension module import in field_operator

--- tests/next_tests/integration_tests/feature_tests/ffront_tests/test_import_from_mod.py
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/test_import_from_mod.py
@@ -16,15 +16,17 @@ from next_tests.integration_tests.feature_tests.ffront_tests.ffront_test_utils i
     mesh_descriptor,
 )
 from next_tests.integration_tests.feature_tests import ffront_tests
+from gt4py.next import broadcast
 
 
 def test_import_dims_module(cartesian_case):
     @gtx.field_operator
-    def mod_op(f: cases.IKField) -> cases.IKField:
-        return f
+    def mod_op(f: cases.IField) -> cases.IKField:
+        f_i_k = broadcast(f, (cases.IDim, cases.KDim))
+        return f_i_k
 
     @gtx.program
-    def mod_prog(f: cases.IKField, out: cases.IKField):
+    def mod_prog(f: cases.IField, out: cases.IKField):
         mod_op(
             f,
             out=out,
@@ -36,7 +38,9 @@ def test_import_dims_module(cartesian_case):
 
     f = cases.allocate(cartesian_case, mod_prog, "f")()
     out = cases.allocate(cartesian_case, mod_prog, "out")()
-    expected = np.zeros_like(f.asnumpy())
-    expected[0:8, 0:3] = f.asnumpy()[0:8, 0:3]
+    expected = np.zeros_like(out.asnumpy())
+    expected[0:8, 0:3] = np.reshape(np.repeat(f.asnumpy(), out.shape[1], axis=0), out.shape)[
+        0:8, 0:3
+    ]
 
     cases.verify(cartesian_case, mod_prog, f, out=out, ref=expected)
