daec4cb3c993aead500c8009c880310ccde49cae
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2024-09-02 16:59:08 +0200

    Add custom pytest option to vaoid returning an exit code error if the collection of tests is empty.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,5 +8,24 @@
 
 """Global configuration of pytest for collecting and running tests."""
 
+import pytest
+
+
 # Ignore hidden folders and disabled tests
 collect_ignore_glob = [".*", "_disabled*"]
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("This project")
+    group.addoption(
+        "--ignore-no-tests-collected",
+        action="store_true",
+        default=False,
+        help='Suppress the "no tests were collected" exit code.',
+    )
+
+
+def pytest_sessionfinish(session, exitstatus):
+    if session.config.getoption("--ignore-no-tests-collected"):
+        if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
+            session.exitstatus = pytest.ExitCode.OK
