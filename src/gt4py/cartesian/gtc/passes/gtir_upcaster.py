a70a46c940ab550964215c81577606f9879e8982
Florian Deconinck <deconinck.florian@gmail.com>
2024-10-24 16:34:56 -0400

    Native function: f32, f64, round

--- src/gt4py/cartesian/gtc/passes/gtir_upcaster.py
+++ src/gt4py/cartesian/gtc/passes/gtir_upcaster.py
@@ -105,7 +105,7 @@ class _GTIRUpcasting(eve.NodeTranslator):
 
     def visit_NativeFuncCall(self, node: gtir.NativeFuncCall, **kwargs: Any) -> gtir.NativeFuncCall:
         # Skip upcasting for cast to int
-        if node.func == NativeFunction.INT:
+        if node.func in [NativeFunction.INT, NativeFunction.F32, NativeFunction.F64]:
             return node
         upcasting_rule = functools.partial(
             _numpy_ufunc_upcasting_rule, ufunc=op_to_ufunc(node.func)
