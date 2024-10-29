b4ab4f4f6b497be40e6ccc65d57c024429fb34c2
Hannes Vogt <hannes@havogt.de>
2024-10-29 14:18:34 +0000

    add a disabled test

--- tests/next_tests/unit_tests/iterator_tests/test_embedded_field_with_list.py
+++ tests/next_tests/unit_tests/iterator_tests/test_embedded_field_with_list.py
@@ -7,11 +7,20 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import numpy as np
+import pytest
 
 import gt4py.next as gtx
 from gt4py.next.embedded import context as embedded_context
 from gt4py.next.iterator import embedded, runtime
-from gt4py.next.iterator.builtins import as_fieldop, deref, make_const_list, map_, neighbors, plus
+from gt4py.next.iterator.builtins import (
+    as_fieldop,
+    deref,
+    if_,
+    make_const_list,
+    map_,
+    neighbors,
+    plus,
+)
 
 
 E = gtx.Dimension("E")
@@ -73,6 +82,30 @@ def test_write_map_neighbors_and_const_list():
         result = ctx.run(testee, inp)
 
     ref = e2v_arr + 42.0
+    np.testing.assert_array_equal(result.asnumpy(), ref)
+
+
+def test_write_map_conditional_neighbors_and_const_list():
+    pytest.xfail(
+        "In the current implementation the type for the temporary field is different for `make_const_list` and `neighbors`."
+    )
+
+    def testee(inp, mask):
+        domain = runtime.UnstructuredDomain({E: range(2)})
+        return as_fieldop(lambda m, x, y: map_(if_)(deref(m), deref(x), deref(y)), domain)(
+            mask,
+            as_fieldop(lambda it: neighbors(E2V, it), domain)(inp),
+            as_fieldop(lambda: make_const_list(42.0), domain)(),
+        )
+
+    inp = gtx.as_field([V], np.arange(3))
+    mask_field = gtx.as_field([E], np.array([True, False]))
+    with embedded_context.new_context(offset_provider={"E2V": e2v_conn}) as ctx:
+        result = ctx.run(testee, inp, mask_field)
+
+    ref = np.empty_like(e2v_arr, dtype=float)
+    ref[0, :] = e2v_arr[0, :]
+    ref[1, :] = 42.0
     np.testing.assert_array_equal(result.asnumpy(), ref)
 
 
