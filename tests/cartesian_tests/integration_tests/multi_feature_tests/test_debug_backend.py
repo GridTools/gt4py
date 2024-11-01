e999ebd6be7edd779cf6b8c9e90ab896e2004ee2
Tobias Wicky-Pfund <tobias.wicky-pfund@ssaihq.com>
2024-11-01 14:18:29 +0100

    add test to debug backend

--- tests/cartesian_tests/integration_tests/multi_feature_tests/test_debug_backend.py
+++ tests/cartesian_tests/integration_tests/multi_feature_tests/test_debug_backend.py
@@ -302,3 +302,26 @@ def test_absolute_k_stencil():
     test_stencil(field_in, field_out)
 
     np.testing.assert_allclose(field_out.view(np.ndarray)[:, :, :], 15)
+
+
+def test_k_only_access_stencil():
+    field_in = np.ones((4,), dtype=np.float64)
+    field_out = gt_storage.zeros(
+        dtype=np.float64, backend="debug", shape=(4, 4, 4), aligned_index=(0, 0, 0)
+    )
+    field_in[:] = [2, 3, 4, 5]
+
+    @gtscript.stencil(backend="debug")
+    def test_stencil(
+        in_field: gtscript.Field[gtscript.K, np.float64],
+        out_field: gtscript.Field[np.float64],
+    ):
+        with computation(PARALLEL):
+            with interval(0, 1):
+                out_field[0, 0, 0] = in_field[1]
+            with interval(1, None):
+                out_field[0, 0, 0] = in_field[-1]
+
+    test_stencil(field_in, field_out)
+
+    np.testing.assert_allclose(field_out.view(np.ndarray)[1, 1, :], [3, 2, 3, 4])
