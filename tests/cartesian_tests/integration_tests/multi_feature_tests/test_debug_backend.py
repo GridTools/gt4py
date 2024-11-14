a8159e2b975fddd5d2602e59af13b62b2a9cfcba
Tobias Wicky-Pfund <tobias.wicky-pfund@ssaihq.com>
2024-11-14 13:24:55 +0100

    test for field access to describe k-access

--- tests/cartesian_tests/integration_tests/multi_feature_tests/test_debug_backend.py
+++ tests/cartesian_tests/integration_tests/multi_feature_tests/test_debug_backend.py
@@ -281,6 +281,31 @@ def test_k_offset_stencil():
     np.testing.assert_allclose(field_out.view(np.ndarray)[:, :, 1], 10)
 
 
+def test_k_offset_field_stencil():
+    field_in = gt_storage.ones(
+        dtype=np.float64, backend="debug", shape=(4, 4, 4), aligned_index=(0, 0, 0)
+    )
+    field_idx = gt_storage.ones(dtype=np.int64, backend="debug", shape=(4, 4), aligned_index=(0, 0))
+    field_out = gt_storage.zeros(
+        dtype=np.float64, backend="debug", shape=(4, 4, 4), aligned_index=(0, 0, 0)
+    )
+    field_in[:, :, 0] *= 10
+    field_idx[:, :] *= -2
+
+    @gtscript.stencil(backend="debug")
+    def test_stencil(
+        in_field: gtscript.Field[np.float64],
+        out_field: gtscript.Field[np.float64],
+        idx_field: gtscript.Field[gtscript.IJ, np.int64],
+    ):
+        with computation(PARALLEL), interval(1, None):
+            out_field[0, 0, 0] = in_field[0, 0, idx_field + 1]
+
+    test_stencil(field_in, field_out, field_idx)
+
+    np.testing.assert_allclose(field_out.view(np.ndarray)[:, :, 1], 10)
+
+
 def test_absolute_k_stencil():
     field_in = gt_storage.ones(
         dtype=np.float64, backend="debug", shape=(4, 4, 4), aligned_index=(0, 0, 0)
