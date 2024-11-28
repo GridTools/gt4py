716fd8d02903c192efdf4d4618b0056de7d1ab54
Sara Faghih-Naini <sara.faghihnaini@ecmwf.int>
2024-11-28 16:30:42 +0100

    Call ConstantFolding after create_global_tmps

--- src/gt4py/next/iterator/transforms/pass_manager.py
+++ src/gt4py/next/iterator/transforms/pass_manager.py
@@ -120,6 +120,8 @@ def apply_common_transforms(
     if extract_temporaries:
         ir = infer(ir, inplace=True, offset_provider_type=offset_provider_type)
         ir = global_tmps.create_global_tmps(ir, offset_provider=offset_provider, uids=tmp_uids)  # type: ignore[arg-type]  # always an itir.Program
+        # create_global_tmps calls domain inference which creates domain unions, i.e., minima and maxima
+        ir = ConstantFolding.apply(ir)  # type: ignore[assignment]  # always an itir.Program
 
     # Since `CollapseTuple` relies on the type inference which does not support returning tuples
     # larger than the number of closure outputs as given by the unconditional collapse, we can
