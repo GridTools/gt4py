eaee70dd4d247e855c932a63014641d688758cf5
Sara Faghih-Naini <sara.faghihnaini@ecmwf.int>
2024-11-29 15:55:04 +0100

    Skipping dace tests

--- tests/next_tests/integration_tests/feature_tests/ffront_tests/test_execution.py
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/test_execution.py
@@ -424,6 +424,9 @@ def test_astype_int(cartesian_case):
 
 
 def test_astype_int_local_field(unstructured_case):
+    if unstructured_case.backend and "dace" in unstructured_case.backend.name:
+        pytest.skip("Skipping dace: dace_itir: deprecated soon, dace_gtir: feature missing")
+
     @gtx.field_operator
     def testee(a: gtx.Field[[Vertex], np.float64]) -> gtx.Field[[Edge], int64]:
         tmp = astype(a(E2V), int64)
