9977316f3d73f5f9f0d6addbc4a1b0cb01388e55
Philip Mueller <philip.mueller@cscs.ch>
2024-11-29 12:04:28 +0100

    Added a transformation that allows add double buffering.
    This should help to solve some issues we oserve with rule 3.
    However, the implementation is very limited and general a bit crapy.
    Also some tests are missing.

--- src/gt4py/next/program_processors/runners/dace_fieldview/transformations/__init__.py
+++ src/gt4py/next/program_processors/runners/dace_fieldview/transformations/__init__.py
@@ -19,6 +19,7 @@ from .gpu_utils import (
     gt_gpu_transformation,
     gt_set_gpu_blocksize,
 )
+from .local_double_buffering import gt_crearte_local_double_buffering
 from .loop_blocking import LoopBlocking
 from .map_fusion_parallel import MapFusionParallel
 from .map_fusion_serial import MapFusionSerial
@@ -52,6 +53,7 @@ __all__ = [
     "SerialMapPromoterGPU",
     "gt_auto_optimize",
     "gt_change_transient_strides",
+    "gt_crearte_local_double_buffering",
     "gt_gpu_transformation",
     "gt_inline_nested_sdfg",
     "gt_set_iteration_order",
