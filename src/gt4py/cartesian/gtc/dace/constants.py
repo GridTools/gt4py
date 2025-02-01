ab28b72a5736c1ccdd50e9e3f9fc559f3a4799cb
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2025-02-01 07:52:01 +0100

    Use variable for connector prefixes

---
+++ src/gt4py/cartesian/gtc/dace/constants.py
@@ -0,0 +1,15 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+from typing import Final
+
+
+# StencilComputation in/out connector prefixes
+CONNECTOR_PREFIX_IN: Final = "__in_"
+CONNECTOR_PREFIX_OUT: Final = "__out_"
