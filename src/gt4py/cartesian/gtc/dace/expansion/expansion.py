3f85165916ca1359a053fdf900dc4d254ecec1c8
Roman Cattaneo <romanc@users.noreply.github.com>
2025-02-17 10:05:11 +0100

    refactor[cartesian]: unexpanded sdfg cleanups (#1860)
    ## Description
    
    Refactors from (recent) debugging sessions around transient arrays in the "unexpanded SDFG" (the one with the library nodes):
    
    - Remove unused `**kwargs` from `OirSDFGBuilder`
    - Forward debugging information about transient arrays to DaCe
    - Use a (constant) variable for connector prefixes of data going into/out of the library nodes
    - Configure the lifetime of transient arrays directly in `OirSDFGBuilder`
    
    This is a follow-up from PR
    https://github.com/GridTools/gt4py/pull/1843. In this PR, we separate the DaCe backend cleanup from the refactor around (re-)using `DeviceType` instead of `"cpu" | "gpu"` string literals.
    
    ## Requirements
    
    - [x] All fixes and/or new features come with corresponding tests.
      Should be covered by existing tests.
    - [ ] Important design decisions have been documented in the appropriate
    ADR inside the [docs/development/ADRs/](docs/development/ADRs/Index.md)
    folder.
      N/A
    
    ---------
    
    Co-authored-by: Roman Cattaneo <>
    Co-authored-by: Roman Cattaneo <1116746+romanc@users.noreply.github.com>
    Co-authored-by: Florian Deconinck <deconinck.florian@gmail.com>

--- src/gt4py/cartesian/gtc/dace/expansion/expansion.py
+++ src/gt4py/cartesian/gtc/dace/expansion/expansion.py
@@ -18,6 +18,7 @@ import dace.subsets
 import sympy
 
 from gt4py.cartesian.gtc.dace import daceir as dcir
+from gt4py.cartesian.gtc.dace.constants import CONNECTOR_PREFIX_IN, CONNECTOR_PREFIX_OUT
 from gt4py.cartesian.gtc.dace.expansion.daceir_builder import DaCeIRBuilder
 from gt4py.cartesian.gtc.dace.expansion.sdfg_builder import StencilComputationSDFGBuilder
 
@@ -77,11 +78,11 @@ class StencilComputationExpansion(dace.library.ExpandTransformation):
         """
         # change connector names
         for in_edge in parent_state.in_edges(node):
-            assert in_edge.dst_conn.startswith("__in_")
-            in_edge.dst_conn = in_edge.dst_conn[len("__in_") :]
+            assert in_edge.dst_conn.startswith(CONNECTOR_PREFIX_IN)
+            in_edge.dst_conn = in_edge.dst_conn.removeprefix(CONNECTOR_PREFIX_IN)
         for out_edge in parent_state.out_edges(node):
-            assert out_edge.src_conn.startswith("__out_")
-            out_edge.src_conn = out_edge.src_conn[len("__out_") :]
+            assert out_edge.src_conn.startswith(CONNECTOR_PREFIX_OUT)
+            out_edge.src_conn = out_edge.src_conn.removeprefix(CONNECTOR_PREFIX_OUT)
 
         # union input and output subsets
         subsets = {}
@@ -125,9 +126,13 @@ class StencilComputationExpansion(dace.library.ExpandTransformation):
     ) -> Dict[str, dace.data.Data]:
         parent_arrays: Dict[str, dace.data.Data] = {}
         for edge in (e for e in parent_state.in_edges(node) if e.dst_conn is not None):
-            parent_arrays[edge.dst_conn[len("__in_") :]] = parent_sdfg.arrays[edge.data.data]
+            parent_arrays[edge.dst_conn.removeprefix(CONNECTOR_PREFIX_IN)] = parent_sdfg.arrays[
+                edge.data.data
+            ]
         for edge in (e for e in parent_state.out_edges(node) if e.src_conn is not None):
-            parent_arrays[edge.src_conn[len("__out_") :]] = parent_sdfg.arrays[edge.data.data]
+            parent_arrays[edge.src_conn.removeprefix(CONNECTOR_PREFIX_OUT)] = parent_sdfg.arrays[
+                edge.data.data
+            ]
         return parent_arrays
 
     @staticmethod
