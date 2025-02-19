04ae43074b89d5df36589d36e5e3ba0380637560
tehrengruber <till.ehrengruber@cscs.ch>
2025-02-19 16:10:53 +0100

    Fix tests

--- src/gt4py/eve/visitors.py
+++ src/gt4py/eve/visitors.py
@@ -129,7 +129,10 @@ def _preserve_annex(
         new_annex_dict = new_node.annex.__dict__
         for key in preserved_annex_attrs:
             if (value := getattr(old_annex, key, NOTHING)) is not NOTHING:
-                assert key not in new_annex_dict or new_annex_dict[key] == value
+                # note: the annex value of the new node might not be equal (in
+                # the sense that the equality comparison is false), but in
+                # the context of the pass they are equalivalent. Therefore we don't
+                # assert equality here.
                 new_annex_dict[key] = value
 
 
