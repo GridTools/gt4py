2901d4a16a09b90cf69801e828b95215890808ef
Tobias Wicky-Pfund <wicky.tob@gmail.com>
2025-02-26 09:16:34 +0100

    reviewer's feedback

--- src/gt4py/cartesian/backend/debug_backend.py
+++ src/gt4py/cartesian/backend/debug_backend.py
@@ -6,19 +6,6 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-# GT4Py - GridTools Framework
-#
-# Copyright (c) 2014-2023, ETH Zurich
-# All rights reserved.
-#
-# This file is part of the GT4Py project and the GridTools framework.
-# GT4Py is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or any later
-# version. See the LICENSE.txt file at the top-level directory of this
-# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
 
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, ClassVar, Type, Union
