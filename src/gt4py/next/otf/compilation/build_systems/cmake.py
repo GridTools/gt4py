3fb25a934b936b298eb820ef7bd52f31c06c37fe
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2025-04-04 15:11:32 +0200

    Refactor code base on review comments

--- src/gt4py/next/otf/compilation/build_systems/cmake.py
+++ src/gt4py/next/otf/compilation/build_systems/cmake.py
@@ -34,16 +34,18 @@ def get_device_arch() -> str | None:
 
 
 def get_cmake_device_arch_option() -> str:
-    cmake_flag = ""
+    cmake_flag: str
 
-    device_archs = os.environ.get("CUDAARCHS", get_device_arch() or "").strip()
-    if device_archs:
-        match core_defs.CUPY_DEVICE_TYPE:
-            case core_defs.DeviceType.CUDA:
-                cmake_flag = f"DCMAKE_CUDA_ARCHITECTURES={device_archs}"
-            case core_defs.DeviceType.ROCM:
-                # `CMAKE_HIP_ARCHITECTURES` is not officially supported yet
-                cmake_flag = f"-DCMAKE_HIP_ARCHITECTURES={device_archs}"
+    match core_defs.CUPY_DEVICE_TYPE:
+        case core_defs.DeviceType.CUDA:
+            device_archs = os.environ.get("CUDAARCHS", "").strip() or get_device_arch()
+            cmake_flag = f"DCMAKE_CUDA_ARCHITECTURES={device_archs}"
+        case core_defs.DeviceType.ROCM:
+            # `HIPARCHS` is not officially supported by CMake yet, but it might be in the future
+            device_archs = os.environ.get("HIPARCHS", "").strip() or get_device_arch()
+            cmake_flag = f"-DCMAKE_HIP_ARCHITECTURES={device_archs}"
+        case None:
+            cmake_flag = ""
 
     return cmake_flag
 
