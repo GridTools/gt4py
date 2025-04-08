a9f61560d78867ddea023b442e605586d510cd0e
Edoardo Paone <edoardo.paone@cscs.ch>
2025-04-08 21:30:17 +0200

    add test marker for transformation tests

--- tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/transformation_tests/__init__.py
+++ tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/transformation_tests/__init__.py
@@ -10,4 +10,7 @@ import pytest
 
 
 #: Attribute defining package-level marks used by a custom pytest hook.
-package_pytestmarks = [pytest.mark.usefixtures("common_dace_config")]
+package_pytestmarks = [
+    pytest.mark.requires_dace,
+    pytest.mark.usefixtures("common_dace_config"),
+]
