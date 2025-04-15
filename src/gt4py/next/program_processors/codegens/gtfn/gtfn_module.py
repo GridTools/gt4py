7a7d0e97827cc522353fa8354a75f62746134d61
Hannes Vogt <vogt@hey.com>
2025-04-16 00:08:55 +0200

    more cleanup

--- src/gt4py/next/program_processors/codegens/gtfn/gtfn_module.py
+++ src/gt4py/next/program_processors/codegens/gtfn/gtfn_module.py
@@ -115,8 +115,6 @@ class GTFNTranslationStep(
         arg_exprs: list[str] = []
 
         for name, connectivity_type in offset_provider_type.items():
-            if name == "exec_info":
-                continue
             if isinstance(connectivity_type, common.NeighborConnectivityType):
                 if connectivity_type.dtype.scalar_type not in [np.int32, np.int64]:
                     raise ValueError(
