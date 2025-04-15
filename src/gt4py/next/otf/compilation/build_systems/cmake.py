GridTools/gt4py

commit b9df32ac1a1029ccbc56b220a8386f359c589235

feat: set the CUDA architecture automatically in gtfn compilation (#1958)
Set the CUDA architecture flag from the device information exposed by
cupy. The user can still override it by defining the standard CMake
`CUDAARGS` env variable.

Additional changes:
- Minor refactoring of the compiledb factory to reuse code from cmake
factory
- Add configure and build scripts to the generated project folders for
letting developers to reproduce and tinker with the build process during
debugging

---------

Co-authored-by: Hannes Vogt <vogt@hey.com>

src/gt4py/next/otf/compilation/build_systems/cmake.py
@@ -9,14 +9,43 @@
 from __future__ import annotations
 
 import dataclasses
+import os
 import pathlib
 import subprocess
-from typing import Optional
 
+from gt4py._core import definitions as core_defs
 from gt4py.next import config
 from gt4py.next.otf import languages, stages
 from gt4py.next.otf.compilation import build_data, cache, common, compiler
 from gt4py.next.otf.compilation.build_systems import cmake_lists
+
+
+def get_device_arch() -> str | None:
+    if core_defs.CUPY_DEVICE_TYPE == core_defs.DeviceType.CUDA:
+        # use `cp` from core_defs to avoid trying to re-import cupy
+        return core_defs.cp.cuda.Device(0).compute_capability  # type: ignore[attr-defined]
+    elif core_defs.CUPY_DEVICE_TYPE == core_defs.DeviceType.ROCM:
+        # TODO(egparedes): Implement this properly, either parsing the output of `$ rocminfo`
+        # or using the HIP low level bindings.
+        # Check: https://rocm.docs.amd.com/projects/hip-python/en/latest/user_guide/1_usage.html
+        return "gfx90a"
+
+    return None
+
+
+def get_cmake_device_arch_option() -> str:
+    cmake_flag = ""
+
+    match core_defs.CUPY_DEVICE_TYPE:
+        case core_defs.DeviceType.CUDA:
+            device_archs = os.environ.get("CUDAARCHS", "").strip() or get_device_arch()
+            cmake_flag = f"-DCMAKE_CUDA_ARCHITECTURES={device_archs}"
+        case core_defs.DeviceType.ROCM:
+            # `HIPARCHS` is not officially supported by CMake yet, but it might be in the future
+            device_archs = os.environ.get("HIPARCHS", "").strip() or get_device_arch()
+            cmake_flag = f"-DCMAKE_HIP_ARCHITECTURES={device_archs}"
+
+    return cmake_flag
 
 
 @dataclasses.dataclass
@@ -31,7 +60,7 @@ class CMakeFactory(
 
     cmake_generator_name: str = "Ninja"
     cmake_build_type: config.CMakeBuildType = config.CMakeBuildType.DEBUG
-    cmake_extra_flags: Optional[list[str]] = None
+    cmake_extra_flags: list[str] = dataclasses.field(default_factory=list)
 
     def __call__(
         self,
@@ -53,8 +82,15 @@ class CMakeFactory(
         if (src_lang := source.program_source.language) in [languages.CUDA, languages.HIP]:
             cmake_languages = [*cmake_languages, cmake_lists.Language(name=src_lang.__name__)]
         cmake_lists_src = cmake_lists.generate_cmakelists_source(
-            name, source.library_deps, [header_name, bindings_name], languages=cmake_languages
+            name,
+            source.library_deps,
+            [header_name, bindings_name],
+            languages=cmake_languages,
         )
+
+        if device_arch_flag := get_cmake_device_arch_option():
+            self.cmake_extra_flags.append(device_arch_flag)
+
         return CMakeProject(
             root_path=cache.get_cache_folder(source, cache_lifetime),
             source_files={
@@ -65,7 +101,7 @@ class CMakeFactory(
             program_name=name,
             generator_name=self.cmake_generator_name,
             build_type=self.cmake_build_type,
-            extra_cmake_flags=self.cmake_extra_flags or [],
+            extra_cmake_flags=self.cmake_extra_flags,
         )
 
 
@@ -111,21 +147,39 @@ class CMakeProject(
             self.root_path,
         )
 
+        self._write_configure_script()
+
+    @property
+    def _config_command(self) -> list[str]:
+        return [
+            "cmake",
+            "-G",
+            self.generator_name,
+            "-S",
+            str(self.root_path),
+            "-B",
+            str(self.root_path / "build"),
+            f"-DCMAKE_BUILD_TYPE={self.build_type.value}",
+            *self.extra_cmake_flags,
+        ]
+
+    def _write_configure_script(self) -> None:
+        # TODO(havogt): additionally we could store all `env` vars
+        configure_script_path = self.root_path / "configure.sh"
+        with configure_script_path.open("w") as build_script_pointer:
+            build_script_pointer.write("#!/bin/sh\n")
+            build_script_pointer.write(" ".join(self._config_command))
+        try:
+            configure_script_path.chmod(0o755)
+        except OSError:
+            # if setting permissions fails, it's not a problem
+            pass
+
     def _run_config(self) -> None:
         logfile = self.root_path / "log_config.txt"
         with logfile.open(mode="w") as log_file_pointer:
             subprocess.check_call(
-                [
-                    "cmake",
-                    "-G",
-                    self.generator_name,
-                    "-S",
-                    str(self.root_path),
-                    "-B",
-                    str(self.root_path / "build"),
-                    f"-DCMAKE_BUILD_TYPE={self.build_type.value}",
-                    *self.extra_cmake_flags,
-                ],
+                self._config_command,
                 stdout=log_file_pointer,
                 stderr=log_file_pointer,
             )

ci/cscs-ci.yml
@@ -145,8 +145,6 @@ build_py310_image_aarch64:
     - SUBPACKAGE: [storage]
       VARIANT: ['cuda12', 'cpu']
   variables:
-    # Grace-Hopper gpu architecture is not enabled by default in CUDA build
-    CUDAARCHS: "90"
     # Limit test parallelism to avoid "OSError: too many open files" in the gt4py build stage.
     # Another problem, observed in test stage, is that gpu tests hang in combination with CUDA MPS,
     # when high test parallelism is used.

src/gt4py/next/otf/compilation/build_systems/cmake_lists.py
@@ -45,12 +45,6 @@ class CMakeListsGenerator(eve.codegen.TemplatedGenerator):
         project({{project_name}})
 
         # Languages
-        if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
-            set(CMAKE_CUDA_ARCHITECTURES 60)
-        endif()
-        if(NOT DEFINED CMAKE_HIP_ARCHITECTURES)
-            set(CMAKE_HIP_ARCHITECTURES gfx90a)
-        endif()
         {{"\\n".join(languages)}}
 
         # Paths

src/gt4py/next/otf/compilation/build_systems/compiledb.py
@@ -22,7 +22,7 @@ from gt4py.next import config, errors
 from gt4py.next.otf import languages, stages
 from gt4py.next.otf.binding import interface
 from gt4py.next.otf.compilation import build_data, cache, compiler
-from gt4py.next.otf.compilation.build_systems import cmake, cmake_lists
+from gt4py.next.otf.compilation.build_systems import cmake
 
 
 SrcL = TypeVar("SrcL", bound=languages.NanobindSrcL)
@@ -38,7 +38,8 @@ class CompiledbFactory(
     Create a CompiledbProject from a ``CompilableSource`` stage object with given CMake settings.
 
     Use CMake to generate a compiledb with the required sequence of build commands.
-    Generate a compiledb only if there isn't one for the given combination of cmake configuration and library dependencies.
+    Generate a compiledb only if there isn't one for the given combination of cmake configuration
+    and library dependencies.
     """
 
     cmake_build_type: config.CMakeBuildType = config.CMakeBuildType.DEBUG
@@ -125,17 +126,24 @@ class CompiledbProject(
             self._run_build()
 
     def _write_files(self) -> None:
-        def ignore_not_libraries(folder: str, children: list[str]) -> list[str]:
+        def ignore_function(folder: str, children: list[str]) -> list[str]:
             pattern = r"((lib.*\.a)|(.*\.lib))"
-            libraries = [child for child in children if re.match(pattern, child)]
-            folders = [child for child in children if (pathlib.Path(folder) / child).is_dir()]
-            ignored = list(set(children) - set(libraries) - set(folders))
+            folder_path = pathlib.Path(folder)
+
+            ignored = []
+            for child in children:
+                if re.match(pattern, child):  # static library -> keep
+                    continue
+                if (folder_path / child).is_dir():  # folder -> keep
+                    continue
+                ignored.append(child)
+
             return ignored
 
         shutil.copytree(
             self.compile_commands_cache.parent,
             self.root_path,
-            ignore=ignore_not_libraries,
+            ignore=ignore_function,
             dirs_exist_ok=True,
         )
 
@@ -166,6 +174,15 @@ class CompiledbProject(
                 )
 
         (self.root_path / "compile_commands.json").write_text(json.dumps(compile_db))
+
+        (build_script_path := self.root_path / "build.sh").write_text(
+            "\n".join(["#!/bin/sh", "cd build", *(entry["command"] for entry in compile_db)])
+        )
+        try:
+            build_script_path.chmod(0o755)
+        except OSError:
+            # if setting permissions fails, it's not a problem
+            pass
 
         build_data.write_data(
             build_data.BuildData(
@@ -244,11 +261,13 @@ def _cc_get_compiledb(
     with lock.Lock(str(cache_path / "compiledb.lock"), lifetime=120):  # type: ignore[attr-defined] # mypy not smart enough to understand custom export logic
         if renew_compiledb or not (compiled_db := _cc_find_compiledb(path=cache_path)):
             compiled_db = _cc_create_compiledb(
-                path=cache_path,
                 prototype_program_source=prototype_program_source,
                 build_type=build_type,
                 cmake_flags=cmake_flags,
+                cache_lifetime=cache_lifetime,
             )
+
+            assert compiled_db.parent == cache_path
 
         return compiled_db
 
@@ -261,35 +280,28 @@ def _cc_find_compiledb(path: pathlib.Path) -> Optional[pathlib.Path]:
 
 
 def _cc_create_compiledb(
-    path: pathlib.Path,
     prototype_program_source: stages.ProgramSource,
     build_type: config.CMakeBuildType,
     cmake_flags: list[str],
+    cache_lifetime: config.BuildCacheLifetime,
 ) -> pathlib.Path:
-    name = prototype_program_source.entry_point.name
-    header_ext = prototype_program_source.language_settings.header_extension
-    src_ext = prototype_program_source.language_settings.file_extension
-    prog_src_name = f"{name}.{header_ext}"
-    binding_src_name = f"{name}.{src_ext}"
-    cmake_languages = [cmake_lists.Language(name="CXX")]
-    if (src_lang := prototype_program_source.language) in [languages.CUDA, languages.HIP]:
-        cmake_languages = [*cmake_languages, cmake_lists.Language(name=src_lang.__name__)]
+    prototype_project = cmake.CMakeFactory(
+        cmake_generator_name="Ninja",
+        cmake_build_type=build_type,
+        cmake_extra_flags=cmake_flags,
+    )(
+        stages.CompilableSource(
+            prototype_program_source, stages.BindingSource(source_code="", library_deps=())
+        ),
+        cache_lifetime,
+    )
 
-    prototype_project = cmake.CMakeProject(
-        generator_name="Ninja",
-        build_type=build_type,
-        extra_cmake_flags=cmake_flags,
-        root_path=path,
-        source_files={
-            **{name: "" for name in [binding_src_name, prog_src_name]},
-            "CMakeLists.txt": cmake_lists.generate_cmakelists_source(
-                name,
-                prototype_program_source.library_deps,
-                [binding_src_name, prog_src_name],
-                cmake_languages,
-            ),
-        },
-        program_name=name,
+    path = prototype_project.root_path
+    name = prototype_project.program_name
+    binding_src_name = next(
+        name
+        for name in prototype_project.source_files.keys()
+        if name.endswith(f"_bindings.{prototype_program_source.language_settings.file_extension}")
     )
 
     prototype_project.build()
@@ -333,4 +345,5 @@ def _cc_create_compiledb(
 
     compile_db_path = path / "compile_commands.json"
     compile_db_path.write_text(json.dumps(compile_db))
+
     return compile_db_path

tests/next_tests/unit_tests/otf_tests/compilation_tests/build_systems_tests/test_cmake.py
@@ -6,11 +6,48 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
+import os
 import pathlib
+from unittest import mock
 
+from gt4py._core import definitions as core_defs
 from gt4py.next import config
-from gt4py.next.otf.compilation import build_data, cache, importer
+from gt4py.next.otf.compilation import build_data, importer
 from gt4py.next.otf.compilation.build_systems import cmake
+
+
+def test_get_cmake_device_arch_option_cuda():
+    with (
+        mock.patch("gt4py._core.definitions.CUPY_DEVICE_TYPE", core_defs.DeviceType.CUDA),
+        mock.patch("gt4py.next.otf.compilation.build_systems.cmake.get_device_arch", lambda: "90"),
+    ):
+        # Test CUDA device without user-provided environment variable
+        with mock.patch.dict(os.environ, {}):
+            assert cmake.get_cmake_device_arch_option() == "-DCMAKE_CUDA_ARCHITECTURES=90"
+        with mock.patch.dict(os.environ, {"CUDAARCHS": ""}):
+            assert cmake.get_cmake_device_arch_option() == "-DCMAKE_CUDA_ARCHITECTURES=90"
+
+        # Test CUDA device with CUDAARCHS environment variable
+        with mock.patch.dict(os.environ, {"CUDAARCHS": "80", "HIPARCHS": "gfx90a"}):
+            assert cmake.get_cmake_device_arch_option() == "-DCMAKE_CUDA_ARCHITECTURES=80"
+
+
+def test_get_cmake_device_arch_option_rocm():
+    with (
+        mock.patch("gt4py._core.definitions.CUPY_DEVICE_TYPE", core_defs.DeviceType.ROCM),
+        mock.patch(
+            "gt4py.next.otf.compilation.build_systems.cmake.get_device_arch", lambda: "gfx942"
+        ),
+    ):
+        # Test ROCM device without user-provided environment variable
+        with mock.patch.dict(os.environ, {}):
+            assert cmake.get_cmake_device_arch_option() == "-DCMAKE_HIP_ARCHITECTURES=gfx942"
+        with mock.patch.dict(os.environ, {"HIPARCHS": ""}):
+            assert cmake.get_cmake_device_arch_option() == "-DCMAKE_HIP_ARCHITECTURES=gfx942"
+
+        # Test ROCM device with HIPARCHS environment variable
+        with mock.patch.dict(os.environ, {"CUDAARCHS": "80", "HIPARCHS": "gfx90a"}):
+            assert cmake.get_cmake_device_arch_option() == "-DCMAKE_HIP_ARCHITECTURES=gfx90a"
 
 
 def test_default_cmake_factory(compilable_source_example, clean_example_session_cache):
@@ -28,3 +65,5 @@ def test_default_cmake_factory(compilable_source_example, clean_example_session_
     assert hasattr(
         importer.import_from_path(otf_builder.root_path / data.module), data.entry_point_name
     )
+
+    assert (otf_builder.root_path / "configure.sh").exists()

tests/next_tests/unit_tests/otf_tests/compilation_tests/build_systems_tests/test_compiledb.py
@@ -6,10 +6,8 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import pathlib
-
 from gt4py.next import config
-from gt4py.next.otf.compilation import build_data, cache, importer
+from gt4py.next.otf.compilation import build_data, importer
 from gt4py.next.otf.compilation.build_systems import compiledb
 
 
@@ -25,7 +23,9 @@ def test_default_compiledb_factory(compilable_source_example, clean_example_sess
     data = build_data.read_data(otf_builder.root_path)
 
     assert data.status == build_data.BuildStatus.COMPILED
-    assert pathlib.Path(otf_builder.root_path / data.module).exists()
+    assert (otf_builder.root_path / data.module).exists()
     assert hasattr(
         importer.import_from_path(otf_builder.root_path / data.module), data.entry_point_name
     )
+
+    assert (otf_builder.root_path / "build.sh").exists()

commit b5f1ef84d12e6a68d6722a94e0b4f3a1eb978943

bug[next]: Fix gtfn backend error when cupy installed but no gpu present (#1972)
Small fix for a bug introduced in #1958

---------

Co-authored-by: Hannes Vogt <hannes@havogt.de>

src/gt4py/next/otf/compilation/build_systems/cmake.py
@@ -12,6 +12,7 @@ import dataclasses
 import os
 import pathlib
 import subprocess
+import warnings
 
 from gt4py._core import definitions as core_defs
 from gt4py.next import config
@@ -23,7 +24,13 @@ from gt4py.next.otf.compilation.build_systems import cmake_lists
 def get_device_arch() -> str | None:
     if core_defs.CUPY_DEVICE_TYPE == core_defs.DeviceType.CUDA:
         # use `cp` from core_defs to avoid trying to re-import cupy
-        return core_defs.cp.cuda.Device(0).compute_capability  # type: ignore[attr-defined]
+        try:
+            return core_defs.cp.cuda.Device(0).compute_capability  # type: ignore[attr-defined]
+        except core_defs.cp.cuda.runtime.CUDARuntimeError as e:  # type: ignore[attr-defined]
+            warnings.warn(
+                UserWarning(f"Could not determine the CUDA compute capability: {e}"), stacklevel=2
+            )
+            return None
     elif core_defs.CUPY_DEVICE_TYPE == core_defs.DeviceType.ROCM:
         # TODO(egparedes): Implement this properly, either parsing the output of `$ rocminfo`
         # or using the HIP low level bindings.
@@ -39,11 +46,13 @@ def get_cmake_device_arch_option() -> str:
     match core_defs.CUPY_DEVICE_TYPE:
         case core_defs.DeviceType.CUDA:
             device_archs = os.environ.get("CUDAARCHS", "").strip() or get_device_arch()
-            cmake_flag = f"-DCMAKE_CUDA_ARCHITECTURES={device_archs}"
+            cmake_flag_template = "-DCMAKE_CUDA_ARCHITECTURES={device_archs}"
         case core_defs.DeviceType.ROCM:
             # `HIPARCHS` is not officially supported by CMake yet, but it might be in the future
             device_archs = os.environ.get("HIPARCHS", "").strip() or get_device_arch()
-            cmake_flag = f"-DCMAKE_HIP_ARCHITECTURES={device_archs}"
+            cmake_flag_template = "-DCMAKE_HIP_ARCHITECTURES={device_archs}"
+
+    cmake_flag = cmake_flag_template.format(device_archs=device_archs) if device_archs else ""
 
     return cmake_flag
 
@@ -81,15 +90,14 @@ class CMakeFactory(
         cmake_languages = [cmake_lists.Language(name="CXX")]
         if (src_lang := source.program_source.language) in [languages.CUDA, languages.HIP]:
             cmake_languages = [*cmake_languages, cmake_lists.Language(name=src_lang.__name__)]
+            if device_arch_flag := get_cmake_device_arch_option():
+                self.cmake_extra_flags.append(device_arch_flag)
         cmake_lists_src = cmake_lists.generate_cmakelists_source(
             name,
             source.library_deps,
             [header_name, bindings_name],
             languages=cmake_languages,
         )
-
-        if device_arch_flag := get_cmake_device_arch_option():
-            self.cmake_extra_flags.append(device_arch_flag)
 
         return CMakeProject(
             root_path=cache.get_cache_folder(source, cache_lifetime),