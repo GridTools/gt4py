332ecdea3439d017e516f0b2ea54bb454b485714
Hannes Vogt <vogt@hey.com>
2025-04-20 22:03:17 +0200

    simplify test

--- tests/next_tests/unit_tests/iterator_tests/ir_utils_test.py/test_misc.py
+++ tests/next_tests/unit_tests/iterator_tests/ir_utils_test.py/test_misc.py
@@ -7,72 +7,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import pytest
-from gt4py.next.iterator.ir_utils import misc, ir_makers as im
-from gt4py.next.iterator import ir as itir
-from gt4py.next.iterator.transforms import inline_lambdas
 
-from gt4py.eve.pattern_matching import ObjectPattern as P
+from gt4py.next.iterator.ir_utils import ir_makers as im, misc
+from gt4py.next.iterator.transforms import inline_lambdas
 
 
 @pytest.mark.parametrize(
-    "expr, expected_projector_matcher, expected_expr",
+    "expr,  expected_expr",
     [
         (
             im.let("a", "b")(im.make_tuple(im.tuple_get(0, "a"), im.tuple_get(42, "a"))),
-            P(
-                itir.Lambda,
-                expr=P(
-                    itir.FunCall,
-                    fun=im.ref("make_tuple"),
-                    args=[
-                        P(
-                            itir.FunCall,
-                            fun=im.ref("tuple_get"),
-                            args=[im.literal_from_value(0), P(itir.SymRef)],
-                        ),
-                        P(
-                            itir.FunCall,
-                            fun=im.ref("tuple_get"),
-                            args=[im.literal_from_value(42), P(itir.SymRef)],
-                        ),
-                    ],
-                ),
-            ),
             im.ref("b"),
         ),
         (
             im.tuple_get(1, im.ref("a")),
-            P(
-                itir.Lambda,
-                expr=P(
-                    itir.FunCall,
-                    fun=im.ref("tuple_get"),
-                    args=[im.literal_from_value(1), P(itir.SymRef)],
-                ),
-            ),
             im.ref("a"),
         ),
         (
             im.make_tuple(im.tuple_get(0, im.ref("a")), im.tuple_get(42, im.ref("a"))),
-            P(
-                itir.Lambda,
-                expr=P(
-                    itir.FunCall,
-                    fun=im.ref("make_tuple"),
-                    args=[
-                        P(
-                            itir.FunCall,
-                            fun=im.ref("tuple_get"),
-                            args=[im.literal_from_value(0), P(itir.SymRef)],
-                        ),
-                        P(
-                            itir.FunCall,
-                            fun=im.ref("tuple_get"),
-                            args=[im.literal_from_value(42), P(itir.SymRef)],
-                        ),
-                    ],
-                ),
-            ),
             im.ref("a"),
         ),
         (
@@ -80,75 +32,31 @@ from gt4py.eve.pattern_matching import ObjectPattern as P
                 im.tuple_get(1, im.ref("a")),
                 im.make_tuple(im.tuple_get(2, im.ref("a")), im.tuple_get(0, im.ref("a"))),
             ),
-            P(
-                itir.Lambda,
-                expr=P(
-                    itir.FunCall,
-                    fun=im.ref("make_tuple"),
-                    args=[
-                        P(
-                            itir.FunCall,
-                            fun=im.ref("tuple_get"),
-                            args=[im.literal_from_value(1), P(itir.SymRef)],
-                        ),
-                        P(
-                            itir.FunCall,
-                            fun=im.ref("make_tuple"),
-                            args=[
-                                P(
-                                    itir.FunCall,
-                                    fun=im.ref("tuple_get"),
-                                    args=[im.literal_from_value(2), P(itir.SymRef)],
-                                ),
-                                P(
-                                    itir.FunCall,
-                                    fun=im.ref("tuple_get"),
-                                    args=[im.literal_from_value(0), P(itir.SymRef)],
-                                ),
-                            ],
-                        ),
-                    ],
-                ),
-            ),
             im.ref("a"),
         ),
         (
             im.tuple_get(3, im.tuple_get(0, im.ref("a"))),
-            P(
-                itir.Lambda,
-                expr=P(
-                    itir.FunCall,
-                    fun=im.ref("tuple_get"),
-                    args=[
-                        im.literal_from_value(3),
-                        P(
-                            itir.FunCall,
-                            fun=im.ref("tuple_get"),
-                            args=[im.literal_from_value(0), P(itir.SymRef)],
-                        ),
-                    ],
-                ),
-            ),
             im.ref("a"),
         ),
         (
             im.plus(im.ref("a"), im.ref("b")),
-            None,
             im.plus(im.ref("a"), im.ref("b")),
         ),
         (
             im.call("as_fieldop")(im.ref("a")),
-            None,
             im.call("as_fieldop")(im.ref("a")),
         ),
     ],
 )
-def test_extract_projector(expr, expected_projector_matcher, expected_expr):
+def test_extract_projector(expr, expected_expr):
     actual_projector, actual_expr = misc.extract_projector(expr)
-    print(actual_projector)
     assert actual_expr == expected_expr
 
-    if expected_projector_matcher is None:
-        assert actual_projector is None
-    else:
-        assert expected_projector_matcher.match(actual_projector, raise_exception=True)
+    applied_projector = (
+        im.call(actual_projector)(actual_expr) if actual_projector is not None else actual_expr
+    )
+
+    # simplify original expression and applied projector for comparison
+    applied_projector = inline_lambdas.InlineLambdas.apply(applied_projector)
+    inlined_expr = inline_lambdas.InlineLambdas.apply(expr)
+    assert applied_projector == inlined_expr
