GridTools/gt4py

commit 0604a69b9d2ce8d2816adfaf4e121bc55e1e8d6f

fix[next]: Prevent extraction from lambdas in cse pass (#2013)
The common subexpression elimination pass already has a mechanism to
avoid extracting from `if_` calls, such that the evaluation of
expressions can be guarded in order to to avoid out-of-bounds accesses
or excessive pre-computation. The former protection can be circumvented
by placing things inside a lambda outside of the protecting if:
```
if cond then
  let f = λ() → ·guarded_it in
    if cond2 then f() else 0
else
  ·guarded_it
```
This PR prevents that by not extracting from lambdas unless they are a
let-form in which case it is clear that the function is unconditionally
evaluated.

---------

Co-authored-by: Enrique González Paredes <enriqueg@cscs.ch>

src/gt4py/next/iterator/transforms/cse.py
@@ -8,10 +8,9 @@
 
 from __future__ import annotations
 
+import collections
 import dataclasses
-import functools
 import math
-import operator
 from typing import Callable, Iterable, TypeVar, Union, cast
 
 import gt4py.next.iterator.ir_utils.ir_makers as im
@@ -149,8 +148,15 @@ class CollectSubexpressions(PreserveLocationVisitor, VisitorWithSymbolTableTrait
         depth = kwargs.pop("depth")
         return super().generic_visit(node, depth=depth + 1, **kwargs)
 
-    def visit(self, node: itir.Node, **kwargs) -> None:  # type: ignore[override] # supertype accepts any node, but we want to be more specific here.
-        if not isinstance(node, SymbolTableTrait) and not _is_collectable_expr(node):
+    def visit(self, node: itir.Node, is_let_form: bool = False, **kwargs) -> None:  # type: ignore[override] # supertype accepts any node, but we want to be more specific here.
+        # do not descent into un-applied lambda
+        can_collect_children = not isinstance(node, itir.Lambda) or is_let_form
+
+        if (
+            can_collect_children
+            and not isinstance(node, SymbolTableTrait)
+            and not _is_collectable_expr(node)
+        ):
             return super().visit(node, **kwargs)
 
         depth = kwargs["depth"]
@@ -158,48 +164,62 @@ class CollectSubexpressions(PreserveLocationVisitor, VisitorWithSymbolTableTrait
         collected_child_node_ids: set[int] = set()
         used_symbol_ids: set[int] = set()
 
-        # Special handling of `if_(condition, true_branch, false_branch)` like expressions that
-        # avoids extracting subexpressions unless they are used in either the condition or both
-        # branches.
-        if isinstance(node, itir.FunCall) and node.fun == itir.SymRef(id="if_"):
-            assert len(node.args) == 3
-            # collect subexpressions for all arguments to the `if_`
-            arg_states = [self.State() for _ in node.args]
-            for arg, state in zip(node.args, arg_states):
-                self.visit(arg, state=state, **{**kwargs, "depth": depth + 1})
+        if can_collect_children:
+            # Special handling of `if_(condition, true_branch, false_branch)` like expressions that
+            # avoids extracting subexpressions unless they are used in either the condition or both
+            # branches.
+            if cpm.is_call_to(node, "if_"):
+                assert len(node.args) == 3
+                # collect subexpressions for all arguments to the `if_`
+                arg_states = [self.State() for _ in node.args]
+                for arg, state in zip(node.args, arg_states):
+                    self.visit(arg, state=state, **(kwargs | {"depth": depth + 1}))
 
-            # remove all subexpressions that are not eligible for collection
-            #  (either they occur in the condition or in both branches)
-            eligible_subexprs = arg_states[0].subexprs.keys() | (
-                arg_states[1].subexprs.keys() & arg_states[2].subexprs.keys()
-            )
-            for arg_state in arg_states:
-                arg_state.remove_subexprs(arg_state.subexprs.keys() - eligible_subexprs)
+                # remove all subexpressions that are not eligible for collection
+                #  (either they occur in the condition or in both branches)
+                eligible_subexprs = arg_states[0].subexprs.keys() | (
+                    arg_states[1].subexprs.keys() & arg_states[2].subexprs.keys()
+                )
+                for arg_state in arg_states:
+                    arg_state.remove_subexprs(arg_state.subexprs.keys() - eligible_subexprs)
 
-            # merge the states of the three arguments
-            subexprs: dict[itir.Node, CollectSubexpressions.SubexpressionData] = {}
-            for state in arg_states:
-                for subexpr, data in state.subexprs.items():
-                    merged_data = subexprs.setdefault(subexpr, self.SubexpressionData())
-                    merged_data.subexprs.extend(data.subexprs)
-                    merged_data.max_depth = max(merged_data.max_depth, data.max_depth)
-            collected_child_node_ids = functools.reduce(
-                operator.or_, (state.collected_child_node_ids for state in arg_states)
-            )
-            used_symbol_ids = functools.reduce(
-                operator.or_, (state.used_symbol_ids for state in arg_states)
-            )
-            # propagate collected subexpressions to parent
-            for subexpr, data in subexprs.items():
-                parent_data = parent_state.subexprs.setdefault(subexpr, self.SubexpressionData())
-                parent_data.subexprs.extend(data.subexprs)
-                parent_data.max_depth = max(merged_data.max_depth, data.max_depth)
-        else:
-            super().visit(
-                node,
-                state=self.State(parent_state.subexprs, collected_child_node_ids, used_symbol_ids),
-                **kwargs,
-            )
+                # merge the states of the three arguments
+                subexprs: dict[itir.Node, CollectSubexpressions.SubexpressionData] = (
+                    collections.defaultdict(self.SubexpressionData)
+                )
+                for state in arg_states:
+                    for subexpr, data in state.subexprs.items():
+                        merged_data = subexprs[subexpr]
+                        merged_data.subexprs.extend(data.subexprs)
+                        merged_data.max_depth = max(merged_data.max_depth, data.max_depth)
+                collected_child_node_ids = set.union(
+                    *(state.collected_child_node_ids for state in arg_states)
+                )
+                used_symbol_ids = set.union(*(state.used_symbol_ids for state in arg_states))
+                # propagate collected subexpressions to parent
+                for subexpr, data in subexprs.items():
+                    parent_data = parent_state.subexprs.setdefault(
+                        subexpr, self.SubexpressionData()
+                    )
+                    parent_data.subexprs.extend(data.subexprs)
+                    parent_data.max_depth = max(merged_data.max_depth, data.max_depth)
+            elif cpm.is_let(node):
+                new_state = self.State(
+                    parent_state.subexprs, collected_child_node_ids, used_symbol_ids
+                )
+                self.visit(
+                    node.fun, state=new_state, is_let_form=True, **(kwargs | {"depth": depth + 1})
+                )
+                for arg in node.args:
+                    self.visit(arg, state=new_state, **(kwargs | {"depth": depth + 1}))
+            else:
+                super().visit(
+                    node,
+                    state=self.State(
+                        parent_state.subexprs, collected_child_node_ids, used_symbol_ids
+                    ),
+                    **kwargs,
+                )
 
         if isinstance(node, SymbolTableTrait):
             # remove symbols used in child nodes if they are declared in the current node

tests/next_tests/unit_tests/iterator_tests/transforms_tests/test_cse.py
@@ -106,6 +106,12 @@ def test_lambda_redef_same_arg():
 
 
 def test_lambda_redef_same_arg_scope():
+    pytest.xfail(
+        reason="Not implemented. The CSE pass may not extract from a lambda function"
+        "unless it knows the function is unconditionally evaluated. For this"
+        "case this currently beyond scope."
+    )
+
     def common_expr():
         return im.lambda_("a")(im.plus("a", im.plus(1, 1)))
 
@@ -292,4 +298,26 @@ def test_scalar_extraction_inside_as_fieldop():
     expected = im.as_fieldop(im.lambda_()(im.let("_cs_1", common)(im.plus("_cs_1", "_cs_1"))))()
 
     actual = CSE.apply(testee, within_stencil=False)
+    assert actual == expected
+
+
+def test_no_extraction_from_unapplied_lambda():
+    testee = im.if_(
+        "cond",
+        im.let("f", im.lambda_()(im.deref("guarded_it")))(im.if_("cond2", im.call("f")(), 0)),
+        im.deref("guarded_it"),
+    )
+
+    actual = CSE.apply(testee, within_stencil=True)
+    assert actual == testee  # no extraction should happen
+
+
+def test_extraction_from_let_form():
+    common = im.plus(1, 2)
+    testee = im.plus(im.let("a", 1)(im.plus("a", common)), im.let("b", 2)(im.plus("b", common)))
+    expected = im.let("_cs_1", common)(
+        im.plus(im.let("a", 1)(im.plus("a", "_cs_1")), im.let("b", 2)(im.plus("b", "_cs_1")))
+    )
+
+    actual = CSE.apply(testee, within_stencil=True)
     assert actual == expected

commit d5f0e416b5a5c801f78805b277d8e30c2745547f

fix[next]: Fix cse extraction of lambda functions (#2018)
Fix a small bug introduced in #2013, where `f` in the following
expression
```
let val = 1 in
  let f = λ() → val in
     opaque_fun(f, g)
  end
end
```
was erroneously extracted beyond the definition of `val`.

src/gt4py/next/iterator/transforms/cse.py
@@ -220,6 +220,9 @@ class CollectSubexpressions(PreserveLocationVisitor, VisitorWithSymbolTableTrait
                     ),
                     **kwargs,
                 )
+        else:
+            # we don't want to collect any expression, but still need to track all symbol refs
+            super().visit(node, state=self.State({}, set(), used_symbol_ids), **kwargs)
 
         if isinstance(node, SymbolTableTrait):
             # remove symbols used in child nodes if they are declared in the current node

tests/next_tests/unit_tests/iterator_tests/transforms_tests/test_cse.py
@@ -25,6 +25,22 @@ def offset_provider_type(request):
     return {"I": common.Dimension("I", kind=common.DimensionKind.HORIZONTAL)}
 
 
+@pytest.fixture
+def opaque_fun(request):
+    return im.ref(
+        "opaque_fun",
+        ts.FunctionType(
+            pos_only_args=[],
+            pos_or_kw_args={
+                "f": ts.DeferredType(constraint=None),
+                "g": ts.DeferredType(constraint=None),
+            },
+            kw_only_args={},
+            returns=ts.DeferredType(constraint=None),
+        ),
+    )
+
+
 def test_trivial():
     common = im.plus("x", "y")
     testee = im.plus(common, common)
@@ -321,3 +337,21 @@ def test_extraction_from_let_form():
 
     actual = CSE.apply(testee, within_stencil=True)
     assert actual == expected
+
+
+def test_sym_ref_collection_from_lambda(opaque_fun):
+    # This is more a regression than a unit test. When `f` & `g` are collected, we don't want to
+    # collect subexpression from their body (as the pass does not recognize them to be evaluated),
+    # but still need to respect that `val` is used inside. Hence, when extracting `f` and `g,` the
+    # extracted lambda needs to be placed after the definition of `val`.
+    testee = im.let("val", 1)(
+        im.let(("f", im.lambda_()("val")), ("g", im.lambda_()("val")))(
+            im.call(opaque_fun)("f", "g")
+        )
+    )
+    expected = im.let("val", 1)(
+        im.let("_cs_1", im.lambda_()("val"))(im.call(opaque_fun)("_cs_1", "_cs_1"))
+    )
+
+    actual = CSE.apply(testee, within_stencil=True)
+    assert actual == expected  # no extraction should happen