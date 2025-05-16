8354e39bcc1590a0535ee979d9f5b57881add6a7
Edoardo Paone <edoardo.paone@cscs.ch>
2025-05-16 12:17:21 +0200

    async sdfg call

--- src/gt4py/next/program_processors/runners/dace/workflow/translation.py
+++ src/gt4py/next/program_processors/runners/dace/workflow/translation.py
@@ -84,6 +84,7 @@ class DaCeTranslator(
         column_axis: Optional[common.Dimension],
         auto_opt: bool,
         on_gpu: bool,
+        async_sdfg_call: bool,
     ) -> dace.SDFG:
         if not self.disable_itir_transforms:
             ir = itir_transforms.apply_fieldview_transforms(ir, offset_provider=offset_provider)
@@ -122,6 +123,20 @@ class DaCeTranslator(
                 gtx_transformations.gt_simplify(sdfg)
                 gtx_transformations.gt_gpu_transformation(sdfg, try_removing_trivial_maps=True)
 
+        if on_gpu and async_sdfg_call:
+            sdfg.append_global_code(f"""\
+DACE_EXPORTED bool __dace_gpu_set_stream({sdfg.name}_state_t *__state, int streamid, gpuStream_t stream);
+DACE_EXPORTED void __set_stream_{sdfg.name}({sdfg.name}_state_t *__state, gpuStream_t stream) {{
+    __dace_gpu_set_stream(__state, 0, stream);
+}}\
+""")
+            sdfg.append_init_code(f"""\
+    __set_stream_{sdfg.name}(__state, nullptr);
+""")
+            for state in sdfg.states():
+                # TODO: add a check that the state contains contains only one gpu map
+                state.nosync = True
+
         return sdfg
 
     def __call__(
@@ -137,6 +152,7 @@ class DaCeTranslator(
             inp.args.column_axis,
             auto_opt=self.auto_optimize,
             on_gpu=(self.device_type == core_defs.CUPY_DEVICE_TYPE),
+            async_sdfg_call=True,
         )
 
         arg_types = tuple(
