45324c88e57b5e8dfc974efa70fa2f2e5e10677f
edopao <edoardo.paone@cscs.ch>
2025-05-27 14:03:20 +0200

    feat[next][dace]: Add cpp timer to dace backend (#1999)
    This PR builds on #1978 and adds support for `COMPUTE` metric timers in
    the dace backend. This metric is measured by means of CPP
    instrumentation of the top-level SDFG, which inserts a chrono timer
    around the SDFG execution. Collection of this metric overrides the
    asynchronous execution mode.
    
    ---------
    
    Co-authored-by: Hannes Vogt <vogt@hey.com>
    Co-authored-by: Enrique Gonzalez Paredes <enriqueg@cscs.ch>

--- src/gt4py/next/program_processors/runners/dace/workflow/compilation.py
+++ src/gt4py/next/program_processors/runners/dace/workflow/compilation.py
@@ -17,8 +17,10 @@ import dace
 import factory
 
 from gt4py._core import definitions as core_defs
-from gt4py.next import config
+from gt4py.next import config, metrics
 from gt4py.next.otf import languages, stages, step_types, workflow
+
+from . import common as dace_common
 
 
 def _get_sdfg_ctype_arglist_callback(
@@ -117,42 +119,15 @@ class DaCeCompiler(
     ) -> CompiledDaceProgram:
         sdfg = dace.SDFG.from_json(inp.program_source.source_code)
 
+        if config.COLLECT_METRICS_LEVEL >= metrics.PERFORMANCE:
+            # measure execution time of the top-level SDFG
+            sdfg.instrument = dace.dtypes.InstrumentationType.Timer
+
         with dace.config.temporary_config():
-            dace.config.Config.set("compiler.build_type", value=self.cmake_build_type.value)
-
-            # We rely on dace cache to avoid recompiling the SDFG.
-            #   Note that the workflow step with the persistent `FileCache` store
-            #   is translating from `CompilableProgram` (ITIR.Program + CompileTimeArgs)
-            #   to `ProgramSource`, so this step is storing in cache only the result
-            #   of the SDFG transformations, not the compiled program binary.
-            dace.config.Config.set("compiler.use_cache", value=True)
-            dace.config.Config.set("cache", value="hash")  # use the SDFG hash as cache key
-            dace.config.Config.set(
-                "default_build_folder", value=str(config.BUILD_CACHE_DIR / "dace_cache")
+            dace_common.set_dace_config(
+                device_type=self.device_type,
+                cmake_build_type=self.cmake_build_type,
             )
-
-            # dace dafault setting use fast-math in both cpu and gpu compilation, don't use it here
-            dace.config.Config.set(
-                "compiler.cpu.args",
-                value="-std=c++14 -fPIC -O3 -march=native -Wall -Wextra -Wno-unused-parameter -Wno-unused-label",
-            )
-            dace.config.Config.set(
-                "compiler.cuda.args",
-                value="-Xcompiler -O3 -Xcompiler -march=native -Xcompiler -Wno-unused-parameter",
-            )
-            dace.config.Config.set(
-                "compiler.cuda.hip_args",
-                value="-std=c++17 -fPIC -O3 -march=native -Wno-unused-parameter",
-            )
-
-            # In some stencils, mostly in `apply_diffusion_to_w` the cuda codegen messes
-            #  up with the cuda streams, i.e. it allocates N streams but uses N+1.
-            #  This is a workaround until this issue if fixed in DaCe.
-            dace.config.Config.set("compiler.cuda.max_concurrent_streams", value=1)
-
-            if self.device_type == core_defs.DeviceType.ROCM:
-                dace.config.Config.set("compiler.cuda.backend", value="hip")
-
             sdfg_program = sdfg.compile(validate=False)
 
         assert inp.binding_source is not None
