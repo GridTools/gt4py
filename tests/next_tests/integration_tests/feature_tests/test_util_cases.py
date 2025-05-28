GridTools/gt4py

commit 942ec7cd9a60b5efe24a0d34ec06dd3f220e97f8

bug[next]: fix tuple output in direct field operator call with domain argument (#2049)
`out` can be a single or multiple, these different scenarios are now
handled by the lambda function

---------

Co-authored-by: Enrique Gonzalez Paredes <enriqueg@cscs.ch>

tests/next_tests/integration_tests/cases.py
@@ -380,6 +380,17 @@ def run(
     fieldview_prog.with_grid_type(case.grid_type).with_backend(case.backend)(*args, **kwargs)
 
 
+def tree_mapped_np_all_close(ref: np.ndarray, data: np.ndarray) -> bool:
+    """Compare two arrays or nested tuples of arrays using np.allclose."""
+    if (is_tuple := isinstance(ref, tuple)) == isinstance(data, tuple):
+        if is_tuple:
+            return all(gt_utils.tree_map(np.allclose)(ref, data))
+        else:
+            return np.allclose(ref, data)
+
+    return False
+
+
 def verify(
     case: Case,
     fieldview_prog: decorator.FieldOperator | decorator.Program,
@@ -389,7 +400,7 @@ def verify(
     out: Optional[FieldViewInout] = None,
     inout: Optional[FieldViewInout] = None,
     offset_provider: Optional[OffsetProvider] = None,
-    comparison: Callable[[Any, Any], bool] = np.allclose,
+    comparison: Callable[[Any, Any], bool] = tree_mapped_np_all_close,
 ) -> None:
     """
     Check the result of executing a fieldview program or operator against ref.
@@ -433,10 +444,11 @@ def verify(
     assert out_comp is not None
     out_comp_ndarray = field_utils.asnumpy(out_comp)
     ref_ndarray = field_utils.asnumpy(ref)
+
     assert comparison(ref_ndarray, out_comp_ndarray), (
         f"Verification failed:\n"
         f"\tcomparison={comparison.__name__}(ref, out)\n"
-        f"\tref = {ref_ndarray}\n\tout = {str(out_comp_ndarray)}"
+        f"\tref = {ref_ndarray!s}\n\tout = {out_comp_ndarray!s}"
     )
 
 
@@ -444,7 +456,7 @@ def verify_with_default_data(
     case: Case,
     fieldop: decorator.FieldOperator,
     ref: Callable,
-    comparison: Callable[[Any, Any], bool] = np.allclose,
+    comparison: Callable[[Any, Any], bool] = tree_mapped_np_all_close,
 ) -> None:
     """
     Check the fieldview code against a reference calculation.

tests/next_tests/integration_tests/feature_tests/ffront_tests/test_gt4py_builtins.py
@@ -324,10 +324,15 @@ def test_conditional_nested_tuple(cartesian_case):
         a,
         b,
         out=cases.allocate(cartesian_case, conditional_nested_tuple, cases.RETURN)(),
-        ref=np.where(
-            mask.asnumpy(),
-            ((a.asnumpy(), b.asnumpy()), (b.asnumpy(), a.asnumpy())),
-            ((np.full(size, 5.0), np.full(size, 7.0)), (np.full(size, 7.0), np.full(size, 5.0))),
+        ref=tuple(
+            np.where(
+                mask.asnumpy(),
+                ((a.asnumpy(), b.asnumpy()), (b.asnumpy(), a.asnumpy())),
+                (
+                    (np.full(size, 5.0), np.full(size, 7.0)),
+                    (np.full(size, 7.0), np.full(size, 5.0)),
+                ),
+            )
         ),
     )
 

src/gt4py/next/ffront/decorator.py
@@ -33,6 +33,7 @@ from gt4py.next import (
     embedded as next_embedded,
     errors,
     metrics,
+    utils,
 )
 from gt4py.next.embedded import operators as embedded_operators
 from gt4py.next.ffront import (
@@ -721,7 +722,7 @@ class FieldOperator(GTCallable, Generic[OperatorNodeT]):
             out = kwargs.pop("out")
             if "domain" in kwargs:
                 domain = common.domain(kwargs.pop("domain"))
-                out = out[domain]
+                out = utils.tree_map(lambda f: f[domain])(out)
 
             args, kwargs = type_info.canonicalize_arguments(
                 self.foast_stage.foast_node.type, args, kwargs

tests/next_tests/integration_tests/feature_tests/ffront_tests/test_arg_call_interface.py
@@ -314,3 +314,21 @@ def test_direct_fo_call_with_domain_arg(cartesian_case):
     ref[1:-1] = inp.ndarray[1:-1]
 
     cases.verify(cartesian_case, testee, inp, out=out, domain={IDim: (1, size - 1)}, ref=ref)
+
+
+@pytest.mark.uses_origin
+def test_direct_fo_call_with_domain_arg_tuple_return(cartesian_case):
+    @field_operator
+    def testee(inp: IField) -> tuple[IField, IField]:
+        return (inp, inp)
+
+    size = cartesian_case.default_sizes[IDim]
+    inp = cases.allocate(cartesian_case, testee, "inp").unique()()
+    out = cases.allocate(
+        cartesian_case, testee, cases.RETURN, strategy=cases.ConstInitializer(42)
+    )()
+    ref = inp.array_ns.zeros(size)
+    ref[0] = ref[-1] = 42
+    ref[1:-1] = inp.ndarray[1:-1]
+
+    cases.verify(cartesian_case, testee, inp, out=out, domain={IDim: (1, size - 1)}, ref=(ref, ref))

commit 68cd934e4c73635f71469a327512e1bec420220c

fix[next]: fix nested tuple comparisons in 'cases' utilities (#2054)
Fix bug in test `cases` utilities introduced in PR #2049 when verifying
structs with multiple nested levels of ndarrays. Some new unit tests have been also added


---------

Co-authored-by: Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>

tests/next_tests/integration_tests/cases.py
@@ -380,13 +380,30 @@ def run(
     fieldview_prog.with_grid_type(case.grid_type).with_backend(case.backend)(*args, **kwargs)
 
 
-def tree_mapped_np_all_close(ref: np.ndarray, data: np.ndarray) -> bool:
+try:
+    RTOL, ATOL, EQUAL_NAN = np.allclose.__wrapped__.__defaults__
+except AttributeError:
+    RTOL, ATOL, EQUAL_NAN = (1e-05, 1e-08, False)
+
+
+def tree_mapped_np_allclose(
+    ref: np.ndarray,
+    data: np.ndarray,
+    *,
+    rtol: float = RTOL,
+    atol: float = ATOL,
+    equal_nan: bool = EQUAL_NAN,
+) -> bool:
     """Compare two arrays or nested tuples of arrays using np.allclose."""
     if (is_tuple := isinstance(ref, tuple)) == isinstance(data, tuple):
+        allclose_with_tols = functools.partial(
+            np.allclose, rtol=rtol, atol=atol, equal_nan=equal_nan
+        )
         if is_tuple:
-            return all(gt_utils.tree_map(np.allclose)(ref, data))
+            allclose_results = gt_utils.tree_map(allclose_with_tols)(ref, data)
+            return all(gt_utils.flatten_nested_tuple(allclose_results))
         else:
-            return np.allclose(ref, data)
+            return allclose_with_tols(ref, data)
 
     return False
 
@@ -400,7 +417,7 @@ def verify(
     out: Optional[FieldViewInout] = None,
     inout: Optional[FieldViewInout] = None,
     offset_provider: Optional[OffsetProvider] = None,
-    comparison: Callable[[Any, Any], bool] = tree_mapped_np_all_close,
+    comparison: Callable[[Any, Any], bool] = tree_mapped_np_allclose,
 ) -> None:
     """
     Check the result of executing a fieldview program or operator against ref.
@@ -456,7 +473,7 @@ def verify_with_default_data(
     case: Case,
     fieldop: decorator.FieldOperator,
     ref: Callable,
-    comparison: Callable[[Any, Any], bool] = tree_mapped_np_all_close,
+    comparison: Callable[[Any, Any], bool] = tree_mapped_np_allclose,
 ) -> None:
     """
     Check the fieldview code against a reference calculation.

tests/next_tests/integration_tests/feature_tests/ffront_tests/test_gt4py_builtins.py
@@ -6,6 +6,7 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
+import functools
 from typing import TypeAlias
 
 import numpy as np
@@ -317,6 +318,8 @@ def test_conditional_nested_tuple(cartesian_case):
     a = cases.allocate(cartesian_case, conditional_nested_tuple, "a")()
     b = cases.allocate(cartesian_case, conditional_nested_tuple, "b")()
 
+    where_with_mask = functools.partial(np.where, mask.asnumpy())
+
     cases.verify(
         cartesian_case,
         conditional_nested_tuple,
@@ -324,15 +327,15 @@ def test_conditional_nested_tuple(cartesian_case):
         a,
         b,
         out=cases.allocate(cartesian_case, conditional_nested_tuple, cases.RETURN)(),
-        ref=tuple(
-            np.where(
-                mask.asnumpy(),
-                ((a.asnumpy(), b.asnumpy()), (b.asnumpy(), a.asnumpy())),
-                (
-                    (np.full(size, 5.0), np.full(size, 7.0)),
-                    (np.full(size, 7.0), np.full(size, 5.0)),
-                ),
-            )
+        ref=(
+            (
+                where_with_mask(a.asnumpy(), np.full(size, 5.0)),
+                where_with_mask(b.asnumpy(), np.full(size, 7.0)),
+            ),
+            (
+                where_with_mask(b.asnumpy(), np.full(size, 7.0)),
+                where_with_mask(a.asnumpy(), np.full(size, 5.0)),
+            ),
         ),
     )
 

tests/next_tests/integration_tests/feature_tests/test_util_cases.py
@@ -17,6 +17,7 @@ from next_tests.integration_tests import cases
 from next_tests.integration_tests.cases import (  # noqa: F401 [unused-import]
     cartesian_case,
     exec_alloc_descriptor,
+    tree_mapped_np_allclose,
 )
 
 
@@ -62,6 +63,43 @@ def test_allocate_const(cartesian_case):
 
     b = cases.allocate(cartesian_case, mixed_args, "b").strategy(cases.ConstInitializer(42))()
     assert b == 42.0
+
+
+def test_tree_mapped_np_all_close_simple_arrays_close():
+    a = np.array([1.0, 2.0, 3.0])
+    b = a + 1e-05
+
+    assert tree_mapped_np_allclose(a, b)
+    assert not tree_mapped_np_allclose(a, b, rtol=1e-06)
+
+
+def test_tree_mapped_np_all_close_simple_arrays_different():
+    a = np.array([1.0, 2.0, 3.0])
+    b = np.array([1.1, 2.0, 3.0])
+    assert not tree_mapped_np_allclose(a, b)
+
+
+def test_tree_mapped_np_all_close_simple_tuples_close():
+    a = (np.array([1.0, 2.0]), np.array([3.0, 4.0]))
+    b = (a[0] + 1e-05, a[1] + 1e-05)
+
+    assert tree_mapped_np_allclose(a, b)
+    assert not tree_mapped_np_allclose(a, b, rtol=1e-06)
+
+
+def test_tree_mapped_np_all_close_nested_tuples():
+    a = (np.array([1.0]), (np.array([2.0]), np.array([3.0])))
+    b = (np.array([1.0]), (np.array([2.0]), np.array([3.0])))
+    c = (np.array([1.0]), (np.array([2.1]), np.array([3.0])))
+
+    assert tree_mapped_np_allclose(a, b)
+    assert not tree_mapped_np_allclose(a, c)
+
+
+def test_tree_mapped_np_all_close_different_types():
+    a = np.array([1.0, 2.0])
+    b = (np.array([1.0]), np.array([2.0]))
+    assert not tree_mapped_np_allclose(a, b)
 
 
 @pytest.mark.parametrize("exec_alloc_descriptor", [definitions.ProgramBackendId.ROUNDTRIP.load()])