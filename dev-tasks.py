ca394d21212d7b713961c99de84a10389e94c7c2
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2025-06-17 17:16:23 +0200

    First iteration alpha

--- dev-tasks.py
+++ dev-tasks.py
@@ -18,80 +18,45 @@
 # ///
 
 
-"""Script for running recurrent development tasks."""
-
-from __future__ import annotations
-
+import importlib
+import importlib.util
 import pathlib
-import subprocess
+import sys
+import types
 from typing import Final
 
 import typer
 
-ROOT_DIR: Final = pathlib.Path(__file__).parent
+TASKS_PACKAGE_NAME: Final = "dev-tasks"
+
+app = typer.Typer()
 
 
-# -- Helpers --
-def gather_versions() -> dict[str, str]:
-    with subprocess.Popen(
-        [*"uv export --frozen --no-hashes --project".split(), ROOT_DIR], stdout=subprocess.PIPE
-    ) as proc:
-        return dict(
-            line.split("==")[:2]  # grab only the package name and version
-            for line in proc.stdout.read().decode().splitlines()
-            if not any(line.startswith(c) for c in ["-", "#"])
-        )
+def import_tasks_package(tasks_pkg_path: pathlib.Path) -> None:
+    """Dynamically import the tasks subcommands defined from the package folder."""
+    spec = importlib.util.spec_from_file_location("tasks", tasks_pkg_path / "__init__.py")
+    assert spec is not None, f"Could not find tasks package at {tasks_pkg_path}"
+    assert spec.loader is not None, f"Could not find tasks package at {tasks_pkg_path}"
+    tasks_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(tasks_module)
+    sys.modules[TASKS_PACKAGE_NAME] = tasks_module
 
 
-# -- CLI --
-app = typer.Typer(no_args_is_help=True)
+def add_commands(app: typer.Typer) -> typer.Typer:
+    """Add all subcommands from the tasks package to the main app."""
+    for name, value in vars(sys.modules[TASKS_PACKAGE_NAME]).items():
+        if (
+            isinstance(value, types.ModuleType)
+            and hasattr(value, "app")
+            and isinstance(value.app, typer.Typer)
+        ):
+            app.add_typer(value.app, name=name)
 
-
-@app.command()
-def sync_precommit() -> None:
-    """Sync versions of tools used in pre-commit hooks with the project versions."""
-    versions = gather_versions()
-    # Update ruff version in pre-commit config
-    subprocess.run(
-        f"""uvx -q --from 'yamlpath' yaml-set --mustexist --change='repos[.repo%https://github.com/astral-sh/ruff-pre-commit].rev' --value='v{versions["ruff"]}' .pre-commit-config.yaml""",
-        cwd=ROOT_DIR,
-        shell=True,
-        check=True,
-    )
-
-    # Update tach version in pre-commit config
-    subprocess.run(
-        f"""uvx -q --from 'yamlpath' yaml-set --mustexist --change='repos[.repo%https://github.com/gauge-sh/tach-pre-commit].rev' --value='v{versions["tach"]}' .pre-commit-config.yaml""",
-        cwd=ROOT_DIR,
-        shell=True,
-        check=True,
-    )
-
-    # Format yaml files
-    subprocess.run(
-        f"uv run --project {ROOT_DIR} pre-commit run pretty-format-yaml --all-files", shell=True
-    )
-
-
-@app.command()
-def update_precommit() -> None:
-    """Update and sync pre-commit hooks with the latest compatible versions."""
-    subprocess.run(f"uv run --project {ROOT_DIR} pre-commit autoupdate", shell=True)
-    sync_precommit()
-
-
-@app.command()
-def update_versions() -> None:
-    """Update all project dependencies to their latest compatible versions."""
-    subprocess.run("uv lock --upgrade", cwd=ROOT_DIR, shell=True, check=True)
-
-
-@app.command()
-def update_all() -> None:
-    """Update all project dependencies and pre-commit hooks."""
-    update_versions()
-    update_precommit()
+    return app
 
 
 if __name__ == "__main__":
+    tasks_pkg_path = pathlib.Path(__file__).parent.resolve().absolute() / "tasks"
+    import_tasks_package(tasks_pkg_path)
+    add_commands(app)
     app()
---
+++ tasks/_definitions.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env -S uv run -q --script
+#
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import pathlib
+from typing import Final
+
+
+ROOT_DIR: Final = pathlib.Path(__file__).parent
