b337324a198d9f9e6c461fb199a8158868398dd1
Hannes Vogt <vogt@hey.com>
2025-06-23 14:25:20 +0200

    add test

--- src/gt4py/next/iterator/runtime.py
+++ src/gt4py/next/iterator/runtime.py
@@ -16,10 +16,12 @@ from typing import TYPE_CHECKING, Callable, Optional, Union
 
 import devtools
 
+from gt4py._core import definitions as core_defs
 from gt4py.next import common, config
-from gt4py.next.iterator import builtins
+from gt4py.next.iterator import builtins, dispatcher
 from gt4py.next.iterator.builtins import BackendNotSelectedError, builtin_dispatch
 from gt4py.next.program_processors import program_formatter
+from gt4py.next.type_system import type_specifications as ts, type_translation as tt
 
 
 if TYPE_CHECKING:
@@ -216,3 +218,15 @@ def set_at(*args):
 @builtin_dispatch
 def if_stmt(*args):
     return BackendNotSelectedError()
+
+
+@builtin_dispatch
+def temporary(*args):
+    return BackendNotSelectedError()
+
+
+def _dtypebuiltin_to_ts(dtype: callable) -> ts.ScalarType:
+    assert isinstance(dtype, dispatcher._fun_dispatcher)
+    dtype_name = dtype.fun.__name__
+    np_dtype = getattr(core_defs, dtype_name)
+    return tt.from_dtype(core_defs.dtype(np_dtype))
