b1242e61ac53ddaf1974ed42fdfc7a39b7154766
Florian Deconinck <deconinck.florian@gmail.com>
2025-06-26 09:27:23 -0400

    Implement cast conversion in tasklet

--- src/gt4py/cartesian/gtc/dace/oir_to_tasklet.py
+++ src/gt4py/cartesian/gtc/dace/oir_to_tasklet.py
@@ -270,6 +270,10 @@ class OIRToTasklet(eve.NodeVisitor):
                 common.NativeFunction.FLOOR: "dace.math.ifloor",
                 common.NativeFunction.CEIL: "ceil",
                 common.NativeFunction.TRUNC: "trunc",
+                common.NativeFunction.I32: "dace.int32",
+                common.NativeFunction.I64: "dace.int64",
+                common.NativeFunction.F32: "dace.float32",
+                common.NativeFunction.F64: "dace.float64",
             }[func]
         except KeyError as error:
             raise NotImplementedError("Not implemented NativeFunction encountered.") from error
