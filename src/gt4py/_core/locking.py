f91527cc5bcdf4cfafbe42e55939fb4e8e2c8c31
Hannes Vogt <vogt@hey.com>
2025-07-04 10:00:33 +0200

    construct identifier from lock

--- src/gt4py/_core/locking.py
+++ src/gt4py/_core/locking.py
@@ -13,24 +13,28 @@ from typing import ContextManager, Protocol
 
 import filelock
 
+from gt4py.eve import utils as eve_utils
+
 
 class _LockConstructor(Protocol):
     def __call__(self, path: str, /) -> ContextManager: ...
 
+    @property
+    def __name__(self) -> str: ...
+
 
 # Changing the lock is private API, but could be made public in the future
-# The second element (str) is an identifier of the lock implementation to avoid conflicts
-# when switching between different lock implementations.
-_lock: tuple[_LockConstructor, str] = (filelock.FileLock, "filelock")
+_lock: _LockConstructor = filelock.FileLock
 
 
 def lock(directory: pathlib.Path | str, suffix: str = ".lock") -> ContextManager:
     """Create a lock for the given path."""
-    constructor, identifier = _lock
-
     directory = pathlib.Path(directory)
     if not directory.is_dir():
         raise ValueError(f"Expected a directory, got: {directory}")
 
+    # Identifier of the lock implementation to avoid conflicts
+    # when switching between different implementations.
+    identifier = f"{eve_utils.a10n(_lock.__module__)}_{eve_utils.a10n(_lock.__name__)}"
     path = directory / f"{identifier}{suffix}"
-    return constructor(str(path))
+    return _lock(str(path))
