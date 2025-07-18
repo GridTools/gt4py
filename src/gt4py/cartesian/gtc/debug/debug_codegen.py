4028606f846c36f0301b5f945d4e2afd19759894
Tobias Wicky-Pfund <wicky.tob@gmail.com>
2025-07-18 15:18:49 +0200

    reorder and adapt to symtable as per discussion with Hannes

--- src/gt4py/cartesian/gtc/debug/debug_codegen.py
+++ src/gt4py/cartesian/gtc/debug/debug_codegen.py
@@ -9,27 +9,37 @@
 
 from collections.abc import Generator
 from contextlib import contextmanager
+from typing import Mapping
 
 from gt4py import eve
-from gt4py.cartesian import utils
 from gt4py.cartesian.gtc import common as gtc_common, definitions as gtc_definitions, oir
 from gt4py.cartesian.gtc.passes.oir_optimizations import utils as optimization_utils
 from gt4py.eve import codegen, concepts
 
 
-class DebugCodeGen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
+def compute_extents(
+    node: oir.Stencil,
+) -> tuple[dict[str, gtc_definitions.Extent], dict[int, gtc_definitions.Extent]]:
+    ctx: optimization_utils.StencilExtentComputer.Context = (
+        optimization_utils.StencilExtentComputer().visit(node)
+    )
+    return ctx.fields, ctx.blocks
+
+
+class DebugCodeGen(eve.VisitorWithSymbolTableTrait):
     def __init__(self) -> None:
-        self.body = utils.text.TextBlock()
-        self.symbol_table: dict[str, oir.FieldDecl] = {}
+        self.body = codegen.TextBlock()
 
-    def visit_Stencil(self, stencil: oir.Stencil, **_) -> str:
+    def visit_Stencil(
+        self, stencil: oir.Stencil, symtable: Mapping[str, oir.FieldDecl], **_
+    ) -> str:
+        field_extents, block_extents = compute_extents(stencil)
+
         self.generate_imports()
-
         self.generate_run_function(stencil)
+        self.initial_declarations(stencil, field_extents)
 
-        field_extents, block_extents = self.compute_extents(stencil)
-        self.symbol_table = self.initial_declarations(stencil, field_extents)
-        self.generate_stencil_code(stencil, block_extents)
+        self.generate_stencil_code(stencil, block_extents, symtable)
 
         return self.body.text
 
@@ -37,52 +47,6 @@ class DebugCodeGen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
         self.body.append("import numpy as np")
         self.body.append("from gt4py.cartesian.gtc import ufuncs")
         self.body.append("from gt4py.cartesian.utils import Field")
-
-    @staticmethod
-    def compute_extents(
-        node: oir.Stencil, **_
-    ) -> tuple[dict[str, gtc_definitions.Extent], dict[int, gtc_definitions.Extent]]:
-        ctx: optimization_utils.StencilExtentComputer.Context = (
-            optimization_utils.StencilExtentComputer().visit(node)
-        )
-        return ctx.fields, ctx.blocks
-
-    def initial_declarations(
-        self, stencil: oir.Stencil, field_extents: dict[str, gtc_definitions.Extent]
-    ) -> dict[str, oir.FieldDecl]:
-        self.body.append("# ===== Domain Description ===== #")
-        self.body.append("i_0, j_0, k_0 = 0, 0, 0")
-        self.body.append("i_size, j_size, k_size = _domain_")
-        self.body.empty_line()
-        self.body.append("# ===== Temporary Declaration ===== #")
-        symbol_table = self.generate_temp_decls(stencil.declarations, field_extents)
-        self.body.empty_line()
-        self.body.append("# ===== Field Declaration ===== #")
-        symbol_table |= self.generate_field_decls(stencil.params)
-        self.body.empty_line()
-        return symbol_table
-
-    def generate_temp_decls(
-        self,
-        temporary_declarations: list[oir.Temporary],
-        field_extents: dict[str, gtc_definitions.Extent],
-    ) -> dict[str, oir.FieldDecl]:
-        symbol_table: dict[str, oir.FieldDecl] = {}
-        for declaration in temporary_declarations:
-            self.body.append(self.visit(declaration, field_extents=field_extents))
-            symbol_table[str(declaration.name)] = declaration
-        return symbol_table
-
-    def generate_field_decls(self, declarations: list[oir.Decl]) -> dict[str, oir.FieldDecl]:
-        symbol_table = {}
-        for declaration in declarations:
-            if isinstance(declaration, oir.FieldDecl):
-                self.body.append(
-                    f"{declaration.name} = Field({declaration.name}, _origin_['{declaration.name}'], "
-                    f"({', '.join([str(x) for x in declaration.dimensions])}))"
-                )
-                symbol_table[str(declaration.name)] = declaration
-        return symbol_table
 
     def generate_run_function(self, stencil: oir.Stencil) -> None:
         function_signature = "def run(*"
@@ -94,17 +58,49 @@ class DebugCodeGen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
         self.body.append(function_signature)
         self.body.indent()
 
+    def initial_declarations(
+        self, stencil: oir.Stencil, field_extents: dict[str, gtc_definitions.Extent]
+    ) -> None:
+        self.body.append("# ===== Domain Description ===== #")
+        self.body.append("i_0, j_0, k_0 = 0, 0, 0")
+        self.body.append("i_size, j_size, k_size = _domain_")
+        self.body.empty_line()
+        self.body.append("# ===== Temporary Declaration ===== #")
+        self.generate_temp_decls(stencil.declarations, field_extents)
+        self.body.empty_line()
+        self.body.append("# ===== Field Declaration ===== #")
+        self.generate_field_decls(stencil.params)
+        self.body.empty_line()
+
+    def generate_temp_decls(
+        self,
+        temporary_declarations: list[oir.Temporary],
+        field_extents: dict[str, gtc_definitions.Extent],
+    ) -> None:
+        for declaration in temporary_declarations:
+            self.body.append(self.visit(declaration, field_extents=field_extents))
+
+    def generate_field_decls(self, declarations: list[oir.Decl]) -> None:
+        for declaration in declarations:
+            if isinstance(declaration, oir.FieldDecl):
+                self.body.append(
+                    f"{declaration.name} = Field({declaration.name}, _origin_['{declaration.name}'], "
+                    f"({', '.join([str(x) for x in declaration.dimensions])}))"
+                )
+
+    # The visitors for all the control flow code that write directly to the `self.body` are in the following section
     def generate_stencil_code(
         self,
         stencil: oir.Stencil,
         block_extents: dict[int, gtc_definitions.Extent],
+        symtable: Mapping[str, oir.FieldDecl],
     ) -> None:
         for loop in stencil.vertical_loops:
             for section in loop.sections:
                 with self.create_k_loop_code(section, loop):
                     for execution in section.horizontal_executions:
                         with self.generate_ij_loop(block_extents, execution):
-                            self.visit(execution)
+                            self.visit(execution, symtable=symtable)
 
     @contextmanager
     def create_k_loop_code(
@@ -137,10 +133,53 @@ class DebugCodeGen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
         while_condition = self.visit(while_node.cond)
         while_code = f"while {while_condition}:"
         self.body.append(while_code)
-        self.body.indent()
-        for statement in while_node.body:
-            self.visit(statement)
-        self.body.dedent()
+        with self.body.indented():
+            for statement in while_node.body:
+                self.visit(statement)
+
+    def visit_HorizontalExecution(
+        self, horizontal_execution: oir.HorizontalExecution, **kwargs
+    ) -> None:
+        for statement in horizontal_execution.body:
+            self.visit(statement, **kwargs)
+
+    def visit_HorizontalMask(self, horizontal_mask: gtc_common.HorizontalMask, **kwargs) -> None:
+        i_min, i_max = self.visit(horizontal_mask.i, var="i", **kwargs)
+        j_min, j_max = self.visit(horizontal_mask.j, var="j", **kwargs)
+        conditions = []
+        if i_min is not None:
+            conditions.append(f"({i_min}) <= i")
+        if i_max is not None:
+            conditions.append(f"i < ({i_max})")
+        if j_min is not None:
+            conditions.append(f"({j_min}) <= j")
+        if j_max is not None:
+            conditions.append(f"j < ({j_max})")
+        assert len(conditions)
+        if_code = f"if( {' and '.join(conditions)} ):"
+        self.body.append(if_code)
+
+    def visit_HorizontalRestriction(
+        self, horizontal_restriction: oir.HorizontalRestriction, **kwargs
+    ) -> None:
+        self.visit(horizontal_restriction.mask, **kwargs)
+        with self.body.indented():
+            self.visit(horizontal_restriction.body, **kwargs)
+
+    def visit_MaskStmt(self, mask_statement: oir.MaskStmt, **kwargs) -> None:
+        self.body.append(f"if {self.visit(mask_statement.mask, **kwargs)}:")
+        with self.body.indented():
+            for statement in mask_statement.body:
+                self.visit(statement, **kwargs)
+
+    def visit_AssignStmt(self, assignment_statement: oir.AssignStmt, **kwargs) -> None:
+        self.body.append(
+            f"{self.visit(assignment_statement.left, **kwargs)} = {self.visit(assignment_statement.right, **kwargs)}"
+        )
+
+    # The visitors for the rest of the code-generation all return their strings directly and are in the following section
+    def visit_VerticalLoop(self) -> None:
+        pass
 
     def visit_FieldDecl(self, field_decl: oir.FieldDecl, **_) -> str:
         return str(field_decl.name)
@@ -201,29 +240,35 @@ class DebugCodeGen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
     def visit_SymbolRef(self, symbol_ref: concepts.SymbolRef) -> str:
         return symbol_ref
 
-    def visit_FieldAccess(self, field_access: gtc_common.FieldAccess, **_) -> str:
-        if str(field_access.name) in self.symbol_table:
-            dimensions = self.symbol_table[str(field_access.name)].dimensions
-            offset_str = self.visit(field_access.offset, dimensions=dimensions)
+    def visit_FieldAccess(
+        self,
+        field_access: gtc_common.FieldAccess,
+        *,
+        symtable: Mapping[str, oir.FieldDecl],
+        **kwargs,
+    ) -> str:
+        if str(field_access.name) in symtable:
+            dimensions = symtable[str(field_access.name)].dimensions
+            offset_str = self.visit(
+                field_access.offset, dimensions=dimensions, symtable=symtable, **kwargs
+            )
         else:
-            offset_str = self.visit(field_access.offset)
+            offset_str = self.visit(field_access.offset, symtable=symtable, **kwargs)
 
         if field_access.data_index:
             data_index_access = ",".join(
-                [self.visit(data_index) for data_index in field_access.data_index]
+                [
+                    self.visit(data_index, symtable=symtable, **kwargs)
+                    for data_index in field_access.data_index
+                ]
             )
             if offset_str == "":
                 return f"{field_access.name}[{data_index_access}]"
             return f"{field_access.name}[{offset_str},{data_index_access}]"
         return f"{field_access.name}[{offset_str}]"
 
-    def visit_AssignStmt(self, assignment_statement: oir.AssignStmt, **_) -> None:
-        self.body.append(
-            f"{self.visit(assignment_statement.left)} = {self.visit(assignment_statement.right)}"
-        )
-
-    def visit_BinaryOp(self, binary: oir.BinaryOp, **_) -> str:
-        return f"( {self.visit(binary.left)} {binary.op} {self.visit(binary.right)} )"
+    def visit_BinaryOp(self, binary: oir.BinaryOp, **kwargs) -> str:
+        return f"( {self.visit(binary.left, **kwargs)} {binary.op} {self.visit(binary.right, **kwargs)} )"
 
     def visit_Literal(self, literal: oir.Literal, **_) -> str:
         if literal.dtype == gtc_common.DataType.BOOL:
@@ -236,28 +281,14 @@ class DebugCodeGen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
 
         return literal_value
 
-    def visit_Cast(self, cast: oir.Cast, **_) -> str:
-        return f"{self.visit(cast.dtype)}({self.visit(cast.expr)})"
+    def visit_Cast(self, cast: oir.Cast, **kwargs) -> str:
+        return f"{self.visit(cast.dtype, **kwargs)}({self.visit(cast.expr, **kwargs)})"
 
-    def visit_HorizontalExecution(self, horizontal_execution: oir.HorizontalExecution, **_) -> None:
-        for statement in horizontal_execution.body:
-            self.visit(statement)
+    def visit_ScalarAccess(self, scalar_access: oir.ScalarAccess, **_) -> str:
+        return scalar_access.name
 
-    def visit_HorizontalMask(self, horizontal_mask: gtc_common.HorizontalMask, **_) -> None:
-        i_min, i_max = self.visit(horizontal_mask.i, var="i")
-        j_min, j_max = self.visit(horizontal_mask.j, var="j")
-        conditions = []
-        if i_min is not None:
-            conditions.append(f"({i_min}) <= i")
-        if i_max is not None:
-            conditions.append(f"i < ({i_max})")
-        if j_min is not None:
-            conditions.append(f"({j_min}) <= j")
-        if j_max is not None:
-            conditions.append(f"j < ({j_max})")
-        assert len(conditions)
-        if_code = f"if( {' and '.join(conditions)} ):"
-        self.body.append(if_code)
+    def visit_ScalarDecl(self, scalar_declaration: oir.ScalarDecl, **_) -> str:
+        return scalar_declaration.name
 
     def visit_HorizontalInterval(
         self, horizontal_interval: gtc_common.HorizontalInterval, **kwargs
@@ -268,40 +299,18 @@ class DebugCodeGen(codegen.TemplatedGenerator, eve.VisitorWithSymbolTableTrait):
             horizontal_interval.end, **kwargs
         ) if horizontal_interval.end else None
 
-    def visit_HorizontalRestriction(
-        self, horizontal_restriction: oir.HorizontalRestriction, **_
-    ) -> None:
-        self.visit(horizontal_restriction.mask)
-        self.body.indent()
-        self.visit(horizontal_restriction.body)
-        self.body.dedent()
-
-    def visit_VerticalLoop(self) -> None:
-        pass
-
-    def visit_ScalarAccess(self, scalar_access: oir.ScalarAccess, **_) -> str:
-        return scalar_access.name
-
-    def visit_ScalarDecl(self, scalar_declaration: oir.ScalarDecl, **_) -> str:
-        return scalar_declaration.name
-
-    def visit_NativeFuncCall(self, native_function_call: oir.NativeFuncCall, **_) -> str:
-        arglist = [self.visit(arg) for arg in native_function_call.args]
+    def visit_NativeFuncCall(self, native_function_call: oir.NativeFuncCall, **kwargs) -> str:
+        arglist = [self.visit(arg, **kwargs) for arg in native_function_call.args]
         arguments = ",".join(arglist)
         return f"ufuncs.{native_function_call.func.value}({arguments})"
 
-    def visit_UnaryOp(self, unary_operator: oir.UnaryOp, **_) -> str:
-        return f"{unary_operator.op.value} {self.visit(unary_operator.expr)}"
+    def visit_UnaryOp(self, unary_operator: oir.UnaryOp, **kwargs) -> str:
+        return f"{unary_operator.op.value} {self.visit(unary_operator.expr, **kwargs)}"
 
-    def visit_TernaryOp(self, ternary_operator: oir.TernaryOp, **_) -> str:
-        return f"{self.visit(ternary_operator.true_expr)} if {self.visit(ternary_operator.cond)} else {self.visit(ternary_operator.false_expr)}"
+    def visit_TernaryOp(self, ternary_operator: oir.TernaryOp, **kwargs) -> str:
+        return f"{self.visit(ternary_operator.true_expr, **kwargs)} if {self.visit(ternary_operator.cond, **kwargs)} else {self.visit(ternary_operator.false_expr, **kwargs)}"
 
-    def visit_MaskStmt(self, mask_statement: oir.MaskStmt, **_) -> None:
-        self.body.append(f"if {self.visit(mask_statement.mask)}:")
-        with self.body.indented():
-            for statement in mask_statement.body:
-                self.visit(statement)
-
+    # The visitors that are blacklisted for all the things that should not occur are here
     def visit_LocalScalar(self, local_scalar: oir.LocalScalar, **__) -> None:
         raise NotImplementedError(
             "This state should not be reached because LocalTemporariesToScalars should not have been called."
