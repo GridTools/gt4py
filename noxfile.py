345cd3b5d97b42ffcfb738d98a48da58f87edcd8
Edoardo Paone <edoardo.paone@cscs.ch>
2025-07-21 18:01:16 +0200

    Revert "restrict tests"
    This reverts commit 4cb7936fdd391a602bfe4862601816941a9fc1bb.

--- noxfile.py
+++ noxfile.py
@@ -165,7 +165,7 @@ def test_cartesian(
     session.run(
         *"pytest --cache-clear -sv -n auto --dist loadgroup".split(),
         *("-m", f"{markers}"),
-        "tests/cartesian_tests/integration_tests/multi_feature_tests",
+        str(pathlib.Path("tests") / "cartesian_tests"),
         *session.posargs,
     )
     session.run(
