5655f1a94df78ee344384ae9a0a72b2fc528fe6c
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-07-22 14:12:04 +0200

    Improved unit test

--- tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/transformation_tests/test_remove_point_view.py
+++ tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/transformation_tests/test_remove_point_view.py
@@ -68,13 +68,9 @@ def _make_sdfg_with_map_with_view(
         dtype=dace.float64,
     )
 
-    # tmp2 = sdfg.add_scalar("tmp2", dtype=dace.float64)
-
     tmp_access_node2 = state.add_transient("tmp2", shape=(1,), dtype=dace.float64)
-
     
     state.add_edge(a, None, mentry, "IN_a", dace.Memlet("a[i, j]"))
-    # Now assemble everything.
     state.add_edge(mentry, "OUT_a", task1, "__in0", dace.Memlet("a[i, j]"))
     state.add_edge(task1, "__out0", tmp_access_node, None, dace.Memlet("tmp[0]"))
     state.add_edge(tmp_access_node, None, tmp_access_node2, None, dace.Memlet("tmp[0]"))
@@ -98,7 +94,10 @@ def test_horizontal_map_fusion():
     sdfg = _make_sdfg_with_map_with_view(N)
 
     assert util.count_nodes(sdfg, dace_nodes.AccessNode) == 4
+    assert len([access_node for access_node, _ in sdfg.all_nodes_recursive() if isinstance(access_node, dace_nodes.AccessNode) and isinstance(access_node.desc(sdfg), dace.data.ArrayView)]) == 1
 
+    res, ref = util.make_sdfg_args(sdfg)
+    util.compile_and_run_sdfg(sdfg, **ref)
 
     sdfg.apply_transformations_once_everywhere(
         gtx_transformations.RemovePointwiseViews,
@@ -106,7 +105,10 @@ def test_horizontal_map_fusion():
         validate_all=True,
     )
 
+    util.compile_and_run_sdfg(sdfg, **res)
+
     assert util.count_nodes(sdfg, dace_nodes.MapEntry) == 1
     assert util.count_nodes(sdfg, dace_nodes.MapExit) == 1
     assert util.count_nodes(sdfg, dace_nodes.AccessNode) == 3
-
+    assert len([access_node for access_node, _ in sdfg.all_nodes_recursive() if isinstance(access_node, dace_nodes.AccessNode) and isinstance(access_node.desc(sdfg), dace.data.ArrayView)]) == 0
+    util.compare_sdfg_res(ref=ref, res=res)
