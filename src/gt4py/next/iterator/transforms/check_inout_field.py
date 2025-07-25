5e4601c09b23ee3fb74bf0f58d49005f5b7217a0
Sara Faghih-Naini <sara.faghihnaini@ecmwf.int>
2025-07-25 18:55:59 +0200

    Add pass which checks if a field that is written to is also read with an offset

---
+++ src/gt4py/next/iterator/transforms/check_inout_field.py
@@ -0,0 +1,80 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import dataclasses
+
+from gt4py.eve import NodeTranslator, PreserveLocationVisitor
+from gt4py.next import common
+from gt4py.next.iterator import ir as itir
+from gt4py.next.iterator.ir_utils import common_pattern_matcher as cpm, ir_makers as im
+from gt4py.next.iterator.transforms import collapse_tuple, trace_shifts
+
+@dataclasses.dataclass(frozen=True)
+class CheckInOutField(PreserveLocationVisitor, NodeTranslator):
+    """
+    Checks within a SetAt if any fields which are written to are also read with an offset and raises a ValueError in this case.
+
+    Example:
+        >>> from gt4py.next.iterator.transforms import infer_domain
+        >>> from gt4py.next.type_system import type_specifications as ts
+        >>> float_type = ts.ScalarType(kind=ts.ScalarKind.FLOAT64)
+        >>> IDim = common.Dimension(value="IDim", kind=common.DimensionKind.HORIZONTAL)
+        >>> i_field_type = ts.FieldType(dims=[IDim], dtype=float_type)
+        >>> offset_provider={"IOff": IDim}
+        >>> cartesian_domain = im.call("cartesian_domain")(im.call("named_range")(itir.AxisLiteral(value="IDim"), 0, 5))
+        >>> ir = itir.Program(
+        ...     id="test",
+        ...     function_definitions=[],
+        ...     params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        ...     declarations=[],
+        ...     body=[
+        ...         itir.SetAt(
+        ...             expr=im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 1)("x"))))(im.ref("inout")),
+        ...             domain=cartesian_domain,
+        ...             target=im.ref("inout"),
+        ...         ),
+        ...     ],
+        ... )
+        >>> CheckInOutField.apply(ir, offset_provider=offset_provider)
+        Traceback (most recent call last):
+        ...
+        ValueError: The target inout is also read with an offset.
+        """
+
+    @classmethod
+    def apply(cls, program: itir.Program, offset_provider: common.OffsetProvider | common.OffsetProviderType):
+        return cls().visit(program, offset_provider=offset_provider)
+
+    def visit_SetAt(self, node: itir.SetAt, **kwargs) -> itir.SetAt:
+        offset_provider = kwargs["offset_provider"]
+
+        def as_fieldop_subexprs(expr):
+            """Return a list of all subexpressions in expr.args, including expr itself."""
+            subexprs = [expr]
+            if cpm.is_applied_as_fieldop(expr):
+                for arg in expr.args:
+                    subexprs.extend(as_fieldop_subexprs(arg))
+            return subexprs
+
+        def check_expr(fun, args, offset_provider):
+            shifts = trace_shifts.trace_stencil(fun, num_args=len(args))
+            for arg, shift in zip(args, shifts):
+                for subexpr in as_fieldop_subexprs(arg):
+                    if subexpr == node.target:
+                        if shift not in (set(), {()}):
+                            # This condition is just to filter out the trivial offsets in the horizontal and vertical.
+                            if any(offset_provider[off.value].kind not in {common.DimensionKind.HORIZONTAL, common.DimensionKind.VERTICAL} or val.value != 0 for off, val in shift):
+                                raise ValueError(f"The target {node.target} is also read with an offset.")
+                if cpm.is_applied_as_fieldop(arg):
+                    check_expr(arg.fun, arg.args, offset_provider)
+
+        if cpm.is_applied_as_fieldop(node.expr):
+            check_expr(node.expr.fun, node.expr.args, offset_provider)
+
+        return node
+
---
+++ tests/next_tests/unit_tests/iterator_tests/transforms_tests/test_check_inout_field.py
@@ -0,0 +1,224 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from typing import Optional
+
+import pytest
+from next_tests.toy_connectivity import e2v_conn
+
+from gt4py.next import common
+from gt4py.next.iterator import ir as itir
+from gt4py.next.iterator.ir_utils import ir_makers as im
+from gt4py.next.iterator.transforms.check_inout_field import CheckInOutField
+from gt4py.next.type_system import type_specifications as ts
+
+float_type = ts.ScalarType(kind=ts.ScalarKind.FLOAT64)
+IDim = common.Dimension(value="IDim", kind=common.DimensionKind.HORIZONTAL)
+offset_provider = {"IOff": IDim}
+i_field_type = ts.FieldType(dims=[IDim], dtype=float_type)
+cartesian_domain = im.call("cartesian_domain")(
+    im.call("named_range")(itir.AxisLiteral(value="IDim"), 0, 5), itir.AxisLiteral(value="JDim"), 0, 7)
+
+
+def program_factory(
+    params: list[itir.Sym],
+    body: list[itir.SetAt],
+    declarations: Optional[list[itir.Temporary]] = None,
+) -> itir.Program:
+    return itir.Program(
+        id="testee",
+        function_definitions=[],
+        params=params,
+        declarations=declarations or [],
+        body=body,
+    )
+
+
+def test_check_inout_no_offset():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.ref("deref"))(im.ref("inout")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    # Should not raise
+    assert ir == CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_zero_offset():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 0)("x"))))(im.ref("inout")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    # Should not raise
+    assert ir == CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_e2v_zero_offset():
+    offset_provider = {"E2V": e2v_conn}  # override
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x")(im.deref(im.shift("E2V", 0)("x"))))(im.ref("inout")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    with pytest.raises(ValueError, match="The target inout is also read with an offset."):
+        CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_offset():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 1)("x"))))(im.ref("inout")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    with pytest.raises(ValueError, match="The target inout is also read with an offset."):
+        CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_shift_different_field():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x", "y")(im.plus(im.deref(im.shift("IOff", 0)("x")), im.deref(im.shift("IOff", 1)("y")))))(im.ref("inout"), im.ref("in")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    assert ir == CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_in_arg():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 1)("x"))))(im.as_fieldop(im.ref("deref"))(im.ref("inout"))),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    with pytest.raises(ValueError, match="The target inout is also read with an offset."):
+        CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_in_arg_two_fields():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x", "y")(im.plus(im.deref(im.shift("IOff", 1)("x")), im.deref(im.shift("IOff", 0)("y")))))(im.as_fieldop(im.ref("deref"))(im.ref("inout")), im.ref("in")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    with pytest.raises(ValueError, match="The target inout is also read with an offset."):
+        CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_in_arg_shift_different_field():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x", "y")(im.plus(im.deref(im.shift("IOff", 0)("x")), im.deref(im.shift("IOff", 1)("y")))))(im.as_fieldop(im.ref("deref"))(im.ref("inout")), im.ref("in")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    assert ir == CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_in_arg_shifted():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x", "y")(im.plus(im.deref(im.shift("IOff", 0)("x")), im.deref(im.shift("IOff", 0)("y")))))(im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 1)("x"))))(im.ref("inout")), im.ref("in")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    with pytest.raises(ValueError, match="The target inout is also read with an offset."):
+        CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_in_arg_nested_shifted():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x", "y")(im.plus(im.deref(im.shift("IOff", 0)("x")), im.deref(im.shift("IOff", 0)("y")))))(im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 0)("x"))))(im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 1)("x"))))(im.ref("inout"))), im.ref("in")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    with pytest.raises(ValueError, match="The target inout is also read with an offset."):
+        CheckInOutField.apply(ir, offset_provider=offset_provider)
+
+
+def test_check_inout_in_arg_nested_shift_different_arg():
+    ir = program_factory(
+        params=[im.sym("inout", i_field_type), im.sym("in", i_field_type)],
+        declarations=[],
+        body=[
+            itir.SetAt(
+                expr=im.as_fieldop(im.lambda_("x", "y")(im.plus(im.deref(im.shift("IOff", 0)("x")), im.deref(im.shift("IOff", 0)("y")))))(im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 0)("x"))))(im.as_fieldop(im.lambda_("x")(im.deref(im.shift("IOff", 1)("x"))))(im.ref("in"))), im.ref("inout")),
+                domain=cartesian_domain,
+                target=im.ref("inout"),
+            ),
+        ],
+    )
+
+    assert ir == CheckInOutField.apply(ir, offset_provider=offset_provider)
