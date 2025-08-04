40d499948fa47a5e795211c686bcb9ecbac81f5e
Roman Cattaneo <romanc@users.noreply.github.com>
2025-08-04 10:48:42 +0200

    feat[cartesian]: literal precision (float and int) (#2187)
    ## Description
    
    This PR adds support for specifying the default literal precision (32 or
    64 bits) when using `int` or `float` declarations/casts. The default
    literal precision determined from the system's architecture and can be
    overwritten by the environment variables `GT4PY_LITERAL_INT_PRECISION`
    and `GT4PY_LITERAL_FLOAT_PRECISION`. Build options allow to specify the
    precision per stencil if needed.
    
    The PR also adds support for explicit casting through `int32` / `int64`
    and `float32` / `float64` casts.
    
    
    [Compared](https://github.com/GridTools/gt4py/pull/2187/files/5d9a37e21a44e199421611e6f77a91df7d725f9f..606f8a29d43c4dfa93eca4858c184740cd4b07c0)
    to work by @FlorianDeconinck, we have
    
    - separated `int`  and  `float` precision specification
    - consolidated the many literal precision defaults into
    `definitions.LITERAL_{INT, FLOAT}_PRECISION`
    - don't expose `int` and `float` in `gtscript`'s `__ALL__` field. They
    aren't defined and doctests thus complained.
    - use `ast.unparse` directly (the wrapper function was removed recently
    because we dropped support for python < 3.10)
    - remove print debug statements
    
    ## Requirements
    
    - [x] All fixes and/or new features come with corresponding tests.
    - [x] Important design decisions have been documented in the appropriate
    ADR inside the [docs/development/ADRs/](docs/development/ADRs/README.md)
    folder.
    
    ---------
    
    Co-authored-by: Roman Cattaneo <1116746+romanc@users.noreply.github.com>
    Co-authored-by: Florian Deconinck <deconinck.florian@gmail.com>
    Co-authored-by: Tobias Wicky-Pfund <wicky.tob@gmail.com>

--- src/gt4py/cartesian/frontend/defir_to_gtir.py
+++ src/gt4py/cartesian/frontend/defir_to_gtir.py
@@ -340,6 +340,10 @@ class DefIRToGTIR(IRNodeVisitor):
         NativeFunction.FLOOR: common.NativeFunction.FLOOR,
         NativeFunction.CEIL: common.NativeFunction.CEIL,
         NativeFunction.TRUNC: common.NativeFunction.TRUNC,
+        NativeFunction.INT32: common.NativeFunction.INT32,
+        NativeFunction.INT64: common.NativeFunction.INT64,
+        NativeFunction.FLOAT32: common.NativeFunction.FLOAT32,
+        NativeFunction.FLOAT64: common.NativeFunction.FLOAT64,
         NativeFunction.ERF: common.NativeFunction.ERF,
         NativeFunction.ERFC: common.NativeFunction.ERFC,
     }
