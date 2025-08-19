2e356b25304736b7fe33abb695226113efe6e5f5
Hannes Vogt <vogt@hey.com>
2025-08-19 14:07:05 +0200

    one more try

--- tests/cartesian_tests/integration_tests/multi_feature_tests/test_code_generation.py
+++ tests/cartesian_tests/integration_tests/multi_feature_tests/test_code_generation.py
@@ -864,7 +864,7 @@ def test_k_offset_field_stencil(backend):
 @pytest.mark.parametrize("backend", ALL_BACKENDS)
 def test_k_only_access_stencil(backend):
     field_in = gt_storage.from_array(
-        [2, 3, 4, 5], dtype=np.float64, backend=backend, shape=(4,), aligned_index=(0,)
+        [2, 3, 4, 5], dtype=np.float64, backend=backend, aligned_index=(0,)
     )
     field_out = gt_storage.zeros(
         dtype=np.float64, backend=backend, shape=(4, 4, 4), aligned_index=(0, 0, 0)
@@ -890,7 +890,7 @@ def test_k_only_access_stencil(backend):
 @pytest.mark.parametrize("backend", ALL_BACKENDS)
 def test_table_access_stencil(backend):
     table_view = gt_storage.from_array(
-        [2, 3, 4, 5], dtype=np.float64, backend=backend, shape=(4,), aligned_index=(0,)
+        [2, 3, 4, 5], dtype=np.float64, backend=backend, aligned_index=(0,)
     )
     field_out = gt_storage.zeros(
         dtype=np.float64, backend=backend, shape=(4, 4, 4), aligned_index=(0, 0, 0)
