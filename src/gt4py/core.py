f4e3f2e322269c56b2e5a76b8009a146980e9211
Hannes Vogt <vogt@hey.com>
2025-08-28 15:43:28 +0200

    more stuff

--- src/gt4py/core.py
+++ src/gt4py/core.py
@@ -6,7 +6,13 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from gt4py._core.definitions import Scalar, is_scalar_type
+from gt4py._core.definitions import CUPY_DEVICE_TYPE, Device, DeviceType, Scalar, is_scalar_type
 
 
-__all__ = ["Scalar", "is_scalar_type"]
+__all__ = [
+    "CUPY_DEVICE_TYPE",
+    "Device",
+    "DeviceType",
+    "Scalar",
+    "is_scalar_type",
+]
