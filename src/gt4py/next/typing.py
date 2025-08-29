3cba669150233f1d7cc7f95e86ea1ec4793a54a9
Hannes Vogt <vogt@hey.com>
2025-08-29 12:16:16 +0200

    put somethign on annotated

--- src/gt4py/next/typing.py
+++ src/gt4py/next/typing.py
@@ -6,7 +6,7 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Annotated, TypeAlias
+from typing import Annotated, Final, TypeAlias
 
 from gt4py._core.definitions import Scalar
 from gt4py.next import backend
@@ -14,10 +14,12 @@ from gt4py.next.common import OffsetProvider
 from gt4py.next.ffront import decorator
 
 
+_ONLY_FOR_TYPING: Final[str] = "only for typing"
+
 # TODO(havogt): alternatively we could introduce Protocols
-Program: TypeAlias = Annotated[decorator.Program, ""]
-FieldOperator: TypeAlias = Annotated[decorator.FieldOperator, ""]
-Backend: TypeAlias = Annotated[backend.Backend, ""]
+Program: TypeAlias = Annotated[decorator.Program, _ONLY_FOR_TYPING]
+FieldOperator: TypeAlias = Annotated[decorator.FieldOperator, _ONLY_FOR_TYPING]
+Backend: TypeAlias = Annotated[backend.Backend, _ONLY_FOR_TYPING]
 
 
 __all__ = [
