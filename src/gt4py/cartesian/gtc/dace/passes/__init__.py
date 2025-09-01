f0609dadf8bdc2e40df75af334c2567b155e486e
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2025-09-01 11:46:06 +0200

    feat[cartesian]: add dace:cpu_kfirst backend
    Differentiate backends per layout as gt does (for now). The default
    `dace:cpu` backend has `K-I-J`-loops and thus a memory layout of `J-I-K`
    (from smallest to biggest stride).
    
    This commit adds a "K-first" layout and loop structure with `I-J-K`
    loops and a `K-J-I` memory layout (from smallest to biggest stride).

---
+++ src/gt4py/cartesian/gtc/dace/passes/__init__.py
@@ -0,0 +1,12 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .PushVerticalMapDown import PushVerticalMapDown
+
+
+__all__ = ["PushVerticalMapDown"]
