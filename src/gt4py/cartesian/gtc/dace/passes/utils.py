a6a68f81497161e4df9be64b53851b2ddacee945
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2025-09-01 11:46:06 +0200

    feat[cartesian]: add dace:cpu_kfirst backend
    Differentiate backends per layout as gt does (for now). The default
    `dace:cpu` backend has `K-I-J`-loops and thus a memory layout of `J-I-K`
    (from smallest to biggest stride).
    
    This commit adds a "K-first" layout and loop structure with `I-J-K`
    loops and a `K-J-I` memory layout (from smallest to biggest stride).

---
+++ src/gt4py/cartesian/gtc/dace/passes/utils.py
@@ -0,0 +1,26 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from dace.sdfg.analysis.schedule_tree import treenodes as tn
+
+
+def list_index(collection: list[tn.ScheduleTreeNode], node: tn.ScheduleTreeNode) -> int:
+    """
+    Get the index of `node` in `collection` with `is` operator. Raises `StopIteration` if not found.
+
+    Comparing with the `is` operator ensures memory comparison. The function `list.index()`
+    uses value comparison and might thus yield different results.
+    """
+    index = 0
+    for element in collection:
+        # compare with "is" to get memory comparison. ".index()" uses value comparison
+        if element is node:
+            return index
+        index += 1
+
+    raise StopIteration
