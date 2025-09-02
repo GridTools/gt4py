7325785400724477aee8e7b113b2a47eaca35e52
Edoardo Paone <edoardo.paone@cscs.ch>
2025-09-02 10:55:30 +0200

    fix test expectation

--- tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_gtir_to_sdfg.py
+++ tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_gtir_to_sdfg.py
@@ -2151,11 +2151,11 @@ def test_gtir_index():
     sdfg = build_dace_sdfg(testee, CARTESIAN_OFFSETS)
 
     ref = np.concatenate(
-        (v[:MARGIN], np.arange(MARGIN, N - MARGIN, dtype=np.int32), v[N - MARGIN :])
+        (v[:MARGIN], np.arange(MARGIN, N - MARGIN, dtype=np.int32) * 2 + 1, v[N - MARGIN :])
     )
 
     sdfg(v, **FSYMBOLS)
-    np.allclose(v, ref)
+    assert np.all(v == ref)
 
 
 def test_gtir_concat_where():
