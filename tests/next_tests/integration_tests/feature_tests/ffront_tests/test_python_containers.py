03b4a29ea33f82d62615a4730937ee0226a13233
Hannes Vogt <vogt@hey.com>
2025-09-26 15:19:34 +0200

    add failing mixed nesting test

--- tests/next_tests/integration_tests/feature_tests/ffront_tests/test_python_containers.py
+++ tests/next_tests/integration_tests/feature_tests/ffront_tests/test_python_containers.py
@@ -148,6 +148,58 @@ def test_named_tuple_like_constructed_inside(cartesian_case, testee):
     )
 
 
+@dataclasses.dataclass
+class NestedContainer:
+    a: tuple[gtx.Field[[IDim, JDim], gtx.float32], NamedTupleContainer]
+    b: tuple[DataclassContainer, gtx.Field[[IDim, JDim], gtx.float32]]
+
+
+@gtx.field_operator
+def nested_mixed_containers(
+    inp: tuple[NestedContainer, gtx.Field[[IDim, JDim], gtx.float32]],
+) -> tuple[gtx.Field[[IDim, JDim], gtx.float32], NestedContainer]:
+    # swap the single fields and the elements of the containers between a and b
+    old_dataclass = inp[0].b[0]
+    old_named_tuple = inp[0].a[1]
+    new_named_tuple = NamedTupleContainer(u=old_dataclass.u, v=old_dataclass.v)
+    new_dataclass = DataclassContainer(u=old_named_tuple.u, v=old_named_tuple.v)
+    new_nested_container = NestedContainer(
+        a=(inp[0].b[1], new_named_tuple), b=(new_dataclass, inp[0].a[0])
+    )
+    return (inp[1], new_nested_container)
+
+
+@gtx.program
+def nested_mixed_containers_program(
+    inp: tuple[NestedContainer, gtx.Field[[IDim, JDim], gtx.float32]],
+    out: tuple[gtx.Field[[IDim, JDim], gtx.float32], NestedContainer],
+) -> None:
+    nested_mixed_containers(inp, out=out)
+
+
+def test_nested_mixed_containers(cartesian_case):
+    inp = cases.allocate(cartesian_case, nested_mixed_containers, "inp")()
+    out = cases.allocate(
+        cartesian_case,
+        nested_mixed_containers,
+        "out",
+    )()
+
+    cases.verify(
+        cartesian_case,
+        nested_mixed_containers,
+        inp,
+        out=out,
+        ref=(
+            inp[1],
+            NestedContainer(
+                a=(inp[0].b[1], NamedTupleContainer(u=inp[0].b[0].u, v=inp[0].b[0].v)),
+                b=(DataclassContainer(u=inp[0].a[1].u, v=inp[0].a[1].v), inp[0].a[0]),
+            ),
+        ),
+    )
+
+
 @pytest.mark.xfail(
     reason="We store the qualified name to the actual Python type in the `NamedTupleType`."
 )
