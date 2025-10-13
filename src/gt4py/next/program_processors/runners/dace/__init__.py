a429691531f583348edf71138218c76563c17259
Edoardo Paone <edoardo.paone@cscs.ch>
2025-10-13 18:31:10 +0200

    remove extra change

--- src/gt4py/next/program_processors/runners/dace/__init__.py
+++ src/gt4py/next/program_processors/runners/dace/__init__.py
@@ -9,10 +9,6 @@
 
 from gt4py.next.program_processors.runners.dace.gtir_to_sdfg import build_sdfg_from_gtir
 from gt4py.next.program_processors.runners.dace.sdfg_callable import get_sdfg_args
-from gt4py.next.program_processors.runners.dace.transformations import (
-    GT4PyAutoOptHook,
-    GT4PyAutoOptHookFun,
-)
 from gt4py.next.program_processors.runners.dace.workflow.backend import (
     make_dace_backend,
     run_dace_cpu,
@@ -25,8 +21,6 @@ from gt4py.next.program_processors.runners.dace.workflow.backend import (
 
 
 __all__ = [
-    "GT4PyAutoOptHook",
-    "GT4PyAutoOptHookFun",
     "build_sdfg_from_gtir",
     "get_sdfg_args",
     "make_dace_backend",
