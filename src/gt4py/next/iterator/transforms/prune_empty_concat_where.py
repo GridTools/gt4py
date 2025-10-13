b2f29d1a3c2a8e440e777995d0c2817510d18ccf
tehrengruber <till.ehrengruber@cscs.ch>
2025-10-13 15:10:34 +0200

    Cleanup

--- src/gt4py/next/iterator/transforms/prune_empty_concat_where.py
+++ src/gt4py/next/iterator/transforms/prune_empty_concat_where.py
@@ -6,7 +6,7 @@
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
 import dataclasses
-from typing import Container
+from typing import Container, Self
 
 from gt4py.eve import NodeTranslator, PreserveLocationVisitor
 from gt4py.next import common
