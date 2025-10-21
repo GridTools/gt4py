GridTools/gt4py

commit 895412097d86bcb8d58b4019cb4c857b435d53ed

feat[next][dace]: collect sdfg execution time with tasklet (#2280)
This PR provides an alternative way to retrieve the SDFG execution time,
which corresponds to the stencil compute time metric in GT4Py.
The previous solution relied on SDFG instrumentation, which is a complex
framework designed to profile full-application SDFGs, to collect
different kinds of counters, and to report metrics across several
iterations of a benchmark run. This framework already exists in GT4Py,
we only need to timestamp the start and end time of the SDFG execution.
The solution presented in this PR is much more lightweight. More
importantly, it surpasses some limitation of the dace framework, namely
the small precision of the time measurements (dace truncates the SDFG
execution time to microseconds).
Another advantage is that the collection of compute time metric, and the
corresponding synchronization point for SDFG execution, can be enabled
or disabled by means of the GT4Py config option without recompiling the
program. This aligns the behavior of the dace backend with the gtfn
backend and makes it easier to collect metrics.

src/gt4py/next/program_processors/runners/dace/workflow/translation.py
@@ -113,6 +113,116 @@ def _has_gpu_schedule(sdfg: dace.SDFG) -> bool:
     )
 
 
+def add_instrumentation(sdfg: dace.SDFG, gpu: bool) -> None:
+    """
+    Instrument SDFG with measurement of total execution time.
+
+    We measure the execution time of one GT4Py program by instrumenting the top-level
+    SDFG with a cpp timer (std::chrono). This timer measures only the computation
+    time, it does not include the overhead of calling the SDFG from Python.
+
+    The execution time is measured in seconds and represented as a 'float64' value.
+    It is returned from the SDFG as a one-element array in the '__return' data node.
+    """
+    output, _ = sdfg.add_array("__return", [1], dace.float64)
+    start_time, _ = sdfg.add_scalar("gt_start_time", dace.float64, transient=True)
+    metrics_level = sdfg.add_symbol(gtx_wfdcommon.SDFG_ARG_METRIC_LEVEL, dace.int32)
+
+    #### 1. Synchronize the CUDA device, in order to wait for kernels completion.
+    # Even when the target device is GPU, it can happen that dace emits code without
+    # GPU kernels. In this case, the cuda headers are not imported and the SDFG is
+    # compiled as plain C++. Therefore, we also check here the schedule of SDFG maps.
+    if gpu and _has_gpu_schedule(sdfg):
+        dace_gpu_backend = dace.Config.get("compiler.cuda.backend")
+        assert dace_gpu_backend in ["cuda", "hip"], f"GPU backend '{dace_gpu_backend}' is unknown."
+
+        # NOTE: We should actually wrap the `DeviceSynchronize` function inside a
+        #   `DACE_GPU_CHECK()` macro. However, this only works in GPU context, but
+        #   here we are in CPU context. Thus we cannot do it.
+        sync_code = f"{dace_gpu_backend}DeviceSynchronize();"
+        has_side_effects = True
+
+    else:
+        sync_code = ""
+        has_side_effects = False
+
+    #### 2. Timestamp the SDFG entry point.
+    entry_state = sdfg.add_state("gt_timer_entry")
+    begin_state = sdfg.add_state_after(
+        entry_state, "gt_timer_begin", condition=f"{metrics_level} >= {metrics.PERFORMANCE}"
+    )
+
+    for source_state in sdfg.source_nodes():
+        if source_state is entry_state:
+            continue
+        sdfg.add_edge(
+            entry_state,
+            source_state,
+            dace.InterstateEdge(condition=f"{metrics_level} < {metrics.PERFORMANCE}"),
+        )
+        sdfg.add_edge(begin_state, source_state, dace.InterstateEdge())
+    assert sdfg.out_degree(begin_state) > 0
+    assert sdfg.out_degree(entry_state) > 1
+
+    tlet_start_timer = begin_state.add_tasklet(
+        "gt_start_timer",
+        inputs={},
+        outputs={"time"},
+        code="""\
+time = static_cast<double>(
+    std::chrono::duration_cast<std::chrono::nanoseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch()).count()
+) / 1e9;
+        """,
+        language=dace.dtypes.Language.CPP,
+    )
+    begin_state.add_edge(
+        tlet_start_timer,
+        "time",
+        begin_state.add_access(start_time),
+        None,
+        dace.Memlet(f"{start_time}[0]"),
+    )
+
+    #### 3. Collect the SDFG end timestamp and produce the compute metric.
+    end_state = sdfg.add_state("gt_timer_end")
+    for sink_state in sdfg.sink_nodes():
+        if sink_state is end_state:
+            continue
+        sdfg.add_edge(
+            sink_state,
+            end_state,
+            dace.InterstateEdge(condition=f"{metrics_level} >= {metrics.PERFORMANCE}"),
+        )
+    assert sdfg.in_degree(end_state) > 0
+
+    tlet_stop_timer = end_state.add_tasklet(
+        "gt_stop_timer",
+        inputs={"run_cpp_start_time"},
+        outputs={"duration"},
+        code=sync_code
+        + """
+double run_cpp_end_time = static_cast<double>(
+    std::chrono::duration_cast<std::chrono::nanoseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch()).count()
+) / 1e9;
+duration = run_cpp_end_time - run_cpp_start_time;
+        """,
+        language=dace.dtypes.Language.CPP,
+        side_effects=has_side_effects,
+    )
+    end_state.add_edge(
+        end_state.add_access(start_time),
+        None,
+        tlet_stop_timer,
+        "run_cpp_start_time",
+        dace.Memlet(f"{start_time}[0]"),
+    )
+    end_state.add_edge(
+        tlet_stop_timer, "duration", end_state.add_access(output), None, dace.Memlet(f"{output}[0]")
+    )
+
+
 def make_sdfg_call_sync(sdfg: dace.SDFG, gpu: bool) -> None:
     """Process the SDFG such that the call is synchronous.
 
@@ -188,7 +298,8 @@ class DaCeTranslator(
 ):
     device_type: core_defs.DeviceType
     auto_optimize: bool
-    async_sdfg_call: bool = False
+    async_sdfg_call: bool
+    use_metrics: bool
     disable_itir_transforms: bool = False
     disable_field_origin_on_program_arguments: bool = False
 
@@ -259,20 +370,13 @@ class DaCeTranslator(
             gtx_transformations.gt_simplify(sdfg)
             gtx_transformations.gt_gpu_transformation(sdfg, try_removing_trivial_maps=True)
 
-        async_sdfg_call = False
-        if config.COLLECT_METRICS_LEVEL != metrics.DISABLED:
-            # We measure the execution time of one program by instrumenting the
-            #   top-level SDFG with a cpp timer (std::chrono). This timer measures
-            #   only the computation time, it does not include the overhead of
-            #   calling the SDFG from Python.
-            sdfg.instrument = dace.dtypes.InstrumentationType.Timer
-        elif self.async_sdfg_call:
-            async_sdfg_call = True
-
-        if async_sdfg_call:
+        if self.async_sdfg_call:
             make_sdfg_call_async(sdfg, on_gpu)
         else:
             make_sdfg_call_sync(sdfg, on_gpu)
+
+        if self.use_metrics:
+            add_instrumentation(sdfg, on_gpu)
 
         return sdfg
 

src/gt4py/next/program_processors/runners/dace/program.py
@@ -96,7 +96,9 @@ class Program(decorator.Program, dace.frontend.python.common.SDFGConvertible):
         # the other parts of the workaround when possible.
         sdfg = dace.SDFG.from_json(
             compile_workflow_translation.replace(
-                disable_itir_transforms=True, disable_field_origin_on_program_arguments=True
+                disable_itir_transforms=True,
+                disable_field_origin_on_program_arguments=True,
+                use_metrics=False,
             )(gtir_stage).source_code
         )
 

src/gt4py/next/program_processors/runners/dace/workflow/backend.py
@@ -77,6 +77,7 @@ def make_dace_backend(
     use_memory_pool: bool,
     blocking_dim: common.Dimension | None,
     blocking_size: int = 10,
+    use_metrics: bool = True,
     use_zero_origin: bool = False,
 ) -> backend.Backend:
     """Helper function to create a dace cached backend with custom config for SDFG
@@ -98,6 +99,7 @@ def make_dace_backend(
             on this dimension.
         blocking_size: Block size to use in 'LoopBlocking' SDFG transformation,
             when enabled.
+        use_metrics: Add SDFG instrumention to collect the stencil compute time.
         use_zero_origin: Can be set to `True` when all fields passed as program
             arguments have zero-based origin. This setting will skip generation
             of range start-symbols `_range_0` since they can be assumed to be zero.
@@ -116,6 +118,7 @@ def make_dace_backend(
         otf_workflow__bare_translation__disable_field_origin_on_program_arguments=use_zero_origin,
         otf_workflow__bare_translation__make_persistent=make_persistent,
         otf_workflow__bare_translation__use_memory_pool=use_memory_pool,
+        otf_workflow__bare_translation__use_metrics=use_metrics,
         otf_workflow__bindings__make_persistent=make_persistent,
     )
 

src/gt4py/next/program_processors/runners/dace/workflow/common.py
@@ -7,12 +7,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import contextlib
-from typing import Any, Generator, Optional
+from typing import Any, Final, Generator, Optional
 
 import dace
 
 from gt4py._core import definitions as core_defs
 from gt4py.next import config
+
+
+SDFG_ARG_METRIC_LEVEL: Final[str] = "gt_metrics_level"
+"""Name of SDFG argument to retrive the GT4Py metrics level."""
 
 
 def set_dace_config(

src/gt4py/next/program_processors/runners/dace/workflow/compilation.py
@@ -85,13 +85,11 @@ class CompiledDaceProgram(stages.CompiledProgram):
             binding_module_name, bind_func_name, binding_source.source_code
         )
 
-    def __call__(self, *args: Any, **kwargs: Any) -> None:
-        result = self.sdfg_program(*args, **kwargs)
-        assert result is None
+    def __call__(self, **kwargs: Any) -> Any:
+        return self.sdfg_program(**kwargs)
 
-    def fast_call(self) -> None:
-        result = self.sdfg_program.fast_call(*self.sdfg_program._lastargs)
-        assert result is None
+    def fast_call(self) -> Any:
+        return self.sdfg_program.fast_call(*self.sdfg_program._lastargs)
 
 
 @dataclasses.dataclass(frozen=True)

src/gt4py/next/program_processors/runners/dace/workflow/decoration.py
@@ -12,18 +12,24 @@ import functools
 from typing import Any, Sequence
 
 import dace
+import numpy as np
 
 from gt4py._core import definitions as core_defs
 from gt4py.next import common as gtx_common, config, metrics, utils as gtx_utils
 from gt4py.next.otf import stages
-from gt4py.next.program_processors.runners.dace import sdfg_callable, workflow as dace_worflow
-from gt4py.next.program_processors.runners.dace.workflow import common as gtx_wfdcommon
+from gt4py.next.program_processors.runners.dace import sdfg_callable
+from gt4py.next.program_processors.runners.dace.workflow import (
+    common as gtx_wfdcommon,
+    compilation as gtx_wfdcompilation,
+)
 
 
 def convert_args(
-    fun: dace_worflow.compilation.CompiledDaceProgram,
+    fun: gtx_wfdcompilation.CompiledDaceProgram,
     device: core_defs.DeviceType = core_defs.DeviceType.CPU,
 ) -> stages.CompiledProgram:
+    # Retieve metrics level from GT4Py environment variable.
+    collect_time = config.COLLECT_METRICS_LEVEL >= metrics.PERFORMANCE
     # We use the callback function provided by the compiled program to update the SDFG arglist.
     update_sdfg_call_args = functools.partial(
         fun.update_sdfg_ctype_arglist, device, fun.sdfg_argtypes
@@ -33,7 +39,7 @@ def convert_args(
         *args: Any,
         offset_provider: gtx_common.OffsetProvider,
         out: Any = None,
-    ) -> None:
+    ) -> Any:
         if out is not None:
             args = (*args, out)
 
@@ -48,38 +54,28 @@ def convert_args(
                 *flat_args,
                 filter_args=False,
             )
+            this_call_args[gtx_wfdcommon.SDFG_ARG_METRIC_LEVEL] = config.COLLECT_METRICS_LEVEL
             with dace.config.set_temporary("compiler", "allow_view_arguments", value=True):
-                fun(**this_call_args)
+                result = fun(**this_call_args)
 
         else:
             # Initialization of `_lastargs` was done by the `CompiledSDFG` object,
             #  so we just update it with the current call arguments.
             update_sdfg_call_args(args, fun.sdfg_program._lastargs[0])
-            fun.fast_call()
+            result = fun.fast_call()
 
-        metric_collection = metrics.get_active_metric_collection()
-        if (metric_collection is not None) and (
-            config.COLLECT_METRICS_LEVEL >= metrics.PERFORMANCE
-        ):
-            # Observe that dace instrumentation adds runtime overhead:
-            # DaCe writes an instrumentation report file for each SDFG run.
-            with gtx_wfdcommon.dace_context(device_type=device):
-                # We need to set the cache folder and key config in order to retrieve
-                # the SDFG report file.
-                prof_report = fun.sdfg_program.sdfg.get_latest_report()
-            if prof_report is None:
+        if collect_time:
+            if result is None:
                 raise RuntimeError(
-                    "Config 'COLLECT_METRICS_LEVEL' is set but the SDFG profiling report was not found."
-                    " This might indicate that the backend is using a precompiled SDFG from persistent cache"
-                    " without instrumentation."
+                    "Config 'COLLECT_METRICS_LEVEL' is set but the SDFG profiling"
+                    " report was not found. This might indicate that the backend"
+                    " is using a precompiled SDFG from persistent cache without"
+                    " metrics instrumentation."
                 )
-            assert len(prof_report.events) == 1
-            # The event name gets truncated in dace, so we only check that
-            # it corresponds to the beginning of SDFG label.
-            assert f"SDFG {fun.sdfg_program.sdfg.label}".startswith(prof_report.events[0].name)
-            duration_secs = (
-                prof_report.events[0].duration / 1e6
-            )  # dace timer returns the duration in microseconds
-            metric_collection.add_sample(metrics.COMPUTE_METRIC, duration_secs)
+            assert len(result) == 1
+            assert isinstance(result[0], np.float64)
+            metric_collection = metrics.get_active_metric_collection()
+            if metric_collection is not None:
+                metric_collection.add_sample(metrics.COMPUTE_METRIC, result[0].item())
 
     return decorated_program

tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_dace_translation.py
@@ -220,6 +220,7 @@ def test_generate_sdfg_async_call(make_async_sdfg_call: bool, device_type: core_
         device_type=device_type,
         auto_optimize=False,
         async_sdfg_call=make_async_sdfg_call,
+        use_metrics=False,
     ).generate_sdfg(program, offset_provider={}, column_axis=None)
 
     if device_type == core_defs.DeviceType.CPU:
@@ -257,6 +258,7 @@ def test_generate_sdfg_async_call_no_map(device_type: core_defs.DeviceType):
         device_type=device_type,
         auto_optimize=False,
         async_sdfg_call=True,
+        use_metrics=False,
     ).generate_sdfg(program, offset_provider={}, column_axis=None)
 
     if device_type == core_defs.DeviceType.CPU:

commit bd62f3563d1a69889185c249dfe19cc0d805f2ce

fix[next][dace]: SDFG instrumentation with empty else-branch caused memory leak (#2321)
This PR fixes a bug introduced by #2280. The previous PR used a
conditional interstate edge to conditionally execute a sink state, which
contained the timer tasklet to produce the stencil compute time.
However, there was no else-branch, and no unique sink node in the SDFG.
If metrics collection was enabled, the new state would be the sink,
otherwise the predecessor state. The lack of a unique sink node caused
some error in SDFG transformations that led to memory leak, because the
GPU temporary arrays were not properly freed.
This PR builds two conditional blocks, one as unique source and another
one as unique sink node for the SDFG. Inside the conditional block,
metrics collection is represented as a branch with conditional
execution. In this way, the SDFG source and sink nodes are always the
same, no matter whether metrics collection is enabled or not.

src/gt4py/next/program_processors/runners/dace/workflow/translation.py
@@ -140,6 +140,25 @@ def _has_gpu_schedule(sdfg: dace.SDFG) -> bool:
     )
 
 
+def _make_if_region_for_metrics_collection(
+    name: str,
+    metrics_level: str,
+    sdfg: dace.SDFG,
+) -> tuple[dace.state.ConditionalBlock, dace.state.SDFGState]:
+    """
+    Helper function to create a conditional block in the given SDFG, with only one
+    branch to be executed if 'metric_level >= metrics.PERFORMANCE' is true.
+    """
+    if_region = dace.sdfg.state.ConditionalBlock(name)
+    sdfg.add_node(if_region, ensure_unique_name=True)
+    then_body = dace.sdfg.state.ControlFlowRegion(f"{if_region.label}_collect_metrics", sdfg=sdfg)
+    then_state = then_body.add_state(f"{if_region.label}_collect_metrics")
+    if_region.add_branch(
+        dace.sdfg.state.CodeBlock(f"{metrics_level} >= {metrics.PERFORMANCE}"), then_body
+    )
+    return if_region, then_state
+
+
 def add_instrumentation(sdfg: dace.SDFG, gpu: bool) -> None:
     """
     Instrument SDFG with measurement of total execution time.
@@ -174,22 +193,17 @@ def add_instrumentation(sdfg: dace.SDFG, gpu: bool) -> None:
         has_side_effects = False
 
     #### 2. Timestamp the SDFG entry point.
-    entry_state = sdfg.add_state("gt_timer_entry")
-    begin_state = sdfg.add_state_after(
-        entry_state, "gt_timer_begin", condition=f"{metrics_level} >= {metrics.PERFORMANCE}"
+    entry_if_region, begin_state = _make_if_region_for_metrics_collection(
+        "program_entry", metrics_level, sdfg
     )
 
     for source_state in sdfg.source_nodes():
-        if source_state is entry_state:
+        if source_state is entry_if_region:
             continue
-        sdfg.add_edge(
-            entry_state,
-            source_state,
-            dace.InterstateEdge(condition=f"{metrics_level} < {metrics.PERFORMANCE}"),
-        )
-        sdfg.add_edge(begin_state, source_state, dace.InterstateEdge())
-    assert sdfg.out_degree(begin_state) > 0
-    assert sdfg.out_degree(entry_state) > 1
+        sdfg.add_edge(entry_if_region, source_state, dace.InterstateEdge())
+        source_state.is_start_block = False
+    assert sdfg.out_degree(entry_if_region) > 0
+    entry_if_region.is_start_block = True
 
     tlet_start_timer = begin_state.add_tasklet(
         "gt_start_timer",
@@ -212,17 +226,17 @@ time = static_cast<double>(
     )
 
     #### 3. Collect the SDFG end timestamp and produce the compute metric.
-    end_state = sdfg.add_state("gt_timer_end")
-    for sink_state in sdfg.sink_nodes():
-        if sink_state is end_state:
-            continue
-        sdfg.add_edge(
-            sink_state,
-            end_state,
-            dace.InterstateEdge(condition=f"{metrics_level} >= {metrics.PERFORMANCE}"),
-        )
-    assert sdfg.in_degree(end_state) > 0
+    exit_if_region, end_state = _make_if_region_for_metrics_collection(
+        "program_exit", metrics_level, sdfg
+    )
 
+    for sink_state in sdfg.sink_nodes():
+        if sink_state is exit_if_region:
+            continue
+        sdfg.add_edge(sink_state, exit_if_region, dace.InterstateEdge())
+    assert sdfg.in_degree(exit_if_region) > 0
+
+    # Populate the branch that computes the stencil time metric
     tlet_stop_timer = end_state.add_tasklet(
         "gt_stop_timer",
         inputs={"run_cpp_start_time"},
@@ -246,8 +260,13 @@ duration = run_cpp_end_time - run_cpp_start_time;
         dace.Memlet(f"{start_time}[0]"),
     )
     end_state.add_edge(
-        tlet_stop_timer, "duration", end_state.add_access(output), None, dace.Memlet(f"{output}[0]")
+        tlet_stop_timer,
+        "duration",
+        end_state.add_access(output),
+        None,
+        dace.Memlet(f"{output}[0]"),
     )
+
     # Check SDFG validity after applying the above changes.
     sdfg.validate()
 