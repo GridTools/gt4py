945ec84412bcfb514a54de89d66edb3f16d216c9
Florian Deconinck <deconinck.florian@gmail.com>
2025-10-21 11:51:31 -0400

    Move parsing base python version Drop 3.10 testing

--- src/gt4py/cartesian/frontend/gtscript_frontend.py
+++ src/gt4py/cartesian/frontend/gtscript_frontend.py
@@ -48,7 +48,7 @@ from gt4py.cartesian.frontend.exceptions import (
 from gt4py.cartesian.utils import meta as gt_meta, warn_experimental_feature
 
 
-PYTHON_AST_VERSION: Final = (3, 10)
+PYTHON_AST_VERSION: Final = (3, 11)
 ELLIPSIS_TYPE = getattr(ast, "Ellipsis", types.EllipsisType)
 
 
