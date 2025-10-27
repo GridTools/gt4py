9fc176494d8d629cc04f117546ac7a26d26b317f
Ioannis Magkanaris <iomagkanaris@gmail.com>
2025-10-27 14:58:59 +0100

    Added comment for map entries

--- src/gt4py/next/program_processors/runners/dace/workflow/translation.py
+++ src/gt4py/next/program_processors/runners/dace/workflow/translation.py
@@ -270,7 +270,9 @@ duration = run_cpp_end_time - run_cpp_start_time;
     if (config.COLLECT_METRICS_LEVEL == metrics.GPU_TX_MARKERS) and gpu:
         sdfg.instrument = dace.dtypes.InstrumentationType.GPU_TX_MARKERS
         for node, _ in sdfg.all_nodes_recursive():
-            if isinstance(node, dace.nodes.MapEntry):
+            if isinstance(
+                node, dace.nodes.MapEntry
+            ):  # Add ranges to scopes and maps that are NOT scheduled to the GPU
                 node.instrument = dace.dtypes.InstrumentationType.GPU_TX_MARKERS
             elif isinstance(node, dace.sdfg.state.SDFGState):
                 node.instrument = dace.dtypes.InstrumentationType.GPU_TX_MARKERS
