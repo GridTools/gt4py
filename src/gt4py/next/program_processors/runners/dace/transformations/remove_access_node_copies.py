f8d8f649ac078839f920f5e39b13f10df4556fc0
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2025-11-12 10:15:55 +0100

    Make formatting happy

--- src/gt4py/next/program_processors/runners/dace/transformations/remove_access_node_copies.py
+++ src/gt4py/next/program_processors/runners/dace/transformations/remove_access_node_copies.py
@@ -261,4 +261,6 @@ class RemoveAccessNodeCopies(dace_transformation.SingleStateTransformation):
 
         # Update strides after changing the data of access nodes
         for modified_node in [second_node, third_node]:
-                gtx_transformations_strides.gt_propagate_strides_from_access_node(sdfg, graph, modified_node)
+            gtx_transformations_strides.gt_propagate_strides_from_access_node(
+                sdfg, graph, modified_node
+            )
