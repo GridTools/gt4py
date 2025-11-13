6e9723223bb2f37915ff915e9b07c42ef6f31d94
Edoardo Paone <edoardo.paone@cscs.ch>
2025-11-13 15:23:38 +0100

    edit

--- tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_gtir_to_sdfg.py
+++ tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_gtir_to_sdfg.py
@@ -172,7 +172,7 @@ def test_gtir_broadcast():
     sdfg = build_dace_sdfg(testee, CARTESIAN_OFFSETS, skip_domain_inference=True)
 
     sdfg(a, **FSYMBOLS)
-    np.testing.assert_array_equal(a, val)
+    np.testing.assert_allclose(a, np.full_like(a, val))
 
 
 def test_gtir_cast():
@@ -206,7 +206,7 @@ def test_gtir_cast():
     sdfg = build_dace_sdfg(testee, CARTESIAN_OFFSETS)
 
     sdfg(a, b, c, **FSYMBOLS)
-    np.testing.assert_array_equal(c, True)
+    assert all(c)
 
 
 def test_gtir_copy_self():
