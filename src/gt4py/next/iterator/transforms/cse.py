532f0c49e3f5047b736546903474121a78491e4e
Hannes Vogt <vogt@hey.com>
2025-11-28 17:59:45 +0100

    allow cse of reduce and map_

--- src/gt4py/next/iterator/transforms/cse.py
+++ src/gt4py/next/iterator/transforms/cse.py
@@ -81,17 +81,11 @@ def _is_collectable_expr(node: itir.Node) -> bool:
     if isinstance(node, itir.FunCall):
         # do not collect (and thus deduplicate in CSE) shift(offsets…) calls. Node must still be
         #  visited, to ensure symbol dependencies are recognized correctly.
-        # do also not collect reduce nodes if they are left in the IR at this point, this may lead to
-        #  conceptual problems (other parts of the tool chain rely on the arguments being present directly
-        #  on the reduce FunCall node (connectivity deduction)), as well as problems with the imperative backend
-        #  backend (single pass eager depth first visit approach)
         # do also not collect lifts or applied lifts as they become invisible to the lift inliner
         #  otherwise
         # do also not collect index nodes because otherwise the right hand side of SetAts becomes a let statement
         #  instead of an as_fieldop
-        if cpm.is_call_to(
-            node, ("lift", "shift", "reduce", "map_", "index")
-        ) or cpm.is_applied_lift(node):
+        if cpm.is_call_to(node, ("lift", "shift", "index")) or cpm.is_applied_lift(node):
             return False
         return True
     # do also not collect make_tuple(index) nodes because otherwise the right hand side of SetAts becomes a let statement
