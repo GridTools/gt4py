d82fd067cf26d635d043c5e8e00e93943ab6c771
Hannes Vogt <vogt@hey.com>
2025-11-28 13:11:40 +0100

    make the unique ids uniquer

--- src/gt4py/next/iterator/transforms/cse.py
+++ src/gt4py/next/iterator/transforms/cse.py
@@ -262,6 +262,19 @@ class CollectSubexpressions(PreserveLocationVisitor, VisitorWithSymbolTableTrait
             state.used_symbol_ids.add(id(symtable[node.id]))
 
 
+def _unique_id(uid_generator: UIDGenerator) -> str:
+    """
+    Generate a unique id with '_cs' prefix using the given uid generator.
+
+    In case cse is used from a different context where the uid generator already has a prefix.
+    """
+    prefix = uid_generator.prefix or ""
+    if prefix != "_cs":
+        prefix = f"{prefix}_cs"
+
+    return uid_generator.sequential_id(prefix=prefix)
+
+
 def extract_subexpression(
     node: itir.Expr,
     predicate: Callable[[itir.Expr, int], bool],
@@ -299,10 +312,10 @@ def extract_subexpression(
         ...     expr, predicate, UIDGenerator(prefix="_subexpr")
         ... )
         >>> print(new_expr)
-        _subexpr_1 + (_subexpr_1 + z)
+        _subexpr_cs_1 + (_subexpr_cs_1 + z)
         >>> for sym, subexpr in extracted_subexprs.items():
         ...     print(f"`{sym}`: `{subexpr}`")
-        `_subexpr_1`: `x + y`
+        `_subexpr_cs_1`: `x + y`
 
         The order of the extraction can be configured using `deepest_expr_first`. By default, the nodes
         closer to the root are eliminated first:
@@ -315,10 +328,10 @@ def extract_subexpression(
         ...     expr, predicate, UIDGenerator(prefix="_subexpr"), deepest_expr_first=False
         ... )
         >>> print(new_expr)
-        _subexpr_1 + _subexpr_1
+        _subexpr_cs_1 + _subexpr_cs_1
         >>> for sym, subexpr in extracted_subexprs.items():
         ...     print(f"`{sym}`: `{subexpr}`")
-        `_subexpr_1`: `x + y + (x + y)`
+        `_subexpr_cs_1`: `x + y + (x + y)`
 
         Since `(x+y)` is a child of one of the expressions it is ignored:
 
@@ -339,10 +352,10 @@ def extract_subexpression(
         ...     deepest_expr_first=True,
         ... )
         >>> print(new_expr)
-        _subexpr_1 + _subexpr_1 + (_subexpr_1 + _subexpr_1)
+        _subexpr_cs_1 + _subexpr_cs_1 + (_subexpr_cs_1 + _subexpr_cs_1)
         >>> for sym, subexpr in extracted_subexprs.items():
         ...     print(f"`{sym}`: `{subexpr}`")
-        `_subexpr_1`: `x + y`
+        `_subexpr_cs_1`: `x + y`
 
         Note that this requires `once_only` to be set right now.
     """
@@ -396,7 +409,7 @@ def extract_subexpression(
         if not eligible_ids:
             continue
 
-        expr_id = uid_generator.sequential_id(prefix="_cs")
+        expr_id = _unique_id(uid_generator)
         extracted[itir.Sym(id=expr_id)] = expr
         expr_ref = itir.SymRef(id=expr_id)
         for id_ in eligible_ids:
