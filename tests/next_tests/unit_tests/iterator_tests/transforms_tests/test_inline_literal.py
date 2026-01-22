0f60c7f370fa05252f94f4d4dd030e98eaaac8f7
Edoardo Paone <edoardo.paone@cscs.ch>
2026-01-22 15:41:38 +0100

    edit

--- tests/next_tests/unit_tests/iterator_tests/transforms_tests/test_inline_literal.py
+++ tests/next_tests/unit_tests/iterator_tests/transforms_tests/test_inline_literal.py
@@ -19,3 +19,26 @@ def test_inline_literal_fieldop():
     expected = im.as_fieldop(im.lambda_("__arg0")(im.plus(im.deref("__arg0"), 1.0)))(x_ref)
     actual = inline_literal.InlineLiteral.apply(testee)
     assert actual == expected
+
+
+def test_inline_literal_scan():
+    IDim = gtx.Dimension("IDim")
+    x_ref = im.ref("x", ts.FieldType(dims=[IDim], dtype=ts.ScalarType(kind=ts.ScalarKind.FLOAT32)))
+    testee = im.as_fieldop(
+        im.scan(
+            im.lambda_("state", "inp", "val")(
+                im.plus("state", im.multiplies_(im.deref("inp"), "val"))
+            ),
+            True,
+            0.0,
+        )
+    )(x_ref, 2.0)
+    expected = im.as_fieldop(
+        im.scan(
+            im.lambda_("state", "inp")(im.plus("state", im.multiplies_(im.deref("inp"), 2.0))),
+            True,
+            0.0,
+        )
+    )(x_ref)
+    actual = inline_literal.InlineLiteral.apply(testee)
+    assert actual == expected
