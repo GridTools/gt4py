GridTools/gt4py

commit dd23d1fc59950d7212fe42f285e6bc031a22a71a

feat[next]: Compiled variant for field operators (#2368)
In #1968 we introduced pre- and jit-compilation of static arguments. In
the first version this was only support for programs, but not field
operators which were directly called. This PR extends to functionality
to also support field operators and scans.

A good amount of technical debt which accumulated around the
`Backend.__call__` method was also removed as it became obsolete with
the unified approach.

Since scans are the only type of callable with generic arguments and
there is a good amount of technical debt surrounding them that was
beyond the scope of this PR, the first version barely implements
something working for them and prints a warning as performance is likely
not great. If performance is needed a simple workaround is to wrap the
scan into a field operator or program.

---------

Co-authored-by: Sara Faghih-Naini <sara.faghihnaini@ecmwf.int>

src/gt4py/next/otf/options.py
@@ -1,0 +1,41 @@
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import dataclasses
+from typing import Sequence, TypedDict
+
+from gt4py.next import common, config
+
+
+class CompilationOptionsArgs(TypedDict, total=False):
+    enable_jit: bool
+    static_params: Sequence[str]
+    connectivities: common.OffsetProvider
+
+
+@dataclasses.dataclass(frozen=True)
+class CompilationOptions:
+    #: Enable Just-in-Time compilation, otherwise a program has to be compiled manually by a call
+    #: to `compile` before calling.
+    # Uses a factory to make changes to the config after module import time take effect. This is
+    # mostly important for testing. Users should not rely on it.
+    enable_jit: bool = dataclasses.field(default_factory=lambda: config.ENABLE_JIT_DEFAULT)
+
+    #: if the user requests static params, they will be used later to initialize CompiledPrograms
+    static_params: Sequence[str] | None = (
+        None  # TODO: describe that this value will eventually be a sequence of strings
+    )
+
+    # TODO(ricoh): replace with common.OffsetProviderType once the temporary pass doesn't require the runtime information
+    #: A dictionary holding static/compile-time information about the offset providers.
+    #: For now, it is used for ahead of time compilation in DaCe orchestrated programs,
+    #: i.e. DaCe programs that call GT4Py Programs -SDFGConvertible interface-.
+    connectivities: common.OffsetProvider | None = None
+
+
+assert CompilationOptionsArgs.__annotations__.keys() == CompilationOptions.__annotations__.keys()

commit a4edab096fa1a35358720a89908e2beea8411738

fix[next]: Fix different static args after `with_backend` (#2475)
The following snippet broke after #2368
```python
prg1 = prog.with_backend(gtfn.run_gtfn)
prg2 = prog.with_backend(gtfn.run_gtfn)

# compile with static args
prg1.compile(cond=[True], offset_provider={})

# compile without static args
prg2.compile(offset_provider={})
```
with the following error
```
ValueError: Argument descriptor StaticArg must be the same for all compiled programs, got [] expected ['cond'].
```
This PR avoids using `object.__setattr__` on the `CompilationOptions` to
pass information to the compiled program pool construction and instead
explicitly constructs the pool on `compile`.

Note that the `enable_jit` argument to `compile` has been removed and
replaced by:
```python
prg.with_compilation_option(enable_jit=...)
```
as the same `object.__setattr__` mechanism was used to pass the
information.

src/gt4py/next/ffront/decorator.py
@@ -19,7 +19,7 @@ import types
 import typing
 import warnings
 from collections.abc import Callable
-from typing import Any, Generic, Optional, TypeVar
+from typing import Any, Generic, Optional, Sequence, TypeVar
 
 from gt4py import eve
 from gt4py._core import definitions as core_defs
@@ -75,27 +75,35 @@ class _ProgramLikeMixin(Generic[ProgramLikeDefinitionT]):
     def with_backend(self, backend: next_backend.Backend) -> Self:
         return dataclasses.replace(self, backend=backend)
 
-    def with_connectivities(
-        self,
-        connectivities: common.OffsetProvider,  # TODO(ricoh): replace with common.OffsetProviderType once the temporary pass doesn't require the runtime information
+    def with_compilation_options(
+        self, **compilation_options: Unpack[options.CompilationOptionsArgs]
     ) -> Self:
         return dataclasses.replace(
             self,
             compilation_options=dataclasses.replace(
-                self.compilation_options, connectivities=connectivities
+                self.compilation_options, **compilation_options
             ),
         )
 
     @functools.cached_property
     def _compiled_programs(self) -> compiled_program.CompiledProgramsPool:
+        # This cached property initializer is only called when JITting the first
+        # program variant of the pool. If the program is compiled by directly
+        # calling `compile()`, the pool is initialized with the options passed
+        # to `compile()` instead of re-using the existing compilations options.
+        return self._make_compiled_programs_pool(
+            static_params=self.compilation_options.static_params or (),
+        )
+
+    def _make_compiled_programs_pool(
+        self,
+        static_params: Sequence[str],
+    ) -> compiled_program.CompiledProgramsPool:
         if self.backend is None or self.backend == eve.NOTHING:
             raise RuntimeError("Cannot compile a program without backend.")
 
-        if self.compilation_options.static_params is None:
-            object.__setattr__(self.compilation_options, "static_params", ())
-
         argument_descriptor_mapping = {
-            arguments.StaticArg: self.compilation_options.static_params,
+            arguments.StaticArg: static_params,
         }
 
         program_type = ffront_type_info.type_in_program_context(self.__gt_type__())
@@ -114,7 +122,6 @@ class _ProgramLikeMixin(Generic[ProgramLikeDefinitionT]):
         | common.OffsetProvider
         | list[common.OffsetProviderType | common.OffsetProvider]
         | None = None,
-        enable_jit: bool | None = None,
         **static_args: list[xtyping.MaybeNestedInTuple[core_defs.Scalar]],
     ) -> Self:
         """
@@ -125,13 +132,16 @@ class _ProgramLikeMixin(Generic[ProgramLikeDefinitionT]):
         but adds the compiled variants to the current program instance.
         """
         # TODO(havogt): we should reconsider if we want to return a new program on `compile` (and
-        # rename to `with_static_args` or similar) once we have a better understanding of the
-        # use-cases.
+        #  rename to `with_static_args` or similar) once we have a better understanding of the
+        #  use-cases.
+        # check if pool has already been initialized. since this is also a cached property go via
+        #  the dict directly. Note that we don't need to check any args, since the pool checks
+        #  this on compile anyway.
+        if "_compiled_programs" not in self.__dict__:
+            self.__dict__["_compiled_programs"] = self._make_compiled_programs_pool(
+                static_params=tuple(static_args.keys()),
+            )
 
-        if enable_jit is not None:
-            object.__setattr__(self.compilation_options, "enable_jit", enable_jit)
-        if self.compilation_options.static_params is None:
-            object.__setattr__(self.compilation_options, "static_params", tuple(static_args.keys()))
         if self.compilation_options.connectivities is None and offset_provider is None:
             raise ValueError(
                 "Cannot compile a program without connectivities / OffsetProviderType."
@@ -267,10 +277,10 @@ class Program(_ProgramLikeMixin[ffront_stages.ProgramDefinition]):
 
     def with_static_params(self, *static_params: str | None) -> Program:
         if not static_params or (static_params == (None,)):
-            _static_params = None
+            _static_params: tuple[str, ...] = ()
         else:
             assert all(p is not None for p in static_params)
-            _static_params = typing.cast(tuple[str], static_params)
+            _static_params = typing.cast(tuple[str, ...], static_params)
         return dataclasses.replace(
             self,
             compilation_options=dataclasses.replace(
@@ -435,7 +445,6 @@ class ProgramWithBoundArgs(Program):
         | common.OffsetProvider
         | list[common.OffsetProviderType | common.OffsetProvider]
         | None = None,
-        enable_jit: bool | None = None,
         **static_args: list[xtyping.MaybeNestedInTuple[core_defs.Scalar]],
     ) -> Self:
         raise NotImplementedError("Compilation of programs with bound arguments is not implemented")

src/gt4py/next/otf/options.py
@@ -26,10 +26,10 @@ class CompilationOptions:
     # mostly important for testing. Users should not rely on it.
     enable_jit: bool = dataclasses.field(default_factory=lambda: config.ENABLE_JIT_DEFAULT)
 
-    #: if the user requests static params, they will be used later to initialize CompiledPrograms
-    static_params: Sequence[str] | None = (
-        None  # TODO: describe that this value will eventually be a sequence of strings
-    )
+    #: If the user requests static params, they will be used later to initialize CompiledPrograms.
+    #: By default the set of static params is set when compiling for the first time, e.g. on call
+    #: when jitting is enabled, or on a call to `compile`.
+    static_params: Sequence[str] | None = None
 
     # TODO(ricoh): replace with common.OffsetProviderType once the temporary pass doesn't require the runtime information
     #: A dictionary holding static/compile-time information about the offset providers.

tests/next_tests/integration_tests/feature_tests/dace/test_orchestration.py
@@ -50,14 +50,14 @@ def test_sdfgConvertible_laplap(cartesian_case):  # noqa: F811
         tmp_field = xp.empty_like(out_field)
         lap_program.with_grid_type(cartesian_case.grid_type).with_backend(
             backend
-        ).with_connectivities(gtx_common.offset_provider_to_type(cartesian_case.offset_provider))(
-            in_field, tmp_field
-        )
+        ).with_compilation_options(
+            connectivities=gtx_common.offset_provider_to_type(cartesian_case.offset_provider)
+        )(in_field, tmp_field)
         lap_program.with_grid_type(cartesian_case.grid_type).with_backend(
             backend
-        ).with_connectivities(gtx_common.offset_provider_to_type(cartesian_case.offset_provider))(
-            tmp_field, out_field
-        )
+        ).with_compilation_options(
+            connectivities=gtx_common.offset_provider_to_type(cartesian_case.offset_provider)
+        )(tmp_field, out_field)
 
     # use unique SDFG folder in dace cache to avoid clashes between parallel pytest workers
     with dace.config.set_temporary("cache", value="unique"):
@@ -109,7 +109,7 @@ def test_sdfgConvertible_connectivities(unstructured_case):  # noqa: F811
         allocator=allocator,
     )
 
-    testee2 = testee.with_backend(backend).with_connectivities({"E2V": e2v})
+    testee2 = testee.with_backend(backend).with_compilation_options(connectivities={"E2V": e2v})
 
     @dace.program
     def sdfg(
@@ -118,7 +118,9 @@ def test_sdfgConvertible_connectivities(unstructured_case):  # noqa: F811
         offset_provider: OffsetProvider_t,
         connectivities: dace.compiletime,
     ):
-        testee2.with_connectivities(connectivities)(a, out, offset_provider=offset_provider)
+        testee2.with_compilation_options(connectivities=connectivities)(
+            a, out, offset_provider=offset_provider
+        )
         return out
 
     connectivities = {"E2V": e2v}  # replace 'e2v' with 'e2v.__gt_type__()' when GTIR is AOT

tests/next_tests/integration_tests/feature_tests/ffront_tests/test_compiled_program.py
@@ -16,7 +16,7 @@ import contextlib
 from gt4py import next as gtx
 from gt4py._core import definitions as core_defs
 from gt4py.next import errors, config
-from gt4py.next.otf import compiled_program, options
+from gt4py.next.otf import compiled_program, options, arguments
 from gt4py.next.ffront.decorator import Program
 from gt4py.next.ffront.fbuiltins import int32, neighbor_sum
 
@@ -202,6 +202,11 @@ def compile_testee_unstructured(unstructured_case):
     return testee
 
 
+@pytest.fixture
+def compile_testee_unstructured_no_jit(compile_testee_unstructured):
+    return compile_testee_unstructured.with_compilation_options(enable_jit=False)
+
+
 def test_compile_unstructured(unstructured_case, compile_testee_unstructured):
     if unstructured_case.backend is None:
         pytest.skip("Embedded compiled program doesn't make sense.")
@@ -236,23 +241,24 @@ def skip_value_mesh_descriptor(exec_alloc_descriptor):
 
 
 def test_compile_unstructured_jit(
-    unstructured_case, compile_testee_unstructured, skip_value_mesh_descriptor
+    unstructured_case, compile_testee_unstructured_no_jit, skip_value_mesh_descriptor
 ):
     if unstructured_case.backend is None:
         pytest.skip("Embedded compiled program doesn't make sense.")
 
     # compiled for skip_value_mesh and simple_mesh
-    compile_testee_unstructured.compile(
+    compile_testee_unstructured_no_jit.compile(
         offset_provider=[
             skip_value_mesh_descriptor.offset_provider,
             unstructured_case.offset_provider,
         ],
-        enable_jit=False,
     )
 
     # and executing the simple_mesh
-    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured)
-    compile_testee_unstructured(*args, offset_provider=unstructured_case.offset_provider, **kwargs)
+    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured_no_jit)
+    compile_testee_unstructured_no_jit(
+        *args, offset_provider=unstructured_case.offset_provider, **kwargs
+    )
 
     v2e_numpy = unstructured_case.offset_provider[V2E.value].asnumpy()
     assert np.allclose(
@@ -262,73 +268,72 @@ def test_compile_unstructured_jit(
 
 
 def test_compile_unstructured_wrong_offset_provider(
-    unstructured_case, compile_testee_unstructured, skip_value_mesh_descriptor
+    unstructured_case, compile_testee_unstructured_no_jit, skip_value_mesh_descriptor
 ):
     if unstructured_case.backend is None:
         pytest.skip("Embedded compiled program doesn't make sense.")
 
     # compiled for skip_value_mesh
-    compile_testee_unstructured.compile(
+    compile_testee_unstructured_no_jit.compile(
         offset_provider=skip_value_mesh_descriptor.offset_provider,
-        enable_jit=False,
     )
 
     # but executing the simple_mesh
-    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured)
+    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured_no_jit)
 
     # make sure the backend is never called
-    object.__setattr__(compile_testee_unstructured, "backend", _raise_on_compile)
+    object.__setattr__(compile_testee_unstructured_no_jit, "backend", _raise_on_compile)
 
     with pytest.raises(RuntimeError, match="No program.*static.*arg.*"):
-        compile_testee_unstructured(
+        compile_testee_unstructured_no_jit(
             *args, offset_provider=unstructured_case.offset_provider, **kwargs
         )
 
 
 def test_compile_unstructured_modified_offset_provider(
-    unstructured_case, compile_testee_unstructured, skip_value_mesh_descriptor
+    unstructured_case, compile_testee_unstructured_no_jit, skip_value_mesh_descriptor
 ):
     if unstructured_case.backend is None:
         pytest.skip("Embedded compiled program doesn't make sense.")
 
     # compiled for skip_value_mesh
-    compile_testee_unstructured.compile(
+    compile_testee_unstructured_no_jit.compile(
         offset_provider=skip_value_mesh_descriptor.offset_provider,
-        enable_jit=False,
     )
 
     # but executing the simple_mesh
-    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured)
+    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured_no_jit)
 
     # make sure the backend is never called
-    object.__setattr__(compile_testee_unstructured, "backend", _raise_on_compile)
+    object.__setattr__(compile_testee_unstructured_no_jit, "backend", _raise_on_compile)
 
     with pytest.raises(RuntimeError, match="No program.*static.*arg.*"):
-        compile_testee_unstructured(
+        compile_testee_unstructured_no_jit(
             *args, offset_provider=unstructured_case.offset_provider, **kwargs
         )
 
 
 def test_compile_unstructured_for_two_offset_providers(
-    unstructured_case, compile_testee_unstructured, skip_value_mesh_descriptor
+    unstructured_case, compile_testee_unstructured_no_jit, skip_value_mesh_descriptor
 ):
     if unstructured_case.backend is None:
         pytest.skip("Embedded compiled program doesn't make sense.")
 
     # compiled for skip_value_mesh and simple_mesh
-    compile_testee_unstructured.compile(
+    compile_testee_unstructured_no_jit.compile(
         offset_provider=[
             skip_value_mesh_descriptor.offset_provider,
             unstructured_case.offset_provider,
         ],
-        enable_jit=False,
     )
 
     # make sure the backend is never called
-    object.__setattr__(compile_testee_unstructured, "backend", _raise_on_compile)
+    object.__setattr__(compile_testee_unstructured_no_jit, "backend", _raise_on_compile)
 
-    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured)
-    compile_testee_unstructured(*args, offset_provider=unstructured_case.offset_provider, **kwargs)
+    args, kwargs = cases.get_default_data(unstructured_case, compile_testee_unstructured_no_jit)
+    compile_testee_unstructured_no_jit(
+        *args, offset_provider=unstructured_case.offset_provider, **kwargs
+    )
 
     v2e_numpy = unstructured_case.offset_provider[V2E.value].asnumpy()
     assert np.allclose(
@@ -400,7 +405,9 @@ def test_compile_variants(cartesian_case, compile_variants_testee):
     # make sure the backend is never called
     object.__setattr__(compile_variants_testee, "backend", _raise_on_compile)
 
-    assert compile_variants_testee.compilation_options.static_params == (
+    assert compile_variants_testee._compiled_programs.argument_descriptor_mapping[
+        arguments.StaticArg
+    ] == (
         "scalar_int",
         "scalar_float",
         "scalar_bool",
@@ -579,7 +586,14 @@ def test_compile_variants_not_compiled_then_reset_static_params(
     field_b = cases.allocate(cartesian_case, compile_variants_testee, "field_b")()
 
     # the compile_variants_testee has static_params set and is compiled (in a previous test)
-    assert len(compile_variants_testee.compilation_options.static_params) > 0
+    assert (
+        len(
+            compile_variants_testee._compiled_programs.argument_descriptor_mapping[
+                arguments.StaticArg
+            ]
+        )
+        > 0
+    )
     assert compile_variants_testee._compiled_programs is not None
 
     # but now we reset the compiled programs
@@ -630,7 +644,14 @@ def test_compile_variants_not_compiled_then_set_new_static_params(
     field_b = cases.allocate(cartesian_case, compile_variants_testee, "field_b")()
 
     # the compile_variants_testee has static_params set and is compiled (in a previous test)
-    assert len(compile_variants_testee.compilation_options.static_params) > 0
+    assert (
+        len(
+            compile_variants_testee._compiled_programs.argument_descriptor_mapping[
+                arguments.StaticArg
+            ]
+        )
+        > 0
+    )
     assert compile_variants_testee._compiled_programs is not None
 
     # but now we reset the compiled programs and fix to other static params

tests/next_tests/unit_tests/otf_tests/test_compiled_program.py
@@ -55,19 +55,22 @@ def test_sanitize_static_args_wrong_type():
 TDim = gtx.Dimension("TDim")
 
 
-@gtx.field_operator
-def fop(cond: bool, a: gtx.Field[gtx.Dims[TDim], float], b: gtx.Field[gtx.Dims[TDim], float]):
-    return a if cond else b
+@pytest.fixture
+def testee_prog():
+    @gtx.field_operator
+    def fop(cond: bool, a: gtx.Field[gtx.Dims[TDim], float], b: gtx.Field[gtx.Dims[TDim], float]):
+        return a if cond else b
 
+    @gtx.program(backend=gtfn.run_gtfn)
+    def prog(
+        cond: bool,
+        a: gtx.Field[gtx.Dims[TDim], gtx.float64],
+        b: gtx.Field[gtx.Dims[TDim], gtx.float64],
+        out: gtx.Field[gtx.Dims[TDim], gtx.float64],
+    ):
+        fop(cond, a, b, out=out)
 
-@gtx.program
-def prog(
-    cond: bool,
-    a: gtx.Field[gtx.Dims[TDim], gtx.float64],
-    b: gtx.Field[gtx.Dims[TDim], gtx.float64],
-    out: gtx.Field[gtx.Dims[TDim], gtx.float64],
-):
-    fop(cond, a, b, out=out)
+    return prog
 
 
 def _verify_program_has_expected_true_value(program: itir.Program):
@@ -78,11 +81,11 @@ def _verify_program_has_expected_true_value(program: itir.Program):
     assert program.body[0].expr.args[0].value  # is True
 
 
-def test_inlining_of_scalars_works():
+def test_inlining_of_scalars_works(testee_prog):
     input_pair = toolchain.CompilableProgram(
-        data=prog.definition_stage,
+        data=testee_prog.definition_stage,
         args=arguments.CompileTimeArgs(
-            args=list(prog.past_stage.past_node.type.definition.pos_or_kw_args.values()),
+            args=list(testee_prog.past_stage.past_node.type.definition.pos_or_kw_args.values()),
             kwargs={},
             offset_provider={},
             column_axis=None,
@@ -96,7 +99,7 @@ def test_inlining_of_scalars_works():
     _verify_program_has_expected_true_value(transformed)
 
 
-def test_inlining_of_scalar_works_integration():
+def test_inlining_of_scalar_works_integration(testee_prog):
     """
     Test that `.compile` replaces the scalar arg in the program.
     Unlike the previous test, this test uses a full backend and makes sure the replacement step is there.
@@ -114,7 +117,7 @@ def test_inlining_of_scalar_works_integration():
 
     hacked_gtfn_backend = gtfn.GTFNBackendFactory(name_postfix="_custom", otf_workflow=pirate)
 
-    testee = prog.with_backend(hacked_gtfn_backend).compile(cond=[True], offset_provider={})
+    testee = testee_prog.with_backend(hacked_gtfn_backend).compile(cond=[True], offset_provider={})
     testee(
         cond=True,
         a=gtx.zeros(domain={TDim: 1}, dtype=gtx.float64),
@@ -124,3 +127,38 @@ def test_inlining_of_scalar_works_integration():
     )
 
     _verify_program_has_expected_true_value(hijacked_program.data)
+
+
+def test_different_static_args_work_after_backend_change(testee_prog):
+    prg1 = testee_prog.with_backend(gtfn.run_gtfn)
+    prg2 = testee_prog.with_backend(gtfn.run_gtfn)
+
+    # compile with static args
+    prg1.compile(cond=[True], offset_provider={})
+
+    # compile without static args
+    prg2.compile(offset_provider={})
+
+
+def test_different_static_args_work_after_static_params_change(testee_prog):
+    testee_prog2 = testee_prog.with_compilation_options(static_params=["cond"])
+
+    # compile without static args
+    testee_prog.compile(offset_provider={})
+
+    # compile with static args
+    testee_prog2.compile(cond=[True], offset_provider={})
+
+
+def test_different_static_args_break_same_prg_after_static_params_change(testee_prog):
+    prg = testee_prog.with_compilation_options(static_params=[])
+
+    # compile without static args
+    prg.compile(offset_provider={})
+
+    # compile with different static args
+    with pytest.raises(
+        ValueError,
+        match="Argument descriptor StaticArg must be the same for all compiled programs",
+    ):
+        prg.compile(cond=[True], offset_provider={})

tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_dace_bindings.py
@@ -281,7 +281,7 @@ def test_cartesian_bind_sdfg(use_metrics, use_zero_origin, monkeypatch):
             a[0] + 2 * a[1][0] + 3 * a[1][1] + 4 * b[0][0] + 5 * b[1]
         )  # skip 'a[1][2]' on purpose to cover unused scalar args
 
-    @gtx.program
+    @gtx.program(enable_jit=False)
     def testee(
         a: tuple[int32, tuple[int32, cases.IJKField, int32]],
         b: tuple[tuple[cases.IJKField], int32],  # use 'b_0' to test tuple with single element
@@ -327,7 +327,7 @@ def test_cartesian_bind_sdfg(use_metrics, use_zero_origin, monkeypatch):
     program = (
         testee.with_grid_type(gtx_common.GridType.CARTESIAN)
         .with_backend(backend)
-        .compile(enable_jit=False, offset_provider={}, **static_args)
+        .compile(offset_provider={}, **static_args)
     )
     program(a, b, out=c, M=M, N=N, K=K)
     assert np.all(c.asnumpy() == ref)
@@ -344,7 +344,7 @@ def test_unstructured_bind_sdfg(use_metrics, use_zero_origin, monkeypatch):
         tmp_2 = neighbor_sum(tmp(V2E), axis=V2EDim)
         return tmp_2
 
-    @gtx.program
+    @gtx.program(enable_jit=False)
     def testee(a: cases.VField, b: cases.VField):
         testee_op(a, out=b)
 
@@ -384,7 +384,7 @@ def test_unstructured_bind_sdfg(use_metrics, use_zero_origin, monkeypatch):
     program = (
         testee.with_grid_type(gtx_common.GridType.UNSTRUCTURED)
         .with_backend(backend)
-        .compile(enable_jit=False, offset_provider=offset_provider, **static_args)
+        .compile(offset_provider=offset_provider, **static_args)
     )
     program(a, b, offset_provider=offset_provider)
     assert np.all(b.asnumpy() == ref)