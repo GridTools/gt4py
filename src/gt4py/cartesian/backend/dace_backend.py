e813c4454b5969f5adbd4ed38d9e0aa81cd1e221
Roman Cattaneo <romanc@users.noreply.github.com>
2026-03-17 18:33:13 +0100

    wip: fix dace passes for remapping with different loop layout

--- src/gt4py/cartesian/backend/dace_backend.py
+++ src/gt4py/cartesian/backend/dace_backend.py
@@ -405,6 +405,8 @@ class SDFGManager:
         #  - we expect all non-cartesian control flow to be innermost
         stree = self.schedule_tree()
 
+        tn.validate_children_and_parents_align(stree)
+
         # Re-order cartesian loops to match loops with memory layout
         # - layout is _always_ given I-J-K
         # - layout is reversed, inner is higher numbers, e.g K-J-I is I=2, J=1, K=0
@@ -423,9 +425,11 @@ class SDFGManager:
             flipper = passes.PushVerticalMapDown()
             flipper.visit(stree)
 
+        tn.validate_children_and_parents_align(stree)
+
         # Create SDFG
         sdfg = stree.as_sdfg(
-            validate=validate,
+            validate=True,
             simplify=simplify,
             skip={"ScalarToSymbolPromotion", "ControlFlowRaising"},
         )
