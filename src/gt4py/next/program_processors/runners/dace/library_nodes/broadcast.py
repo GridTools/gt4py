e7bd089c7e947e887e5932614fc7c5e90b4771b6
Philip Mueller, CSCS <philip.mueller@cscs.ch>
2026-04-29 15:59:20 +0200

    Started with an inline expansion, but the regular expansion needs to be updated yet.

--- src/gt4py/next/program_processors/runners/dace/library_nodes/broadcast.py
+++ src/gt4py/next/program_processors/runners/dace/library_nodes/broadcast.py
@@ -8,10 +8,18 @@
 
 from __future__ import annotations
 
+import copy
 from typing import Any, Final, Sequence
 
 import dace
-from dace import library as dace_library, nodes as dace_nodes, properties as dace_properties
+from dace import (
+    data as dace_data,
+    library as dace_library,
+    nodes as dace_nodes,
+    properties as dace_properties,
+    subsets as dace_sbs,
+)
+from dace.sdfg import graph as dace_graph
 from dace.transformation import transformation as dace_transform
 
 
@@ -21,79 +29,166 @@ _OUTPUT_NAME: Final[str] = "_outp"
 
 @dace_library.node
 class Broadcast(dace_nodes.LibraryNode):
-    """Implements write of a scalar value over an array subset."""
+    """Implements write of a scalar value over an array subset.
+
+    ndims(output) == ndims(value_to_broadcast) + len(broadcast_in_dim)
+    Same as XLA.
+    broadcast_in_dim[i] describes where dimension `i` of the `value_to_broadcast`
+    goes. In case of a scalar it is empty.
+    Furthermore the following has to hold:
+    ```python
+    for i in range(len(broadcast_in_dim):
+        assert output.shape[broadcast_in_dim[i]] == value_to_broadcast.shape[i]
+    ```
+
+    Args:
+        broadcast_in_dim: How to broadcast.
+
+    Todo:
+        - While for the output it is probably okay to always require an adjacent
+            AccessNode for the input it might be possible to be on the other side
+            of a Map.
+    """
 
     implementations: Final[dict[str, dace_transform.ExpandTransformation]] = {}
     default_implementation: Final[str | None] = "pure"
 
-    # TODO(phimuell, edopao): This information is probably redundant and to simplify
-    #   implementation, everything should be encoded in the destination subset of
-    #   the edge that connects the library node with its output. This makes it
-    #   simpler to generate/copy the library node.
-    #   Also `value` maybe only allow one style which makes handling certain things
-    #   simpler.
-    axes = dace_properties.ListProperty(element_type=int)
-    src_origin = dace_properties.ListProperty(element_type=str)
-    dst_origin = dace_properties.ListProperty(element_type=str)
-    value = dace_properties.Property(allow_none=True)
+    brodcast_in_dims = dace_properties.ListProperty(element_type=int)
 
     def __init__(
         self,
         name: str,
-        axes: Sequence[int] | None = None,
-        src_origin: Sequence[dace.symbolic.SymbolicType] | None = None,
-        dst_origin: Sequence[dace.symbolic.SymbolicType] | None = None,
-        value: dace.symbolic.SymbolicType | None = None,
+        broadcast_in_dims: Sequence[int],
         debuginfo: dace.dtypes.DebugInfo | None = None,
     ):
         # TODO(philip, edopao): I would propose to drop `value` then. This makes it
         #   simpler to handle in the transformations.
-        inputs = {_INPUT_NAME} if value is None else None
-        super().__init__(name, inputs=inputs, outputs={_OUTPUT_NAME})
+        super().__init__(name, inputs={_INPUT_NAME}, outputs={_OUTPUT_NAME})
 
-        self.axes = [] if axes is None else list(axes)
-        self.src_origin = [] if src_origin is None else [str(o) for o in src_origin]
-        self.dst_origin = [] if dst_origin is None else [str(o) for o in dst_origin]
-        self.value = value
+        self.brodcast_in_dims = list(broadcast_in_dims)
         self.debuginfo = debuginfo
 
     def validate(self, sdfg: dace.SDFG, state: dace.SDFGState) -> None:
-        if any(i < 0 for i in self.axes):
-            raise ValueError("Invalid negative axis value.")
+        if len(self.brodcast_in_dims) == len(set(self.brodcast_in_dims)):
+            raise ValueError("`FCan not broadcast to multiple dimensions at the same time.")
 
-        if len(self.axes) != len(set(self.axes)):
-            raise ValueError("Axes must be unique.")
+        if state.in_degree(self) != 1 and next(iter(state.in_edges(self))).dst_conn == _INPUT_NAME:
+            raise ValueError("GT4Py Broadcast node needs exactly one input.")
+        if (
+            state.out_degree(self) != 1
+            and next(iter(state.out_edges(self))).src_conn == _OUTPUT_NAME
+        ):
+            raise ValueError("GT4Py Broadcast node needs exactly one output.")
 
-        if len(list(state.out_edges_by_connector(self, _OUTPUT_NAME))) != 1:
-            raise ValueError(f"Expected one output edge on connector {_OUTPUT_NAME}.")
-        outedge = next(state.out_edges_by_connector(self, _OUTPUT_NAME))
-        if not isinstance(outedge.dst, dace_nodes.AccessNode):
-            raise ValueError("Output node must be an access node.")
+        bcast_value_node: dace_nodes.AccessNode = next(iter(state.in_edges(self))).src
+        if not isinstance(bcast_value_node, dace_nodes.AccessNode):
+            raise ValueError("Source of broadcasting must be an AccessNode.")
+        bcast_value_desc = bcast_value_node.desc(sdfg)
+        if isinstance(bcast_value_desc, dace_data.View):
+            raise ValueError("Can not broadcast from a view.")
 
-        if self.value is None:  # expect an input connection
-            if len(list(state.in_edges_by_connector(self, _INPUT_NAME))) != 1:
-                raise ValueError(f"Expected one input edge on connector {_INPUT_NAME}.")
-            inedge = next(state.in_edges_by_connector(self, _INPUT_NAME))
-            if not isinstance(inedge.src, dace_nodes.AccessNode):
-                raise ValueError("Input node must be an access node.")
+        bcast_result_node: dace_nodes.AccessNode = next(iter(state.out_edges(self))).dst
+        if not isinstance(bcast_result_node, dace_nodes.AccessNode):
+            raise ValueError("Broadcast result must be an AccessNode.")
+        bcast_result_desc = bcast_result_node.desc(sdfg)
+        if isinstance(bcast_result_desc, dace_data.View):
+            raise ValueError("Broadcast result can not be a view.")
 
-            if len(self.axes) == 0:
-                if inedge.src.desc(sdfg).shape != (1,):
-                    raise ValueError("Axes cannot be None with array source.")
-            else:
-                if len(self.axes) != len(inedge.src.desc(sdfg).shape):
-                    raise ValueError("The provided axes are incompatible with source shape.")
-                elif max(self.axes) >= len(outedge.dst.desc(sdfg).shape):
-                    raise ValueError("The provided axes are incompatible with destination shape.")
-                if len(self.src_origin) != len(self.axes):
-                    raise ValueError("Invalid source origin.")
-                elif len(self.dst_origin) != len(outedge.dst.desc(sdfg).shape):
-                    raise ValueError("Invalid destination origin.")
-        else:  # broadcast of a literal value
-            if len(list(state.in_edges_by_connector(self, _INPUT_NAME))) != 0:
-                raise ValueError("Unexpected input connection with literal value.")
-            if self.axes:
-                raise ValueError("Unexpected domain axes with literal value.")
+        if isinstance(bcast_value_desc, dace_data.Scalar):
+            if len(self.brodcast_in_dims) == 0:
+                raise ValueError("For a scalar `broadcast_in_dims` must be empty.")
+        else:
+            expected_output_dims = len(self.brodcast_in_dims) + len(bcast_value_desc.shape)
+            got_output_dims = len(bcast_result_desc.shape)
+            if expected_output_dims != got_output_dims:
+                raise ValueError(
+                    f"Expected output to have {expected_output_dims}, but it only had {got_output_dims}"
+                )
+
+            for src_dim, bcast_dst_dim in enumerate(self.brodcast_in_dims):
+                if bcast_dst_dim < 0:
+                    raise ValueError("Negative broadcast")
+                if bcast_dst_dim >= len(bcast_result_desc.shape):
+                    raise ValueError("Out of range broadcast dim found.")
+
+                # Only do the size matching test if the sizes are known, as different
+                #  symbols can have the same value.
+                src_size = bcast_value_desc.shape[src_dim]
+                dst_size = bcast_result_desc.shape[bcast_dst_dim]
+                if str(src_size).isdigit() and str(dst_size).isdigit() and (src_size != dst_size):
+                    raise ValueError("Size mismatch found.")
+
+
+def inplace_broadcast_expander(
+    sdfg: dace.SDFG, state: dace.SDFGState, bcast_node: Broadcast
+) -> None:
+    """Perform expansion of `bcast_node` inside `state`.
+
+    The main difference between this and the normal expansion transformation is
+    that this function does not generate nested SDFG and instead performs the
+    expansion in place.
+    """
+
+    input_edge: dace_graph.MultiConnectorEdge[dace.Memlet] = next(iter(state.in_edges(bcast_node)))
+    output_edge: dace_graph.MultiConnectorEdge[dace.Memlet] = next(
+        iter(state.out_edges(bcast_node))
+    )
+
+    map_ranges: dict[str, dace_sbs.Range] = {}
+    output_subset: list[str] = []
+    for dst_dim, sbs in enumerate(output_edge.data.subset):
+        assert isinstance(sbs, tuple) and len(sbs) == 3
+        output_subset.append(f"__bcast{dst_dim}")
+        map_ranges[output_subset[-1]] = dace_sbs.Range(sbs)
+
+    if len(bcast_node.brodcast_in_dims) == 0:
+        input_subset = ["0"]
+    else:
+        # TODO(phimuell): Do we need a correction here because map range can be offsetted.
+        #   Because of our requierement it is probably irrelevant, i.e. the dimension is always full.
+        input_subset = [f"__bcast{dst_dim}" for dst_dim in bcast_node.brodcast_in_dims]
+
+    me, mx = state.add_map(f"__gt4py_broadcast_map_{bcast_node.name}", ndrange=map_ranges)
+    bcast_tlet = state.add_tasklet(
+        f"__gt4py_broadcast_tasklet_{bcast_node.name}",
+        inputs={"__in"},
+        outputs={"__out"},
+        code="__out = __in",
+    )
+
+    state.add_edge(
+        input_edge.src,
+        input_edge.src_conn,
+        me,
+        f"IN_{input_edge.data.data}",
+        dace.Memlet.from_array(input_edge.data.data, sdfg.arrays[input_edge.data.data]),
+    )
+    state.add_edge(
+        me,
+        f"OUT_{input_edge.data.data}",
+        bcast_tlet,
+        "__in",
+        dace.Memlet(data=input_edge.data.data, subset=", ".join(input_subset)),
+    )
+    me.add_scope_connectors(input_edge.data.data)
+
+    state.add_edge(
+        bcast_tlet,
+        "__out",
+        mx,
+        f"IN_{output_edge.data.data}",
+        dace.Memlet(data=output_edge.data.data, subset=", ".join(output_subset)),
+    )
+    state.add_edge(
+        mx,
+        f"OUT_{output_edge.data.data}",
+        output_edge.dst,
+        output_edge.dst_conn,
+        dace.Memlet(data=output_edge.data.data, subset=copy.deepcopy(output_edge.data.subset)),
+    )
+    mx.add_scope_connectors(output_edge.data.data)
+
+    state.remove_node(bcast_node)
 
 
 @dace_library.register_expansion(Broadcast, "pure")
