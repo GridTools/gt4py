01a05c70b539a34e2cc482e0bc129421617945c5
Edoardo Paone <edoardo.paone@cscs.ch>
2026-05-21 18:43:19 +0200

    Revert "impl based on reduce node"
    This reverts commit 7d769d68edc36fdd4e2d74ec232088fd5187efbc.

--- src/gt4py/next/program_processors/runners/dace/transformations/utils.py
+++ src/gt4py/next/program_processors/runners/dace/transformations/utils.py
@@ -18,6 +18,8 @@ from dace.sdfg import graph as dace_graph, nodes as dace_nodes
 from dace.transformation import pass_pipeline as dace_ppl
 from dace.transformation.passes import analysis as dace_analysis
 
+from gt4py.next.program_processors.runners.dace import library_nodes as gtx_lib
+
 
 _PassT = TypeVar("_PassT", bound=dace_ppl.Pass)
 
@@ -556,7 +558,7 @@ def reconfigure_dataflow_after_rerouting(
         #  the full array, but essentially slice a bit.
         pass
 
-    elif isinstance(other_node, dace_stdlib.Reduce):
+    elif isinstance(other_node, (dace_stdlib.Reduce, gtx_lib.ReduceWithSkipValues)):
         # For now we only handle the case that the reduction node is writing into
         #  `new_node`, before the data was written into `old_node`. In that case
         #  there is nothing to do, we just do some checks.
