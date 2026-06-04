d5732e422b8c8ecfdbf4dfc5a674058ee3fbb490
Philip Mueller, CSCS <philip.mueller@cscs.ch>
2026-06-04 15:48:32 +0200

    Let's even more restrict it.

--- noxfile.py
+++ noxfile.py
@@ -271,7 +271,7 @@ def test_next(
         *"pytest --cache-clear -sv -n auto --dist loadgroup".split(),
         *("-m", f"{markers}"),
         # str(pathlib.Path("tests") / "next_tests"),  # noqa ERA001
-        "tests/next_tests/integration_tests/feature_tests/",
+        "tests/next_tests/integration_tests/feature_tests/ffront_tests/test_where.py",
         *session.posargs,
         success_codes=[0, NO_TESTS_COLLECTED_EXIT_CODE],
     )
