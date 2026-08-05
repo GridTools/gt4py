8ed9aeaad565a4b17ab6d02bbdb5975b1d24fd10
Edoardo Paone <edoardo.paone@cscs.ch>
2026-08-05 15:50:38 +0200

    edit

--- tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_dace_backend.py
+++ tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/test_dace_backend.py
@@ -476,8 +476,9 @@ def test_multi_streams_sychronizes_on_anchor_stream(async_sdfg_call: bool, with_
     """The external sync stream pointer (or default stream 0) is passed to the SDFG call."""
     import cupy as cp
 
+    # The DaCe HIP backend currently crashes when `num_streams == 4`.
     # TODO(edopao): Increase this limit once the DaCe HIP backend supports multi-streams.
-    num_streams = 1 if cp.cuda.runtime.is_hip else 4
+    num_streams = 2 if cp.cuda.runtime.is_hip else 4
 
     sync_stream = cp.cuda.Stream(non_blocking=True) if with_sync_stream else None
     expected_stream_ptr = cp.cuda.Stream(null=True).ptr if sync_stream is None else sync_stream.ptr
