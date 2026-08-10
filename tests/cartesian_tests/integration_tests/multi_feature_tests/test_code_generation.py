Fix bad precision

tests/cartesian_tests/integration_tests/multi_feature_tests/test_code_generation.py
@@ -1849,7 +1849,7 @@ class MyEnum(IntEnum):
 def test_enum_runtime(backend):
 
     @gtscript.stencil(backend=backend)
-    def the_stencil(out_field: Field[np.int32], order: MyEnum): # type: ignore
+    def the_stencil(out_field: Field[int], order: MyEnum): # type: ignore
         with computation(PARALLEL), interval(0, 1):
             out_field = 32
             if order < MyEnum.A: