1cb76a81c0f37ea8ac155b06f44c84e91f82d7d1
Hannes Vogt <vogt@hey.com>
2026-08-25 10:39:49 +0200

    Revert "fix[next]: print and parse `NoneLiteral`"
    This reverts commit ec444fb15c8dac45ec373907453f6dc9447d9cf5.

--- tests/next_tests/unit_tests/iterator_tests/test_pretty_roundtrip.py
+++ tests/next_tests/unit_tests/iterator_tests/test_pretty_roundtrip.py
@@ -227,7 +227,6 @@ ROUNDTRIP_CASES = [
         ),
         id="program",
     ),
-    pytest.param(ir.NoneLiteral(), id="none_literal"),
     pytest.param(ir.InfinityLiteral.POSITIVE, id="infinity_positive"),
     pytest.param(ir.InfinityLiteral.NEGATIVE, id="infinity_negative"),
     pytest.param(
