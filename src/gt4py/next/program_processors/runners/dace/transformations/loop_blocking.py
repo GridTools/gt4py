f463f013a4a305340300ae406d91daafce3a7e4e
Edoardo Paone <edoardo.paone@cscs.ch>
2026-09-18 14:10:39 +0200

    refactor[next-dace]: use `add_scope_connectors()` (#2902)
    Addresses review comment in #2874. Replaces in `next` code base:
    ```
            me.add_in_connector(f"IN_{name}")
            me.add_out_connector(f"OUT_{name}")
    ```
    with:
    ```
            me.add_scope_connector(name)`
    ```

--- src/gt4py/next/program_processors/runners/dace/transformations/loop_blocking.py
+++ src/gt4py/next/program_processors/runners/dace/transformations/loop_blocking.py
@@ -1043,8 +1043,7 @@ class LoopBlocking(dace_transformation.SingleStateTransformation):
                     new_memlet_outside = dace.Memlet.from_array(
                         out_edge.data.data, sdfg.arrays[out_edge.data.data]
                     )
-                    inner_entry.add_in_connector(new_in_conn)
-                    inner_entry.add_out_connector(new_out_conn)
+                    inner_entry.add_scope_connectors(new_map_conn)
 
                 state.add_edge(
                     out_edge.src,
@@ -1123,8 +1122,7 @@ class LoopBlocking(dace_transformation.SingleStateTransformation):
                             ),
                         ),
                     )
-                    inner_entry.add_in_connector(new_in_conn)
-                    inner_entry.add_out_connector(new_out_conn)
+                    inner_entry.add_scope_connectors(new_map_conn)
                     state.remove_edge(in_edge)
 
                 else:
@@ -1164,8 +1162,7 @@ class LoopBlocking(dace_transformation.SingleStateTransformation):
                     ),
                 ),
             )
-            inner_exit.add_in_connector("IN_" + edge_conn)
-            inner_exit.add_out_connector("OUT_" + edge_conn)
+            inner_exit.add_scope_connectors(edge_conn)
 
         # There is an invalid cache state in the SDFG, that makes the memlet
         #  propagation fail, to clear the cache we call the hash function.
--- tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/transformation_tests/test_remove_point_view.py
+++ tests/next_tests/unit_tests/program_processor_tests/runners_tests/dace_tests/transformation_tests/test_remove_point_view.py
@@ -77,9 +77,7 @@ def _make_sdfg_with_map_with_view(
         state.add_edge(tmp_view_access_node, None, mexit, "IN_out", dace.Memlet("out[i, j]"))
 
     mentry.add_scope_connectors("a")
-    mentry.add_in_connector("IN_a")
-    mexit.add_in_connector("IN_out")
-    mexit.add_out_connector("OUT_out")
+    mexit.add_scope_connectors("out")
     state.add_edge(mexit, "OUT_out", out, None, dace.Memlet(f"out[0:{N}, 0:{N}]"))
 
     dace_propagation.propagate_states(sdfg)
